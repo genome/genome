48ee940dc1c3b28a6038b06112ff05fa5a85a332
Eddie Belter <ebelter@genome.wustl.edu>
2012-09-04 16:16:04 -0500

    MC16s: can specify pp to run status command

--- lib/perl/Genome/Model/MetagenomicComposition16s/Command/RunStatus.pm
+++ lib/perl/Genome/Model/MetagenomicComposition16s/Command/RunStatus.pm
@@ -21,6 +21,17 @@ class Genome::Model::MetagenomicComposition16s::Command::RunStatus {
             doc => 'Region Number.',
         },
     ],
+    has_optional => [
+        processing_profile => {
+            is => 'Genome::ProcessingProfile::MetagenomicComposition16s',
+            doc => 'Limit models to this processing profile. Current default profiles are 2571784 (rdp 2.2) and 2752939 (rdp2.5). But any MC16s processing profile can be specified.'
+        },
+        separator => {
+            is => 'Text',
+            default_value => ',',
+            doc => 'Separator character to use when printing, saving and email attachments. For tab separator, use "tab".',
+        },
+    ],
 };
 
 sub help_brief { 
@@ -44,7 +55,7 @@ sub execute {
         return;
     }
 
-    my $default_processing_profile_id = Genome::Model::MetagenomicComposition16s->default_processing_profile_id;
+    my $processing_profile_id = $self->processing_profile->id;
     my @subjects_to_skip = map { qr/$_/ } (qw/ ^nctrl$ ^n\-cntrl$ /);
     my @rows;
     for my $instrument_data ( @instrument_data ) {
@@ -56,7 +67,7 @@ sub execute {
         push @rows, \@row;
         push @row, $sample->name;
         push @row, $instrument_data->id;
-        my ($model) = sort { $b->id <=> $a->id } grep { $_->subject_id eq $instrument_data->sample_id } grep { $_->processing_profile_id eq $default_processing_profile_id } map { $_->model } Genome::Model::Input->get(name => 'instrument_data', value_id => $instrument_data->id,);
+        my ($model) = sort { $b->id <=> $a->id } grep { $_->subject_id eq $instrument_data->sample_id } grep { $_->processing_profile_id eq $processing_profile_id } map { $_->model } Genome::Model::Input->get(name => 'instrument_data', value_id => $instrument_data->id,);
         if ( not $model ) {
             push @row, '', '', '', ''. '', '';
             next;
@@ -77,6 +88,7 @@ sub execute {
         push @row, $files_string;
     }
 
+    my $sep = ( $self->separator =~ /^tab$/i ? "\t" : $self->separator );
     print join( "\n", map { join(",", @$_) } [qw/ sample-name instrument-data-id model-id build-id status process-success oriented-fastas /], @rows)."\n"; 
 
     return 1;
