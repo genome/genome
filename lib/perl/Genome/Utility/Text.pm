89bdbd1edd270ec8f2e42d4dab7721e381919fb1
Nathaniel Nutter <nnutter@genome.wustl.edu>
2012-09-10 09:14:21 -0500

    Revert "Add Utility::Text tools"
    This reverts commit 1748b7b4eecc2b36a977ab57ab99e9cea503e71e.

--- lib/perl/Genome/Utility/Text.pm
+++ lib/perl/Genome/Utility/Text.pm
@@ -2,12 +2,10 @@ package Genome::Utility::Text;
 
 use strict;
 use warnings;
-use feature 'switch';
 
 use Genome;
 
 use Data::Dumper 'Dumper';
-use POSIX "floor";
 require Carp;
 
 class Genome::Utility::Text {
@@ -31,7 +29,7 @@ sub camel_case_to_string {
         return;
     }
     my @words = split( /(?=(?<![A-Z])[A-Z])|(?=(?<!\d)\d)/, $camel_case); #split on the first capital or the start of a number
-    my $join = ( @_ ) ? $_[0] : ' ';
+    my $join = ( @_ ) ? $_[0] : ' '; 
     return join($join, map { lc } @words);
 }
 
@@ -62,7 +60,7 @@ sub param_string_to_hash {
         if ( not defined $value or $value eq '' ) {
             $params{$key} = 1;
         }
-        elsif ( defined $value_split ) {
+        elsif ( defined $value_split ) { 
             $params{$key} = [ split($value_split, $value) ];
         }
         else {
@@ -108,250 +106,6 @@ sub capitalize_words {
     my $regexp = qr/[$seps]+/;
 
     return join(' ', map { ucfirst } split($regexp, $string));
-}
-
-# put text side_by_side
-# args:
-#     strings => ARRAY of strings
-# kwargs:
-#     separator => String
-#     justification => String or ARRAY of Strings
-#           must be one of 'left', 'right' or 'center'
-#     fill => String or ARRAY of Strings
-#     max_width => Integer, lines longer than this will be tucked.
-#           if = -1 no tucking will occur.
-#     stack => Boolean, if true side_by_side will stack instead of tuck.
-# Tucking:
-#     If putting the strings side-by-side would result in strings longer
-#     than <max_width>, then 1 or more strings will be tucked under
-#     their leftward neighbor instead of placed to the right of them.
-sub side_by_side {
-    my $strings = shift;
-    my @strings = @{$strings};
-    my %params = @_;
-
-    my $separator = delete $params{separator} || " ";
-    my $justification = delete $params{justification} || "left";
-    my $fill = delete $params{fill} || " ";
-    my $max_width = delete $params{max_width} || -1;
-    my $stack = delete $params{stack} || 0;
-    if(keys %params) {
-        Carp::croak("Unsupported optional argument(s) supplied to " .
-            "side_by_side:" . join(", ", keys %params));
-    }
-
-    my @justifications;
-    if(ref($justification)) {
-        @justifications = @{$justification};
-        if(scalar(@justifications) != scalar(@strings)) {
-            Carp::croak("You must supply as many justifications as strings.");
-        }
-    } else {
-        @justifications = map {$justification || 'left'} (0..$#strings);
-    }
-
-    my @fills;
-    if(ref($fill)) {
-        @fills = @{$fill};
-        if(scalar(@fills) != scalar(@strings)) {
-            Carp::croak("You must supply as many fills as strings.");
-        }
-    } else {
-        @fills = map {$fill || ' '} (0..$#strings);
-    }
-
-    my @line_arrays;
-    my @widths;
-    for my $i (0..$#strings) {
-        my $string = $strings[$i];
-
-        my $width = width($string);
-        my @lines = split("\n", $string);
-        push(@line_arrays, \@lines);
-
-        my $fill = $fills[$i];
-        $width += width($fill)*2 if $fill ne ' ';
-
-        push(@widths, $width);
-    }
-
-    my @combined_lines;
-    my @num_lines = map {scalar(@{$_})} @line_arrays;
-    my $max_i = (List::Util::max(@num_lines) || 1) - 1;
-    for my $i (0..$max_i) {
-        my $combined_line = '';
-        for my $j (0..$#line_arrays) {
-            my $array_ref = $line_arrays[$j];
-            my $this_part = $array_ref->[$i];
-            $this_part = '' unless defined($this_part);
-
-            my $width = $widths[$j];
-            my $justification = $justifications[$j];
-            my $fill = $fills[$j];
-
-            my $tucking = '';
-            my $new_length = $width + width($combined_line) +
-                             width($separator);
-            if($new_length > $max_width and $max_width != -1) {
-                if($stack) {
-                    return join("\n\n", @strings);
-                }
-                $tucking = "\n" . '  'x($new_length/$max_width) . '`-';
-            }
-
-            my $next_final_length = $width + width($combined_line) +
-                                    ($widths[$j+1] || 0) +
-                                    2*width($separator);
-            if($next_final_length > $max_width) {
-                $fill = undef; #don't fill if will tuck next line.
-            }
-
-            $this_part = justify($this_part, $justification, $width, $fill);
-            $combined_line .= $tucking . $this_part;
-            $combined_line .= $separator unless $j == $#line_arrays;
-        }
-        push(@combined_lines, $combined_line);
-    }
-    return join("\n", @combined_lines);
-}
-
-sub strip_color {
-    my ($string) = @_;
-    $string =~ s/\e\[[\d;]*[a-zA-Z]//g;
-    return $string;
-}
-
-# return the VISIBLE width of the of a string.
-sub width {
-    my ($string) = @_;
-    $string = strip_color($string);
-    my @lines = split(/\n/, $string);
-
-    my $width = length($string);
-    if(scalar(@lines)) {
-        $width = List::Util::max(map {length($_ . '')} @lines);
-    }
-    return $width;
-}
-
-sub justify {
-    my ($string, $kind, $field_width, $fill) = @_;
-    $fill = $fill || " ";
-
-    my $num_spaces_needed = $field_width - width($string);
-    my $fill_string = "$fill"x$num_spaces_needed;
-
-    my $spacer = '';
-    if($num_spaces_needed > 0) {
-        $spacer = ' ';
-        $num_spaces_needed -= 1;
-        return "$string " if $num_spaces_needed == 0;
-    } else {
-        return $string;
-    }
-
-    my $format;
-    given($kind) {
-        when('left') {
-            $format = "%s" . $spacer .
-                    substr($fill_string, -1 * $num_spaces_needed);
-        }
-        when('right') {
-            $format = substr($fill_string, 0, $num_spaces_needed ) .
-                    $spacer . "%s";
-        }
-        when('center') {
-            my $left_spaces = floor($num_spaces_needed/2);
-            my $right_spaces = $num_spaces_needed - $left_spaces;
-            $format = substr($fill_string, 0, $left_spaces) .  "%s" .
-                      substr($fill_string, -1 * $right_spaces)
-        }
-        default {
-            Carp::croak("kind argument must be one of 'left',".
-                        " 'right', or 'center', not '$kind'");
-        }
-    }
-    return sprintf($format, $string);
-}
-
-sub _next_foundation {
-    my ($foundation, $prefix) = @_;
-
-    return '' unless defined($prefix);
-    if($prefix =~ m/\|/) {
-        return $foundation . "| ";
-    } else {
-        return $foundation . "  ";
-    }
-}
-
-sub tree_to_condensed_string {
-    return tree_to_string(shift, shift || '', shift || '', 1);
-}
-
-sub tree_to_string {
-    my ($tree, $foundation, $prefix, $condensed) = @_;
-
-    my $result = '';
-    given(ref($tree)) {
-        when('HASH') {
-            my @keys = sort(keys %{$tree});
-            my $count = 0;
-            my $next_foundation = $foundation;
-            unless($condensed) {
-                $result .= tree_to_string('#', $foundation, $prefix, $condensed);
-                $next_foundation = _next_foundation($foundation, $prefix);
-            }
-            for my $key (@keys) {
-                my $next_prefix;
-                if($count == $#keys) {
-                    $next_prefix = '`-';
-                } else {
-                    $next_prefix = '|-';
-                }
-                if($condensed) {
-                    $next_prefix = '  ';
-                }
-                $result .= tree_to_string($key, $next_foundation,
-                        $next_prefix, $condensed);
-
-                my $child_foundation = _next_foundation($next_foundation,
-                        $next_prefix);
-                # can only have a single 'value' for each key.
-                $result .= tree_to_string($tree->{$key}, $child_foundation,
-                        '`-', $condensed);
-                $count += 1;
-            }
-        }
-        when('ARRAY') {
-            my @items = @{$tree};
-            my $count = 0;
-            my $next_foundation = $foundation;
-            unless($condensed) {
-                $result .= tree_to_string('\\', $foundation, $prefix, $condensed);
-                $next_foundation = _next_foundation($foundation, $prefix);
-            }
-            for my $item (@items) {
-                my $next_prefix;
-                if($count == $#items) {
-                    $next_prefix = '`-';
-                } else {
-                    $next_prefix = '|-';
-                }
-                if($condensed) {
-                    $next_prefix = ' -';
-                }
-
-                $result .= tree_to_string($item, $next_foundation,
-                        $next_prefix, $condensed);
-                $count += 1;
-            }
-        }
-        default {
-            $result .= sprintf("%s%s%s\n", $foundation, $prefix, $tree);
-        }
-    }
-    return $result;
 }
 
 1;
