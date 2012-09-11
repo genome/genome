5c0e32c437bba20c7ccb0c122fdceb078272f95c
David Morton <dmorton@genome.wustl.edu>
2012-09-11 16:42:34 -0500

    Add helper routines to Utility::Vcf

--- lib/perl/Genome/Utility/Vcf.pm
+++ lib/perl/Genome/Utility/Vcf.pm
@@ -11,6 +11,8 @@ our @EXPORT_OK = qw(open_vcf_file
                     get_vcf_header
                     diff_vcf_file_vs_file
                     diff_vcf_text_vs_text
+                    convert_string_to_hash
+                    convert_hash_to_string
                     );
 
 # opens up either a .vcf file or a .vcf.gz file for reading.
@@ -26,21 +28,35 @@ sub open_vcf_file {
     return $fh;
 }
 
-# returns only the header of the vcf file in one big string.
+# return the vcf header as a string.
+# if evaluated in ARRAY context it also returns the open file-handle 
+# positioned at the first line that is not a header.
 sub get_vcf_header {
-    my ($filename) = @_;
+    my $filename = @_;
+    my $input_fh = open_vcf_file($filename);
 
-    my $fh = open_vcf_file($filename);
-    my $header = "";
-    while(my $line = $fh->getline()) {
-        if($line =~ m/^#/) {
+    my $header = '';
+    my $header_end = 0;
+    while (!$header_end) {
+        my $line = $input_fh->getline();
+        if ($line =~ m/^##/) {
             $header .= $line;
+        } elsif ($line =~ m/^#/) {
+            $header .= $line;
+            $header_end = 1;
         } else {
-            chomp($header);
-            return $header
+            Carp::croak("Missed the final header line with the" .
+                    " sample list? Last line examined: $line Header so far: " .
+                    $header);
         }
     }
-    Carp::croak("Vcf file ($filename) has no body... only header.");
+
+    if(wantarray()) {
+        return $header, $input_fh;
+    } else {
+        $input_fh->close();
+        return $header;
+    }
 }
 
 # diffs vcf files ignoring the fileDate header entry.
@@ -73,4 +89,36 @@ sub diff_vcf_text_vs_text {
 
     return Genome::Sys->diff_text_vs_text($t1, $t2);
 }
+
+# return the hash representation of the formatted string
+sub convert_string_to_hash {
+    my ($string, $format) = @_;
+
+    my @keys = split(":", $format);
+    my @values = split(":", $string);
+
+    my %result;
+    for my $i (0..$#keys) {
+        $result{$keys[$i]} = $values[$i];
+    }
+    return %result;
+}
+
+# return the formatted string representation of the sample hash.
+sub convert_hash_to_string {
+    my ($hash, $format) = @_;
+
+    my @keys = split(":", $format);
+    my $result = '';
+    my $count = 0;
+    for my $key (@keys) {
+        $result .= $hash->{$key};
+        $result .= ":" if $count < $#keys;
+        $count += 1;
+    }
+
+    return $result;
+}
+
+
 
