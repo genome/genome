77d9d07148487a592562e1b9bee09cc14c70e9f8
Mark Burnett <mburnett@genome.wustl.edu>
2012-09-12 16:36:05 -0500

    Add ability to get gap data from multiple tables

--- lib/perl/Genome/Db/Ucsc/GapList.pm
+++ lib/perl/Genome/Db/Ucsc/GapList.pm
@@ -26,8 +26,34 @@ our @_HEADINGS = (
 );
 
 our %_DB_TABLES_NAMES = (
-    'NCBI-human-build36' => ['hg18', 'gap'],
-    'GRCh37-lite-build37' => ['hg19', 'gap'],
+    'GRCh37-lite-build37' => ['hg19', ['gap']],
+    'NCBI-human-build36' => ['hg18', [
+        'chr1_gap',
+        'chr2_gap',
+        'chr2_gap',
+        'chr3_gap',
+        'chr4_gap',
+        'chr5_gap',
+        'chr6_gap',
+        'chr7_gap',
+        'chr8_gap',
+        'chr9_gap',
+        'chr10_gap',
+        'chr11_gap',
+        'chr12_gap',
+        'chr13_gap',
+        'chr14_gap',
+        'chr15_gap',
+        'chr16_gap',
+        'chr17_gap',
+        'chr18_gap',
+        'chr19_gap',
+        'chr20_gap',
+        'chr21_gap',
+        'chr22_gap',
+        'chrX_gap',
+        'chrY_gap',
+    ]],
 );
 
 
@@ -72,22 +98,25 @@ sub execute {
 
     $self->_add_header_to_output_file();
     $self->_sort_into_output_file($temp_output_file);
-    return $self->gap_filename;
+    return 1;
 }
 
 sub _fetch_data_from_ucsc {
     my $self = shift;
 
-    my ($database_name, $table_name) = $self->_resolve_database_and_table_names();
-    my $query = $self->_resolve_query($table_name);
+    my ($database_name, $table_names) = $self->_resolve_database_and_table_names();
+    my $query = $self->_resolve_query($table_names);
 
     my $temp_output_file = Genome::Sys->create_temp_file_path();
     my $command = sprintf(
-        "mysql --user=%s --password=%s --host=%s -N -A -D %s -e '%s' > %s",
+        'mysql --user=%s --password=%s --host=%s -N -A -D %s -e \'%s\' > %s',
         $self->db_user, $self->db_password, $self->db_host,
         $database_name, $query, $temp_output_file);
 
-    Genome::Sys->shellcmd(cmd => $command);
+    unless(Genome::Sys->shellcmd(cmd => $command)) {
+        $self->error_message($!);
+        die $self->error_message;
+    }
 
     return $temp_output_file
 }
@@ -106,9 +135,14 @@ sub _resolve_database_and_table_names {
 }
 
 sub _resolve_query {
-    my ($self, $table_name) = @_;
+    my ($self, $table_names) = @_;
 
-    return sprintf("select %s from %s", join(", ", @_HEADINGS), $table_name);
+    my @select_statements;
+    for my $tn (@{$table_names}) {
+        push(@select_statements,
+            sprintf("select %s from %s", join(", ", @_HEADINGS), $tn));
+    }
+    return join("; ", @select_statements);
 }
 
 sub _add_header_to_output_file {
@@ -124,7 +158,12 @@ sub _add_header_to_output_file {
 sub _sort_into_output_file {
     my ($self, $unsorted_filename) = @_;
 
-    my $command = sprintf("sort -V > %s",
+    unless (-s $unsorted_filename) {
+        $self->error_message("Empty file passed to sort.");
+        die $self->error_message;
+    }
+
+    my $command = sprintf('sort -V %s >> %s',
         $unsorted_filename, $self->gap_filename);
 
     Genome::Sys->shellcmd(cmd => $command);
