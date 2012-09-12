d4c8deb628772ea71c1f185d831402cf1f243c1b
Thomas Mooney <tmooney@genome.wustl.edu>
2012-09-12 15:53:50 -0500

    Use the is_input magic of Models rather than defining explicit relations.

--- lib/perl/Genome/Model/GenePrediction/Bacterial.pm
+++ lib/perl/Genome/Model/GenePrediction/Bacterial.pm
@@ -44,66 +44,42 @@ class Genome::Model::GenePrediction::Bacterial {
         },
     ],
     has_optional => [
-        dev => {
-            is => 'Boolean',
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'dev' ],
-            doc => 'If set, dev databases are used instead of production databases',
-        },
         locus_id => {
             via => 'subject',
             to => 'locus_tag',
         },
+    ],
+    has_optional_input => [
+        dev => {
+            is => 'Boolean',
+            doc => 'If set, dev databases are used instead of production databases',
+        },
         run_type => {
             is => 'String', # TODO Does this affect processing? Why do we need to note it?
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'run_type' ],
             doc => 'A three letter identifier appended to locus id, (DFT, FNL, etc)',
         },
         locus_suffix => {
             is => 'Text',
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'locus_suffix', value_class_name => 'UR::Value::Text' ],
             doc => 'appended to the locus_id (and included in the locus tag). (Used for testing.)',
-            is_mutable => 1,
-            is_many => 0,
         },
         assembly_version => {
             is => 'String', # TODO Can this be removed or derived from the assembly in some way?
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'assembly_version' ],
             doc => 'This notes the assembly version, but doesn\'t really seem to change...',
         },
         project_type => {
             is => 'String', # TODO What is this? Why do we need it?
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'project_type' ],
             doc => 'The type of project this data is being generated for (HGMI, for example)',
         },
         pipeline_version => {
             is => 'String', # TODO Can this be removed? Why do we need it?
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'pipeline_version' ],
             doc => 'Apparently, this notes the pipeline version.', 
         },
         acedb_version => {
             is => 'String', # TODO If we can figure out a way to automate switching to a new db, this can go away
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'acedb_version' ],
             doc => 'Notes the version of aceDB that results should be uploaded to',
         },
         nr_database_location => {
-            is => 'Path', # TODO Once using local NR is fully tested and trusted, this param can be removed
-            via => 'inputs',
-            to => 'value_id',
-            where => [ name => 'nr_database_location' ],
+            is => 'FilePath', # TODO Once using local NR is fully tested and trusted, this param can be removed
             doc => 'The NR database that should be used by default, may be overridden by local copies',
         },
     ],
@@ -111,21 +87,20 @@ class Genome::Model::GenePrediction::Bacterial {
 
 sub create {
     my $class = shift;
-    my %params = @_;
-    
-    # Anything left in the params hash will be made into an input on the model
-    my $self = $class->SUPER::create(
-        name                             => delete $params{name},
-        processing_profile_id            => delete $params{processing_profile_id},
-        subject_id                       => delete $params{subject_id},
-        subject_class_name               => delete $params{subject_class_name},
-        auto_assign_inst_data            => delete $params{auto_assign_inst_data},
-        auto_build_alignments            => delete $params{auto_build_alignments},
-        create_assembly_model            => delete $params{create_assembly_model},
-        assembly_processing_profile_name => delete $params{assembly_processing_profile_name},
-        start_assembly_build             => delete $params{start_assembly_build},
-        assembly_contigs_file            => delete $params{assembly_contigs_file},
-    );
+
+    my ($bx, @extra) = $class->define_boolexpr(@_);
+    my %params = @extra;
+
+    #Grab the other options specific to creating GenePrediction models
+    # Anything left in the params will be made into an input on the model
+    my %additional_create_options;
+    for my $option ('create_assembly_model', 'assembly_processing_profile_name', 'start_assembly_build', 'assembly_contigs_file') {
+        if(exists $params{$option}) {
+            $additional_create_options{$option} = delete $params{$option};
+        }
+    }
+
+    my $self = $class->SUPER::create($bx->params_list, %additional_create_options);
     return unless $self;
 
     # Add inputs to the model
