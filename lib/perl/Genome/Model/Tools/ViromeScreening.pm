4974095a3ce3ad96b02fcd2370d4078910e47db1
Kyung Kim <kkyung@genome.wustl.edu>
2012-09-24 10:06:47 -0500

    Made databases to be used command option

--- lib/perl/Genome/Model/Tools/ViromeScreening.pm
+++ lib/perl/Genome/Model/Tools/ViromeScreening.pm
@@ -12,8 +12,8 @@ use Mail::Sender;
 
 UR::Object::Type->define(
     class_name => __PACKAGE__,
-    is         => 'Command',
-    has        => [
+    is => 'Command',
+    has => [
 	fasta_file => {
 	    doc => 'file of reads to be checked for contamination',
 	    is => 'String',
@@ -24,7 +24,7 @@ UR::Object::Type->define(
 	    is => 'String',
 	    is_input => 1,
 	},
-	dir     => {
+	dir => {
 	    doc => 'directory of inputs',
 	    is => 'String',
 	    is_optional => 1,
@@ -36,6 +36,26 @@ UR::Object::Type->define(
 	    is_optional => 1,
 	    default => "logfile.txt",
 	},
+        human_db => {
+            doc => 'human blast db',
+            is => 'String',
+            is_optional => 1,
+            default => '/gscmnt/sata835/info/medseq/virome/blast_db/human_genomic/2009_07_09.humna_genomic',
+        },
+        nt_db => {
+            doc => 'nt sequence blast db',
+            is => 'String',
+            is_input => 1,
+            is_optional => 1,
+            default => '/gscmnt/sata835/info/medseq/virome/blast_db/nt/nt',
+        },
+        virus_db => {
+            doc => 'Virus sequence blast db',
+            is => 'String',
+            is_input => 1,
+            is_optional => 1,
+            default => '/gscmnt/sata835/info/medseq/virome/blast_db/viral/viral.genomic.fna',
+        },
     ],
 );
 
@@ -50,12 +70,16 @@ sub help_detail {
 sub execute {
     my $self = shift;
     unlink($self->logfile) if (-e $self->logfile);
+    $self->_log_dbs_used;
     my $output = run_workflow_lsf(
-	$ENV{GENOME_TEST_INPUTS} . '/Genome-Model-Tools-ViromeScreening/virome-screening2.xml',
+	$ENV{GENOME_TEST_INPUTS} . '/Genome-Model-Tools-ViromeScreening/virome-screening3.xml',
 	'fasta_file'  => $self->fasta_file,
 	'barcode_file'=> $self->barcode_file,
 	'dir'         => $self->dir,
 	'logfile'     => $self->logfile,
+        'human_db'    => $self->human_db,
+        'nt_db'       => $self->nt_db,
+        'virus_db'    => $self->virus_db,
 	);
     my $mail_dest = Genome::Config->user_email;
     my $sender = Mail::Sender->new({
@@ -70,6 +94,16 @@ sub execute {
                    "\t--barcode-file=" . $self->barcode_file . " --fasta-file=" . $self->fasta_file . " --dir=" . $self->dir . " --logfile=" . $self->logfile,
     });
     return 1;
+}
+
+sub _log_dbs_used {
+    my $self = shift;
+    my $fh = Genome::Sys->open_file_for_writing( $self->logfile );
+    for my $name ( qw/ human nt virus / ) {
+        my $db = $name.'_db';
+        $fh->printf("%-15s%40s\n", uc $name.' db:', $self->$db);
+    }
+    $fh->close;
 }
 
 1;
