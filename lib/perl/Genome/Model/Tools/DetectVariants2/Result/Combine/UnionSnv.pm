8114cb35cd8a73f5f4096dfbf96a9015f8acf30f
Gabriel Sanderson <gsanders@genome.wustl.edu>
2012-10-11 16:01:57 -0500

    Remove unnecessary vcf generation code in combines.
    I believe this code has been out of date for awhile. And it passes unit tests and builds look fine.
    The real combined vcf generation should happen in Genome::Model::Tools::DetectVariants2::Result::Vcf::Combine

--- lib/perl/Genome/Model/Tools/DetectVariants2/Result/Combine/UnionSnv.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Result/Combine/UnionSnv.pm
@@ -40,38 +40,6 @@ sub _combine_variants {
     my $lq_file = $self->temp_staging_directory."/snvs.lq.bed";
     `touch $lq_file`;
     
-    $self->_generate_vcf;
-
-
-    return 1;
-}
-
-sub _generate_vcf {
-    my $self = shift;
-    my $input_a_vcf = $self->input_directory_a."/snvs.vcf.gz";
-    unless(-s $input_a_vcf){
-        $self->status_message("Could not find vcf at: ".$input_a_vcf." not creating a vcf for this operation.");
-        return;
-    }
-    my $input_b_vcf = $self->input_directory_b."/snvs.vcf.gz";
-    unless(-s $input_b_vcf){
-        $self->status_message("Could not find vcf at: ".$input_b_vcf." not creating a vcf for this operation.");
-        return;
-    }
-    my $output_file = $self->temp_staging_directory."/snvs.vcf.gz";
-
-    my $merge_cmd = Genome::Model::Tools::Joinx::VcfMerge->create(
-        input_files => [ ($input_a_vcf,$input_b_vcf)],
-        output_file => $output_file,
-        merge_samples => 1,
-        clear_filters => 1,
-        use_bgzip => 1,
-        use_version => "1.6",
-    );
-    unless($merge_cmd->execute){
-        die $self->error_message("Could not complete merge operation.");
-    }
-
     return 1;
 }
 
