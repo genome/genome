a98c3499b7091c3235f9a53fb9597e01a827f321
dmorton <dmorton@genome.wustl.edu>
2012-11-06 14:08:41 -0600

    Fix how get_vcf_header was being used.

--- lib/perl/Genome/Model/Tools/Relationship/HeterozygousDominant.pm
+++ lib/perl/Genome/Model/Tools/Relationship/HeterozygousDominant.pm
@@ -119,8 +119,8 @@ sub find_dominant_heterozygotes {
     my @unaffecteds = $self->find_indices($ped_hash,'unaffected');
     $DB::single=1;
     my ($output_fh, $output_name) = Genome::Sys->create_temp_file;
-    my @header = get_vcf_header($input_vcf);
-    $output_fh->print(@header);
+    my $header = get_vcf_header($input_vcf);
+    $output_fh->print($header);
 
     my $fh = open_vcf_file($input_vcf);
     while(my $line = $fh->getline) {
@@ -153,7 +153,8 @@ sub find_dominant_heterozygotes {
 sub parse_ped {
     my ($self, $ped_file, $input_vcf) = @_;
     my %ped_hash;
-    my @header_lines = get_vcf_header($input_vcf);
+    my $header_lines = get_vcf_header($input_vcf);
+    my @header_lines = split("\n", $header_lines);
     my $header_line = $header_lines[-1];
     my ($chr, $pos, $id, $ref, $alt, $qual, $filter, $info, $format, @samples) = split "\t", $header_line;
     for (my $i=0; $i < scalar(@samples); $i++) {
