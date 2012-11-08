2e763a4a2cc959aa4879f0a3bbe33a55fe271042
Nathan Nutter <nnutter@genome.wustl.edu>
2012-11-08 15:05:38 -0600

    fix typo
    Change-Id: I5e4c644b5f05cc3bd8b74dc8e83e6b6b8fdf913c

--- lib/perl/Genome/Disk/Allocation.pm
+++ lib/perl/Genome/Disk/Allocation.pm
@@ -666,10 +666,10 @@ sub _archive {
 
         # Reallocate so we're reflecting the correct size at time of archive.
         $self->reallocate();
-        if (!$ENV{UR_DBI_NO_COMMIT} and $self->allocated_kb < 1048576) { # Must be greater than 1GB to be archived
+        if (!$ENV{UR_DBI_NO_COMMIT} and $self->kilobytes_requested < 1048576) { # Must be greater than 1GB to be archived
             confess(sprintf(
                 "Total size of files at path %s is only %s, which is not greater than 1GB!",
-                $current_allocation_path, $self->allocated_kb));
+                $current_allocation_path, $self->kilobytes_requested));
         }
 
         my $mkdir_cmd = "mkdir -p $archive_allocation_path";
