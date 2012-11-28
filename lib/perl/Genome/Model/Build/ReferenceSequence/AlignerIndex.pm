44ebbeb3defc63b9464329fbb534a28ce251e219
Mark Burnett <mburnett@genome.wustl.edu>
2012-11-27 19:11:42 -0600

    Remove lots of duplicate code
    Remove duplicate copies of _gather_params_for_get_or_create in various
    sub classes of SR.

--- lib/perl/Genome/Model/Build/ReferenceSequence/AlignerIndex.pm
+++ lib/perl/Genome/Model/Build/ReferenceSequence/AlignerIndex.pm
@@ -252,39 +252,6 @@ sub _gather_params_for_get_or_create {
     return $p;
 }
 
-# TODO push this up
-sub X_gather_params_for_get_or_create {
-    my $class = shift;
-    my $bx = UR::BoolExpr->resolve_normalized_rule_for_class_and_params($class, @_);
-
-    my %params = $bx->params_list;
-    my %is_input;
-    my %is_param;
-    my $class_object = $class->__meta__;
-    for my $key ($class->property_names) {
-        my $meta = $class_object->property_meta_for_name($key);
-        if ($meta->{is_input} && exists $params{$key}) {
-            $is_input{$key} = $params{$key};
-        } elsif ($meta->{is_param} && exists $params{$key}) {
-            $is_param{$key} = $params{$key};
-        }
-    }
-
-    my $inputs_bx = UR::BoolExpr->resolve_normalized_rule_for_class_and_params($class, %is_input);
-    my $params_bx = UR::BoolExpr->resolve_normalized_rule_for_class_and_params($class, %is_param);
-
-    my %software_result_params = (params_id=>$params_bx->id,
-                                  inputs_id=>$inputs_bx->id,
-                                  subclass_name=>$class);
-
-    return {
-        software_result_params => \%software_result_params,
-        subclass => $class,
-        inputs=>\%is_input,
-        params=>\%is_param,
-    };
-}
-
 sub resolve_allocation_subdirectory {
     my $self = shift;
     my $aligner_name_tag = $self->aligner_name;
