d7cd9e067a071ecc737fd90a6dad783d1727c809
Nathan Nutter <nnutter@genome.wustl.edu>
2012-12-10 18:02:43 -0600

    refactor conditionals as hash

--- lib/perl/Genome/Site/TGI/Observers.pm
+++ lib/perl/Genome/Site/TGI/Observers.pm
@@ -3,21 +3,32 @@ package Genome::Site::TGI::Observers;
 use strict;
 use warnings;
 
+my %observer_class_name = (
+    'Command::V1' => 'Genome::Site::TGI::Observers::Command',
+    'Genome::ModelGroup' => 'Genome::Site::TGI::Observers::ModelGroup',
+    'Genome::Project' => 'Genome::Site::TGI::Observers::Project',
+);
+
 UR::Object::Type->add_observer(
     aspect => 'load',
     callback => sub {
         my $meta = shift;
         my $class_name = $meta->class_name;
-        if ($class_name eq 'Genome::ModelGroup') {
-            require Genome::Site::TGI::Observers::ModelGroup;
-        } elsif ($class_name eq 'Genome::Project') {
-            require Genome::Site::TGI::Observers::Project;
-        } elsif ($class_name eq 'Command::V1') {
-            require Genome::Site::TGI::Observers::Command;
+        if (exists $observer_class_name{$class_name}) {
+            my $observer_class_name = $observer_class_name{$class_name};
+            my $observer_class_path = file_name($observer_class_name);
+            require $observer_class_path;
+            $observer_class_name->import($class_name);
         }
         die $@ if $@;
     },
 );
 
-1;
+sub file_name {
+    my $class_name = shift;
+    $class_name =~ s/::/\//g;
+    $class_name .= '.pm';
+    return $class_name;
+}
 
+1;
