b72e60566c3506d008f4b1b6b17e9c9684e0b048
Eddie Belter <ebelter@genome.wustl.edu>
2012-12-17 16:34:35 -0600

    MC16s: add generate reports to build steps

--- lib/perl/Genome/Model/Build/MetagenomicComposition16s.pm
+++ lib/perl/Genome/Model/Build/MetagenomicComposition16s.pm
@@ -878,6 +878,98 @@ sub classify_amplicons {
     return 1;
 }
 
+#< Reports >#
+sub generate_reports {
+    my $self = shift;
+    
+    my @report_names = (qw/ summary /);
+    push @report_names, 'composition' if not $self->model->is_for_qc;
+
+    for my $report_name ( @report_names ) {
+        $self->_generate_and_save_report($report_name);
+    }
+
+    return 1;
+}
+
+sub _generate_and_save_report {
+    my ($self, $name) = @_;
+
+    $self->status_message(ucfirst($name).' report...');
+
+    my $class = 'Genome::Model::MetagenomicComposition16s::Report::'.Genome::Utility::Text::string_to_camel_case($name);
+    my $generator = $class->create(
+        build_id => $self->id,
+    );
+    unless ( $generator ) {
+        $self->error_message("Could not create $name report generator for ".$self->description);
+        return;
+    }
+    my $report = $generator->generate_report;
+    unless ( $report ) {
+        $self->error_message("Could not generate $name report for ".$self->description);
+        return;
+    }
+
+    unless ( $self->add_report($report) ) {
+        $self->error_message("Could not save $name report for ".$self->description);
+    }
+
+    my @datasets = $report->get_datasets;
+    unless ( @datasets ) { # not ok
+        $self->error_message("No datasets in $name report for ".$self->description);
+        return;
+    }
+    my $file_base = sprintf(
+        '%s/%s',
+        $self->reports_directory,
+        $report->name_to_subdirectory($report->name),
+    );
+
+    for my $dataset ( @datasets ) {
+        my $dataset_name = $dataset->name;
+        my $file = sprintf(
+            '%s/%s.%s.tsv',
+            $file_base,
+            $self->model->subject_name,
+            $dataset_name,
+        );
+        unlink $file if -e $file;
+        my $fh = Genome::Sys->open_file_for_writing($file)
+            or return; # not ok
+        my ($svs) = $dataset->to_separated_value_string(separator => "\t");
+        unless ( $svs ) { # not ok
+            $self->error_message("Could not get dataset ($dataset) for $name report for ".$self->description);
+            return;
+        }
+        $fh->print($svs);
+        $fh->close;
+    }
+
+    my $xsl_file = $generator->get_xsl_file_for_html;
+    if ( -e $xsl_file ) {
+        my $xslt = Genome::Report::XSLT->transform_report(
+            report => $report,
+            xslt_file => $xsl_file,
+        );
+        unless ( $xslt ) {
+            $self->error_message("Can't transform report to html.");
+            return;
+        }
+        # Save
+        my $html_file = $report->directory.'/report.html';
+        my $fh = Genome::Sys->open_file_for_writing($html_file);
+        unless ( $fh ) {
+        }
+        $fh->print( $xslt->{content} );
+    }
+
+    $self->status_message(ucfirst($name).' report...OK');
+    
+    return $report;
+}
+#<>#
+
 #< QC Email >#
 sub perform_post_success_actions {
     my $self = shift;
