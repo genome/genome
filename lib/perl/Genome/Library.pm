4608229e8be7df482ec5e2609b49bdf6cbba9080
Eddie Belter <ebelter@genome.wustl.edu>
2013-01-03 16:16:43 -0600

    Revert "Library: rm protocol_name; add protocol, original and library insert size - all in table"
    This reverts commit 2e5ab16f5f580f29985ebc8186a3e6270ed9e852.

--- lib/perl/Genome/Library.pm
+++ lib/perl/Genome/Library.pm
@@ -32,24 +32,10 @@ class Genome::Library {
         },
     ],
     has_optional => [
-        original_insert_size => {
-            is => 'Text',
-            column_name => 'ORIGINAL_INSERT_SIZE',
-            doc => 'The original insert size of the fragments. This may be a number or a range.'
-        },
-        library_insert_size => {
-            is => 'Text',
-            column_name => 'LIBRARY_INSERT_SIZE',
-            doc => 'The relative insert size of fragments. This may be a number or a range.'
-        },
         fragment_size_range => {
             is => 'Text',
             column_name => 'LIBRARY_INSERT_SIZE',
-        },
-        protocol => {
-            is => 'Text',
-            column_name => 'PROTOCOL',
-            doc => 'Protocol used to generate the library.'
+            doc => 'intended size range of fragments from library construction'
         },
         taxon_id => {
             is => 'Number',
@@ -62,6 +48,10 @@ class Genome::Library {
         species_name => {
             is => 'Text',
             via => 'taxon',
+        },
+        protocol_name => {
+            is_transient => 1,
+            is => 'Text',
         },
         sample_source => {
             via => 'sample',
@@ -89,6 +79,8 @@ class Genome::Library {
             reverse_as => 'library',
             is_many => 1,
         },
+        # what is this??? -ss
+        protocol_name           => { is_transient => 1, is => 'Text', },
     ],
     table_name => 'FRAGMENT_LIBRARY',
     schema_name => 'GMSchema',
