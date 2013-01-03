a9279fb3a19f6c862b55064053c023d5363a6bdb
Eddie Belter <ebelter@genome.wustl.edu>
2013-01-03 14:08:54 -0600

    MetaShot: update status to give more detailed output

--- lib/perl/Genome/Model/MetagenomicShotgun/Command/Status.pm
+++ lib/perl/Genome/Model/MetagenomicShotgun/Command/Status.pm
@@ -5,61 +5,119 @@ use warnings;
 
 use Genome;
 
+require Term::ANSIColor;
+
 class Genome::Model::MetagenomicShotgun::Command::Status {
     is => 'Command::V2',
     has => [
-        models => {
+        model => {
             is => 'Genome::Model::MetagenomicShotgun',
-            is_many => 1,
             shell_args_position => 1,
             require_user_verify => 0,
-            doc => 'The metagenomic shotgun models.',
+            doc => 'The metagenomic shotgun model.',
         },
     ],
-    doc => 'show the status of metagenomic shotgun models',
+    doc => 'Status the details of a metagenomic shotgun model',
 };
+
+
+sub help_brief {
+    return 'Detailed status and info about a metagenomic shotgun model';
+}
+
+sub help_detail {
+    return 'This command will list the sub models of metagenomic shotgun model. The listing includes status, pprocessing profile and reference information.';
+}
 
 sub execute {
     my $self = shift;
 
-    my @status;
-    for my $model ( $self->models ) {
-        push @status, $self->_status_for_model($model);
-    }
+    my $show_main_model = $self->_show_main_model;
+    return if not $show_main_model;
 
-    $self->status_message( join("\n", @status) );
+    my $show_sub_models = $self->_show_sub_models;
+    return if not $show_sub_models;
 
     return 1;
 }
 
-sub _status_for_model {
-    my ($self, $model) = @_;
+sub _show_main_model {
+    my $self = shift;
 
-    my @sub_model_methods = (qw/ contamination_screen_model metagenomic_nucleotide_model metagenomic_protein_model viral_nucleotide_model viral_protein_model /);
+    my $model = $self->model;
+    my $model_status = $self->_model_status($model, 'main_model');
+    $self->status_message($model_status);
 
-    my $length = (sort { $b <=> $a } map { length } @sub_model_methods )[0] + 2;
-    my $spacify = sub{ return $_[0].( ' ' x ($length - length($_[0])) ); };
+    return 1;
+}
 
-    my $get_status_for_model = sub{
-        my ($model) = @_;
+sub _show_sub_models {
+    my $self = shift;
+
+    for my $label ( Genome::Model::MetagenomicShotgun->sub_model_labels ) {
+        my $method = $label.'_model';
+        my $model = $self->model->$method;
+        my $pp = $model->processing_profile;
+        my $ref = $model->reference_sequence_build;
         my $build = $model->latest_build;
-        if ( not $build ) {
-            return $model->id.' NO_BUILD';
-        }
-        return join(' ', $model->id, $build->id, $build->status, $build->data_directory);
-    };
-
-    my @status = ( 
-        $spacify->('MAIN MODEL:').' '.$get_status_for_model->($model),
-    );
-    for my $sub_model_method ( @sub_model_methods ) {
-        my $sub_model = $model->$sub_model_method;
-        my $sub_status = $spacify->(ucfirst( join(' ', grep { $_ ne 'model' } split('_', $sub_model_method)) ).':');
-        $sub_status .= ' '.$get_status_for_model->($sub_model);
-        push @status, $sub_status;
+        my $model_status = $self->_model_status($model, $label);
+        $self->status_message($model_status);
+        #$self->status_message(ucfirst(join(' ', split('_', $label))).' model: '.$model->id);
     }
 
-    return join("\n", @status);
+    return 1;
+}
+
+my %status_colors = (
+    na          => "yellow",
+    'new'       => "yellow",
+    scheduled   => "yellow",
+    running     => "cyan",
+    succeeded   => "green",
+    failed      => "red",
+    unstartable => "red",
+    abandoned   => "magenta",
+);
+sub _model_status {
+    my ($self, $model, $label) = @_;
+
+    my @info;
+    my $display_name_format = '%-10s %s';
+
+    my $build = $model->latest_build;
+    my $build_display = 'NA';
+    my $build_status = 'NA';
+    if ( $build ) {
+        $build_display = sprintf($display_name_format, $build->id, $build->data_directory);
+        $build_status = $build->status;
+    }
+    push @info, [ Status => Term::ANSIColor::colored($build_status, $status_colors{lc($build_status)}) ];
+    push @info, [ Build => $build_display ];
+    push @info, [ Model => $model->id.' '.$model->name ];
+
+    my $processing_profile = $model->processing_profile;
+    push @info , [ PP => sprintf($display_name_format, $model->processing_profile->id, $model->processing_profile->name) ];
+
+    my $ref = eval{ $model->reference_sequence_build; };
+    if ( $ref ) {
+        push @info , [ Ref => sprintf($display_name_format, $ref->id, $ref->name) ];
+        push @info , [ Fasta => $ref->fasta_file ];
+    }
+
+    if ( eval{ $processing_profile->read_aligner_name; } ) {
+        push @info , [ Aligner => $processing_profile->read_aligner_name.' (version '.$processing_profile->read_aligner_version.')'];
+        push @info , [ Params => ( $processing_profile->read_aligner_params || 'NA' ) ];
+    }
+
+    my $status = Term::ANSIColor::colored( join(' ', map { ucfirst } split('_', $label)), 'underline' )."\n";
+    my $format = '%10s';
+    for my $info ( @info ) {
+        $status .= Term::ANSIColor::colored(sprintf($format, ucfirst($info->[0].': ')), 'bold');
+        $status .= $info->[1]."\n";
+    }
+    $status .= "\n";
+
+    return $status;
 }
 
 1;
