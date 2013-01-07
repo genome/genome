160f183b8f54a04bb390cc251e175bb2e54770ed
Eddie Belter <ebelter@genome.wustl.edu>
2013-01-07 17:34:54 -0600

    Sync: add sample attr and pop group member misc update subclasses

---
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/PopulationGroupMember.pm
@@ -0,0 +1,11 @@
+package Genome::Site::TGI::Synchronize::Classes::MiscUpdate::PopulationGroupMember;
+
+use strict;
+use warnings;
+
+class Genome::Site::TGI::Synchronize::Classes::MiscUpdate::PopulationGroupMember {
+    is => 'Genome::Site::TGI::Synchronize::Classes::MiscUpdate',
+};
+
+1;
+
---
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/SampleAttribute.pm
@@ -0,0 +1,11 @@
+package Genome::Site::TGI::Synchronize::Classes::MiscUpdate::SampleAttribute;
+
+use strict;
+use warnings;
+
+class Genome::Site::TGI::Synchronize::Classes::MiscUpdate::SampleAttribute {
+    is => 'Genome::Site::TGI::Synchronize::Classes::MiscUpdate',
+};
+
+1;
+
