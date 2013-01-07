c26dfa3d36f85f58b9e1b13c5f026c5f8c2e3331
Eddie Belter <ebelter@genome.wustl.edu>
2013-01-07 16:29:30 -0600

    Sync: dir for misc updates

---
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/OrganismTaxon.pm
@@ -0,0 +1,11 @@
+package Genome::Site::TGI::Synchronize::Classes::MiscUpdate::OrganismTaxon;
+
+use strict;
+use warnings;
+
+class Genome::Site::TGI::Synchronize::Classes::MiscUpdate::OrganismTaxon {
+    is => 'Genome::Site::TGI::Synchronize::Classes::MiscUpdate::Subject',
+};
+
+1;
+
---
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/PopulationGroup.pm
@@ -0,0 +1,11 @@
+package Genome::Site::TGI::Synchronize::Classes::MiscUpdate::PopulationGroup;
+
+use strict;
+use warnings;
+
+class Genome::Site::TGI::Synchronize::Classes::MiscUpdate::PopulationGroup {
+    is => 'Genome::Site::TGI::Synchronize::Classes::MiscUpdate::Subject',
+};
+
+1;
+
