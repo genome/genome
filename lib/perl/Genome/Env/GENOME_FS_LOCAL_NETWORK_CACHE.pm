c9cd3d6283a25260848c846c1b093b786ef5a274
Scott Smith <ssmith@genome.wustl.edu>
2013-01-14 09:19:59 -0600

    added GENOME_FS_LOCAL_NETWORK_CACHE environment variable to replace hard-coded /var/cache/tgisan and updated Genome::Site::TGI

---
+++ lib/perl/Genome/Env/GENOME_FS_LOCAL_NETWORK_CACHE.pm
@@ -0,0 +1,20 @@
+package Genome::Env::GENOME_FS_LOCAL_NETWORK_CACHE;
+our $default_value = '/var/lib/genome/db';
+
+=pod
+
+=head1 NAME
+
+GENOME_FS_LOCAL_NETWORK_CACHE
+
+=head1 DESCRIPTION
+
+The GENOME_FS_LOCAL_NETWORK_CACHE environment variable is the root directory for data base storage.
+
+=head1 DEFAULT VALUE
+
+ /var/lib/genome/db/
+
+=cut
+
+1;
