c65a08fcbab5c43575c92ee1a8c037ced68b9003
Jason Walker <jwalker@genome.wustl.edu>
2013-01-30 13:17:09 -0600

    Use hash references rather than objects and separated value readers rather than UR::FileMux

--- lib/perl/Genome/Model/GenePrediction/Command/Eukaryotic/CodingGenesToGff.pm
+++ lib/perl/Genome/Model/GenePrediction/Command/Eukaryotic/CodingGenesToGff.pm
@@ -5,6 +5,7 @@ use warnings;
 use Genome;
 use Carp 'confess';
 
+
 class Genome::Model::GenePrediction::Command::Eukaryotic::CodingGenesToGff {
     is => 'Genome::Command::Base',
     has_input => [
@@ -38,29 +39,54 @@ sub execute {
     my @coding_genes = Genome::Prediction::CodingGene->get(directory => $prediction_dir);
     $self->status_message("Found " . scalar @coding_genes . " coding gene predictions in $prediction_dir!");
 
+    my $coding_gene_reader = Genome::Utility::IO::SeparatedValueReader->create(
+        input => $prediction_dir.'/coding_genes.csv',
+        headers => Genome::DataSource::Predictions::CodingGenes->column_order,
+    );
+
+    my $transcript_reader = Genome::Utility::IO::SeparatedValueReader->create(
+        input => $prediction_dir .'/transcripts.csv',
+        headers => Genome::DataSource::Predictions::Transcripts->column_order,
+    );
+
+    my $exon_reader = Genome::Utility::IO::SeparatedValueReader->create(
+        input => $prediction_dir .'/exons.csv',
+        headers => Genome::DataSource::Predictions::Exons->column_order,
+    );
+    
     my $output_fh = IO::File->new($self->output_file, 'w');
     confess 'Could not get file handle for output file ' . $self->output_file unless $output_fh;
+    my $exon = $exon_reader->next;
+    while( my $gene = $coding_gene_reader->next) {
+        my $transcript = $transcript_reader->next;
+        unless ($transcript->{coding_gene_name} eq $gene->{gene_name}) {
+            die('Coding Gene and Transcript files are out of order!');
+        }
 
-    for my $gene (@coding_genes) {
-        my $transcript = $gene->transcript;
-        my @exons = $transcript->exons;
-
-        my ($start, $end) = (1, $transcript->spliced_length);
-        my $desc = join(';', "ID=" . $gene->id, "Name=" . $gene->gene_name, "Target=" . 
-            join(' ', $gene->gene_name, $start, $end, substr($gene->strand, 0, 1)));
-        $output_fh->print(join("\t", $gene->sequence_name, $gene->source, 'match', $gene->start, 
-                $gene->end, '.', substr($gene->strand, 0, 1), '.', $desc) . "\n");
+        my @exons;
+        while ($exon && ($exon->{transcript_name} eq $transcript->{transcript_name}) ) {
+            push @exons, $exon;
+            $exon = $exon_reader->next;
+        }
+        unless (@exons) {
+            die ('Coding Gene, Transcript and Exon files are out of order!');
+        }
+        my ($start, $end) = (1, $self->spliced_length(\@exons));
+        my $desc = join(';', "ID=" . $gene->{gene_name}, "Name=" . $gene->{gene_name}, "Target=" . 
+            join(' ', $gene->{gene_name}, $start, $end, substr($gene->{strand}, 0, 1)));
+        $output_fh->print(join("\t", $gene->{sequence_name}, $gene->{source}, 'match', $gene->{start}, 
+                $gene->{end}, '.', substr($gene->{strand}, 0, 1), '.', $desc) . "\n");
 
         my $exon_count = 0;
         my $exon_length = 0;
         for my $exon (@exons) {
-            my ($exon_start, $exon_end) = ($exon_length + 1, $exon_length + $exon->length);
-            $exon_length += $exon->length;
+            my ($exon_start, $exon_end) = ($exon_length + 1, $exon_length + $self->exon_length($exon));
+            $exon_length += $self->exon_length($exon);
 
-            my $exon_desc = join(';', "ID=" . $gene->id . ":cds:$exon_count", "Parent=" . $gene->id, "Name=" . $gene->gene_name, 
-                "Target=" . join(' ', $gene->id, $exon_start, $exon_end, substr($exon->strand, 0, 1)));
-            $output_fh->print(join("\t", $exon->sequence_name, $gene->source, 'match_part', $exon->start,
-                    $exon->end, $exon->score, substr($exon->strand, 0, 1), '.', $exon_desc) . "\n");
+            my $exon_desc = join(';', "ID=" . $gene->{gene_name} . ":cds:$exon_count", "Parent=" . $gene->{gene_name}, "Name=" . $gene->{gene_name}, 
+                "Target=" . join(' ', $gene->{gene_name}, $exon_start, $exon_end, substr($exon->{strand}, 0, 1)));
+            $output_fh->print(join("\t", $exon->{sequence_name}, $gene->{source}, 'match_part', $exon->{start},
+                    $exon->{end}, $exon->{score}, substr($exon->{strand}, 0, 1), '.', $exon_desc) . "\n");
             $exon_count++;
         }
     }
@@ -68,6 +94,20 @@ sub execute {
     $output_fh->close;
     $self->status_message("Successfully completed conversion to gff, output file at " . $self->output_file);
     return 1;
+}
+
+sub exon_length {
+    my ($self,$exon) = @_;
+    return abs($exon->{start} - $exon->{end}) + 1;
+}
+
+sub spliced_length {
+    my ($self,$exons) = @_;
+    my $sum = 0;
+    for my $exon (@$exons) {
+        $sum += $self->exon_length($exon);
+    }
+    return $sum;
 }
 
 1;
