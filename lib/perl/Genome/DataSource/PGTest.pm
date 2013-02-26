6d11256b77933a69d9a6aa27b290f75360120634
Ben Oberkfell <boberkfe@genome.wustl.edu>
2013-02-26 15:58:26 -0600

    SELECT queries cause Postgres to open locks that sit open all transaction.  Fix this by not opening transactions except when writing to the database.

--- lib/perl/Genome/DataSource/PGTest.pm
+++ lib/perl/Genome/DataSource/PGTest.pm
@@ -23,4 +23,44 @@ sub _ds_tag {
     'Genome::DataSource::PGTest';
 }
 
+sub _dbi_connect_args {
+    my $self = shift;
+    
+    my @connection = $self->SUPER::_dbi_connect_args(@_);
+    
+    $connection[3] = { AutoCommit => 1, RaiseError => 0 };
+
+    return @connection;
+}
+
+sub _sync_database {
+    my $self = shift;
+    my $dbh = $self->get_default_handle();
+   
+    # do the actual sync in an eval, so we can set autocommit back
+    my $sync_rv;
+    eval {
+        $dbh->begin_work(); 
+        $sync_rv = $self->SUPER::_sync_database(@_);
+    };
+    
+    # rethrow the exception if we got one
+    if ($@) {
+        die $@;
+    }
+    
+    return $sync_rv;
+}
+
+# UR::DBI wants to rollback when we disconnect.  Open a transaction
+# so it doesn't barf when we disconnect.
+sub prepare_for_fork {
+    my $self = shift;
+    if ($self->has_default_handle) {
+        my $dbh = $self->get_default_handle();
+        $dbh->begin_work();
+    }
+    $self->SUPER::prepare_for_fork(@_);
+}
+
 1;
