b4aaca5c3fdac4ad901298849fb410ada7373ad2
Eddie Belter <ebelter@genome.wustl.edu>
2013-02-26 19:52:22 -0600

    Sync: add lims table info; add properties to copy, keep updated and lims to genome conversion

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/IndexIllumina.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/IndexIllumina.pm
@@ -5,6 +5,76 @@ use warnings;
 
 use Genome;
 
+=comment
+# FLOW_CELL_ILLUMINA
+CREATION_EVENT_ID NUMBER   (15)                    {null} NOT NULL NOT_SYNCED
+FLOW_CELL_ID      VARCHAR2 (15)                    {null} NOT NULL NOT_SYNCED
+GROUP_NAME        VARCHAR2 (64)                    {null} {null}   NOT_SYNCED
+MACHINE_NAME      VARCHAR2 (64)                    {null} NOT NULL NOT_SYNCED
+RUN_NAME          VARCHAR2 (64)                    {null} NOT NULL ok
+RUN_TYPE          VARCHAR2 (25)                    {null} NOT NULL NOT_SYNCED [used in join, but value stored is calculated from reads]
+TEAM_NAME         VARCHAR2 (64)                    {null} {null}   NOT_SYNCED
+
+# INDEX_ILLUMINA
+ANALYSIS_ID                    NUMBER   (15)                    {null} NOT NULL {null} ok [id]
+ANALYSIS_NAME                  VARCHAR2 (15)                    {null} NOT NULL {null} NOT_SYNCED
+ANALYSIS_SOFTWARE_VERSION      VARCHAR2 (32)                    {null} NOT NULL {null} ok
+AVG_FILT_CLUSTERS_ACROSS_TILES NUMBER   (10)                    {null} {null}   {null} NOT_SYNCED
+CLUSTERS                       NUMBER   (10)                    {null} NOT NULL {null} NOT_SYNCED
+CLUSTERS_AVG                   NUMBER   (12,2)                  {null} NOT NULL {null} NOT_SYNCED
+CLUSTERS_STDEV                 NUMBER   (12,2)                  {null} NOT NULL {null} NOT_SYNCED
+CREATION_EVENT_ID              NUMBER   (15)                    {null} NOT NULL {null} NOT_SYNCED
+EXPERIMENT_PURPOSE             VARCHAR2 (32)                    {null} {null}   {null} NOT_SYNCED
+FILT_CLUSTERS                  NUMBER   (17,2)                  {null} NOT NULL {null} ok [clusters]
+FILT_CLUSTERS_AVG              NUMBER   (14,2)                  {null} NOT NULL {null} NOT_SYNCED
+FILT_CLUSTERS_STDEV            NUMBER   (14,2)                  {null} NOT NULL {null} NOT_SYNCED
+FLOW_CELL_ID                   VARCHAR2 (15)                    {null} NOT NULL {null} ok
+GERALD_DIRECTORY               VARCHAR2 (255)                   {null} NOT NULL {null} ok
+INDEX_SEQUENCE                 VARCHAR2 (64)                    {null} {null}   {null} ok
+INTEN_AVG                      NUMBER   (15)                    {null} {null}   {null} NOT_SYNCED
+INTEN_STDEV                    NUMBER   (15)                    {null} {null}   {null} NOT_SYNCED
+LANE                           NUMBER   (3)                     {null} NOT NULL {null} ok
+LIBRARY_ID                     NUMBER   (20)                    {null} NOT NULL {null} ok
+LIBRARY_STRATEGY               VARCHAR2 (30)                    {null} {null}   {null} NOT_SYNCED
+MEDIAN_INSERT_SIZE             NUMBER   (7)                     {null} {null}   {null} ok
+RESEARCH_PROJECT               VARCHAR2 (64)                    {null} {null}   {null} NOT_SYNCED
+SD_ABOVE_INSERT_SIZE           NUMBER   (7)                     {null} {null}   {null} ok
+SD_BELOW_INSERT_SIZE           NUMBER   (7)                     {null} {null}   {null} ok
+SEQ_ID                         NUMBER   (15)                    {null} NOT NULL {null} NOT_SYNCED
+SS_ID                          NUMBER   (15)                    {null} {null}   {null} NOT_SYNCED
+TAGGED_DNA_ID                  NUMBER   (20)                    {null} {null}   {null} NOT_SYNCED
+TARGET_REGION_SET_NAME         VARCHAR2 (512)                   {null} {null}   {null} ok
+
+# READ_ILLUMINA
+II_SEQ_ID                   NUMBER   (15)                    {null} NOT NULL NOT_SYNCED
+READ_NUMBER                 NUMBER   (1)                     {null} NOT NULL NOT_SYNCED
+READ_LENGTH                 NUMBER   (3)                     {null} NOT NULL ok [from r2]
+INTEN_AFTER_20_CYCLES_PCT   NUMBER   (9,2)                   {null} {null}   NOT_SYNCED
+INTEN_AFTER_20_CYCLES_STDEV NUMBER   (9,2)                   {null} {null}   NOT_SYNCED
+FILT_ALIGNED_CLUSTERS_PCT   NUMBER   (9,2)                   {null} {null}   ok [fwd_filt_aligned_clusters_pct from r1; rev_filt_aligned_clusters_pct from r2]
+FILT_ALIGNED_CLUSTERS_STDEV NUMBER   (9,2)                   {null} {null}   NOT_SYNCED
+FILT_ERROR_RATE_AVG         NUMBER   (9,2)                   {null} {null}   ok [from r2; fwd_filt_error_rate_avg from r1; rev_filt_error_rate_avg from r2]
+FILT_ERROR_RATE_STDEV       NUMBER   (9,2)                   {null} {null}   NOT_SYNCED
+FIRST_CYCLE_INTEN_AVG       NUMBER   (17,2)                  {null} NOT NULL NOT_SYNCED
+FIRST_CYCLE_INTEN_STDEV     NUMBER   (17,2)                  {null} NOT NULL NOT_SYNCED
+PHASING_PCT                 NUMBER   (9,2)                   {null} NOT NULL NOT_SYNCED
+PREPHASING_PCT              NUMBER   (9,2)                   {null} NOT NULL NOT_SYNCED
+KILOBASES_READ              NUMBER   (12)                    {null} NOT NULL ok [fwd_kilobases_read from r2 if r1.seq_id is not null; rev_kilobases_read from r2 if r1.seq_id is not null] 
+RECIPE_READ_NUMBER          NUMBER   (1)                     {null} NOT NULL NOT_SYNCED
+ORIENTATION                 VARCHAR2 (50)                    {null} NOT NULL NOT_SYNCED
+SLS_SEQ_ID                  NUMBER   (15)                    {null} {null}   ok [fwd_seq_id from r1; rev_seq_id from r2 if r1.sed_id is not null]
+SEQ_ID                      NUMBER   (15)                    {null} NOT NULL NOT_SYNCED
+N_BASE_COUNT                NUMBER   (15)                    {null} {null}   NOT_SYNCED
+N_READ_COUNT                NUMBER   (15)                    {null} {null}   NOT_SYNCED
+Q2_BASE_COUNT               NUMBER   (15)                    {null} {null}   NOT_SYNCED
+Q2_READ_COUNT               NUMBER   (15)                    {null} {null}   NOT_SYNCED
+Q20_BASE_COUNT              NUMBER   (15)                    {null} {null}   NOT_SYNCED
+BASE_QUALITY_SUM            NUMBER   (15)                    {null} {null}   NOT_SYNCED
+Q30_BASE_COUNT              NUMBER   (15)                    {null} {null}   NOT_SYNCED
+FIRST_AVG_Q_BELOW_20        VARCHAR2 (7)                     {null} {null}   NOT_SYNCED
+
+=cut
+
 class Genome::Site::TGI::Synchronize::Classes::IndexIllumina {
     table_name => <<EOS
         (
@@ -12,8 +82,9 @@ class Genome::Site::TGI::Synchronize::Classes::IndexIllumina {
                 --Index Illumina
                 to_char(i.analysis_id) id,
                 i.library_id library_id,
-                i.index_sequence,
-                i.lane,
+                i.index_sequence index_sequence,
+                i.flow_cell_id
+                i.lane lane,
                 i.target_region_set_name,
                 i.gerald_directory,
                 i.median_insert_size,
@@ -28,12 +99,11 @@ class Genome::Site::TGI::Synchronize::Classes::IndexIllumina {
 
                 --Flow Cell
                 fc.run_name run_name,
-                fc.flow_cell_id,
 
-                --Read Illumina #1
+                --Read Illumina #1 r1 is always reverse when defined [only if the run is paired end]
                 (case when r1.seq_id is not null then 'Paired' else 'Standard' end) run_type,
 
-                --Read Illumina #2
+                --Read Illumina #2 r2 is fwd for fragment and rev for paired end
                 r2.read_length,
                 r2.filt_error_rate_avg,
 
@@ -138,4 +208,37 @@ EOS
     data_source => 'Genome::DataSource::GMSchema',
 };
 
+# TODO Require rebuild?
+# library_id
+# target_region_set_name
+# gerald_directory
+# TODO not updated. Fix?
+# run_name [on flow_cell_illumina]
+sub properties_to_copy {# 12
+    return ( 'id', 'library_id', 'gerald_directory', 'target_region_set_name', properties_to_keep_updated() );
+}
+
+sub properties_to_keep_updated {# 8
+    return (qw/ 
+        analysis_software_version
+        clusters
+        flow_cell_id
+        index_sequence
+        lane
+        median_insert_size
+        sd_above_insert_size
+        sd_below_insert_size
+        /);
+}
+
+sub lims_property_name_to_genome_property_name {
+    my ($class, $name) = @_;
+    my %lims_to_genome = (
+        filt_clusters => 'clusters',
+    );
+    return $lims_to_genome{$name} if exists $lims_to_genome{$name};
+    return $name;
+}
+
 1;
+
