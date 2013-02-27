267635f661e202fb3ef2dcbf08ff0f684afb453a
Eddie Belter <ebelter@genome.wustl.edu>
2013-02-27 16:52:27 -0600

    Sync: convert misc update ii to base for attrs

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/BaseForAttributes.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/BaseForAttributes.pm
@@ -7,7 +7,7 @@ class Genome::Site::TGI::Synchronize::Classes::MiscUpdate::BaseForAttributes {
     is => 'Genome::Site::TGI::Synchronize::Classes::MiscUpdate::Base',
 };
 
-sub _get_current_value {
+sub _get_current_attr {
     my $self = shift;
 
     my $genome_entity = $self->genome_entity;
@@ -16,7 +16,16 @@ sub _get_current_value {
     my $genome_property_name = $self->genome_property_name;
     return if not $genome_property_name;
 
-    my $current_attr = $self->_get_attribute($genome_property_name);
+    return $genome_entity->attributes(
+        attribute_label => $genome_property_name,
+        nomenclature => 'WUGC',
+    );
+}
+
+sub _get_current_value {
+    my $self = shift;
+
+    my $current_attr = $self->_get_current_attr;
     return if not $current_attr;
     
     return $current_attr->attribute_value;
@@ -25,13 +34,20 @@ sub _get_current_value {
 sub _update_value {
     my $self = shift;
 
+    my $genome_entity = $self->genome_entity;
+    return if not $genome_entity;
+
     my $genome_property_name = $self->genome_property_name;
     return if not $genome_property_name;
 
-    my $current_attr = $self->_get_attribute($genome_property_name);
+    my $current_attr = $self->_get_current_attr;
     $current_attr->delete if $current_attr;
 
-    my $new_attr = $self->_update_attribute($genome_property_name, $self->new_value);
+    my $new_attr = $genome_entity->add_attribute(
+        attribute_label => $genome_property_name,
+        attribute_value => $self->new_value,
+        nomenclature => 'WUGC',
+    );
     return if not $new_attr;
 
     if ( my $after_update_value = $self->can('_after_update_value') ) {
@@ -40,49 +56,6 @@ sub _update_value {
     }
 
     return $new_attr->attribute_value;
-}
-
-sub _update_attribute {
-    my ($self, $attribute_label, $new_value) = @_;
-
-    my $current_attr = $self->_get_attribute($attribute_label);
-    if ( $current_attr ) {
-        if ( $current_attr->attribute_value eq $new_value ) {
-            return $current_attr;
-        }
-        $current_attr->delete;
-    }
-
-    my $new_attr = $self->_create_attribute($attribute_label, $new_value);
-    return $new_attr;
-}
-
-
-sub _get_attribute {
-    my ($self, $attribute_label) = @_;
-
-    return Genome::SubjectAttribute->get(
-        subject_id => $self->subject_id,
-        attribute_label => $attribute_label,
-        nomenclature => 'WUGC',
-    );
-}
-
-sub _create_attribute {
-    my ($self, $attribute_label, $attribute_value) = @_;
-
-    my $new_attr = Genome::SubjectAttribute->create(
-        subject_id => $self->subject_id,
-        attribute_label => $attribute_label,
-        attribute_value => $attribute_value,
-        nomenclature => 'WUGC',
-    );
-    if ( not $new_attr ) {
-        $self->error_message('Failed to create instrument data attribute!');
-        return;
-    }
-
-    return $new_attr;
 }
 
 1;
--- lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/IndexIllumina.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/MiscUpdate/IndexIllumina.pm
@@ -7,47 +7,11 @@ class Genome::Site::TGI::Synchronize::Classes::MiscUpdate::IndexIllumina {
     is => 'Genome::Site::TGI::Synchronize::Classes::MiscUpdate::BaseForAttributes',
 };
 
-sub _get_attribute {
-    my ($self, $attribute_label) = @_;
-
-    return Genome::InstrumentDataAttribute->get(
-        instrument_data_id => $self->subject_id,
-        attribute_label => $attribute_label,
-        nomenclature => 'WUGC',
-    );
-}
-
-sub _create_attribute {
-    my ($self, $attribute_label, $attribute_value) = @_;
-
-    my $new_attr = Genome::InstrumentDataAttribute->create(
-        instrument_data_id => $self->subject_id,
-        attribute_label => $attribute_label,
-        attribute_value => $attribute_value,
-        nomenclature => 'WUGC',
-    );
-    if ( not $new_attr ) {
-        $self->error_message('Failed to create instrument data attribute!');
-        return;
-    }
-
-    return $new_attr;
-}
-
 sub _after_update_value {
     my $self = shift;
 
     my $genome_property_name = $self->genome_property_name;
-    if ( $genome_property_name eq 'lane' or $genome_property_name eq 'index_sequence' ) {
-        my $update_subset_name = $self->_update_subset_name;
-        return if not $update_subset_name;
-    }
-
-    return 1;
-}
-
-sub _update_subset_name {
-    my $self = shift;
+    return 1 if not grep { $genome_property_name eq $_ } (qw/  lane index_sequence /);
 
     my $genome_entity = $self->genome_entity;
     my $lane = $genome_entity->lane;
