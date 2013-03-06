41ca7bda64fd715b2091d3a0d28e36562c195f28
Scott Smith <ssmith@genome.wustl.edu>
2013-03-05 22:51:50 -0600

    updated "gmt htseq" and related infrastructure

--- lib/perl/Genome/Command/WithSavedResults.pm
+++ lib/perl/Genome/Command/WithSavedResults.pm
@@ -29,5 +29,46 @@ sub _copyable_properties {
     return Genome::SoftwareResult::Default::_copyable_properties(@_);
 }
 
+sub execute {
+    my $self = shift;
+    
+    my $result_version = $self->result_version;
+    my $method = "_execute_v$result_version";
+    $method =~ s/\./_/g;
+    unless ($self->can($method)) {
+        die "no implementation ($method) for version $result_version!";
+    }
+
+    my @run_commands;
+    my $parallelize_by = $self->__meta__->parallelize_by;
+    if ($parallelize_by and @$parallelize_by) {
+        if (@$parallelize_by > 1) {
+            die "support for multiplexed paralleized_by not implimented!: @$parallelize_by";
+        }
+        my $prop = $parallelize_by->[0];
+        my @values = $self->$prop;
+        if (@values > 1) {
+            my %props = $self->_copyable_properties($self->class);
+            for my $value (@values) {
+                print "breakdown for value " . $value->__display_name__ . "\n";
+                $props{$prop} = [$value];
+                my $partial = $self->class->create(%props);
+                unless ($partial) {
+                    die "failed to create partial for $prop " . $value->__display_name__;
+                }
+                push @run_commands, $partial;
+            }
+            
+            print "run commands: @run_commands\n";
+            for my $cmd (@run_commands) {
+                $cmd->$method(@_);
+            } 
+            die "no merge logic yet!";
+        }
+    }
+   
+    return $self->$method(@_);
+}
+
 1;
 
