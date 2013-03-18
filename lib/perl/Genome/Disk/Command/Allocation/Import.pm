fb2a01502fd60f5a6e5463dc0f8f1c021982464f
Brian Derickson <bdericks@genome.wustl.edu>
2013-03-18 15:44:56 -0500

    Changes to CreateForExisting allocation command
    Renamed to import. Primitivized method arguments for easier testing.

--- lib/perl/Genome/Disk/Command/Allocation/Import.pm
+++ lib/perl/Genome/Disk/Command/Allocation/Import.pm
@@ -1,4 +1,4 @@
-package Genome::Disk::Command::Allocation::CreateForExistingData;
+package Genome::Disk::Command::Allocation::Import;
 
 use strict;
 use warnings;
@@ -6,10 +6,10 @@ use warnings;
 use Genome;
 use Cwd 'realpath';
 
-class Genome::Disk::Command::Allocation::CreateForExistingData {
+class Genome::Disk::Command::Allocation::Import {
     is => 'Command::V2',
     has => [
-        target_path => {
+        path => {
             is => 'DirectoryPath',
             doc => 'Path at which data exists, allocation will be made to point to this location',
         },
@@ -29,7 +29,7 @@ class Genome::Disk::Command::Allocation::CreateForExistingData {
         volume_prefix => {
             is => 'Text',
             default_value => '/gscmnt',
-            doc => 'Volume prefix to expect at start of target_path',
+            doc => 'Volume prefix to expect at start of the path',
         },
         allocation => {
             is => 'Genome::Disk::Allocation',
@@ -65,13 +65,13 @@ EOS
 sub execute {
     my $self = shift;
 
-    my $kb = $self->_validate_and_get_size_of_path;
-    my ($mount_path, $group_subdir, $allocation_path) = $self->_parse_path;
+    my $kb = $self->_validate_and_get_size_of_path($self->path);
+    my ($mount_path, $group_subdir, $allocation_path) = $self->_parse_path($self->path, $self->volume_prefix);
     my $volume = $self->_get_and_validate_volume($mount_path);
     my $group = $self->_get_and_validate_group($group_subdir, $volume->groups);
     $self->_verify_allocation_path($allocation_path);
 
-    $self->status_message("Parsed and verified target path " . $self->target_path . ", creating allocation.");
+    $self->status_message("Parsed and verified target path " . $self->path . ", creating allocation.");
 
     my %params = (
         disk_group_name => $group->disk_group_name,
@@ -92,8 +92,8 @@ sub execute {
     $self->status_message("Created allocation " . $allocation->id . ", moving to final location");
 
     $allocation->allocation_path($allocation_path);
-    unless ($allocation->absolute_path eq $self->target_path) {
-        die "Somehow, absolute path of new allocation does not match expected value " . $self->target_path;
+    unless ($allocation->absolute_path eq $self->path) {
+        die "Somehow, absolute path of new allocation does not match expected value " . $self->path;
     }
 
     unless (Genome::Sys->remove_directory_tree($temp_location)) {
@@ -105,7 +105,7 @@ sub execute {
 }
 
 sub _verify_allocation_path {
-    my ($self, $allocation_path) = @_;
+    my ($class, $allocation_path) = @_;
     # I don't know why this method is private, I don't think it should be
     if (my $parent_allocation = Genome::Disk::Allocation->_get_parent_allocation($allocation_path)) {
         die "Parent allocation (" . $parent_allocation->id . ") found for $allocation_path!";
@@ -120,10 +120,11 @@ sub _verify_allocation_path {
 }
 
 sub _parse_path {
-    my $self = shift;
-    my $volume_prefix = $self->volume_prefix;
+    my ($class, $target_path, $volume_prefix) = @_;
+    die "_parse_path method must be given a target path!" unless $target_path;
+    die "_parse_path method must be given a volume prefix!" unless $volume_prefix;
     my ($mount_path, $group_subdir, $allocation_path) =
-        $self->target_path =~  /^($volume_prefix\/\w+)\/(\w+)\/(.+)$/;
+        $target_path =~  /^($volume_prefix\/\w+)\/(\w+)\/(.+)$/;
     unless ($mount_path and $group_subdir and $allocation_path) {
         die "Could not determine mount path, group subdirectory, or allocation path from given path!";
     }
@@ -131,7 +132,7 @@ sub _parse_path {
 }
 
 sub _get_and_validate_volume {
-    my ($self, $mount_path) = @_;
+    my ($class, $mount_path) = @_;
     my $volume = Genome::Disk::Volume->get(
         mount_path => $mount_path,
         disk_status => 'active',
@@ -144,7 +145,7 @@ sub _get_and_validate_volume {
 }
 
 sub _get_and_validate_group {
-    my ($self, $group_subdir, @groups) = @_;
+    my ($class, $group_subdir, @groups) = @_;
     my $group;
     for my $candidate_group (@groups) {
         if ($candidate_group->subdirectory eq $group_subdir) {
@@ -159,21 +160,21 @@ sub _get_and_validate_group {
 }
 
 sub _validate_and_get_size_of_path {
-    my $self = shift;
-    if (-l $self->target_path) {
-        die "Path is a link, you should try allocating " . realpath($self->target_path)
-            . " instead of " . $self->target_path;
+    my ($class, $target_path) = @_;
+    if (-l $target_path) {
+        die "Path is a link, you should try allocating " . realpath($target_path)
+            . " instead of " . $target_path;
     }
-    if (not -d $self->target_path) {
-        die "Path does not exist or is not a directory: " . $self->target_path;
+    if (not -d $target_path) {
+        die "Path does not exist or is not a directory: " . $target_path;
     }
-    if (realpath($self->target_path) ne $self->target_path) {
+    if (realpath($target_path) ne $target_path) {
         die "Target path and absolute path differ; you should try allocating "
-            . realpath($self->target_path) . " instead of " . $self->target_path;
+            . realpath($target_path) . " instead of " . $target_path;
     }
-    my $kb = Genome::Sys->disk_usage_for_path($self->target_path);
+    my $kb = Genome::Sys->disk_usage_for_path($target_path);
     unless (defined $kb) {
-        die "Could not determine size of " . $self->target_path;
+        die "Could not determine size of " . $target_path;
     }
     return $kb;
 }
