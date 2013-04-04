36032b3a456f99b351cff665eb2b2e37c3001d37
nnutter <nnutter@genome.wustl.edu>
2013-04-04 15:16:26 -0500

    disable tests because they require special permissions to function well

--- lib/perl/Genome/Disk/Allocation/t-lib/GenomeDiskAllocationTest.pm
+++ lib/perl/Genome/Disk/Allocation/t-lib/GenomeDiskAllocationTest.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Carp qw(croak);
 use File::Basename qw(dirname);
-use Test::More;
+use Test::More skip_all => 'Test requires fuseext2 and leaves a mess that has to be cleaned up manually if correct permissions are not setup.';
 use Time::HiRes qw(usleep);
 use Sys::Hostname qw(hostname);
 use above 'Genome';
