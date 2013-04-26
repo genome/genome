5d9dd90f4f8b5cda43e05daa4e8aa79ac57c2b05
Eddie Belter <ebelter@genome.wustl.edu>
2013-04-26 14:19:14 -0500

    DeNovo: initial commit of read processor parser

---
+++ lib/perl/Genome/Model/DeNovoAssembly/SxReadProcessor.pm
@@ -0,0 +1,146 @@
+package Genome::Model::DeNovoAssembly::SxReadProcessor;
+
+use strict;
+use warnings;
+
+use Genome;
+
+use Data::Dumper 'Dumper';
+use Parse::RecDescent;
+
+class Genome::Model::DeNovoAssembly::SxReadProcessor { 
+    has => [
+        read_processor => {
+            is => 'Text',
+            doc => '',
+        },
+        instrument_data => {
+            is => 'Genome::InstrumentData',
+            is_many => 1,
+            doc => 'Instrument data to be processed with SX.',
+        },
+    ],
+    has_optional => [
+        genome_size => {
+            is => 'Number',
+            doc => 'Include genome size to determine coverage limits.',
+        },
+    ],
+    has_transient_optional => [
+        default_read_processor => { is => 'HASH', },
+        _parsed_read_processors => { is => 'ARRAY', },
+    ],
+};
+
+our $parser; # singleton
+sub parser {
+    my $self = shift;
+
+    return $parser if $parser;
+
+    my $grammar = q{
+
+        read_processor: specification(s)
+        { $item[1]; }
+
+        specification: default "(" processor ", coverage " coverage ")"
+        { $return = { condition => $item{default}, processor => $item{processor}, coverage => $item{coverage}, } }
+        | default "(" processor ")"
+        { $return = { condition => $item{default}, processor => $item{processor}, } }
+        | condition(s) "(" processor ", coverage " coverage ")"
+        { $return = { condition => $item{'condition(s)'}, processor => $item{processor}, coverage => $item{coverage}, } }
+        | condition(s) "(" processor ")"
+        { $return = { condition => $item{'condition(s)'}, processor => $item{processor}, } }
+
+        default: /DEFAULT/
+        { $item[1]; }
+
+        condition: /([\w\d\.\=\<\>\*]+)/
+        { $item[1]; }
+
+        processor: /([\w\d\-\.\/\_\:\= ]+)/
+        { $item[1]; }
+
+        coverage: /(\d+)X/
+        { $item[1] =~ s/X//; $item[1]; }
+    };
+
+    $parser = Parse::RecDescent->new($grammar);
+    if ( not $parser ) {
+        $self->error_message("Failed to create parser from sx read processor grammar!");
+        return;
+    }
+
+    return $parser
+}
+
+sub __errors__ {
+    my $self = shift;
+
+    my @errors = $self->SUPER::__errors__(@_);
+    return @errors if @errors;
+
+    my $parser = $self->parser;
+    my $read_processor = $self->read_processor;
+    my $parsed_read_processors;
+    if ( $read_processor =~ /^DEF/ ) {
+        $parsed_read_processors = $parser->read_processor($read_processor);
+        if ( not $parsed_read_processors ) {
+            return UR::Object::Tag->create(
+                type => 'invalid',
+                properties => [qw/ read_processor /],
+                desc => 'Failed to parse read processor specification!',
+            );
+        }
+        $parsed_read_processors = [ $parsed_read_processors ] if not ref $parsed_read_processors eq 'ARRAY';
+    }
+    else {
+        $read_processor =~ s/^gmt sx //;
+        $parsed_read_processors = [{
+                condition => 'DEFAULT',
+                processor => $read_processor,
+            },];
+    }
+    $self->_parsed_read_processors($parsed_read_processors);
+    print Dumper($parsed_read_processors);
+
+    my @default_read_processors = grep { $_->{condition} eq 'DEFAULT' } @$parsed_read_processors;
+    if ( @default_read_processors != 1 ) {
+        return UR::Object::Tag->create(
+            type => 'invalid',
+            properties => [qw/ read_processor /],
+            desc => ( @default_read_processors ? 'Multiple' : 'No' ).' DEFAULT read processor(s) in specification!',
+        );
+    }
+    $self->default_read_processor($default_read_processors[0]);
+
+    return;
+}
+
+sub create {
+    my $class = shift;
+
+    my $self = $class->SUPER::create(@_);
+    return if not $self;
+
+    my @errors = $self->__errors__;
+    if ( @errors ) {
+        for my $error ( @errors ) {
+            $self->error_message( $error->__display_name__ );
+        }
+        return;
+    }
+
+    return $self;
+}
+
+sub determine_processing_for_instrument_data {
+    my ($self, $instrument_data) = @_;
+
+    my $processing;
+
+    return $processing;
+}
+
+1;
+
---
+++ lib/perl/Genome/Model/DeNovoAssembly/SxReadProcessor.t
@@ -0,0 +1,84 @@
+#! /gsc/bin/perl
+
+use strict;
+use warnings;
+
+use above 'Genome';
+
+use Test::More;
+
+use_ok('Genome::Model::DeNovoAssembly::SxReadProcessor') or die;
+
+my $sample = Genome::Sample->create(name => '__TEST_SAMPLE__');
+ok($sample, 'create test sample');
+my $library = Genome::Library->create(name => '__TEST_LIBRARY__', sample => $sample);
+ok($library, 'create library');
+my @instrument_data;
+my @inst_data_attrs = (
+    {
+        original_est_fragment_size => 250,
+        read_count => 10000,
+        read_length => 100,
+    },
+    {
+        original_est_fragment_size => 3000,
+        read_count => 10000,
+        read_length => 100,
+    },
+    {
+        original_est_fragment_size => 8000,
+        read_count => 10000,
+        read_length => 100,
+    },
+);
+for my $inst_data_attr ( @inst_data_attrs ) {
+    push @instrument_data, Genome::InstrumentData::Imported->create(
+        library => $library,
+        %$inst_data_attr,
+    );
+}
+is(@instrument_data, 3, 'create inst data');
+
+diag('SUCCESS (NEW WAY DEFAULT ONLY)');
+my $processor = Genome::Model::DeNovoAssembly::SxReadProcessor->create(
+    instrument_data => \@instrument_data,
+    read_processor => 'DEFAULT (trim default --param 1, coverage 10X)',
+);
+ok($processor, 'failed to create sx read processor');
+is_deeply($processor->default_read_processor, { condition => 'DEFAULT', processor => 'trim default --param 1', coverage => 10, }, 'got default processing');
+
+diag('SUCESS (NEW WAY, FULL TEST)');
+$processor = Genome::Model::DeNovoAssembly::SxReadProcessor->create(
+    instrument_data => \@instrument_data,
+    read_processor => 'DEFAULT (trim default --param 1) original_est_fragment_size > 0 and original_est_fragment_size <= 1000 (DEFAULT, coverage 10X) original_est_fragment_size > 1000 and original_est_fragment_size <= 6000 (trim insert-size --min 1001 --max 6000 then filter by-length --length 50, coverage 20X) original_est_fragment_size > 6000 and original_est_fragment_size <= 10000 (trim insert-size --min 6001 --max 10000) original_est_fragment_size <= 2.5 * read_length (DEFAULT, coverage 30X) read_length == 100 (filter --param 1 --qual 30)',
+);
+ok($processor, 'create sx read processor');
+ok($processor->parser, 'got parser');
+
+diag('SUCCESS (OLD WAY)');
+$processor = Genome::Model::DeNovoAssembly::SxReadProcessor->create(
+    instrument_data => \@instrument_data,
+    read_processor => 'trim default --param 1',
+);
+ok($processor, 'failed to create sx read processor');
+is_deeply($processor->default_read_processor, { condition => 'DEFAULT', processor => 'trim default --param 1', }, 'got default processing');
+
+# FAILS
+# no default
+my $failed_processor = Genome::Model::DeNovoAssembly::SxReadProcessor->create(
+    instrument_data => \@instrument_data,
+    read_processor => 'DEFULT trim default --param 1',
+);
+ok(!$failed_processor, 'failed to create sx read processor');
+
+# more than one default
+$failed_processor = Genome::Model::DeNovoAssembly::SxReadProcessor->create(
+    instrument_data => \@instrument_data,
+    read_processor => 'DEFAULT (trim default --param 1) DEFAULT (trim default2 --param 1)',
+);
+ok(!$failed_processor, 'failed to create sx read processor');
+
+
+
+done_testing();
+
