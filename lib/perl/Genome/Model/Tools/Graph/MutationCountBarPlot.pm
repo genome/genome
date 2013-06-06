f153c0ce895b9304d8e5cfb6c5ee0ff6d53b6f2e
Yang Li <yli@genome.wustl.edu>
2013-06-06 13:11:13 -0500

    Make the tool to grab directories from Model Group IDs

--- lib/perl/Genome/Model/Tools/Graph/MutationCountBarPlot.pm
+++ lib/perl/Genome/Model/Tools/Graph/MutationCountBarPlot.pm
@@ -8,9 +8,9 @@ use Genome;
 class Genome::Model::Tools::Graph::MutationCountBarPlot {
     is => 'Command',
     has => [
-        input_file_path => {
-            type => 'String',
-            doc => "input file path",
+        model_group_id => {
+            type => 'Int',
+            doc => "model group id",
             is_optional => 0,
         },
         output_file_path => {
@@ -37,6 +37,12 @@ class Genome::Model::Tools::Graph::MutationCountBarPlot {
             default_value => 768,
             is_optional => 1,
         },
+        screen_split =>{
+            type => 'Int',
+            doc => 'screen split',
+            default_value => 5,
+            is_optional => 1,
+        },
     ],
 };
 
@@ -52,7 +58,7 @@ HELP
 
 sub help_detail {# this is what the user will see with the longer version of help.
 <<'HELP';
-    The tool takes a user supplied list of input directories to search for model data builds. (Each line contains and only contains a build directory.) It goes through the builds and save the mutation counts into a data table, which is later processed by a R script to plot the bar chart.
+    The tool takes a user supplied model group id to search for model data builds (last successful builds). It goes through the builds and save the mutation counts into a data table, which is later processed by a R script to plot the bar chart.
 HELP
 }
 
@@ -65,18 +71,31 @@ HELP
 ################################################################################################################
 sub execute {
     my $self = shift;
-    my $input_file_path = $self->input_file_path;
+    my $model_group_id = $self->model_group_id;
     my $output_file_path = $self->output_file_path;
     my $plot_title = $self->plot_title;
     my $res_X = $self->res_X;
     my $res_Y = $self->res_Y;
+    my $screen_split = $self->screen_split;
+	my @directories;
+	my @names;
+
+	my @model_ids =  map {$_-> id} Genome::Model->get("model_groups.id" => $model_group_id);
+	for (my $i=0; $i < @model_ids; $i++){
+		my $model_id = $model_ids[$i];
+		my $model = Genome::Model->get($model_id);
+		my $name = $model -> subject -> name;
+		my $build = $model -> last_succeeded_build;
+		my $build_dir = $build->data_directory;
+		
+		
+		push (@directories, $build_dir);
+		push (@names, $name);
+	}
 	
 	# Create temp files
 	my $temp_data_path = Genome::Sys->create_temp_file_path;
 	my $temp_R_path = Genome::Sys->create_temp_file_path;
-	
-	# Read the input file
-	open (my $input_file, "<$input_file_path") or die "Read input directory failed. $!.\n";
 	
 	# Create temporary file to store the data
 	open (my $temp_data, ">$temp_data_path") or die "Create temporary data file failed. $!.\n";
@@ -89,30 +108,42 @@ sub execute {
 	print $temp_data $header;
 	
 	# Loop through the input file and get the sample directories stored in a list
-	while (my $line = <$input_file>){
-		chomp $line;
-		my $position = rindex($line, "/") + 1;
-		my $ending = substr($line, $position);
-		my $sb .= $ending; # Construct one line of output, starting with the sample name
+	for (my $i=0; $i<@model_ids; $i++){
+		my $directory = $directories[$i];
+		my $sb .= $names[$i]; # Construct one line of output, starting with the sample name
 		
 		for (my $i=1;$i<5;$i++){ # Iterate through all four tiers
-			my $tierLine = $line."/effects/snvs.hq.tier$i.v1.annotated.top";
-			if (!(-e $tierLine)) { # Examine if file exists
-				$tierLine = 0; # If file doesn't exist, use a 0 flag
+			my $mutationFile = $directory."/effects/snvs.hq.tier$i.v1.annotated.top";
+			if (!(-e $mutationFile)) { # Examine if file exists
+				$mutationFile = 0; # If file doesn't exist, use a 0 flag
+				$sb .= "\t0"; # Append 0 to the string
+			} else {
+				my $wc = `wc -l $mutationFile`;
+				my @parts = split(' ', $wc);
+				my $count = $parts[0];
+				$sb .= "\t$count"; # Append a tier count to the string 
 			}
-			my $wc = `wc -l $tierLine`;
-			my @parts = split(' ', $wc);
-			my $count = $parts[0];
-			$sb .= "\t$count"; # Append a tier count to the string 
 		}
 		$sb .= "\n";
 		print $temp_data $sb; # Write to $temp_data
 	}
 	
+	
+	# Split the window into rows 
+	my $splits = @model_ids / $screen_split;
+	
+	# Multiply the vertical resolution for multiple rows
+	$res_Y *= $splits;
+
 	# Prepare R script
 	print $temp_R "mutations <- read.table('$temp_data_path');\n";
 	print $temp_R "png(filename = '$output_file_path', width = $res_X, height = $res_Y, bg = 'white');\n";
-	print $temp_R "barplot(t(as.matrix(mutations)), col=rainbow(4), beside=T, main='$plot_title', ylab='Mutation Counts');\n";
+	print $temp_R "par(mfrow=c($splits,1));\n";
+	for (my $i = 1; $i<=@model_ids; $i=$i+$screen_split) {
+		my $upperBound = $i+$screen_split-1;
+		print $temp_R "mutations_matrix = t(as.matrix(mutations));\n";
+		print $temp_R "barplot(mutations_matrix[,$i:$upperBound], col=rainbow(4), beside=T, main='$plot_title', ylab='Mutation Counts');\n";
+	}
 	print $temp_R "legend(140, 18000, c('Tier1', 'Tier2', 'Tier3', 'Tier4'), fill=rainbow(4));\n";
 	print $temp_R "dev.off();\n";
 	
