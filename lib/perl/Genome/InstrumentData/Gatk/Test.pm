737b3085ad0acaaa2475cff4d6295b2580173070
Eddie Belter <ebelter@genome.wustl.edu>
2013-06-11 15:11:46 -0500

    InstData Gatk: add known indels to Test.pm

--- lib/perl/Genome/InstrumentData/Gatk/Test.pm
+++ lib/perl/Genome/InstrumentData/Gatk/Test.pm
@@ -15,12 +15,40 @@ class Genome::InstrumentData::Gatk::Test {
         },
         reference_build => {
             calculate_from => [qw/ test_data_dir /],
-            calculate => q| return Genome::Model::Build::ImportedReferenceSequence->__define__(name => 'Test Ref Build v1', data_directory => $test_data_dir.'/reference'); |,
+            calculate => q| 
+                Genome::Model::Build::ImportedReferenceSequence->__define__(
+                    name => 'Test Ref Build v1',
+                    data_directory => $test_data_dir.'/reference',
+                ); 
+            |,
         },
         bam_source => {
             calculate_from => [qw/ test_data_dir /],
-            calculate => q| Genome::InstrumentData::AlignmentResult::Merged->__define__(id => 9999, output_dir => $test_data_dir.'/inputs'); |,
-        }
+            calculate => q| 
+                Genome::InstrumentData::AlignmentResult::Merged->__define__(
+                    id => 9999,
+                    output_dir => $test_data_dir.'/inputs',
+                ); 
+            |,
+        },
+        known_indel => {
+            calculate_from => [qw/ indel_result /],
+            calculate => q|
+                Genome::Model::Build::ImportedVariationList->__define__(
+                    id => 9998,
+                    indel_result => $indel_result,
+                );
+            |,
+        },
+        indel_result => {
+            calculate_from => [qw/ test_data_dir /],
+            calculate => q|
+                Genome::Model::Tools::DetectVariants2::Result::Manual->__define__(
+                    id => 9997,
+                    output_dir => $test_data_dir.'/Mills_and_1000G_gold_standard/',
+                );
+            |,
+        },
     ],
 };
 
