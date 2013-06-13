6328f556458b80f144639fab38ace2498362d120
Thomas Mooney <tmooney@genome.wustl.edu>
2013-06-13 15:53:50 -0500

    Don't do logging in the debugger.
    Logging has exits. Makes debugging pointless.

--- lib/perl/Genome/Site/TGI/Security.pm
+++ lib/perl/Genome/Site/TGI/Security.pm
@@ -43,15 +43,17 @@ sub log_command {
     my @argv = @ARGV;
 
     # Fork twice... grandchild process will be an orphan, so parent won't wait for it to complete
-    unless(defined &DB::DB) { #don't fork in the debugger
-        my $pid = UR::Context::Process->fork();
-        if ($pid) {
-            return 1;
-        } else {
-            my $pid2 = UR::Context::Process->fork();
-            if($pid2) {
-                exit 0; #let the grandchild do the work
-            }
+    if(defined &DB::DB) { #don't do logging in the debugger
+        return 1;
+    }
+
+    my $pid = UR::Context::Process->fork();
+    if ($pid) {
+        return 1;
+    } else {
+        my $pid2 = UR::Context::Process->fork();
+        if($pid2) {
+            exit 0; #let the grandchild do the work
         }
     }
 
