a4cd5b1012c8703679e15e4af88821c0d023553d
Feiyu Du <fdu@genome.wustl.edu>
2013-06-26 11:06:47 -0500

    fix the broken unit test

--- lib/perl/Genome/Model/Tools/Vcf.pm
+++ lib/perl/Genome/Model/Tools/Vcf.pm
@@ -21,7 +21,7 @@ use Genome;
 #17: Indel Normalization
 #18: Indel Normalization using joinx 1.7 (since 1.6 is bad)
 #19: Sort normalized indels (numeric)
-#20: Switch to natural sorting of normalized indels
+#20: Switch to natural sorting of normalized indels and change null GT from '.' to './.' 
 
 my $VCF_VERSION = "20";
 
