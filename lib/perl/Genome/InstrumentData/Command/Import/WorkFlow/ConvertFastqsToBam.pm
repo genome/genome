e606bf97409b74a0ecccfa12a1653f87df579dce
Eddie Belter <ebelter@genome.wustl.edu>
2013-07-02 14:46:32 -0500

    InstData Import: convert fastqs to bam does not need inst data

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/ConvertFastqsToBam.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/ConvertFastqsToBam.pm
@@ -13,16 +13,24 @@ require List::MoreUtils;
 class Genome::InstrumentData::Command::Import::WorkFlow::ConvertFastqsToBam { 
     is => 'Command::V2',
     has_input => [
-        instrument_data => { 
-            is => 'Genome::InstrumentData',
-            is_output => 1,
-            doc => 'Instrument data.',
+        working_directory => {
+            is => 'Text',
+            doc => 'Detination directory for fastqs.',
+        },
+        fastq_paths => { 
+            is => 'Text',
+            is_many => 1,
+            doc => 'Paths of the fastq[s] to convert to a bam.',
+        },
+        sample_name => {
+            is => 'Text',
+            doc => 'The sample name to use and then derive the read group name.',
         },
     ],
     has_output => [ 
         bam_path => {
-            calculate_from => [qw/ instrument_data /],
-            calculate => q( return $instrument_data->data_directory.'/tmp/unsorted.bam'; ),
+            calculate_from => [qw/ working_directory sample_name /],
+            calculate => q( return $working_directory.'/'.$sample_name.'.bam'; ),
             doc => 'The path of the bam.',
         },
     ],
@@ -45,16 +53,12 @@ sub _convert_fastqs_to_bam {
     my $self = shift;
     $self->status_message('Run picard fastq to sam...');
 
-    my $instrument_data = $self->instrument_data;
-    my @fastqs = Genome::InstrumentData::Command::Import::WorkFlow::Helpers->local_source_files_for_instrument_data($instrument_data);
-    if ( not @fastqs ) {
-        $self->error_message('No local source files for instrument data! '.$instrument_data->id);
-        return;
-    }
-
+    my $sample_name = $self->sample_name;
+    my $read_group_name = $sample_name.'-extlibs';
+    my @fastqs = $self->fastq_paths;
     $self->status_message("Fastq 1: $fastqs[0]");
     my $bam_path = $self->bam_path;
-    my $cmd = "gmt picard fastq-to-sam --fastq $fastqs[0] --output $bam_path --quality-format Standard --sample-name ".$instrument_data->sample->name.' --read-group-name '.$instrument_data->library->name;
+    my $cmd = "gmt picard fastq-to-sam --fastq $fastqs[0] --output $bam_path --quality-format Standard --sample-name $sample_name --read-group-name $read_group_name";
     if ( $fastqs[1] ) {
         $self->status_message("Fastq 2: $fastqs[1]") if $fastqs[1];
         $cmd .= ' --fastq2 '.$fastqs[1]
