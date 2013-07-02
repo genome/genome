f5bcd57fb8375675a9c0a8284a036e0f0d9159ab
Eddie Belter <ebelter@genome.wustl.edu>
2013-07-02 14:50:19 -0500

    InstData Import: merge create inst data & verify and move bam to create inst data and copy bam

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/CreateInstrumentDataAndCopyBam.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/CreateInstrumentDataAndCopyBam.pm
@@ -1,4 +1,4 @@
-package Genome::InstrumentData::Command::Import::WorkFlow::CreateInstrumentData;
+package Genome::InstrumentData::Command::Import::WorkFlow::CreateInstrumentDataAndCopyBam;
 
 use strict;
 use warnings;
@@ -8,13 +8,12 @@ use Genome;
 require Cwd;
 require File::Basename;
 
-class Genome::InstrumentData::Command::Import::WorkFlow::CreateInstrumentData { 
+class Genome::InstrumentData::Command::Import::WorkFlow::CreateInstrumentDataAndCopyBam { 
     is => 'Command::V2',
     has_input => [
-        source_files => {
+        bam_path => {
             is => 'Text',
-            is_many => 1,
-            doc => 'Source files to import. If importing multiple files, put the file containing the forward reads first.',
+            doc => 'The path of the bam to verify and move.',
         },
         sample => {
             is => 'Genome::Sample',
@@ -36,22 +35,48 @@ class Genome::InstrumentData::Command::Import::WorkFlow::CreateInstrumentData {
         library => { is => 'Genome::Library', },
         kilobytes_requested => { is => 'Number', },
     ],
+    has_calculated => [
+        final_bam_path => {
+            calculate_from => [qw/ instrument_data /],
+            calculate => q( return $instrument_data->data_directory.'/all_sequences.bam'; ),
+            doc => 'The path of the final bam.',
+        },
+        flagstat_path => {
+            calculate_from => [qw/ final_bam_path /],
+            calculate => q( return $final_bam_path.'.flagstat'; ),
+        },
+    ],
 };
 
 sub execute {
     my $self = shift;
-    $self->status_message('Create instrument data...');
+    $self->status_message('Create instrument data and copy bam...');
 
     my $library = $self->_resvolve_library;
     return if not $library;
 
-    my $validate_source_files = $self->_validate_source_files;
-    return if not $validate_source_files;
-
     my $instrument_data = $self->_create_instrument_data;
     return if not $instrument_data;
 
-    $self->status_message('Create instrument data...done');
+    my $helpers = Genome::InstrumentData::Command::Import::WorkFlow::Helpers->get;
+
+    my $move_ok = $helpers->move_file($self->bam_path, $self->final_bam_path);
+    return if not $move_ok;
+
+    my $flagstat_path = $self->flagstat_path;
+    $self->status_message("Flagstat path: $flagstat_path");
+    my $flagstat = $helpers->run_flagstat($self->final_bam_path, $self->flagstat_path);
+    return if not $flagstat;
+
+    $instrument_data->add_attribute(attribute_label => 'bam_path', attribute_value => $self->final_bam_path);
+    $instrument_data->add_attribute(attribute_label => 'read_count', attribute_value => $flagstat->{total_reads});
+    $instrument_data->add_attribute(attribute_label => 'is_paired_end', attribute_value => $flagstat->{is_paired_end});
+
+    $self->status_message('Reallocate...');
+    $instrument_data->allocations->reallocate;# with move??
+    $self->status_message('Reallocate...done');
+
+    $self->status_message('Create instrument data and copy bam...done');
     return 1;
 }
 
@@ -84,51 +109,30 @@ sub _resvolve_library {
     return $self->library($library);
 }
 
-#< Validate Source Files >#
-sub _validate_source_files {
-    my $self = shift;
-    $self->status_message('Validate source files...');
-
-    my @source_files = $self->source_files;
-    for my $source_file ( @source_files ) { $self->status_message("Source file(s): $source_file"); }
-    $self->status_message("Source file count: ".@source_files);
-
-    my $helpers = Genome::InstrumentData::Command::Import::WorkFlow::Helpers->get;
-    my $kilobytes_requested = $helpers->kilobytes_needed_for_processing_of_source_files(@source_files);
-    return if not $kilobytes_requested;
-    $self->kilobytes_requested($kilobytes_requested);
-    $self->status_message('Kilobytes requested: '.$self->kilobytes_requested);
-
-    $self->status_message('Validate source files...done');
-    return 1;
-}
-
-sub _resolve_kilobytes_requested {
-    my ($self, @source_files) = @_;
-
-    my $kilobytes_requested;
-    for my $source_file ( @source_files ) {
-        my $size = -s $source_file;
-        if ( not $size ) {
-            $self->error_message("Source file does not exist! $source_file");
-            return;
-        }
-        $size = int( $size / 1024 );
-        $size *= 3 if $source_file =~ /\.gz$/; # assume ~30% compression rate for gzipped fasta/q
-        $kilobytes_requested += $size;
-    }
-
-    my $multiplier = ( $self->original_format =~ /bam/ ? 3 : 2 );
-    return $kilobytes_requested * $multiplier;
-}
-
 sub _create_instrument_data {
     my $self = shift;
 
+    my %additional_properties;
+    for my $name_value ( $self->instrument_data_properties ) {
+        my ($name, $value) = split('=', $name_value);
+        if ( not defined $value or $value eq '' ) {
+            $self->error_message('Failed to parse with instrument data property name/value! '.$name_value);
+            return;
+        }
+        if ( exists $additional_properties{$name} and $value ne $additional_properties{$name} ) {
+            $self->error_message(
+                "Multiple values for instrument data property! $name => ".join(', ', sort $value, $additional_properties{$name})
+            );
+            return;
+        }
+        $additional_properties{$name} = $value;
+    }
+
     $self->status_message('Checking if source files were previously imported...');
-    my %properties = (
+    my $original_data_path = delete $additional_properties{original_data_path};
+    my %properties = ( #FIXME when importing multiples...
         library => $self->library,
-        original_data_path => join(',',  $self->source_files),
+        original_data_path => $original_data_path,
     );
     my $instrument_data = Genome::InstrumentData::Imported->get(%properties);
     if ( $instrument_data ) {
@@ -148,25 +152,17 @@ sub _create_instrument_data {
     }
     $self->status_message('Instrument data id: '.$instrument_data->id);
 
-    for my $name_value ( $self->instrument_data_properties ) {
-        my ($name, $value) = split('=', $name_value);
-        if ( not defined $value or $value eq '' ) {
-            $self->error_message('Failed to parse with instrument data property name/value! '.$name_value);
-            return;
-        }
-        if ( exists $properties{$name} and $value ne $properties{$name} ) {
-            $self->error_message(
-                "Multiple values for instrument data property! $name => ".join(', ', sort $value, $properties{$name})
-            );
-            return;
-        }
-        $instrument_data->add_attribute(attribute_label => $name, attribute_value => $value);
+    for my $name ( keys %additional_properties ) {
+        $self->status_message('Add attribute: '.$name.' => '.$additional_properties{$name});
+        $instrument_data->add_attribute(attribute_label => $name, attribute_value => $additional_properties{$name});
     }
 
+    my $bam_path = $self->bam_path;
+    my $kilobytes_requested = (-s $bam_path) + 1024;
     my $allocation = Genome::Disk::Allocation->create(
         disk_group_name => 'info_alignments',
         allocation_path => 'instrument_data/imported/'.$instrument_data->id,
-        kilobytes_requested => $self->kilobytes_requested,
+        kilobytes_requested => $kilobytes_requested,
         owner_class_name => $instrument_data->class,
         owner_id => $instrument_data->id,
     );
@@ -177,10 +173,6 @@ sub _create_instrument_data {
     $self->status_message('Allocation id: '.$allocation->id);
     $self->status_message('Allocation path: '.$allocation->absolute_path);
     
-    my $tmp_dir = $allocation->absolute_path.'/tmp';
-    Genome::Sys->create_directory($tmp_dir);
-    $self->status_message('Allocation tmp path: '.$tmp_dir);
-
     $self->status_message('Create instrument data...done');
     return $self->instrument_data($instrument_data);
 }
