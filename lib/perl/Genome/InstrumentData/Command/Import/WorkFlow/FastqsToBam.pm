ce6200c8c44c31c3de89e40aeb3182643c2b4bae
Eddie Belter <ebelter@genome.wustl.edu>
2013-07-03 14:59:15 -0500

    InstData Import: verify bam read count after converting from fastqs

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/FastqsToBam.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/FastqsToBam.pm
@@ -8,7 +8,6 @@ use Genome;
 use Data::Dumper 'Dumper';
 require File::Basename;
 require List::Util;
-require List::MoreUtils;
 
 class Genome::InstrumentData::Command::Import::WorkFlow::FastqsToBam { 
     is => 'Command::V2',
@@ -43,9 +42,8 @@ sub execute {
     my $fastq_to_bam_ok = $self->_fastqs_to_bam;
     return if not $fastq_to_bam_ok;
 
-    my $helpers = Genome::InstrumentData::Command::Import::WorkFlow::Helpers->get;
-    my $flagstat = $helpers->run_flagstat($self->bam_path);
-    return if not $flagstat;
+    my $verfiy_read_count_ok = $self->_verify_read_counts;
+    return if not $verfiy_read_count_ok;
 
     $self->status_message('Fastqs to bam...done');
     return 1;
@@ -75,6 +73,29 @@ sub _fastqs_to_bam {
     }
 
     $self->status_message('Run picard fastq to sam...done');
+    return 1;
+}
+
+sub _verify_read_counts {
+    my $self = shift;
+    $self->status_message('Verify read count...');
+
+    my $helpers = Genome::InstrumentData::Command::Import::WorkFlow::Helpers->get;
+    my $flagstat = $helpers->run_flagstat($self->bam_path);
+    return if not $flagstat;
+    $self->status_message('Bam read count: '.$flagstat->{total_reads});
+
+    my $counts = $helpers->load_read_count_for_fastq_paths($self->fastq_paths);
+    return if not $counts;
+    my $fastq_read_count = List::Util::sum(values %$counts);
+    $self->status_message('Fastq read count: '.$fastq_read_count);
+
+    if ( $flagstat->{total_reads} != $fastq_read_count ) {
+        $self->error_message('Fastq and bam rread counts do not match!');
+        return;
+    }
+
+    $self->status_message('Verify read count...done');
     return 1;
 }
 
