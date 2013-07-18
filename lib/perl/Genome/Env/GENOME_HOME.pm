93c81c39b45963f27530e3246d0c033cc1293651
Ben Oberkfell <boberkfe@genome.wustl.edu>
2013-07-18 15:13:06 -0500

    GENOME_HOME was not an available enviroment variable

---
+++ lib/perl/Genome/Env/GENOME_HOME.pm
@@ -0,0 +1,15 @@
+package Genome::Env::GENOME_HOME;
+
+=pod
+
+=head1 NAME
+
+GENOME_HOME
+
+=head1 DESCRIPTION
+
+The GENOME_HOME environment variable is the base directory for the Genome installation
+
+=cut
+
+1;
