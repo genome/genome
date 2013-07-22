9fad62ca9d97c93c35cf119fd7d47af5d710eb2d
Scott Smith <ssmith@genome.wustl.edu>
2013-07-22 10:14:53 -0500

    fix docs on GENOME_HOME environment variable: merge conflict cruft in the docs

--- lib/perl/Genome/Env/GENOME_HOME.pm
+++ lib/perl/Genome/Env/GENOME_HOME.pm
@@ -8,9 +8,6 @@ GENOME_HOME
 
 =head1 DESCRIPTION
 
-<<<<<<< Updated upstream
-The GENOME_HOME environment variable is the base directory for the Genome installation
-=======
 The GENOME_HOME is the directory of the GMS install, typically /opt/gms/$GENOME_SYS_ID.
 
 Other GMS instances may be mounted next-to the home install, in the same directory.
@@ -18,7 +15,6 @@ Other GMS instances may be mounted next-to the home install, in the same directo
 =head1 DEFAULT VALUE
 
 /opt/gms/$GENOME_SYS_ID
->>>>>>> Stashed changes
 
 =cut
 
