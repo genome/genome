97e4f1268643b532f9acc67e38fbe705688379dd
apregier <aregier@genome.wustl.edu>
2013-07-26 14:33:21 -0500

    Revert "Build can return multiple statuses"
    This reverts commit 916410ae1cceae5fceea90bc355f283650449f79.

--- lib/perl/Genome/Model/ImportedAnnotation.pm
+++ lib/perl/Genome/Model/ImportedAnnotation.pm
@@ -59,7 +59,8 @@ class Genome::Model::ImportedAnnotation{
 sub build_by_version {
     my $self = shift;
     my $version = shift;
-    my @builds = grep{ grep {$_ eq 'Succeeded' or $_ eq 'Running'} $_->status } $self->builds;
+
+    my @builds = grep{ $_->status eq 'Succeeded' || $_->status eq 'Running' } $self->builds;
     @builds =  grep { $_->version eq $version } @builds;
     if (@builds > 1) {
         my $versions_string = join("\n", map { "model_id ".$_->model_id." build_id ".$_->build_id." version ".$_->version } @builds);
