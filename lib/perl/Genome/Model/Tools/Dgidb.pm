922e855abe2b37f9e2d2ea4e87a19e5874785a38
Feiyu Du <fdu@genome.wustl.edu>
2013-08-14 17:09:09 -0500

    Add a new tool to query gene from DGIDB database

---
+++ lib/perl/Genome/Model/Tools/Dgidb.pm
@@ -0,0 +1,17 @@
+package Genome::Model::Tools::Dgidb;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::Dgidb {
+    is  => 'Command::Tree',
+    doc => 'Toolkit for DGIDB related process',
+};
+
+sub help_detail {
+    "These commands are used to perform DGIDB related process";
+}
+
+1;
