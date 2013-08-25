5cd7c6bdb280ea039dcd5a8733f8de11a34228ce
Nathan Nutter <nnutter@genome.wustl.edu>
2013-08-25 11:18:17 -0500

    Revert "factor in GENOME_SOFTWARE_RESULT_TEST_NAME into the version"
    This reverts commit b76311302c51a1c82828e8f1f00a5ca498f9566d.
    
    GENOME_SOFTWARE_RESULT_TEST_NAME is not always set so this had no
    effect.  Will look for an alternative solution.

--- lib/perl/Genome/Model/SomaticValidation/Command/ValidateSvs/CreateAssembledContigReference.pm
+++ lib/perl/Genome/Model/SomaticValidation/Command/ValidateSvs/CreateAssembledContigReference.pm
@@ -87,14 +87,6 @@ sub execute {
 
     my $version = "500bp_assembled_contigs_sv";
     #don't overwrite an existing model...
-
-    # Using GENOME_SOFTWARE_RESULT_TEST_NAME here is possibly a mistake but
-    # it's what we set when we run tests to allow them to generate objects
-    # indepdendently, i.e. the subsequent imported reference sequence.
-    if ( $ENV{GENOME_SOFTWARE_RESULT_TEST_NAME} ) {
-        $version = $ENV{GENOME_SOFTWARE_RESULT_TEST_NAME};
-    }
-
     my $prefix = $sample_id . "_SV_Contigs";
     $version = $self->check_ref_build_name($prefix, $version);
 
