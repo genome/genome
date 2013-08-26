c0e80a03efa81e9b9e34d91b4e7097016a726fc5
dmorton <dmorton@genome.wustl.edu>
2013-08-26 18:38:02 -0500

    Added python_script sub to Base

--- lib/perl/Genome/Model/Tools/Dindel/Base.pm
+++ lib/perl/Genome/Model/Tools/Dindel/Base.pm
@@ -21,4 +21,14 @@ sub dindel_executable {
     return File::Spec->join($self->dindel_install_dir, "binaries", "dindel-1.01-linux-64bit");
 }
 
+sub python_script {
+    my ($self, $name) = @_;
+    my $path = File::Spec->join($self->dindel_install_dir, 'dindel-1.01-python', $name . '.py');
+    if (-s $path) {
+        return $path;
+    } else {
+        die "Couldn't find Dindel python script named '$name' at: $path\n";
+    }
+}
+
 1;
