2f4c2c310d4cb561521920ecbe9a88b57bf24b88
Eddie Belter <ebelter@genome.wustl.edu>
2013-08-27 15:20:17 -0500

    Sync UAC: method for creating genome objects

--- lib/perl/Genome/Site/TGI/Synchronize/UpdateApipeClasses.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/UpdateApipeClasses.pm
@@ -39,7 +39,8 @@ class Genome::Site::TGI::Synchronize::UpdateApipeClasses {
 # it can lead to some attributes not being copied over.
 sub objects_to_sync {
     return (
-        'Genome::Site::TGI::Taxon' => 'Genome::Taxon',
+        #'Genome::Site::TGI::Taxon' => 'Genome::Taxon',
+        'Genome::Site::TGI::Synchronize::Classes::OrganismSample' => 'Genome::Sample',
     );
     return (
         'Genome::Site::TGI::Taxon' => 'Genome::Taxon',
@@ -123,15 +124,12 @@ sub execute {
 
     # Load instrument data successful pidfas.
     # We only sync instrument data the have a successful pidfa.
-    my $load_pidfas = 1;
+    my $load_pidfas = 1; #FIXME
     #my $load_pidfas = $self->_load_successful_pidfas;
     if ( not $load_pidfas ) {
         $self->error_message('Failed to load instruemnt data successful pidfas!');
         return;
     }
-
-    # Stores copied and missing IDs for each type
-    my %report;
 
     my @classes_to_sync = $self->objects_to_sync;
     for ( my $i = 0; $i < @classes_to_sync; $i += 2 ) {
@@ -140,11 +138,24 @@ sub execute {
 
         my $genome_class = $classes_to_sync[$i + 1];
         my $genome_ids = $self->_get_ids_for($genome_class);
+
+        $self->status_message('Detemine IDs to create...');
+        my $ids_to_create = $lims_ids->difference($genome_ids);
+        $self->status_message('Detemine IDs to create...done');
+        $self->status_message('Found IDs to create: '.scalar(@{$ids_to_create}));
+
+        if ( not $ids_to_create->is_empty ) {
+            $self->_create_genome_objects_for_lims_objects(
+                ids_to_create => $ids_to_create,
+                lims_class => $lims_class,
+                genome_class => $genome_class,
+            );
+        }
     }
 
     $self->_unlock_me;
 
-    $self->_report(\%report);
+    print Data::Dumper::Dumper($self->_report);
     return 1;
 }
 
@@ -158,6 +169,7 @@ sub _get_ids_for {
         $set->insert($obj->id);
         #$obj->unload;
     };
+    $self->status_message("Found ".scalar(@$set)." $class IDs.");
 
     if ( $set->is_empty ) {
         Carp::confess('Failed to get ids for class! '.$class);
@@ -169,6 +181,52 @@ sub _get_ids_for {
 
     $self->status_message("Getting IDs for $class...done");
     return $set;
+}
+
+sub _resolve_create_method_for {
+    my ($self, $lims_class) = @_;
+
+    Carp::confess('No LIMS class to get create method!') if not $lims_class;
+
+    my $method_base = $lims_class;
+    $method_base =~ s/Genome::Site::TGI::(Synchronize::Classes::)?//i;
+    $method_base =~ s/::/_/g;
+    my $create_method = '_create_' . lc($method_base);
+    if ( not $self->can($create_method)) {
+        Carp::confess "Did not find create method ($create_method) for LIMS class! ".$lims_class;
+    }
+
+    return $create_method;
+}
+
+sub _create_genome_objects_for_lims_objects {
+    my ($self, %params) = @_;
+
+    my $ids_to_create = delete $params{ids_to_create};
+    Carp::confess('No lims ids to create genome objects!') if not $ids_to_create;
+    my $lims_class = delete $params{lims_class};
+    Carp::confess('No lims class to create genome objects!') if not $lims_class;
+    my $genome_class = delete $params{genome_class};
+    Carp::confess('No genome class to create genome objects!') if not $genome_class;
+
+    $self->status_message('Loading LIMS objects to create in Genome...');
+    my $iterator = $lims_class->create_iterator(id => [ @{$ids_to_create} ]);
+    $self->status_message('Loading LIMS objects to create in Genome...done');
+
+    my $create_method = $self->_resolve_create_method_for($lims_class);
+    my $report = $self->_report;
+    $self->status_message('Create objects in Genome...');
+    while ( my $lims_obj = $iterator->next ) {
+        my $genome_obj = $self->$create_method($lims_obj, $genome_class);
+        if ( $genome_obj ) {
+            push @{$report->{$genome_class}->{'copied'}}, $lims_obj->id;
+        }
+    }
+    $self->status_message('Create objects in Genome...done');
+
+    $self->_report($report);
+
+    return 1;
 }
 
 # For each pair of classes above, determine which objects exist in both the old and new schemas and
@@ -206,7 +264,7 @@ sub Xexecute {
 
     # Maps new classes with old classes
     #my %types = $self->objects_to_sync;
-        #for my $old_type ($self->sync_order) {
+    #for my $old_type ($self->sync_order) {
     my @classes_to_sync = $self->objects_to_sync;
     for ( my $i = 0; $i < @classes_to_sync; $i += 2 ) {
         my $old_type = $classes_to_sync[$i];
@@ -305,7 +363,7 @@ sub Xexecute {
             print STDERR "Looked at $seen_old $old_short_name objects. Found $found existing and created $created_objects $new_type objects\r";
         }
         print STDERR "\n";
-        
+
         $self->status_message("Commit $new_type...");
         confess 'Could not commit!' unless UR::Context->commit;
         $self->status_message("Commit...OK");
@@ -353,7 +411,7 @@ sub _get_direct_and_indirect_properties_for_object {
         next if $property->is_many;
         next if $property->id_by;
         next if $property->via and $property->via ne 'attributes';
-    
+
         my $property_name = $property->property_name;
         next unless $original_object->can($property_name);
         next if @ignore and grep { $property_name eq $_ } @ignore;
@@ -425,7 +483,7 @@ sub _create_genotyping {
     # Successful PIDFA required! The value is the genotype file. It must exist, too!
     my $genotype_file = $self->instrument_data_with_successful_pidfas->{$original_object->id};
     return 0 unless $genotype_file and -s $genotype_file;
-    
+
     my $library_name = $original_object->sample_name.'-microarraylib';
     my ($library) = Genome::Library->get(name => $library_name, sample_id => $original_object->sample_id);
     if ( not $library ) {
@@ -448,7 +506,7 @@ sub _create_genotyping {
         );
     };
     confess "Could not create new object of type $new_object_class based on object of type " .
-        $original_object->class . " with id " . $original_object->id . ":\n$@" if not $object;
+    $original_object->class . " with id " . $original_object->id . ":\n$@" if not $object;
 
     my $new_genotype_file = eval{ Genome::InstrumentData::Microarray->update_genotype_file($object, $genotype_file); };
     confess "$@\nFailed to update genotype_file: $genotype_file on instrument data: ".$object->id if not $new_genotype_file;
@@ -463,13 +521,13 @@ sub _create_indexillumina {
     return 0 unless exists $self->instrument_data_with_successful_pidfas->{$original_object->id};
     # Bam path required!
     return 0 unless $original_object->bam_path;
-    
+
     my ($direct_properties, $indirect_properties) = $self->_get_direct_and_indirect_properties_for_object(
         $original_object,
         $new_object_class, 
         qw/ sample_name sample_id /
     );
-    
+
     my $object = eval {
         $new_object_class->create(
             id => $original_object->id,
@@ -480,7 +538,7 @@ sub _create_indexillumina {
         );
     };
     confess "Could not create new object of type $new_object_class based on object of type " .
-        $original_object->class . " with id " . $original_object->id . ":\n$@" unless $object;
+    $original_object->class . " with id " . $original_object->id . ":\n$@" unless $object;
 
     return 1;
 }
@@ -510,7 +568,7 @@ sub _create_regionindex454 {
         )
     };
     confess "Could not create new object of type $new_object_class based on object of type " .
-        $original_object->class . " with id " . $original_object->id . ":\n$!" unless $object;
+    $original_object->class . " with id " . $original_object->id . ":\n$!" unless $object;
 
     my $add_attrs = $self->_add_attributes_to_instrument_data($object, $indirect_properties);
     Carp::confess('Failed to add attributes to instrument data: '.$object->__display_name__) if not $add_attrs;
@@ -553,10 +611,10 @@ sub _create_object {
         next if not defined $value;
         $params{$name} = $value;
     }
-    
+
     my $object = eval { $new_object_class->create(%params); };
     confess "Could not create new object of type $new_object_class based on object of type " .
-        $original_object->class . " with id " . $original_object->id . ":\n$@" unless $object;
+    $original_object->class . " with id " . $original_object->id . ":\n$@" unless $object;
 
     return 1;
 }
@@ -570,7 +628,7 @@ sub _create_populationgroup {
         my $property_name = $property->property_name;
         $params{$property_name} = $original_object->{$property_name} if defined $original_object->{$property_name};
     }
-    
+
     # Grab members from old object and pass to create parameters
     my @member_ids = map { $_->id } $original_object->members;
     $params{member_ids} = \@member_ids;
@@ -583,7 +641,7 @@ sub _create_populationgroup {
         ) 
     };
     confess "Could not create new object of type $new_object_class based on object of type " .
-        $original_object->class . " with id " . $original_object->id . ":\n$@" unless $object;
+    $original_object->class . " with id " . $original_object->id . ":\n$@" unless $object;
 
     return 1;
 }
