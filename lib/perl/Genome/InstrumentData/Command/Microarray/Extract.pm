9cc0d970fae164b0b424c9bf760f08b7a44194e2
Anthony Brummett <abrummet@genome.wustl.edu>
2013-08-27 15:34:16 -0500

    Instead of picking the "latest" library, exit when there are more than one with the given name
    As part of the postgres-id-sorting change, we can't rely on the "highest" ID
    being the newest thing anymore.  There's no obvious time associated with a
    library so no good way to keep the old behavior, which seems bad anyway

--- lib/perl/Genome/InstrumentData/Command/Microarray/Extract.pm
+++ lib/perl/Genome/InstrumentData/Command/Microarray/Extract.pm
@@ -165,12 +165,17 @@ sub _resolve_instrument_data_from_sample_default_genotype_id {
 sub _resolve_instrument_data_from_library {
     my ($self, $sample) = @_;
 
-    my ($library) = sort {$b->id <=> $a->id} Genome::Library->get(name => $sample->name.'-microarraylib');
-    if ( not $library ) {
+    my $lib_name = $sample->name.'-microarraylib';
+    my @libraries = Genome::Library->get(name => $lib_name);
+    if (@library > 1) {
+        $self->error_message("Mulitple libraries found with name $lib_name for sample ".$sample->__display_name__);
+        return;
+    } elsif ( ! @libraries ) {
         $self->error_message('Failed to get microarry library for sample: '.$sample->__display_name__);
         return;
     }
 
+    my $library = $libraries[0];
     my %params = (
         library => $library,
         'import_source_name in' => ( $self->use_external )
