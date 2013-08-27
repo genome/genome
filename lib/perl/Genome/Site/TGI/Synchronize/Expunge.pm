712226db189b408a657bb8a72dbea8fe4fbac188
Eddie Belter <ebelter@genome.wustl.edu>
2013-08-27 16:12:21 -0500

    Sync Expunge: do not delete [temporary]

--- lib/perl/Genome/Site/TGI/Synchronize/Expunge.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Expunge.pm
@@ -26,6 +26,9 @@ sub execute {
     for my $class (keys %report){
         next unless $class =~ m/Genome::InstrumentData/; #only remove instrument data for now
         next if $class eq 'Genome::InstrumentData::Imported'; #imported instrument data doesn't come from LIMS, so skip it
+        print "WOULD DELETE $class FOR IDS:\n";
+        print Data::Dumper::Dumper($report{$class}->{missing});
+        next;
         my @ids = @{$report{$class}->{missing}} if $report{$class}->{missing};
         my @deleted;
         for my $id (@ids){
