06d88201f642f81b2f8a9ca52536406e055d3278
Scott Smith <ssmith@genome.wustl.edu>
2013-08-30 15:57:14 -0500

    extended MakeCircosPlot module with stub code, added a test case

--- lib/perl/Genome/Model/ClinSeq/Command/MakeCircosPlot.pm
+++ lib/perl/Genome/Model/ClinSeq/Command/MakeCircosPlot.pm
@@ -1,13 +1,13 @@
 package Genome::Model::ClinSeq::Command::MakeCircosPlot;
-
-#Written by Ben Ainscough and Scott Smith
-
 use strict;
 use warnings;
 use Genome; 
 use Data::Dumper;
 
-class Genome::Model::ClinSeq::Command::MakeCircosPlot{
+# Written by Ben Ainscough and Scott Smith, based on prototype from Obi Griffith
+# See JIRA issue https://jira.gsc.wustl.edu/browse/TD-691
+
+class Genome::Model::ClinSeq::Command::MakeCircosPlot {
     is => 'Command::V2',
     has => [
         build                   => { is => 'Genome::Model::Build::ClinSeq',
@@ -20,28 +20,63 @@ class Genome::Model::ClinSeq::Command::MakeCircosPlot{
     doc => 'This script attempts to get read counts, frequencies and gene expression values for a series of genome positions',
 };
 
-
 sub sub_command_category { 'pipeline' }
 
-#sub help_detail {
-#  return <<EOS
-#EOS
-#}
+sub help_detail {
+  return <<EOS
+Generate a Circos plot for a clinseq build.
+EOS
+}
 
-#sub help_synopsis {
-#  return <<EOS
-#EOS
-#}
+sub help_synopsis {
+  return <<EOS
+    genome model clin-seq make-circos-plot --build model.name~bainsc%clinseq1,is_last_complete=1 --output-directory /tmp/outdir
+EOS
+}
 
-#sub help_usage {
-#  return <<EOS
-#EOS
-#}
 
 sub execute {
     my $self = shift;
-    $self->status_message("Hello world.  My build is " . $self->build . " and my output dir is " . $self->output_directory);
-    die "Under development";
+    $self->warning_message("!!!!!!!!!!!!!!! This module is under development and does not run yet !!!!!!!!!!!!!!!!");
+   
+    # grap params from $self
+    my $build = $self->build;
+    my $output_directory = $self->output_directory;
+    $self->status_message("Running on build " . $build->__display_name__); 
+    $self->status_message("Output directory is " . $self->output_directory);
+
+    # initialize directories
+    unless (-d $output_directory) {
+        # this module has wrappers which do logging, throw exceptions, around regular tasks
+        Genome::Sys->create_directory($output_directory);
+    }
+
+    # example accessing data:
+    
+    # get an input from the build
+    my $wgs_build = $build->wgs_build;
+    
+    # all inputs are optional, so test for it being there before using it
+    if ($wgs_build) {
+        $self->status_message("This clinseq build has WGS somatic data, build: " . $wgs_build->__display_name__);
+       
+        # when you have a build, you can get the path for a file in the data directory through an API call
+        my $wgs_somatic_snvs_tier1_path = $wgs_build->data_set_path('effects/snvs.hq.novel.tier1','2','bed');
+        $self->status_message("WGS somatic variants are at $wgs_somatic_snvs_tier1_path");
+        
+        # the above is the same as the following, but the following circumvents the API
+        #my $wgs_somatic_snvs_tier1_path = $wgs_build->data_directory . '/effects/snvs.hq.novel.tier1.v2.bed'; 
+    }
+    else {
+        $self->status_message("No WGS data on this clinseq model.");
+    }
+
+    # get an input from an input
+    my $wgs_tumor_refalign = $wgs_build->tumor_build;
+
+    # eventually you do this 
+    # Genome::Sys->shellcmd(cmd => "cd $output_direcotry; circos");
+
     return 1;
 }
 
---
+++ lib/perl/Genome/Model/ClinSeq/Command/MakeCircosPlot.t
@@ -0,0 +1,47 @@
+#!/usr/bin/env genome-perl
+use strict;
+use warnings;
+use above "Genome"; # only use "above" in test cases, never in modules
+use Genome::Model::ClinSeq::TestData;
+use Test::More tests => 5;
+
+# this gets a canonical test build, and is set up to not really hit the db
+# we temporarily have it overridden to test against Obi's example
+#my $test_ids = Genome::Model::ClinSeq::TestData::load();
+#my $test_build_id = $test_ids->{CLINSEQ_BUILD};
+my $test_build_id = 142467783; ## temp override
+my $test_build = Genome::Model::Build->get($test_build_id);
+ok($test_build, "got test build $test_build_id");
+
+# this directory lives under /gsc/var/cache/testsuite/data at TGI
+# it is also temporarily overridden for Obi's example
+my $expected_output_dir = $ENV{"GENOME_TEST_INPUTS"} . "Genome-Model-ClinSeq-Command-MakeCircosPolot/2013-08-30/";
+$expected_output_dir = '/gscuser/ogriffit/Projects/ALL1/circos/'; ## temp override
+ok(-e $expected_output_dir, "expected output dir exists");
+
+# make a temp directory for output
+my $actual_output_dir = Genome::Sys->create_temp_directory();
+ok(-e $actual_output_dir, "test output dir exists");
+
+# run the command
+my $result = Genome::Model::ClinSeq::Command::MakeCircosPlot->execute(
+    build => $test_build,
+    output_directory => $actual_output_dir,
+);
+ok($result, "execution succeeded");
+
+# verify results
+# any tests in the TODO block will still run, but will not count as "failure" in the harness
+TODO: {
+    local $TODO = "under development";
+    
+    my @differences = `diff $expected_output_dir $actual_output_dir`;
+    is(scalar(@differences), 0, "no differences found: diff $expected_output_dir $actual_output_dir")
+        or do {
+            print "DIFF:\n", @differences;
+            # un-comment these to keep the output for debugging 
+            #my $debug_location = "/tmp/last-output-make-circos-plot-$$";
+            #system "mv $actual_output_dir $debug_location";
+        };
+}
+
