53c321b1e4f11cb6498a2c14fadbb3d8f90a1cdb
dmorton <dmorton@genome.wustl.edu>
2013-08-30 14:13:45 -0500

    Added output_directory as an input in dindel base

--- lib/perl/Genome/Model/Tools/Dindel/Base.pm
+++ lib/perl/Genome/Model/Tools/Dindel/Base.pm
@@ -9,7 +9,34 @@ use File::Spec;
 class Genome::Model::Tools::Dindel::Base {
     is => 'Command',
     is_abstract => 1,
+    has_input => [
+        output_directory => {
+            is => 'Path',
+        }
+    ],
 };
+
+sub help_synopsis {
+    return <<EOS
+EOS
+}
+
+sub help_detail {
+    return <<EOS
+EOS
+}
+
+
+
+sub create_output_directory {
+    my $self = shift;
+    if (! -d $self->output_directory) {
+        Genome::Sys->create_directory($self->output_directory);
+    }
+    if (! -d $self->output_directory) {
+        die "Couldn't create output_directory " . $self->output_directory;
+    }
+}
 
 sub dindel_install_dir {
     my $self = shift;
