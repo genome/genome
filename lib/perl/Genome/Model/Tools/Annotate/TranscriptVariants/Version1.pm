31e8ae80aa7369f7b4403b42333a564154ccdb1e
Mark Burnett <mburnett@genome.wustl.edu>
2013-09-03 10:12:50 -0500

    move more prioritization methods into base
    code-deduplication: mburnett, nnutter

--- lib/perl/Genome/Model/Tools/Annotate/TranscriptVariants/Version1.pm
+++ lib/perl/Genome/Model/Tools/Annotate/TranscriptVariants/Version1.pm
@@ -35,13 +35,6 @@ sub transcript_status_priorities {
     );
 }
 
-sub _resolve_intersector_sub_name {
-    my ($self) = @_;
-
-    my $structure_class = $self->transcript_structure_class_name;
-    return $structure_class->__meta__->data_source_id . '::intersector_sub';
-}
-
 # Given a nucleotide sequence, translate to amino acid sequence and return
 # The translator->translate method can take 1-3 bp of sequence. If given
 # 1 bp, an empty string is returned. If given 2 bp, it attempts to translate
@@ -72,52 +65,6 @@ sub translate {
         $translation.=$aa;
     }
     return $translation;
-}
-
-# Reverse complement the given sequence
-sub reverse_complement {
-    my ($self, $seq) = @_;
-    return unless defined $seq;
-
-    $seq =~ tr/acgtrymkswhbvdnxACGTRYMKSWHBVDNX/tgcayrkmswdvbhnxTGCAYRKMSWDVBHNX/;
-    my $rev_com = CORE::reverse $seq;
-    return $rev_com;
-}
-
-# Prioritizes a list of annotations and returns the highest priority annotation per gene
-# I.E. If 6 annotations go in, from 3 different genes, it will select the "best" annotation
-# that each gene has, and return 3 total annotations, one per gene
-# See the _prioritize_annotations method for details of sorting
-# Corresponds to gene filter in Genome::Model::Tools::Annotate::TranscriptVariants
-sub prioritized_transcripts {
-    my ($self, %variant) = @_;
-    my @annotations = $self->transcripts(%variant) or return;
-
-    my @prioritized_annotations = $self->_prioritize_annotations(@annotations);
-    my %gene_annotations;
-    for my $annotation (@prioritized_annotations) {
-        unless (exists $gene_annotations{$annotation->{gene_name}}) {
-            $gene_annotations{$annotation->{gene_name}} = $annotation;
-        }
-    }
-    return values %gene_annotations;
-}
-
-# Returns the annotation with highest priority
-# Does not sort the full list of annotations, just does a single pass to find the highest priority annotation,
-# which should have run time O(n) instead of O(nlogn)
-# See the _prioritize_annotations method for details for sorting
-# Corresponds to top filter in Genome::Model::Tools::Annotate::TranscriptVariants
-sub prioritized_transcript{
-    my ($self, %variant) = @_;
-    my @annotations = $self->transcripts(%variant) or return;
-
-    my $highest = shift @annotations;
-    for my $anno (@annotations) {
-        my @sorted_list = $self->_prioritize_annotations($highest, $anno);
-        $highest = shift @sorted_list;
-    }
-    return $highest;
 }
 
 # Find which substructures intersect the given variant as quickly as possible.
