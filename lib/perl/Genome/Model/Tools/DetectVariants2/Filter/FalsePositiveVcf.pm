5daef3ed89ae0fa920c7be8de841d30bb2378da8
Mark Burnett <mburnett@genome.wustl.edu>
2013-09-04 17:23:27 -0500

    extract should_skip filter from FPVcf
    code-deduplication: gsanders, mburnett

--- lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalsePositiveVcf.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Filter/FalsePositiveVcf.pm
@@ -37,6 +37,15 @@ sub input_file_path {
     }
 }
 
+sub region_path {
+    my $self = shift;
+    return $self->output_directory."/regions";
+}
+
+sub should_skip_filter {
+    return;
+}
+
 ##########################################################################################
 # Capture filter for high-depth, lower-breadth datasets
 # Contact: Dan Koboldt (dkoboldt@genome.wustl.edu)
@@ -60,14 +69,15 @@ sub _filter_variants {
     # FIXME this will work after the polymuttdenovo filter, but not directly after polymutt due to the separate denovo and standard filenames
     my $input_file = $self->input_file_path;
 
-    ## Build temp file for positions where readcounts are needed ##
-    #my $region_path = $self->_temp_scratch_directory."/regions";
-    my $region_path = $self->output_directory."/regions";
+    $self->print_region_list($input_file, $self->region_path);
 
-    $self->print_region_list($input_file, $region_path);
-
+    if ($self->should_skip_filter) {
+        $self->status_message("No variants found, copying file over and reporting success (this filter was a no-op");
+        Genome::Sys->copy_file($input_file, $self->output_file_path);
+        return 1;
+    }
     ## Run BAM readcounts in batch mode to get read counts for all positions in file ##
-    my $readcount_searcher_by_sample = $self->generate_and_run_readcounts_in_parallel($region_path);
+    my $readcount_searcher_by_sample = $self->generate_and_run_readcounts_in_parallel($self->region_path);
 
     # Advance to the first variant line and print out the header
     my ($input_fh, $header) = $self->parse_vcf_header($input_file);
