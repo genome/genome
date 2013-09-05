e82224de3f2c123e5b4f2f8cf99036bf6799eb1d
Scott Smith <ssmith@genome.wustl.edu>
2013-09-05 16:06:31 -0500

    updated "genome sys node {attach,detach}"

--- lib/perl/Genome/Sys/Node.pm
+++ lib/perl/Genome/Sys/Node.pm
@@ -40,7 +40,7 @@ class Genome::Sys::Node {
 
         attachment_method => {
                               is => 'Text',
-                              valid_values => ['nfs','ssh','ftp','http'],
+                              valid_values => [ __PACKAGE__->_supported_protocols() ],
                               doc => 'the protocol used to attach the system to the current one', 
                             },
     ],
@@ -53,6 +53,10 @@ class Genome::Sys::Node {
     },
 };
 
+sub _supported_protocols {
+    return ('nfs','ssh','ftp','http');
+}
+
 sub attach {
     my $self = shift;
     my $protocol_override = shift;
@@ -62,33 +66,123 @@ sub attach {
         @protocols_to_try = ($protocol_override);
     }
     else {
-        @protocols_to_try = @{ $self->__meta__->property('attachment_method')->valid_values };
+        @protocols_to_try = $self->_supported_protocols;
     }
-    $self->status_message("protocols to test @protocols_to_try");
+    $self->debug_message("protocols to test @protocols_to_try");
 
-    my $hostname = $self->hostname;
+    my $is_already_attached_via = $self->attached_via;
 
     for my $protocol (@protocols_to_try) {
         my $method = "_attach_$protocol";
         unless ($self->can($method)) {
-            warn "no support for $protocol yet...\n";
+            $self->debug_message("no support for $protocol yet...\n");
             next;
         }
+
         $self->$method();
+        
+        my $underlying_mount_point = $self->_underlying_mount_point_for_protocol($protocol);
+        my $mount_point_symlink = $self->mount_point;
+
+        if (-e $mount_point_symlink) {
+            unlink $mount_point_symlink;
+            Genome::Sys->create_symlink($underlying_mount_point, $mount_point_symlink);  
+        }
+
+        if ($self->is_attached) {
+            $self->status_message("attached " . $self->id . " via " . $protocol);
+            last;
+        }
+        else {
+            $self->error_message("error attaching " . $self->id . " via " . $protocol);
+        }
     }
 
     return 1;
 }
 
-sub mount_point_for_protocol {
+sub detach {
+    my $self = shift;
+    my @protocols = @_;
+   
+    unless (@protocols) {
+        @protocols = $self->_supported_protocols();
+    }
+
+    my @errors;
+    for my $protocol (@protocols) {
+        my $underlying_mount_point = $self->_underlying_mount_point_for_protocol($protocol);
+        unless (-e $underlying_mount_point) {
+            next;
+        }
+        my $method = "_detach_$protocol";
+        eval { $self->$method; };
+        if ($@) {
+            push @errors, $@;
+        }
+    }
+    
+    if (@errors) {
+        die join("\n",@errors);
+    }
+    
+    return 1;
+}
+
+sub attached_via {
     my $self = shift;
     my $mount_point_symlink = $self->mount_point;
-
+    if (-l $mount_point_symlink) {
+        # this can be true even if -e is false
+        my $path = readlink($mount_point_symlink);
+        my ($protocol) = ($path =~ /.([^\.]+$)/);
+        return $protocol;
+    }
+    elsif (-e $mount_point_symlink) {
+        die "expected $mount_point_symlink to be a symlink!";
+    }
+    elsif (not -e $mount_point_symlink) {
+        return;
+    }
 }
+
+sub _protocol_for_underlying_mount_point {
+    my $self = shift;
+    my $mount_point = shift;
+    $mount_point ||= readlink($self->mount_point);
+    unless ($mount_point) {
+        die "no mount point given, and no mount point behind default symlink!";
+    }
+    my ($protocol) = ($mount_point =~ /.([^\.]+$)/);
+    return $protocol;
+}
+
+sub _underlying_mount_point_for_protocol {
+    my $self = shift;
+    my $protocol = shift;
+    die "no protocol specified!" unless $protocol;
+    my $mount_point_symlink = $self->mount_point;
+    my $underlying_mount_point = $mount_point_symlink;
+    $underlying_mount_point =~ s|/opt/gms/|/opt/gms/.|;
+    $underlying_mount_point .= '.' . $protocol;
+    return $underlying_mount_point;
+}
+
 sub _attach_ftp {
     my $self = shift;
-    my $cmd = '';
-    die "failed to ftp mount!";
+    my $hostname = $self->hostname;
+    my $ftp_detail = $self->ftp_detail;
+    my $underlying_mount_point = $self->_underlying_mount_point_for_protocol('ftp');
+    my $cmd = "curlftpfs 'ftp://$hostname/$ftp_detail' '$underlying_mount_point' -o tcp_nodelay,kernel_cache,direct_io";
+    Genome::Sys->shellcmd(cmd => $cmd);    
+
+}
+
+sub _detach_ftp {
+    my $self = shift;
+    my $underlying_mount_point = $self->_underlying_mount_point_for_protocol('ftp');
+    my $cmd = "fusermount -u '$underlying_mount_point'";
+    Genome::Sys->shellcmd(cmd => $cmd);
 }
 
 1;
--- lib/perl/Genome/Sys/Node/Command/Detach.pm
+++ lib/perl/Genome/Sys/Node/Command/Detach.pm
@@ -3,24 +3,24 @@ use strict;
 use warnings;
 use Genome;
 
-package Genome::Sys::Node::Command::Attach;
+package Genome::Sys::Node::Command::Detach;
 
-class Genome::Sys::Node::Command::Attach {
+class Genome::Sys::Node::Command::Detach {
     is => 'Command::V2',
     has => [
         systems => { is => 'Genome::Sys::Node',
                 is_many => 1,
                 shell_args_position => 1,
-                doc => 'the system to attach'
+                doc => 'the system to detach'
               },
     ],
-    doc => 'attach the specified GMS gateways to the current GMS'
+    doc => 'detach the specified GMS gateways to the current GMS'
 };
 
 sub execute {
     my $self = shift;
     for my $sys ($self->systems) {
-      $sys->attach();
+      $sys->detach();
     }
     return 1;
 }
