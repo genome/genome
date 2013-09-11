caaff76313442cc6a6b22a656daff0ad6ec7e71e
Scott Smith <ssmith@genome.wustl.edu>
2013-09-11 17:52:26 -0500

    updated "genome model copy --recurse" to avoid redundant copies within a given recursive copy event

--- lib/perl/Genome/Model/Pair.pm
+++ lib/perl/Genome/Model/Pair.pm
@@ -6,8 +6,8 @@ use Genome;
 class Genome::Model::Pair {
     is => 'UR::Value',
     id_by => [
-        first   => { is => 'Genome::Model', id_by => 'id1' },
-        second  => { is => 'Genome::Model', id_by => 'id2' },
+        first   => { is => 'Genome::Model', id_by => 'first_id' },
+        second  => { is => 'Genome::Model', id_by => 'second_id' },
     ],
     doc => 'a pair of genome models',
 };
