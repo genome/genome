b3d535a9dc96c5e8cc23888b89018914f6a75377
Scott Smith <ssmith@genome.wustl.edu>
2013-09-11 18:23:27 -0500

    set the ID seperator for Genome::Model::Pair to "/" for easier use on the command line.  Comma would have been preferable but breaks the parser.  Slash only breaks the shortened in-clause syntax, which is deprecated anyway.

--- lib/perl/Genome/Model/Pair.pm
+++ lib/perl/Genome/Model/Pair.pm
@@ -5,12 +5,17 @@ use Genome;
 
 class Genome::Model::Pair {
     is => 'UR::Value',
+    composite_id_separator => '/',
     id_by => [
         first   => { is => 'Genome::Model', id_by => 'first_id' },
         second  => { is => 'Genome::Model', id_by => 'second_id' },
     ],
     doc => 'a pair of genome models',
 };
+
+sub __display_name__ {
+    return $_[0]->first->__display_name__ . ", " . $_[0]->second->__display_name__;
+}
 
 1;
 
