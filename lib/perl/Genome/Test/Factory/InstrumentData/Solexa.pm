e14cc2a30c21fc838a3aef3803f1c3ab7951df8a
Nathan Nutter <nnutter@genome.wustl.edu>
2013-09-16 16:17:31 -0500

    create valid objects for Genome::InstrumentData::Solexa

--- lib/perl/Genome/Test/Factory/InstrumentData/Solexa.pm
+++ lib/perl/Genome/Test/Factory/InstrumentData/Solexa.pm
@@ -3,11 +3,20 @@ use base qw(Genome::Test::Factory::Base);
 
 use strict;
 use warnings;
+
 use Genome;
+use Genome::Test::Factory::Library;
+
+our @required_params = qw(library_id);
 
 sub generate_obj {
     my $self = shift;
     return Genome::InstrumentData::Solexa->create(@_);
+}
+
+sub create_library_id {
+    my $lib = Genome::Test::Factory::Library->setup_object();
+    return $lib->id;
 }
 
 1;
