9efc832c216b5b475cdf86f7e06ba2f8fb626706
Nathan Nutter <nnutter@genome.wustl.edu>
2013-09-18 10:14:16 -0500

    fix typo

--- lib/perl/Genome/Model/Command/Admin/ModelSummary.pm
+++ lib/perl/Genome/Model/Command/Admin/ModelSummary.pm
@@ -279,7 +279,7 @@ sub previous_build {
     my $build = shift;
 
     my $model = $build->model;
-    my @prior_builds = grep { $_->date_scheduled lt $build->scheduled } $model->builds;
+    my @prior_builds = grep { $_->date_scheduled lt $build->date_scheduled } $model->builds;
 
     my $previous_build = @prior_builds ? $prior_builds[-1] : undef;
     return $previous_build;
