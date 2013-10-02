49b5534af195898c98aeee7b0135841a5f35d420
apregier <aregier@genome.wustl.edu>
2013-10-02 11:57:12 -0500

    Allow user to select subsets of clinseq inputs

--- lib/perl/Genome/Model/ClinSeq/TestData.pm
+++ lib/perl/Genome/Model/ClinSeq/TestData.pm
@@ -21,6 +21,7 @@ use Genome::Test::Factory::ProcessingProfile::SomaticVariation;
 use Genome::Utility::Test;
 
 sub load {
+    my %params = @_;
     my %ids;
     #need a way to access the data
     my $base_dir = $ENV{GENOME_TEST_INPUTS}."Genome-Model-ClinSeq-TestData/2013-09-12";
@@ -108,44 +109,57 @@ sub load {
     $tumor_model->add_instrument_data($tumor_inst_data);
     $ids{TUMOR_REFALIGN_MODEL} = $tumor_model->id;
     my $tumor_build = Genome::Test::Factory::Build->setup_object(model_id => $tumor_model->id, status => 'Succeeded');
-    my $wgs_pp = Genome::Test::Factory::ProcessingProfile::SomaticVariation->setup_object();
-    $ids{WGS_PP} = $wgs_pp->id;
-    my $wgs_model = Genome::Test::Factory::Model::SomaticVariation->setup_object(
-        subject_id => $tumor_model->subject->id,
-        normal_model => $normal_model,
-        tumor_model => $tumor_model,
-        processing_profile_id => $wgs_pp->id,
-        annotation_build => $annotation_build,
-        previously_discovered_variations_build => $dbsnp_build,
-    );
-    $ids{WGS_MODEL} = $wgs_model->id;
-    my $wgs_build = Genome::Test::Factory::Build->setup_object(model_id => $wgs_model->id, status => 'Succeeded',
-                                                                  data_directory => $base_dir."/som_var_dir",
-                                                                 );
-    $ids{WGS_BUILD} = $wgs_build->id;
-    my $exome_pp = Genome::Test::Factory::ProcessingProfile::SomaticVariation->setup_object(
-        snv_detection_strategy => "strelka 0.4.6.2 [isSkipDepthFilters = 0]");
-    $ids{EXOME_PP} = $exome_pp->id;
-    my $rna_seq_pp = Genome::Test::Factory::ProcessingProfile::RnaSeq->setup_object();
-    $ids{RNASEQ_PP} = $rna_seq_pp->id;
-    my $rna_seq_model = Genome::Test::Factory::Model::RnaSeq->setup_object(
-        subject_id => $tumor_rna_sample->id,
-        processing_profile_id => $rna_seq_pp->id,
-        annotation_build => $annotation_build,
-        reference_sequence_build => $ref_seq_build,
-    );
-    $ids{TUMOR_RNASEQ_MODEL} = $rna_seq_model->id;
-    $rna_seq_model->add_instrument_data($rna_inst_data);
-    my $rna_seq_build = Genome::Test::Factory::Build->setup_object(model_id => $rna_seq_model->id, status => 'Succeeded');
-
-    my $diff_ex_pp = Genome::Test::Factory::ProcessingProfile::DifferentialExpression->setup_object;
-    $ids{DIFFEXP_PP} = $diff_ex_pp->id;
+    my %clinseq_model_params;
+    unless ($params{exclude_wgs_model}) {
+        my $wgs_pp = Genome::Test::Factory::ProcessingProfile::SomaticVariation->setup_object();
+        $ids{WGS_PP} = $wgs_pp->id;
+        my $wgs_model = Genome::Test::Factory::Model::SomaticVariation->setup_object(
+            subject_id => $tumor_model->subject->id,
+            normal_model => $normal_model,
+            tumor_model => $tumor_model,
+            processing_profile_id => $wgs_pp->id,
+            annotation_build => $annotation_build,
+            previously_discovered_variations_build => $dbsnp_build,
+        );
+        $ids{WGS_MODEL} = $wgs_model->id;
+        my $wgs_build = Genome::Test::Factory::Build->setup_object(model_id => $wgs_model->id, status => 'Succeeded',
+            data_directory => $base_dir."/som_var_dir",
+        );
+        $ids{WGS_BUILD} = $wgs_build->id;
+        $clinseq_model_params{wgs_model} = $wgs_model;
+    }
+    unless ($params{exclude_exome_model}) {
+        my $exome_pp = Genome::Test::Factory::ProcessingProfile::SomaticVariation->setup_object(
+            snv_detection_strategy => "strelka 0.4.6.2 [isSkipDepthFilters = 0]");
+        $ids{EXOME_PP} = $exome_pp->id;
+    }
+    unless ($params{exclude_normal_rnaseq_model}) {
+        my $rna_seq_pp = Genome::Test::Factory::ProcessingProfile::RnaSeq->setup_object();
+        $ids{RNASEQ_PP} = $rna_seq_pp->id;
+        my $rna_seq_model = Genome::Test::Factory::Model::RnaSeq->setup_object(
+            subject_id => $tumor_rna_sample->id,
+            processing_profile_id => $rna_seq_pp->id,
+            annotation_build => $annotation_build,
+            reference_sequence_build => $ref_seq_build,
+        );
+        $rna_seq_model->add_instrument_data($rna_inst_data);
+        $ids{RNASEQ_MODEL} = $rna_seq_model->id;
+    }
+    unless ($params{exclude_tumor_rna_seq_model}) {
+        $ids{TUMOR_RNASEQ_MODEL} = $ids{RNASEQ_MODEL};
+        my $rna_seq_build = Genome::Test::Factory::Build->setup_object(model_id => $ids{TUMOR_RNASEQ_MODEL}, status => 'Succeeded');
+        $clinseq_model_params{tumor_rnaseq_model} = Genome::Model->get($ids{TUMOR_RNASEQ_MODEL});
+    }
+    unless ($params{exclude_diff_exp_model}) {
+        my $diff_ex_pp = Genome::Test::Factory::ProcessingProfile::DifferentialExpression->setup_object;
+        $ids{DIFFEXP_PP} = $diff_ex_pp->id;
+    }
     my $clin_seq_pp = Genome::Test::Factory::ProcessingProfile::ClinSeq->setup_object;
     $ids{CLINSEQ_PP} = $clin_seq_pp->id;
+    
     my $clin_seq_model = Genome::Test::Factory::Model::ClinSeq->setup_object(
+        %clinseq_model_params,
         processing_profile_id => $clin_seq_pp->id,
-        wgs_model => $wgs_model,
-        tumor_rnaseq_model => $rna_seq_model,
     );
     $ids{CLINSEQ_MODEL} = $clin_seq_model->id;
     my $clin_seq_build = Genome::Test::Factory::Build->setup_object(model_id => $clin_seq_model->id, status => "Succeeded");
