df3b88a0e5421676b32a05a89cf2f55bb2a7aae4
Chris Miller <cmiller@genome.wustl.edu>
2013-10-04 12:25:00 -0500

    skeleton of a new tool to munge through a processing profile and spit out a text description. Still needs a ton of work

---
+++ lib/perl/Genome/Model/Tools/Text.pm
@@ -0,0 +1,9 @@
+package Genome::Model::Tools::Text;
+
+class Genome::Model::Tools::Text {
+    is => 'Command::Tree',
+    doc => 'Tools to generate human readable text describing data or processes'
+};
+
+1;
+
---
+++ lib/perl/Genome/Model/Tools/Text/ModelToText.t
@@ -0,0 +1,10 @@
+#!/usr/bin/env genome-perl
+
+use strict;
+use warnings;
+
+use above 'Genome';
+use Test::More tests => 1;
+
+use_ok('Genome::Model::Tools::Text::ModelToText');
+
