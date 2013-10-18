6d5cf7cbd8d17c401abed9f380498a9561c8104e
Eddie Belter <ebelter@genome.wustl.edu>
2013-10-18 12:13:05 -0500

    InstData Import: rename fiel to path in helpers retrieve path methods

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/Helpers.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/Helpers.pm
@@ -56,10 +56,10 @@ sub add_operation_to_workflow {
 #<>#
 
 #<MOVE and COPY>#
-sub move_file {
+sub move_path {
     my ($self, $from, $to) = @_;
+    $self->status_message('Move path...');
 
-    $self->status_message('Move file...');
     my $from_sz = -s $from;
     $self->status_message("From: $from");
     $self->status_message("To: $to");
@@ -74,27 +74,27 @@ sub move_file {
         return;
     }
 
-    $self->status_message('Move file...done');
+    $self->status_message('Move path...done');
     return 1;
 }
 
-sub retrieve_file {
+sub retrieve_path {
     my ($self, $from, $to) = @_;
 
     Carp::confess('No from path to retrieve file!') if not $from;
     Carp::confess('No to path to retrieve file!') if not $to;
 
     if ( $from =~ /^http/ ) {
-        return $self->_retrieve_remote_file($from, $to);
+        return $self->_retrieve_remote_path($from, $to);
     }
     else {
-        return $self->_retrieve_local_file($from, $to);
+        return $self->_retrieve_local_path($from, $to);
     }
 }
 
-sub _retrieve_local_file {
+sub _retrieve_local_path {
     my ($self, $from, $to) = @_;
-    $self->status_message('Retrieve local file...');
+    $self->status_message('Retrieve local path...');
 
     $self->status_message("From: $from");
     $self->status_message("To: $to");
@@ -113,13 +113,13 @@ sub _retrieve_local_file {
         return;
     }
 
-    $self->status_message('Retrieve local file...done');
+    $self->status_message('Retrieve local path...done');
     return 1;
 }
 
-sub _retrieve_remote_file {
+sub _retrieve_remote_path {
     my ($self, $from, $to) = @_;
-    $self->status_message('Retrieve remote file...');
+    $self->status_message('Retrieve remote path...');
 
     $self->status_message("From: $from");
     $self->status_message("To: $to");
@@ -128,7 +128,7 @@ sub _retrieve_remote_file {
     my $response = $agent->get($from, ':content_file' => $to);
     if ( not $response->is_success ) {
         $self->error_message($response->message) if $response->message;
-        $self->error_message('GET failed for remote file!');
+        $self->error_message('GET failed for remote path!');
         return
     }
 
@@ -137,11 +137,11 @@ sub _retrieve_remote_file {
     my $to_sz = -s $to;
     $self->status_message("To size: $to_sz");
     if ( not $to_sz or $to_sz != $from_sz ) {
-        $self->error_message("GET remote file succeeded, but destination size is different from original! $to_sz vs $from_sz");
+        $self->error_message("GET remote path succeeded, but destination size is different from original! $to_sz vs $from_sz");
         return;
     }
 
-    $self->status_message('Retrieve remote file...done');
+    $self->status_message('Retrieve remote path...done');
     return 1;
 }
 #<>#
