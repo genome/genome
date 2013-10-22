ff249f80b053ba8cd2b3c5f7a8b09bcca560b1aa
Eddie Belter <ebelter@genome.wustl.edu>
2013-10-22 14:47:36 -0500

    InstData Import: move retireve methods from helpers to retrieve source path

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/Helpers.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/Helpers.pm
@@ -35,7 +35,7 @@ sub local_source_files_for_instrument_data {
 }
 #<>#
 
-#<MOVE and COPY>#
+#<MOVE>#
 sub move_path {
     my ($self, $from, $to) = @_;
     $self->status_message('Move path...');
@@ -55,73 +55,6 @@ sub move_path {
     }
 
     $self->status_message('Move path...done');
-    return 1;
-}
-
-sub retrieve_path {
-    my ($self, $from, $to) = @_;
-
-    Carp::confess('No from path to retrieve file!') if not $from;
-    Carp::confess('No to path to retrieve file!') if not $to;
-
-    if ( $from =~ /^http/ ) {
-        return $self->_retrieve_remote_path($from, $to);
-    }
-    else {
-        return $self->_retrieve_local_path($from, $to);
-    }
-}
-
-sub _retrieve_local_path {
-    my ($self, $from, $to) = @_;
-    $self->status_message('Retrieve local path...');
-
-    $self->status_message("From: $from");
-    $self->status_message("To: $to");
-    my $move_ok = File::Copy::copy($from, $to);
-    if ( not $move_ok ) {
-        $self->error_message('Copy failed!');
-        return;
-    }
-
-    my $from_sz = -s $from;
-    $self->status_message("From size: $from_sz");
-    my $to_sz = -s $to;
-    $self->status_message("To size: $to_sz");
-    if ( not $to_sz or $to_sz != $from_sz ) {
-        $self->error_message("Copy succeeded, but destination size is diffeerent from original! $to_sz vs $from_sz");
-        return;
-    }
-
-    $self->status_message('Retrieve local path...done');
-    return 1;
-}
-
-sub _retrieve_remote_path {
-    my ($self, $from, $to) = @_;
-    $self->status_message('Retrieve remote path...');
-
-    $self->status_message("From: $from");
-    $self->status_message("To: $to");
-
-    my $agent = LWP::UserAgent->new;
-    my $response = $agent->get($from, ':content_file' => $to);
-    if ( not $response->is_success ) {
-        $self->error_message($response->message) if $response->message;
-        $self->error_message('GET failed for remote path!');
-        return
-    }
-
-    my $from_sz = $response->headers->content_length;
-    $self->status_message("From size: $from_sz");
-    my $to_sz = -s $to;
-    $self->status_message("To size: $to_sz");
-    if ( not $to_sz or $to_sz != $from_sz ) {
-        $self->error_message("GET remote path succeeded, but destination size is different from original! $to_sz vs $from_sz");
-        return;
-    }
-
-    $self->status_message('Retrieve remote path...done');
     return 1;
 }
 #<>#
--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/RetrieveSourcePath.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/RetrieveSourcePath.pm
@@ -49,7 +49,7 @@ sub _retrieve_source_path {
     my $self = shift;
     $self->status_message('Retrieve source path...');
 
-    my $retrieve_ok = $self->helpers->retrieve_path($self->source_path, $self->destination_path);
+    my $retrieve_ok = $self->retrieve_path($self->source_path, $self->destination_path);
     return if not $retrieve_ok;
     
     $self->status_message('Retrieve source path...done');
@@ -67,12 +67,78 @@ sub _retrieve_source_md5 {
         return 1;
     }
 
-    my $retrieve_ok = $self->helpers->retrieve_path($md5_path, $self->destination_path.'.md5-orig');
+    my $retrieve_ok = $self->retrieve_path($md5_path, $self->destination_path.'.md5-orig');
     return if not $retrieve_ok;
 
     $self->status_message('Retrieve source MD5 path...done');
     return 1;
 }
 
+sub retrieve_path {
+    my ($self, $from, $to) = @_;
+
+    Carp::confess('No from path to retrieve file!') if not $from;
+    Carp::confess('No to path to retrieve file!') if not $to;
+
+    if ( $from =~ /^http/ ) {
+        return $self->_retrieve_remote_path($from, $to);
+    }
+    else {
+        return $self->_retrieve_local_path($from, $to);
+    }
+}
+
+sub _retrieve_local_path {
+    my ($self, $from, $to) = @_;
+    $self->status_message('Retrieve local path...');
+
+    $self->status_message("From: $from");
+    $self->status_message("To: $to");
+    my $move_ok = File::Copy::copy($from, $to);
+    if ( not $move_ok ) {
+        $self->error_message('Copy failed!');
+        return;
+    }
+
+    my $from_sz = -s $from;
+    $self->status_message("From size: $from_sz");
+    my $to_sz = -s $to;
+    $self->status_message("To size: $to_sz");
+    if ( not $to_sz or $to_sz != $from_sz ) {
+        $self->error_message("Copy succeeded, but destination size is diffeerent from original! $to_sz vs $from_sz");
+        return;
+    }
+
+    $self->status_message('Retrieve local path...done');
+    return 1;
+}
+
+sub _retrieve_remote_path {
+    my ($self, $from, $to) = @_;
+    $self->status_message('Retrieve remote path...');
+
+    $self->status_message("From: $from");
+    $self->status_message("To: $to");
+
+    my $agent = LWP::UserAgent->new;
+    my $response = $agent->get($from, ':content_file' => $to);
+    if ( not $response->is_success ) {
+        $self->error_message($response->message) if $response->message;
+        $self->error_message('GET failed for remote path!');
+        return
+    }
+
+    my $from_sz = $response->headers->content_length;
+    $self->status_message("From size: $from_sz");
+    my $to_sz = -s $to;
+    $self->status_message("To size: $to_sz");
+    if ( not $to_sz or $to_sz != $from_sz ) {
+        $self->error_message("GET remote path succeeded, but destination size is different from original! $to_sz vs $from_sz");
+        return;
+    }
+
+    $self->status_message('Retrieve remote path...done');
+    return 1;
+}
 1;
 
