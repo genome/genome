738bdf97f4ecc122cbf85ffb26e5426760f0fc26
Gabriel Sanderson <gsanders@genome.wustl.edu>
2013-10-23 17:33:28 -0500

    Added VarscanBase

---
+++ lib/perl/Genome/Model/Tools/DetectVariants2/VarscanBase.pm
@@ -0,0 +1,48 @@
+package Genome::Model::Tools::DetectVariants2::VarscanBase;
+
+use strict;
+use warnings;
+use Genome;
+
+class Genome::Model::Tools::DetectVariants2::VarscanBase {
+    is_abstract => 1,
+    is => ['Genome::Model::Tools::DetectVariants2::Detector'],
+    has => [
+    ],
+};
+
+# This method breaks the samtools params down into individual params for the varscan modules
+sub _process_samtools_params {
+    my ($self, $params) = @_;
+    my $samtools_version;
+    my $use_baq = 1;
+
+    # Grab version if it exists
+    if ($params =~ m/version/) {
+        ($samtools_version) = ($params =~ m/--version\s*(\S+)/);
+        $params =~ s/--version\s*(\S+)\s*//;
+    }
+
+    # Grab baq boolean
+    if ($params =~ m/--nobaq/) {
+        $use_baq = 0;
+        $params =~ s/--nobaq\s*//;
+    }
+
+    return ($samtools_version, $use_baq, $params);
+}
+
+# Params should be set up as <samtools params>:<varscan params>
+# If there is no : present, assume everything is varscan params (legacy processing profiles)
+sub _split_params {
+    my ($self, $params) = @_;
+    my ($samtools_params, $varscan_params);
+    if ($params =~ m/:/) {
+        ($samtools_params, $varscan_params) = split ":", $params;
+    } else {
+        $varscan_params = $params;
+    }
+    return ($samtools_params, $varscan_params);
+}
+
+1;
