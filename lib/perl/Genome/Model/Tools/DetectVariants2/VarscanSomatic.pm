f6dfd6a3279f389787c1c731da1189a5d9b45881
Gabriel Sanderson <gsanders@genome.wustl.edu>
2013-10-23 17:25:58 -0500

    Enable samtools parameter processing in VarscanSomatic (previously only worked in Varscan germline)

--- lib/perl/Genome/Model/Tools/DetectVariants2/VarscanSomatic.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/VarscanSomatic.pm
@@ -45,15 +45,25 @@ sub _detect_variants {
         die $self->error_message("A version of VarscanSomatic must be specified");
     }
 
+    my $params = $self->params;
+    my ($samtools_params, $varscan_params) = $self->_split_params($params);
+    my ($samtools_version, $use_baq, $other_params) = $self->_process_samtools_params($samtools_params);
+
+    my %optional_samtools_params;
+    $optional_samtools_params{samtools_version} = $samtools_version if $samtools_version;
+    $optional_samtools_params{samtools_use_baq} = $use_baq if $use_baq;
+    $optional_samtools_params{samtools_params} = $other_params if $other_params;
+
     my $varscan = Genome::Model::Tools::Varscan::Somatic->create(
         normal_bam => $self->control_aligned_reads_input,
         tumor_bam => $self->aligned_reads_input,,
         reference => $self->reference_sequence_input,
         output_snp => $output_snp,
         output_indel => $output_indel,
-        varscan_params => $self->params,
+        varscan_params => $varscan_params,
         no_headers => 1,
         version => $self->version,
+        %optional_samtools_params,
     );
 
     unless($varscan->execute()) {
@@ -127,6 +137,40 @@ sub _parse_snv_for_bed_intersection {
     my ($chromosome, $position, $reference, undef, $depth1, $depth2, undef, undef, undef, $qual, undef, $consensus, @extra) = split("\t", $line);
 
     return [$chromosome, $position, $reference, (Genome::Info::IUB->variant_alleles_for_iub($reference, $consensus), $consensus)];
+}
+
+# This method breaks the samtools params down into individual params for the varscan modules
+sub _process_samtools_params {
+    my ($self, $params) = @_;
+    my $samtools_version;
+    my $use_baq = 1;
+
+    # Grab version if it exists
+    if ($params =~ m/version/) {
+        ($samtools_version) = ($params =~ m/--version\s*(\S+)/);
+        $params =~ s/--version\s*(\S+)\s*//;
+    }
+
+    # Grab baq boolean
+    if ($params =~ m/--nobaq/) {
+        $use_baq = 0;
+        $params =~ s/--nobaq\s*//;
+    }
+    
+    return ($samtools_version, $use_baq, $params);
+}
+
+# Params should be set up as <samtools params>:<varscan params>
+# If there is no : present, assume everything is varscan params (legacy processing profiles)
+sub _split_params {
+    my ($self, $params) = @_;
+    my ($samtools_params, $varscan_params);
+    if ($params =~ m/:/) {
+        ($samtools_params, $varscan_params) = split ":", $params;
+    } else {
+        $varscan_params = $params;
+    }
+    return ($samtools_params, $varscan_params);
 }
 
 1;
