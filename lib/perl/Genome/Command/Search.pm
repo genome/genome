743d016856429718363061bd5fca9c96afcac29c
Mark Burnett <mburnett@genome.wustl.edu>
2013-10-24 14:01:29 -0500

    make `genome search` to be more like web search

--- lib/perl/Genome/Command/Search.pm
+++ lib/perl/Genome/Command/Search.pm
@@ -14,6 +14,13 @@ class Genome::Command::Search {
             is_many => 1,
             doc => 'The string, id, or partial id to search for.',
         },
+
+        rows => {
+            is => 'Integer',
+            is_optional => 1,
+            default_value => 15,
+            doc => 'Maximum number of rows to return',
+        },
     ],
 };
 
@@ -21,130 +28,24 @@ class Genome::Command::Search {
 sub execute {
     my $self = shift;
 
-    $self->show($self->get_docs);
+    for my $doc ($self->get_docs) {
+        printf("%s: %s\n", $doc->{display_type}, $doc->{display_title});
+    }
 
     return 1;
 }
-
-
-sub show {
-    my ($self, $docs) = @_;
-    my $type = $docs->[0]->{type};
-
-    my @objects = $self->get_objects($docs);
-    $self->show_objects($type, @objects);
-}
-
-
-my %OBJECT_GETTERS = (
-    'build' => 'Genome::Model::Build',
-#    "illumina_run"
-#    "imported_instrument_data"
-#    "individual"
-#    "instrument data"
-#    "library"
-#    "mail"
-    "model - ClinSeq" => 'Genome::Model',
-    "model - alignment" => 'Genome::Model',
-    "model - convergence" => 'Genome::Model',
-    "model - lane_qc" => 'Genome::Model',
-    "model - microarray" => 'Genome::Model',
-    "model - other" => 'Genome::Model',
-    "model - rna" => 'Genome::Model',
-    "model - somatic" => 'Genome::Model',
-    "modelgroup" => 'Genome::ModelGroup',
-#    "population_group"
-#    "processing_profile"
-#    "project"
-#    "sample"
-#    "solexa_instrument_data"
-#    "taxon"
-#    "user"
-#    "wiki-page"
-#    "work-order"
-);
-
-sub get_objects {
-    my ($self, $docs) = @_;
-
-    my $end = $self->find_different_getter($docs);
-
-    my @useful_docs = (@$docs)[0..$end];
-    my @ids = map {$_->{object_id}} @useful_docs;
-
-    my $getter = $OBJECT_GETTERS{$docs->[0]->{type}};
-    return $getter->get(id => \@ids);
-}
-
-sub find_different_getter {
-    my ($self, $docs) = @_;
-    my $len = scalar @$docs;
-
-    my $getter = $OBJECT_GETTERS{$docs->[0]->{type}};
-    for (my $i = 0; $i < $len; $i++) {
-        if (exists $OBJECT_GETTERS{$docs->[$i]->{type}}) {
-            my $this_getter = $OBJECT_GETTERS{$docs->[$i]->{type}};
-            if ($getter ne $this_getter) {
-                return $i - 1;
-            }
-        } else {
-            return $i - 1;
-        }
-    }
-    return $len - 1;
-}
-
-my %OBJECT_SHOWERS = (
-    'build' => 'Genome::Command::Search::Build',
-#    "illumina_run"
-#    "imported_instrument_data"
-#    "individual"
-#    "instrument data"
-#    "library"
-#    "mail"
-    "model - ClinSeq" => 'Genome::Command::Search::Model',
-    "model - alignment" => 'Genome::Command::Search::Model',
-    "model - convergence" => 'Genome::Command::Search::Model',
-    "model - lane_qc" => 'Genome::Command::Search::Model',
-    "model - microarray" => 'Genome::Command::Search::Model',
-    "model - other" => 'Genome::Command::Search::Model',
-    "model - rna" => 'Genome::Command::Search::Model',
-    "model - somatic" => 'Genome::Command::Search::Model',
-    "modelgroup" => 'Genome::Command::Search::ModelGroup',
-#    "population_group"
-#    "processing_profile"
-#    "project"
-#    "sample"
-#    "solexa_instrument_data"
-#    "taxon"
-#    "user"
-#    "wiki-page"
-#    "work-order"
-);
-
-sub show_objects {
-    my ($self, $type, @objects) = @_;
-
-    my $shower = $OBJECT_SHOWERS{$type};
-
-    if (scalar(@objects) > 1) {
-        $shower->display_many(\@objects);
-    } else {
-        $shower->display_single($objects[0]);
-    }
-}
-
 sub get_docs {
     my $self = shift;
 
     my $content = $self->get_content;
-    return $content->{response}{docs};
+    return @{$content->{response}{docs}};
 }
 
 sub get_content {
     my $self = shift;
 
-    my $response = Genome::Search->search(join(' ', $self->target));
+    my $response = Genome::Search->search(join(' ', $self->target),
+        {rows => $self->rows});
     unless (defined($response)) {
         die "Invalid response from Search";
     }
@@ -158,7 +59,6 @@ sub get_content {
 my $SCORE_WARNING_THRESHOLD = 5;
 sub validate_response_content {
     my ($self, $content) = @_;
-
 
     if ($content->{response}{numFound} <= 0) {
         die "No results found";
