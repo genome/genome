b00592afd60d33bfeb977b731d26ee1f382da0d7
Jim Weible <jweible@genome.wustl.edu>
2013-10-25 14:09:35 -0500

    get a working version of CopyCatSomaticWithBamWindow and a test

--- lib/perl/Genome/Model/Tools/DetectVariants2/CopyCatSomaticWithBamWindow.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/CopyCatSomaticWithBamWindow.pm
@@ -17,6 +17,14 @@ class Genome::Model::Tools::DetectVariants2::CopyCatSomaticWithBamWindow{
             default_value => 'rusage[mem=4000] select[type==LINUX64 && maxtmp>10000] span[hosts=1]',
         },
     ],
+    has => [
+        bamwindow_filter_to_chromosomes => {
+            is => 'Text',
+            is_optional => 1,
+            is_many => 1,
+            doc => 'Chromosomes that bamwindow will filter output to (generally for test purposes)',
+        },
+    ],
 };
 
 
@@ -78,13 +86,8 @@ sub _detect_variants {
     $input{normal_bam_file} = $self->control_aligned_reads_input;
     $input{tumor_window_file} = join('/', $self->_temp_staging_directory, 'tumor_window_file');
     $input{normal_window_file} = join('/', $self->_temp_staging_directory, 'normal_window_file');
-
-    # my $normal_window_dir = join('/', $self->_temp_staging_directory, 'normal_window_dir');
-    # mkdir $normal_window_dir;
-    # $input{normal_window_dir} = $normal_window_dir;
-    # my $tumor_window_dir = join('/', $self->_temp_staging_directory, 'tumor_window_dir');
-    # mkdir $tumor_window_dir;
-    # $input{tumor_window_dir} = $tumor_window_dir; 
+    my @bamwindow_filter_to_chromosomes = $self->bamwindow_filter_to_chromosomes;
+    $input{bamwindow_filter_to_chromosomes} = \@bamwindow_filter_to_chromosomes;
 
     #for copycat
     $input{per_read_length} = $per_read_length;
@@ -96,14 +99,10 @@ sub _detect_variants {
     $input{genome_build} = $genome_build;
 
 
-    # $self->_dump_workflow($workflow);
-    print "DEBUG: INPUTS:", Data::Dumper->Dumper(%input), "\n";
-
-    # my $log_dir = $self->output_directory;
-    my $log_dir = '/tmp/awesome';
-    # if(Workflow::Model->parent_workflow_log_dir) {
-        # $log_dir = Workflow::Model->parent_workflow_log_dir;
-    # }
+    my $log_dir = $self->output_directory;
+    if(Workflow::Model->parent_workflow_log_dir) {
+        $log_dir = Workflow::Model->parent_workflow_log_dir;
+    }
     $workflow->log_dir($log_dir);
 
     # Launch workflow
@@ -117,7 +116,6 @@ sub _detect_variants {
         }
         die $self->error_message("Workflow did not return correctly");
     }
-    $self->_workflow_result($result);
 
     return 1;
 }
@@ -129,15 +127,13 @@ sub has_version {
 }
 
 sub _sort_detector_output {
-    return 0;
+    return 1;
 }
 
-##todo comment
 sub get_samtools_results{
     my $self = shift;
     my $bam_path = shift;
     my $return_snp_file;
-    #TODO: This will never work in testing for obvious reasons
     my @results = Genome::Model::Tools::DetectVariants2::Result::DetectionBase->get(
         detector_name => "Genome::Model::Tools::DetectVariants2::Samtools",
         aligned_reads =>$bam_path );
@@ -152,6 +148,7 @@ sub get_samtools_results{
 
 sub _get_genome_build{
     my $self = shift;
+    return 'hg19.chr14only' if($self->bamwindow_filter_to_chromosomes); #TODO: fix this hack. currently exists for testing purposes
     my $reference_build = Genome::Model::Build->get($self->reference_build_id);
     return 37 if $reference_build->build_name =~ /37/;
     return 'mm9' if $reference_build->build_name =~/mm9/;
@@ -169,14 +166,14 @@ __DATA__
     <link fromOperation="input connector" fromProperty="bamwindow_params" toOperation="BamWindow Normal" toProperty="options" />
     <link fromOperation="input connector" fromProperty="bamwindow_version" toOperation="BamWindow Normal" toProperty="version" />
     <link fromOperation="input connector" fromProperty="normal_window_file" toOperation="BamWindow Normal" toProperty="output_file" />
-    <!-- <link fromOperation="input connector" fromProperty="reference_build_id" toOperation="BamWindow Normal" toProperty="reference_build_id" /> -->
+    <link fromOperation="input connector" fromProperty="bamwindow_filter_to_chromosomes" toOperation="BamWindow Normal" toProperty="filter_to_chromosomes" />
 
 
     <link fromOperation="input connector" fromProperty="tumor_bam_file" toOperation="BamWindow Tumor" toProperty="bam_file" />
     <link fromOperation="input connector" fromProperty="bamwindow_params" toOperation="BamWindow Tumor" toProperty="options" />
     <link fromOperation="input connector" fromProperty="bamwindow_version" toOperation="BamWindow Tumor" toProperty="version" />
     <link fromOperation="input connector" fromProperty="tumor_window_file" toOperation="BamWindow Tumor" toProperty="output_file" />
-    <!-- <link fromOperation="input connector" fromProperty="reference_build_id" toOperation="BamWindow Tumor" toProperty="reference_build_id" /> -->
+    <link fromOperation="input connector" fromProperty="bamwindow_filter_to_chromosomes" toOperation="BamWindow Tumor" toProperty="filter_to_chromosomes" />
 
 
     <link fromOperation="BamWindow Normal" fromProperty="output_file" toOperation="CopyCat Somatic" toProperty="normal_window_file" />
@@ -185,8 +182,6 @@ __DATA__
     <link fromOperation="input connector" fromProperty="per_read_length" toOperation="CopyCat Somatic" toProperty="per_read_length" />
     <link fromOperation="input connector" fromProperty="tumor_samtools_file" toOperation="CopyCat Somatic" toProperty="tumor_samtools_file" />
     <link fromOperation="input connector" fromProperty="normal_samtools_file" toOperation="CopyCat Somatic" toProperty="normal_samtools_file" />
-    <!-- <link fromOperation="input connector" fromProperty="reference_build_id" toOperation="CopyCat Somatic" toProperty="reference_build_id" /> -->
-    <!-- <link fromOperation="input connector" fromProperty="copycat_params" toOperation="CopyCat Somatic" toProperty="params" /> --> 
     <link fromOperation="input connector" fromProperty="annotation_directory" toOperation="CopyCat Somatic" toProperty="annotation_directory" />
     <link fromOperation="input connector" fromProperty="genome_build" toOperation="CopyCat Somatic" toProperty="genome_build" />
     <link fromOperation="input connector" fromProperty="copycat_output_directory" toOperation="CopyCat Somatic" toProperty="output_directory" />
@@ -216,15 +211,14 @@ __DATA__
     <inputproperty>normal_window_file</inputproperty>
     <inputproperty>bamwindow_params</inputproperty>
     <inputproperty>bamwindow_version</inputproperty>
-    <!-- <inputproperty>reference_build_id</inputproperty> --> 
     <inputproperty>per_read_length</inputproperty>
     <inputproperty>per_library</inputproperty>
     <inputproperty>tumor_samtools_file</inputproperty>
     <inputproperty>normal_samtools_file</inputproperty>
-    <!-- <inputproperty>copycat_params</inputproperty> -->
     <inputproperty>copycat_output_directory</inputproperty>
     <inputproperty>annotation_directory</inputproperty>
     <inputproperty>genome_build</inputproperty>
+    <inputproperty isOptional="Y">bamwindow_filter_to_chromosomes</inputproperty>
 
     <outputproperty>bam_window_normal_output_file</outputproperty>
     <outputproperty>bam_window_tumor_output_file</outputproperty>
