4c87e74ec87ae25b582fd2279eab2112cfd51608
Eddie Belter <ebelter@genome.wustl.edu>
2013-10-30 17:56:42 -0500

    Sync: update lims inst data base genome class

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/LimsInstDataBase.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/LimsInstDataBase.pm
@@ -11,7 +11,9 @@ class Genome::Site::TGI::Synchronize::Classes::LimsInstDataBase {
 
 sub genome_class_for_create {
     my $self = shift;
-    return 'Genome::InstrumentData::'.Genome::Utility::Text::string_to_camel_case($self->entity_name);
+    my $entity_name = $self->entity_name;
+    $entity_name =~ s/instrument data //;
+    return 'Genome::InstrumentData::'.Genome::Utility::Text::string_to_camel_case($entity_name);
 }
 
 1;
