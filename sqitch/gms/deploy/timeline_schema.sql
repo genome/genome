ebf257b51358b0905aece5ec680e5bd65546af7e
Anthony Brummett <abrummet@genome.wustl.edu>
2013-10-31 11:28:28 -0500

    add schema timeline

---
+++ sqitch/gms/deploy/timeline_schema.sql
@@ -0,0 +1,8 @@
+-- Deploy timeline_schema
+-- requires: empty_db
+
+BEGIN;
+
+CREATE SCHEMA timeline AUTHORIZATION genome;
+
+COMMIT;
---
+++ sqitch/gms/revert/timeline_schema.sql
@@ -0,0 +1,7 @@
+-- Revert timeline_schema
+
+BEGIN;
+
+DROP SCHEMA timeline;
+
+COMMIT;
