edad43d3b4331b4f3439b934dadd9ca14b96960f
Mark Burnett <mburnett@genome.wustl.edu>
2013-10-31 11:03:42 -0500

    formatting

--- lib/perl/Genome/Disk/Detail/Allocation/Mover.pm
+++ lib/perl/Genome/Disk/Detail/Allocation/Mover.pm
@@ -39,7 +39,8 @@ sub move {
     my $group_name = $self->disk_group_name;
     my $new_mount_path = $self->target_mount_path;
 
-    my ($allocation_object, $allocation_lock) = Genome::Disk::Allocation->get_with_lock($id);
+    my ($allocation_object, $allocation_lock) =
+        Genome::Disk::Allocation->get_with_lock($id);
 
     my $original_absolute_path = $allocation_object->absolute_path;
 
@@ -58,7 +59,8 @@ sub move {
 
     # The shadow allocation is just a way of keeping track of our temporary
     # additional disk usage during the move.
-    my $shadow_allocation = Genome::Disk::Allocation->shadow_get_or_create(%creation_params);
+    my $shadow_allocation = Genome::Disk::Allocation->shadow_get_or_create(
+        %creation_params);
 
     my $shadow_absolute_path = $shadow_allocation->absolute_path;
 
@@ -79,7 +81,8 @@ sub move {
         Genome::Sys->unlock_resource(resource_lock => $allocation_lock);
         confess(sprintf(
                 "Could not copy allocation %s from %s to %s: %s",
-                $allocation_object->id, $original_absolute_path, $shadow_absolute_path, $copy_error_message));
+                $allocation_object->id, $original_absolute_path,
+                $shadow_absolute_path, $copy_error_message));
     }
 
     my $new_volume_final_path = Genome::Disk::Allocation->_absolute_path(
@@ -94,8 +97,10 @@ sub move {
         my $shadow_allocation_abs_path = $shadow_allocation->absolute_path;
         $shadow_allocation->delete;
         confess($allocation_object->error_message(sprintf(
-                "Could not move shadow allocation path (%s) to final path (%s).  This should never happen, even when 100%% full.",
-                $shadow_allocation_abs_path, $allocation_object->absolute_path)));
+                "Could not move shadow allocation path (%s) to final path (%s)."
+                . "  This should never happen, even when 100%% full.",
+                $shadow_allocation_abs_path,
+                $allocation_object->absolute_path)));
     }
 
     # Change the shadow allocation to reserve some disk on the old volume until
@@ -109,7 +114,8 @@ sub move {
     $allocation_object->_update_owner_for_move;
 
     Genome::Timeline::Event::Allocation->moved(
-        sprintf("moved from %s to %s", $original_absolute_path, $allocation_object->absolute_path),
+        sprintf("moved from %s to %s", $original_absolute_path,
+            $allocation_object->absolute_path),
         $allocation_object,
     );
 
@@ -123,8 +129,10 @@ sub move {
     $shadow_allocation->delete;
 
     Genome::Disk::Allocation->_create_observer(
-        Genome::Disk::Allocation->_mark_for_deletion_closure($original_absolute_path),
-        Genome::Disk::Allocation->_remove_directory_closure($original_absolute_path),
+        Genome::Disk::Allocation->_mark_for_deletion_closure(
+            $original_absolute_path),
+        Genome::Disk::Allocation->_remove_directory_closure(
+            $original_absolute_path),
     );
 }
 
