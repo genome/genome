1c2f32af4e63410920f79d9fdf66e7bbac354b92
Avinash Ramu <aramu@genome.wustl.edu>
2013-11-14 21:24:22 -0600

    use build variable

--- lib/perl/Genome/Model/ClinSeq.pm
+++ lib/perl/Genome/Model/ClinSeq.pm
@@ -311,7 +311,7 @@ sub map_workflow_inputs {
             push @dirs, $tumor_filtered_fusion_dir;
             if ($wgs_build){
                 #Check for SV calls file
-                if(-e $build->wgs_build->data_directory . '/effects/svs.hq.annotated'){
+                if(-e $wgs_build->data_directory . '/effects/svs.hq.annotated'){
                     my $wgs_sv_file = $build->wgs_build->data_directory . '/effects/svs.hq.annotated';
                     my $tumor_filtered_intersected_fusion_file =  $tumor_filtered_fusion_dir . '/chimeras.filtered.intersected.bedpe';
                     push @inputs, wgs_sv_file => $wgs_sv_file;
