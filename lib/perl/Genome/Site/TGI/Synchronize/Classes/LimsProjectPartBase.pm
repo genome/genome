9c69e6479bb8e2d13d87fbf375f540ed1059be37
Eddie Belter <ebelter@genome.wustl.edu>
2013-11-15 15:33:20 -0600

    Sync: move some propeteries to methods in lims proj part base

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/LimsProjectPartBase.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/LimsProjectPartBase.pm
@@ -7,21 +7,7 @@ use Genome;
 
 class Genome::Site::TGI::Synchronize::Classes::LimsProjectPartBase {
     is => 'Genome::Site::TGI::Synchronize::Classes::LimsBase',
-    has_constant_calculated => [
-        entity_id => {
-            calculate_from => [qw/ label /],
-            calculate => q| my $method = $label.'_id'; return $self->$method; |, 
-        },
-        entity_class_name => { 
-            calculate_from => [qw/ entity_base_name /],
-            calculate => q| return 'Genome::'.join('', map { ucfirst } split(/\s+/, $entity_base_name)); |, 
-        },
-        label => {
-            calculate_from => [qw/ entity_base_name /],
-            calculate => q| join('_', split(/\s+/, $entity_base_name)); |, 
-        },
-    ],
-    schema_name => 'GMSchema',
+     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::Oltp',
 };
 
@@ -45,7 +31,40 @@ sub genome_class_for_comparison {
 sub genome_class_for_create { return 'Genome::ProjectPart'; }
 
 sub properties_to_copy {
-    return (qw/ project_id entity_class_name entity_id label /);
+    my $self = shift;
+    return ( 'project_id', $self->entity_id_method );
+}
+
+sub entity_id_method {
+    my $self = shift;
+    return $self->label.'_id';
+}
+
+sub entity_id {
+    my $self = shift;
+    my $entity_id_method = $self->entity_id_method;
+    return $self->$entity_id_method; 
+}
+
+sub entity_class_name { 
+    my $self = shift;
+    return 'Genome::'.join('', map { ucfirst } split(/\s+/, $self->entity_base_name));
+}
+
+sub label{
+    my $self = shift;
+    join('_', split(/\s+/, $self->entity_base_name));
+}
+
+sub params_for_create_in_genome {
+    my $self = shift;
+    my $entity_id_method = $self->entity_id_method;
+    return (
+        project_id => $self->project_id,
+        entity_id => $self->$entity_id_method,
+        entity_class_name => $self->entity_class_name,
+        label => $self->label,
+    );
 }
 
 1;
