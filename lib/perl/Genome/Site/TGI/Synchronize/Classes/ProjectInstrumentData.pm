c398981bca7dc9567f58fc340478d96786b718e5
Eddie Belter <ebelter@genome.wustl.edu>
2013-11-20 15:10:07 -0600

    Sync: genome project part rename specific names to entity_name

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/ProjectInstrumentData.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/ProjectInstrumentData.pm
@@ -8,7 +8,7 @@ use Genome;
 class Genome::Site::TGI::Synchronize::Classes::ProjectInstrumentData {
     table_name => <<SQL
     (
-        select part.project_id project_id, part.part_id part_id
+        select part.project_id project_id, part.part_id entity_id
         from subject.project_part part
         where part.label = 'instrument_data'
     ) project_part
@@ -16,7 +16,7 @@ SQL
 ,
     id_by => [
         project_id => { is => 'Text', },
-        part_id => { is => 'Text', },
+        entity_id => { is => 'Text', },
     ],
     schema_name => 'GMSchema',
     data_source => 'Genome::DataSource::GMSchema',
