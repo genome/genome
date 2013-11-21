4a9d1f19cce05dbdf674d3f0cd0a483fe62ba92a
Nathan Nutter <nnutter@genome.wustl.edu>
2013-11-20 18:10:12 -0600

    add new GENOME_LSF_QUEUE_* environment variables

---
+++ lib/perl/Genome/Env/GENOME_LSF_QUEUE_BIGMEM.pm
@@ -0,0 +1,15 @@
+package Genome::Env::GENOME_LSF_QUEUE_BIGMEM;
+
+=pod
+
+=head1 NAME
+
+GENOME_LSF_QUEUE_BIGMEM
+
+=head1 DESCRIPTION
+
+Specified the LSF queue to submit big memory jobs to.
+
+=cut
+
+1;
