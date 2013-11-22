01328667b721b8ba1dfd7dfb00a9932545d81693
Eddie Belter <ebelter@genome.wustl.edu>
2013-11-21 19:36:48 -0600

    Sync: change table name to sql in APID

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/AnalysisProjectInstrumentData.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/AnalysisProjectInstrumentData.pm
@@ -6,8 +6,14 @@ use warnings;
 use Genome;
 
 class Genome::Site::TGI::Synchronize::Classes::AnalysisProjectInstrumentData {
-    is => 'UR::Object',
-    table_name => 'config.instrument_data_analysis_project_bridge',
+    table_name => <<SQL
+    (
+        select instrument_data_id, analysis_project_id
+        from config.instrument_data_analysis_project_bridge
+    )
+    ap_instdata
+SQL
+    ,
     data_source => 'Genome::DataSource::GMSchema',
     id_by => [
         instrument_data_id => { is => 'Text', },
