95be6004a7d07e51a8f5870100ea6f2f666e234b
Thomas Mooney <tmooney@genome.wustl.edu>
2013-11-27 16:11:58 -0600

    Remove extraneous "use" statement--the autoloader will load this.

--- lib/perl/Genome/Model/ClinSeq/Command/SummarizeSvs.pm
+++ lib/perl/Genome/Model/ClinSeq/Command/SummarizeSvs.pm
@@ -6,7 +6,6 @@ use strict;
 use warnings;
 use Genome;
 use Data::Dumper;
-use Genome::Model::ClinSeq;
 use Genome::Model::ClinSeq::Util qw(:all);
 
 class Genome::Model::ClinSeq::Command::SummarizeSvs {
