b77a9c78323da8e86f7f56d05af5233c3ce14feb
Eddie Belter <ebelter@genome.wustl.edu>
2013-11-29 13:28:41 -0600

    Sync: move expunge command into sync lims and genome

--- lib/perl/Genome/Site/TGI/Synchronize/SyncLimsAndGenome.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/SyncLimsAndGenome.pm
@@ -34,13 +34,13 @@ sub execute {
     if (not $rv) {
         die $self->error_message("$@\nFailed to execute update apipe classes!");
     }
+    $self->_report($update->report);
 
-    my $expunge = eval { Genome::Site::TGI::Synchronize::Expunge->execute(report => $update->_report) };
+    my $expunge = $self->expunge;
     unless ($expunge) {
         $self->error_message("Failed to Expunge: $@");
         die $self->error_message;
     }
-    $self->_report($expunge->report);
 
     my $report_string = $self->generate_report;
     print $report_string;
@@ -102,4 +102,44 @@ sub write_report_file {
     return 1;
 }
 
+sub expunge {
+    my $self = shift;
+
+    my $report = $self->_report;
+
+    my $dictionary = Genome::Site::TGI::Synchronize::Classes::Dictionary->get;
+    for my $entity_name (keys %$report){
+        my $lims_class = $dictionary->lims_class_for_entity_name($entity_name);
+        my $class = $lims_class->genome_class_for_create;
+        next unless $class =~ m/Genome::InstrumentData/; #only remove instrument data for now
+        next if $class eq 'Genome::InstrumentData::Imported'; #imported instrument data doesn't come from LIMS, so skip it
+        my @ids = @{$report->{$class}->{missing}} if $report->{$class}->{missing};
+        next if not @ids;
+        printf("DELETING %s %s\n", $class, join(' ', @ids));
+        my @deleted;
+        for my $id (@ids){
+            my $successfully_deleted = $self->_remove_expunged_object($class, $id);
+            push @deleted, $successfully_deleted;
+        }
+        $report->{$class}->{deleted} = \@deleted;
+    }
+
+    $self->_report($report);
+
+    return 1;
+}
+
+sub _remove_expunged_object {
+    my $self = shift;
+    my $class = shift;
+    my $id = shift;
+
+    my $object = $class->get($id);
+
+    $object->delete;
+
+    return $id;
+}
+
 1;
+
