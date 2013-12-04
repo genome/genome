c1d9c5678656778a7a924eec36c899fad091bba9
Adam Coffman <acoffman@genome.wustl.edu>
2013-12-04 16:19:19 -0600

    add factory method for creating profiles from an already existing config file

--- lib/perl/Genome/Config/Profile/Item.pm
+++ lib/perl/Genome/Config/Profile/Item.pm
@@ -51,20 +51,45 @@ sub concretize {
     return if $self->_is_concrete();
 
     my $original_file_path = $self->file_path;
-    my $filename = File::Basename::basename($original_file_path);
+    return $self->_create_allocation_for_file($original_file_path);
+}
+
+sub create_from_file_path {
+    my $class = shift;
+    my %params = @_;
+    my $file = delete $params{file};
+
+    die('Must supply the path to a file as "file"!') unless $file;
+
+    my $profile_item = $class->create(%params);
+    $profile_item->_create_allocation_for_file($file);
+    return $profile_item;
+}
+
+sub _create_allocation_for_file {
+    my $self = shift;
+    my $file_to_store = shift;
 
     my $allocation = Genome::Disk::Allocation->create(
         owner_id            => $self->id,
         disk_group_name     => 'info_apipe_ref',
         allocation_path     => 'analysis_configuration/' . $self->id,
         owner_class_name    => $self->class,
-        kilobytes_requested => $self->_get_allocaton_size(),
+        kilobytes_requested => $self->_get_size_in_kb($file_to_store),
     );
 
+    return $self->_copy_file_to_allocation($file_to_store, $allocation);
+}
+
+sub _copy_file_to_allocation {
+    my $self = shift;
+    my $original_file_path = shift;
+    my $allocation = shift;
+
+    my $filename = File::Basename::basename($original_file_path);
     my $destination_file_path = File::Spec->catdir($allocation->absolute_path, $filename);
     Genome::Sys->copy_file($original_file_path, $destination_file_path);
     $allocation->reallocate();
-
     return $destination_file_path;
 }
 
@@ -82,6 +107,6 @@ sub _get_concrete_file_path {
     return $files[0];
 }
 
-sub _get_allocaton_size { return ((-s shift->file_path)/1024) + 1; }
+sub _get_size_in_kb { return ((-s $_[1])/1024) + 1; }
 
 1;
