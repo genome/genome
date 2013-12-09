bfa13b1fa396936ea8861214076dc1c27b676aca
Adam Coffman <acoffman@genome.wustl.edu>
2013-12-09 12:41:40 -0600

    update analysis project lister

--- lib/perl/Genome/Config/AnalysisProject/Command.pm
+++ lib/perl/Genome/Config/AnalysisProject/Command.pm
@@ -7,7 +7,7 @@ use Genome::Command::Crud;
 Genome::Command::Crud->init_sub_commands(
     target_class => 'Genome::Config::AnalysisProject',
     target_name => 'analysis_project',
-    list => { show => 'id,name,created_by,_configuration_set_id,created_at,updated_at' },
+    list => { show => 'id,name,created_by,status,created_at,updated_at' },
     create => { do_not_init => 1 },
     update => { do_not_init => 1 },
 
