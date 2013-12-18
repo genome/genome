e94065f37b31a9464d1d6dd82ac2ccb10bd1589a
Adam Coffman <acoffman@genome.wustl.edu>
2013-12-18 15:44:23 -0600

    enable analysis project name changes via cli

--- lib/perl/Genome/Config/AnalysisProject/Command.pm
+++ lib/perl/Genome/Config/AnalysisProject/Command.pm
@@ -9,7 +9,7 @@ Genome::Command::Crud->init_sub_commands(
     target_name => 'analysis_project',
     list => { show => 'id,name,created_by,status,created_at,updated_at' },
     create => { do_not_init => 1 },
-    update => { do_not_init => 1 },
+    update => { include_only => ['name'] },
 
 );
 
