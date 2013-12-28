63d07d776db1d8ac41b24f2c977a7e235bc895f0
Malachi Griffith <mgriffit@genome.wustl.edu>
2013-12-27 23:38:45 -0600

    Add Env modules for new variables added to the standalone GMS (in /etc/genome.conf), also add Required.pm and update Env.pm to accommodate these additions.

---
+++ lib/perl/Genome/Env/GENOME_DS_GMSCHEMA_OWNER.pm
@@ -0,0 +1,17 @@
+package Genome::Env::GENOME_DS_GMSCHEMA_OWNER;
+use base 'Genome::Env::Required';
+
+=pod
+
+=head1 NAME
+
+GENOME_DS_GMSCHEMA_OWNER
+
+=head1 DESCRIPTION
+
+The GENOME_DS_GMSCHEMA_OWNER environment variable holds the default
+schema/owner for the tables in the GMSchema data source.
+
+=cut
+
+1;
