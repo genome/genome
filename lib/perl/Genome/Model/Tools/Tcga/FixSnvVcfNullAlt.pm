cc33ce321bcef937414288dd6e237d67fa1f8b52
Feiyu Du <fdu@genome.wustl.edu>
2014-01-11 18:11:38 -0600

    change it back until sample meta into bug fixed

--- lib/perl/Genome/Model/Tools/Tcga/FixSnvVcfNullAlt.pm
+++ lib/perl/Genome/Model/Tools/Tcga/FixSnvVcfNullAlt.pm
@@ -20,22 +20,33 @@ class Genome::Model::Tools::Tcga::FixSnvVcfNullAlt {
 	],
 };
 
-
+#TODO This should be replaced by Genome::File::Vcf::Reader/Writer, but
+#currently a bug in sample metainfo fails to properly parse sample
+#info if sample field contains more than one sample info like:
+###SAMPLE <XXXX>,<xxxxx>,<xxxx>
 sub execute {
     my $self = shift;
     my $input_file  = $self->input_file;
     my $output_file = $self->output_file;
 
-    my $reader = Genome::File::Vcf::Reader->new($input_file);
-    my $header = $reader->header;
-    my $writer = Genome::File::Vcf::Writer->new($output_file, $header);
+    my $in_fh  = Genome::Sys->open_file_for_reading($input_file)  or die "Failed to open $input_file for reading";
+    my $out_fh = Genome::Sys->open_file_for_writing($output_file) or die "Failed to open $output_file for writing";
 
-    while (my $entry = $reader->next) {
-        unless (@{$entry->{alternate_alleles}}) {
-            $entry->{alternate_alleles} = ['N'];
+    while (my $line = $in_fh->getline) {
+        if ($line =~ /^#/) {
+            $out_fh->print($line);
         }
-        $writer->write($entry)
+        else {
+            my @columns = split /\t/, $line;
+            if ($columns[4] eq '.') {
+                $columns[4] = 'N';
+                $line = join "\t", @columns;
+            }
+            $out_fh->print($line);
+        }
     }
+    $in_fh->close;
+    $out_fh->close;
 
     return 1;
 }
