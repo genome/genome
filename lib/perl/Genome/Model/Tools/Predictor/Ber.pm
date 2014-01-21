ef6bc02309fd5ff2cce59eaed4a55996c5314bba
Craig Pohl <cpohl@genome.wustl.edu>
2014-01-21 14:37:14 -0600

    adding Ber test and layout for new pipeline

--- lib/perl/Genome/Model/Tools/Predictor/Ber.pm
+++ lib/perl/Genome/Model/Tools/Predictor/Ber.pm
@@ -15,44 +15,7 @@ use English;
 
 class Genome::Model::Tools::Predictor::Ber {
     is => 'Genome::Model::Tools::Predictor::Base',
-    doc => 'execute Ber',
-	
-	#'parameters' from Base.pm will consist of 'sequence_set_id' 'config' 'phase' and 'dev' in that order, space deliniated
-	#'version' from Base.pm will override 'ber_directory'
-	#'internalhash' has yet to be resolved
-
-    #parameters originally from AmgapBerProtName.pm
-    has_input => [
-					# 'config'          => { 
-							      # is => 'String',
-							      # doc => "YAML file for reading.  This is the same config file used to start hgmi tools.",
-							     # },
-                    # 'ber_directory' => {
-                                  # is => 'String',
-                                  # doc => "base directory to use for BER; use this to specify different releases of BER",
-                                  # is_optional => 1,
-                                  # default => "/gscmnt/ams1102/info/annotation/BER/autoannotate_v2.5",
-                                  # default => "/gscmnt/sata835/info/annotation/BER/autoannotate_v2.5",
-                    # },
-					'internalhash'    => {
-							      is => 'HashRef',
-							      doc => "internal",
-							      is_optional => 1,
-							     },
-					# 'phase'          =>{
-							    # is          => 'Interger',
-							    # doc         => "Phase of protein to dump from Oracle.  Default is phase 5. ",
-							    # default     => 5,
-							    # is_optional => 1,
-							   # },
-                     # 'dev' => {
-                           # is => 'Boolean',
-                           # doc => "use dev databases",
-                           # default => 0,
-                           # is_optional => 1,
-                     # },
-				       ]
-    ],
+    doc => 'execute BER gene predictor',
 };
 
 sub help_brief
@@ -80,237 +43,125 @@ sub requires_chunking {
 
 sub run_predictor {
     my $self = shift;
-	
-	my @param = split(' ', $parameters);
-	
-	my $ber_directory = #get header of $input_fasta_file;
 
-	#figure out what internalhash does
-	#move into read_config?
-	if(-f $self->config)
-    {
-        $self->internalhash(LoadFile($self->config));
-    }
-    else
-    {
-        # blow up...
-        my $file = $self->config;
-        carp "non-existent file $file Berpn.pm\n\n";
-        return 0;
+    $self->status_message("Starting BER");
+    $self->status_message("Creating kegg output directory at " . $self->output_directory);
+
+    if (-d $self->output_directory) {
+        $self->warning_message("Existing output directory found at " . $self->output_directory . ", removing!");
+        my $rv = system("rm -rf " . $self->output_directory);
+        confess "Could not remove " . $self->output_directory unless defined $rv and $rv == 0;
     }
 
-    my $config = $self->internalhash;
-	
-	################################################
-    #Prepare BER
-    ################################################
-    my $cwd        = getcwd();
-    #my $berbasedir = qq{/gscmnt/ams1102/info/annotation/BER/autoannotate_v2.5};
-    #$berbasedir = $self->ber_directory; # let us change the ber directory at will.
-    my $berbasedir = $config->{ber_base_directory};
-    my $configdir  = qq{$berbasedir/data/db/CSV};
-	
-	unless ( -d $configdir ){
+    make_path($self->output_directory);
+    chmod(0775, $self->output_directory);
 
-      croak qq{The directory: '$configdir', does not exist, from  AmgapPrepareBER.pm: $OS_ERROR};
 
-    }
-	
-	unless ( $cwd eq $configdir  ) {
 
-      chdir($configdir)  or croak "Failed to change to '$configdir', from  AmgapBerProtName.pm: $OS_ERROR";
+#Requirements for re-run: Not in any modules
+#-If we rerun BER we usually delete the sqlite db that exists
+#-If we run BER more than one time on same genome and have diff data sets then can’t use same config files. Can’t change the locus tag becuase gene name is the same for those that still exist.
+#-Can overwrite config files with new name list.
 
-    }
-	
-	warn qq{\nPreparing BER config files and directory structure...\n\n};
 
-    my $apber = Genome::Model::Tools::Ber::AmgapPrepareBER->create(
-								   'locus_tag'        => $config->{locus_tag},
-								   'sequence_set_id'  => $self->$param[0],
-								   'phase'            => $self->$param[2],
-								  );
-								  
-	if ($apber)
-      {
-	$apber->execute() or croak "\nCan't run AmgapPrepareBER.pm ... from AmgapBerProtName.pm\n\n";
-
-      } else {
-	croak "Can't set up AmgapPrepareBER.pm ... from AmgapBerProtName.pm\n\n";
-      }
-	  
-	 
-	#Find out if this is still needed
-	
-	################################################
-    #Amgap Dump Protein Biosql
-    ################################################
-
-    my $genomesdir  = qq{$berbasedir/data/genomes};
-    my $locustagdir = qq{$genomesdir/$config->{locus_tag}};
-    my $fastadir    = qq{$locustagdir/fasta};
-    my $berdir      = qq{$locustagdir/ber};
-    my $hmmdir      = qq{$locustagdir/hmm};
-    my $bsubfiledir = qq{$locustagdir/bsubERRfiles};
-	#my $blpqf       = qq{/gscmnt/temp110/info/annotation/ktmp/BER_TEST/hmp/autoannotate/data/panda/AllGroup/AllGroup.niaa};
-    my $blpqf       = qq{$berbasedir/data/panda/AllGroup/AllGroup.niaa};
-    #my $hmmdb       = qq{/gscmnt/temp110/info/annotation/ktmp/BER_TEST/hmp/autoannotate/data/ALL_LIB.20081108.HMM};
-    #my $hmmdb       = qq{/gscmnt/temp110/info/annotation/ktmp/BER_TEST/hmp/autoannotate/data/ALL_LIB.HMM};
-    my $hmmdb       = qq{$berbasedir/data/ALL_LIB.HMM};
-	
-	chdir($genomesdir) or croak  "\nFailed to change directories to '$genomesdir' ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-
-    unless (-d $locustagdir) {
-      mkdir $locustagdir or croak "Failed to create '$locustagdir', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    unless (-d $fastadir) {
-      mkdir $fastadir or croak "Failed to create '$fastadir', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    unless (-d $berdir ) {
-      mkdir $berdir or croak "Failed to create '$berdir', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    unless (-d $hmmdir ) {
-      mkdir $hmmdir or croak "Failed to create '$hmmdir', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    unless (-d $bsubfiledir ) {
-      mkdir $bsubfiledir or croak "Failed to create '$bsubfiledir', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    unless ( -e $blpqf ) {
-	croak "Failed to find '$blpqf', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    unless ( -e $hmmdb ) {
-	croak "Faild to find '$hmmdb', from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-
-     chdir($fastadir) or croak  "\nFailed to change directories to '$genomesdir' ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-
-    warn qq{Dumping protein from BioSQL...\n\n};
-
-    my $adpb = Genome::Model::Tools::Ber::AmgapDumpProteinBiosql->create(
-									 'locus_tag'  => $config->{locus_tag},
-									);
-	
-	# get from dev???
-    if($self->dev) {
-        $adpb->dev(1);
-    }
-
-    if ($adpb) {
-      $adpb->execute() or croak "\nCan't run AmgapDumpProteinBiosql.pm ... from AmgapBerProtName.pm\n\n";
-    } else {
-      croak "Can't set up AmgapDumpProteinBiosql.pm ... from AmgapBerProtName.pm\n\n";
-    }
-
-    ################################################
-    #Ber Run Blastp hmmpfam
-    ################################################
+    $self->_create_config_files();
+    $self->_setup_dirs();
+    $self->_prep_input_files();
+    $self->_run_anno_sqlite();
+    $self->_finish();
     
-	#Checks current directory and changes to $fastadir if necessary
-	$cwd = getcwd();
-    unless ( $cwd eq $fastadir ) {
-      chdir($fastadir)  or croak "Failed to change to '$fastadir', from  AmgapBerProtName.pm: $OS_ERROR";
-    }
 
-    warn qq{Running blastp and hmmpfam...\n\n};
-	
-    my $brbh = Genome::Model::Tools::Ber::BerRunBlastphmmpfam->create(
-								      'locus_tag'       => $config->{locus_tag},
-								      'proteinnamefile' => $adpb->proteinnamefile,
-								      'fastadirpath'    => $fastadir,
-								      'berdirpath'      => $berdir,
-								      'hmmdirpath'      => $hmmdir,
-								      'bsubfiledirpath' => $bsubfiledir,
-								      'blpqueryfile'    => $blpqf,
-								      'hmmdatabase'     => $hmmdb,
-								     );
-    if ($brbh) {
-      $brbh->execute() or croak "\nCan't run BerRunBlastphmmpfam.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    } else {
-      croak "Can't set up BerRunBlastphmmpfam.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-      }
-    ################################################
-    #Ber Run Btap Htab
-    ################################################
-	
-	#Checks current directory and changes to $srcdir if necessary
-    $cwd = getcwd();
-    #my $srcdir = qq{/gscmnt/temp110/info/annotation/ktmp/BER_TEST/hmp/autoannotate/src};
-    my $srcdir = qq{$berbasedir/src};
-    unless ($cwd eq $srcdir) {
-	chdir($srcdir) or die "Failed to change to '$srcdir'...  from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-
-    warn qq{Running Btab and Htab...\n\n};
-
-    my $brbtht = Genome::Model::Tools::Ber::BerRunBtabhmmtab->create(
-	                                                             'locus_tag'       => $config->{locus_tag},
-								     'bsubfiledirpath' => $bsubfiledir,
-								     'fastadirpath'    => $fastadir,
-								     'berdirpath'      => $berdir,
-								     'hmmdirpath'      => $hmmdir,
-								     'srcdirpath'      => $srcdir,
-								    );
-
-    if ($brbtht) {
-	$brbtht->execute() or croak "\nCan't run BerRunBtabhmmtab.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    } else {
-	croak "Can't set up BerRunBtabhmmtab.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-
-    $cwd = getcwd();
-    unless ($cwd eq $srcdir) {
-	chdir($srcdir) or die "Failed to change to '$srcdir'...  from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-    warn qq{Running anno-sqlite.bash \n\n};
-
-    #my $outdir = qq{/gscmnt/temp110/info/annotation/ktmp/BER_TEST/hmp/autoannotate/out};
-    my $outdir = qq{$berbasedir/out};
-	
-    my $bras =  Genome::Model::Tools::Ber::BerRunAnnoSqlite->create(
-                                                                    'locus_tag'       => $config->{locus_tag},
-								    'srcdirpath'      => $srcdir,
-								    'outdirpath'      => $outdir,
-                                                                   );
-	
-    if ($bras) {
-	$bras->execute() or croak "\nCan't run BerRunAnnoSqlite.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    } else {
-	croak "Can't set up BerRunAnnoSqlite.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-	
-    $cwd = getcwd();
-    unless ($cwd eq $outdir) {
-	chdir($outdir) or die "Failed to change to '$outdir'...  from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-
-    warn qq{Running Finish...\n\n};
-
-	my $brfin = Genome::Model::Tools::Ber::BerRunFinish->create(
-			'locus_tag'       => $config->{locus_tag},
-			'outdirpath'      => $outdir,
-			'sqlitedatafile'  => $bras->sqlitedatafile,
-			'sqliteoutfile'   => $bras->sqliteoutfile,
-			'acedb_version'   => $config->{acedb_version},
-			'amgap_path'      => $config->{path},
-			'pipe_version'    => $config->{pipe_version},
-			'project_type'    => $config->{project_type},
-			'org_dirname'     => $config->{org_dirname},
-			'assembly_name'   => $config->{assembly_name},
-			'assembly_version'   => $config->{assembly_version},
-			'sequence_set_id' => $self->sequence_set_id,
-			'locustagdir'	  => $locustagdir,
-			'berbasedir'	  => $berbasedir,
-			);
-
-    if ($brfin) {
-	$brfin->execute() or croak "\nCan't run BerRunFinish.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    } else {
-	croak "Can't set up BerRunFinish.pm ... from AmgapBerProtName.pm: $OS_ERROR\n\n";
-    }
-
-    warn qq{\n\nThe Amgap BER Product Naming has finished, Thanks!\n\n};
 
     return 1;
+}
 
+
+sub _create_config_files {
+
+Genome/Model/Tools/Ber/AmgapPrepareBER.pm
+
+Manually running details:
+> perl ~/git/xu_script/BER_config_from_txtfile.pl -locus TELCIRDFT Final_BER_Naming.05-15-13.fof
+
+This command creates 4 files
+TELCIRDFT_asm_feature
+TELCIRDFT_asmbl_data
+TELCIRDFT_ident2
+TELCIRDFT_stan
+
+run the command todos on the 4 files
+> todos TELCIRDFT_*
+
+}
+
+sub _setup_dirs {
+Genome/Model/Tools/Ber/AmgapBerProtName.pm
+
+Manually running details:
+location is (this is taken from the MGAP pipeline config file that’s made manually )
+/gscmnt/gc9002/info/annotation/BER/autoannotate_v2.5/data/genomes
+
+mkdir TELCIRDFT
+cd TELCIRDFT
+mkdir fasta hmm ber
+ln -s /gscmnt/gc6125/info/annotation/worm_analysis/T_circumcincta/T_circumcincta.14.0.ec.cg.pg/Version_1.0/PAP/Version_1.0/BER/*pep .
+
+cd fasta
+dbshatter ../*pep
+
+cd ../../../db/CSV
+
+cp /gscmnt/gc6125/info/annotation/worm_analysis/T_circumcincta/T_circumcincta.14.0.ec.cg.pg/Version_1.0/PAP/Version_1.0/BER/Version_1.0/TELCIRDFT_* .
+
+cd /gscmnt/gc9002/info/annotation/BER/autoannotate_v2.5/data/genomes/TELCIRDFT/fasta
+
+}
+
+sub _prep_input_files {
+
+3. Prep input files for BER
+
+   i) Running blastp & hmmpfam
+       Genome/Model/Tools/Ber/BerRunBlastphmmpfam.pm
+
+       Manully running details:
+perl ~/git/xu_script/BER_config_from_txtfile.pl -locus TELCIRDFT Final_BER_Naming.05-15-13.fof
+
+}
+
+sub _run_anno_sqlite {
+4. Run anno-sqlite.bash
+   
+   Genome/Model/Tools/Ber/BerRunAnnoSqlite.pm
+   
+   Manually running details:
+  /gscmnt/gc9002/info/annotation/BER/autoannotate_v2.5/src
+  > bsub -o TELCIRDFT.out -e TELCIRDFT.err -R 'select[type=LINUX64]' ./anno-sqlite.bash  
+  TELCIRDFT 130521 gram-
+
+  This will take X hours to run, depends on how many genes to process. For TELCIRDFT  
+  Processed 25,567 genes
+  Started 08:01am
+  Ended
+
+  To check that the process is running ok
+  1. bjobs | grep gram
+  2. wc /gscmnt/gc9002/info/annotation/BER/autoannotate_v2.5/out/sqlite-locusID-date.dat
+
+Number of sequences in the pep file should be equal to the number of lines in the sqlite dat file.Should do this check in PAP to make sure all is complete.
+
+}
+
+sub _finish {
+Genome/Model/Tools/Ber/BerRunFinish.pm
+    
+   This above module has a lot of stuff that’s very specific to MGAP pipeline. To me, this may not be of much use except the ace file generation part.
+
+   Additional info:
+   In house script to do the above:
+   ~kpepin/scripts/parse_dat2ace.pl sqlite-locusID-date.dat > sqlite-locusID-date.dat.ace
+
+   Parse into acedb and redump new .tbl file to get naming issues that need to be resolved
 
 }
 
