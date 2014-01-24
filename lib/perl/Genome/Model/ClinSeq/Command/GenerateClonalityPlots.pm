34b1a14c641c5da0c41a99d96612819654ee0d33
Obi Griffith <ogriffit@genome.wustl.edu>
2014-01-23 22:36:00 -0600

    added logic to skip clonality plot generation when no variants present

--- lib/perl/Genome/Model/ClinSeq/Command/GenerateClonalityPlots.pm
+++ lib/perl/Genome/Model/ClinSeq/Command/GenerateClonalityPlots.pm
@@ -261,12 +261,16 @@ sub execute {
     my $output_image_file1a = "$output_dir"."$common_name".".clonality.pdf";
     my $r_script_file = "$output_dir"."clonality.R";
     my $uc_common_name = uc($common_name);
-    my $clonality_cmd1a = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file1a, r_script_output_file=>$r_script_file, varscan_file=>$varscan_file, analysis_type=>'wgs', sample_id=>$uc_common_name);
-    $clonality_cmd1a->execute();
-
-    my $output_image_file1b = "$output_dir"."$common_name".".clonality.cn2.pdf";
-    my $clonality_cmd1b = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file1b, r_script_output_file=>$r_script_file, varscan_file=>$varscan_file, analysis_type=>'wgs', sample_id=>$uc_common_name, plot_only_cn2=>1);
-    $clonality_cmd1b->execute();
+    if (-s $varscan_file){
+      my $clonality_cmd1a = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file1a, r_script_output_file=>$r_script_file, varscan_file=>$varscan_file, analysis_type=>'wgs', sample_id=>$uc_common_name);
+      $clonality_cmd1a->execute();
+    
+      my $output_image_file1b = "$output_dir"."$common_name".".clonality.cn2.pdf";
+      my $clonality_cmd1b = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file1b, r_script_output_file=>$r_script_file, varscan_file=>$varscan_file, analysis_type=>'wgs', sample_id=>$uc_common_name, plot_only_cn2=>1);
+      $clonality_cmd1b->execute();
+    }else{
+      $self->warning_message("Empty snv file: $varscan_file");
+    }
 
     #Step 7-B. With clusters - not working - deactivate until the new version of clonality is available
     #my $clustered_data_output_file = $output_dir . $common_name . ".clustered.data.tsv";
@@ -292,14 +296,16 @@ sub execute {
     }
     close(SNV);
     close(SNV2);
-    my $output_image_file3a = "$output_dir"."$common_name".".clonality.filtered_snvs.pdf";
-    my $clonality_cmd3a = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file3a, r_script_output_file=>$r_script_file, varscan_file=>$filtered_file, analysis_type=>'wgs', sample_id=>$uc_common_name);
-    $clonality_cmd3a->execute();
-
-    my $output_image_file3b = "$output_dir"."$common_name".".clonality.filtered_snvs.cn2.pdf";
-    my $clonality_cmd3b = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file3b, r_script_output_file=>$r_script_file, varscan_file=>$filtered_file, analysis_type=>'wgs', sample_id=>$uc_common_name, plot_only_cn2=>1);
-    $clonality_cmd3b->execute();
-
+    if (-s $filtered_file){
+      my $output_image_file3a = "$output_dir"."$common_name".".clonality.filtered_snvs.pdf";
+      my $clonality_cmd3a = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file3a, r_script_output_file=>$r_script_file, varscan_file=>$filtered_file, analysis_type=>'wgs', sample_id=>$uc_common_name);
+      $clonality_cmd3a->execute();
+      my $output_image_file3b = "$output_dir"."$common_name".".clonality.filtered_snvs.cn2.pdf";
+      my $clonality_cmd3b = Genome::Model::Tools::Validation::ClonalityPlot->create(cnvhmm_file=>$cnvhmm_file, output_image=>$output_image_file3b, r_script_output_file=>$r_script_file, varscan_file=>$filtered_file, analysis_type=>'wgs', sample_id=>$uc_common_name, plot_only_cn2=>1);
+      $clonality_cmd3b->execute();
+    }else{
+      $self->warning_message("Empty filtered snv file: $filtered_file");
+    }
     #Keep the files that were needed to run the cna-seg and clonality plot steps so that someone can rerun with different parameters 
 
     #Define the output parameter for the cnvhmm so that it can be fed into downstream steps in a workflow
