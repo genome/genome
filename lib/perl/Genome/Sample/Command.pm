f3ea92e8f3d7978b4a27954004d3a174db5ade80
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2014-02-10 08:07:34 -0600

    allow direct sample creation

--- lib/perl/Genome/Sample/Command.pm
+++ lib/perl/Genome/Sample/Command.pm
@@ -7,7 +7,6 @@ use Genome::Command::Crud;
 Genome::Command::Crud->init_sub_commands(
     target_class => 'Genome::Sample',
     target_name => 'sample',
-    create => { do_not_init => 1, }, # use importers
     list => { show => 'id,name,species_name,patient_common_name,common_name,tissue_label,tissue_desc,extraction_type,extraction_label,extraction_desc' },
     update => { only_if_null => 1, },
     delete => { do_not_init => 1, },
