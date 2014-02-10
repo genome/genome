b6443bbf56680cf99fe39b730a6fc79553524fe1
Jim Weible <jweible@genome.wustl.edu>
2014-02-10 14:14:57 -0600

    add lister for analysis menu items

---
+++ lib/perl/Genome/Config/AnalysisMenu.pm
@@ -0,0 +1,13 @@
+package Genome::Config::AnalysisMenu;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Config::AnalysisMenu {
+    is => 'Command::Tree',
+    doc => 'commands that deal with analysis menus',
+};
+
+1;
---
+++ lib/perl/Genome/Config/AnalysisMenu/Command.pm
@@ -0,0 +1,13 @@
+package Genome::Config::AnalysisMenu::Command;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Config::AnalysisMenu::Command {
+    is => 'Command::Tree',
+    doc => 'commands that act on analysis menu items',
+};
+
+1;
