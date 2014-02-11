67773df571cb472e34215691b62b0aff0665f528
Nathaniel Nutter <nnutter@genome.wustl.edu>
2014-02-10 18:06:56 -0600

    extract color_screen

--- lib/perl/Genome/Logger.pm
+++ lib/perl/Genome/Logger.pm
@@ -13,7 +13,21 @@ sub logger {
     assert_class_method(shift);
 
     my $logger = Log::Dispatch->new(@_);
+    $logger->add(color_screen());
+    return $logger;
+}
 
+sub assert_class_method_error { 'Must be called as class method' }
+sub assert_class_method {
+    my $class = shift;
+
+    # to ensure memoize works we are strict about this
+    unless ($class && $class eq __PACKAGE__) {
+        croak assert_class_method_error();
+    }
+}
+
+sub color_screen {
     my $screen = Log::Dispatch::Screen::Color->new(
         name => 'screen',
         min_level => 'info',
@@ -42,19 +56,6 @@ sub logger {
             },
         },
     );
-    $logger->add($screen);
-
-    return $logger;
-}
-
-sub assert_class_method_error { 'Must be called as class method' }
-sub assert_class_method {
-    my $class = shift;
-
-    # to ensure memoize works we are strict about this
-    unless ($class && $class eq __PACKAGE__) {
-        croak assert_class_method_error();
-    }
 }
 
 1;
