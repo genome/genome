174c523bfb52a7dfae91c13a07c4a83635270ed0
Nathaniel Nutter <nnutter@genome.wustl.edu>
2014-02-10 18:51:58 -0600

    Revert "switch to Module::Runtime"
    This reverts commit a163bf85a690bbd917e240c6e822818b1bfb601e because it
    breaks /gsc/bin/perl.

--- lib/perl/Genome/Site.pm
+++ lib/perl/Genome/Site.pm
@@ -7,13 +7,14 @@ use Carp qw(croak);
 use File::Spec qw();
 use Sys::Hostname qw(hostname);
 use UR::Util qw();
-use Module::Runtime qw(require_module);
 
 our $VERSION = $Genome::VERSION;
 
 sub import {
     if (my $config = $ENV{GENOME_CONFIG}) {
-        require_module($config);
+        # call the specified configuration module;
+        eval "use $config";
+        die $@ if $@;
     }
     else {
         load_host_config();
