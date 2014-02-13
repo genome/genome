7beb3f08a4694e785df270f8c8d5d5baecc1d042
Gabriel Sanderson <gsanders@genome.wustl.edu>
2014-02-13 15:29:53 -0600

    Add a filenames option to tack on the original file basename to the output

--- lib/perl/Genome/Disk/Command/Allocation/PathsToAllocations.pm
+++ lib/perl/Genome/Disk/Command/Allocation/PathsToAllocations.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Genome;
+use File::Basename qw(basename);
 
 class Genome::Disk::Command::Allocation::PathsToAllocations {
     is => 'Command::V2',
@@ -14,24 +15,37 @@ class Genome::Disk::Command::Allocation::PathsToAllocations {
             shell_args_position => 1,
             doc => 'Absolute path for which an allocation needs to be found',
         },
+        filenames => {
+            is => 'Boolean',
+            default => 0,
+            doc => 'Set to true if you would like the output paths to contain the original file (if provided) rather than just the directory'
+        },
     ],
 };
 
 sub execute {
     my $self = shift;
 
-    my @allocations;
-    for my $path ($self->absolute_paths) {
-        my $allocation = Genome::Disk::Allocation->get_allocation_for_path($path);
+    my %allocations;
+    for my $given_path ($self->absolute_paths) {
+        my $allocation = Genome::Disk::Allocation->get_allocation_for_path($given_path);
         if ($allocation) {
-            push @allocations, $allocation;
+            $allocations{$given_path} = $allocation;
         } else {
-            $self->warning_message("Found no allocation for path $path");
+            $self->warning_message("Found no allocation for path $given_path");
         }
     }
 
-    for my $allocation (@allocations) {
-        print join("\t", $allocation->id, $allocation->absolute_path) . "\n";
+    for my $given_path (keys %allocations) {
+        my $allocation = $allocations{$given_path};
+        my $allocation_path = $allocation->absolute_path;
+        if ($self->filenames) {
+            my $file = basename($given_path);
+            if (defined $file) {
+                $allocation_path = File::Spec->join($allocation_path, $file);
+            }
+        }
+        print join("\t", $allocation->id, $allocation_path) . "\n";
     }
     return 1;
 }
