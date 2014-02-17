6d0cca02152672cfbb920608963c38ff4e4eff97
Eddie Belter <ebelter@genome.wustl.edu>
2014-02-17 14:00:19 -0600

    GM: factory to create a genotpye file reader

---
+++ lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/ReaderFactory.pm
@@ -0,0 +1,69 @@
+package Genome::Model::GenotypeMicroarray::GenotypeFile::ReaderFactory;
+
+use strict;
+use warnings;
+
+use Genome;
+
+use Genome::Model::GenotypeMicroarray::GenotypeFile::ReadTsv;
+use Genome::Model::GenotypeMicroarray::GenotypeFile::ReadTsvAndAnnotate;
+
+class Genome::Model::GenotypeMicroarray::GenotypeFile::ReaderFactory { 
+    is => 'UR::Singleton',
+};
+
+sub build_reader {
+    my ($class, $for, $variation_list_build) = @_;
+
+    Carp::confess('Nothing given to build reader!') if not $for;
+
+    if ( $for->isa('Genome::InstrumentData') ) {
+        Carp::confess('No variation list build given to build reader!') if not $variation_list_build;
+        return $class->_build_reader_for_instrument_data($for, $variation_list_build);
+    }
+    elsif ( $for->isa('Genome::Model::Build::GenotypeMicroarray') ) {
+        return $class->_build_reader_for_build($for);
+    }
+    else {
+        $class->error_message('Do not know how to build genotype file reader for '.$for->class);
+        return;
+    }
+
+}
+
+sub _build_reader_for_instrument_data {
+    my ($class, $instrument_data, $variation_list_build) = @_;
+
+    my $genotype_file = eval{ $instrument_data->attributes(attribute_label => 'genotype_file')->attribute_value; };
+    if ( not $genotype_file or not -s $genotype_file ) {
+        $class->error_message('No genotype file for instrument data! '.$instrument_data->id);
+        return;
+    }
+
+    my $snp_id_mapping = Genome::InstrumentData::Microarray->get_snpid_hash_for_variant_list(
+        $instrument_data, $variation_list_build
+    );
+
+    my $reader = Genome::Model::GenotypeMicroarray::GenotypeFile::ReadTsvAndAnnotate->create(
+        input => $genotype_file,
+        variation_list_build => $variation_list_build,
+        snp_id_mapping => $snp_id_mapping,
+    );
+
+    return $reader;
+}
+
+sub _build_reader_for_build {
+    my ($class, $build) = @_;
+
+    # TODO add vcf!
+    my $genotype_file = $build->original_genotype_file_path;
+    my $reader = Genome::Model::GenotypeMicroarray::GenotypeFile::ReadTsv->create(
+        input => $genotype_file,
+    );
+
+    return $reader;
+}
+
+1;
+
