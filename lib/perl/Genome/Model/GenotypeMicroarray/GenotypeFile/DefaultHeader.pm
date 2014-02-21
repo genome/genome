94c1d3a14f8baf764faea758e2fe8f6679622544
Eddie Belter <ebelter@genome.wustl.edu>
2014-02-21 15:44:26 -0600

    GM: additions to genotype file default header

--- lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/DefaultHeader.pm
+++ lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/DefaultHeader.pm
@@ -11,17 +11,53 @@ class Genome::Model::GenotypeMicroarray::GenotypeFile::DefaultHeader {
     is => 'UR::Singleton',
 };
 
+our $supported_info_fields = [
+    # SORTED BY ID!
+    {
+        name => 'allele_frequency',
+        id => 'AF',
+        header => ',Number=1,Type=Float,Description="Allele frequency for each ALT allele in the same order as listed">',
+    },
+    {
+        name => 'cnv_confidence',
+        id => 'CC',
+        header => ',Number=1,Type=Float,Description="CNV Confidence">',
+    },
+    {
+        name => 'cnv_value',
+        id => 'CV',
+        header => ',Number=1,Type=Float,Description="CNV Value">',
+    },
+    {
+        name => 'log_r_ratio',
+        id => 'LR',
+        header => ',Number=1,Type=Float,Description="Log R Ratio">',
+    },
+    {
+        name => 'gc_score',
+        id => 'GC',
+        header => ',Number=1,Type=Float,Description="GC Score">',
+    },
+    {
+        name => 'alleles', # origninal_genotype
+        id => 'OG',
+        header => ',Number=1,Type=Integer,Description="Original genotype calls">', 
+    },
+];
+
+my $header_lines;
 sub header_lines {
-    no warnings;
-    return [
+
+    return $header_lines if $header_lines;
+
+    $header_lines = [
         '##fileformat=VCFv4.1',
-        '##INFO=<ID=AF,Number=1,Type=Float,Description="Allele frequency for each ALT allele in the same order as listed">',
-        '##INFO=<ID=CC,Number=1,Type=Float,Description="CNV Confidence">',
-        '##INFO=<ID=CV,Number=1,Type=Float,Description="CNV Value">',
-        '##INFO=<ID=GC,Number=1,Type=Float,Description="GC Score">',
-        '##INFO=<ID=LR,Number=1,Type=Float,Description="Log R Ratio">',
-        '##INFO=<ID=OG,Number=1,Type=Integer,Description="Original genotype calls">',
     ];
+    for my $field ( @$supported_info_fields ) {
+        push @$header_lines, '##INFO=<ID='.$field->{id}.$field->{header};
+    }
+
+    return $header_lines;
 }
 
 our $header;
@@ -29,6 +65,20 @@ sub header {
     return $header if $header;
     $header = Genome::File::Vcf::Header->create(lines => header_lines());
     return $header;
+}
+
+our $info_order;
+sub info_order { 
+    return $info_order if $info_order;
+    $info_order = [ map { $_->{id} } sort { $a->{id} cmp $b->{id} } @$supported_info_fields ];
+    return $info_order;
+}
+
+our $info_names_and_ids;
+sub info_names_and_ids { 
+    return $info_names_and_ids if $info_names_and_ids;
+    $info_names_and_ids = { map { $_->{name} => $_->{id} } @$supported_info_fields };
+    return $info_names_and_ids;
 }
 
 1;
--- lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/DefaultHeader.t
+++ lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/DefaultHeader.t
@@ -9,6 +9,9 @@ use Test::More;
 
 use_ok('Genome::Model::GenotypeMicroarray::GenotypeFile::DefaultHeader') or die;
 
+is(ref Genome::Model::GenotypeMicroarray::GenotypeFile::DefaultHeader->header_lines, 'ARRAY', 'header_lines');
 isa_ok(Genome::Model::GenotypeMicroarray::GenotypeFile::DefaultHeader->header, 'Genome::File::Vcf::Header');
+is(ref Genome::Model::GenotypeMicroarray::GenotypeFile::DefaultHeader->info_order, 'ARRAY', 'info_order');
+is(ref Genome::Model::GenotypeMicroarray::GenotypeFile::DefaultHeader->info_names_and_ids, 'HASH', 'info_names_and_ids');
 
 done_testing();
