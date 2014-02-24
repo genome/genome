c1b641194e39806199258ba950211407d1a8beae
Eddie Belter <ebelter@genome.wustl.edu>
2014-02-24 12:56:09 -0600

    GM: rm unneeded use from writer factory

--- lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/WriterFactory.pm
+++ lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/WriterFactory.pm
@@ -5,8 +5,6 @@ use warnings;
 
 use Genome;
 
-use Genome::Utility::IO::SeparatedValueWriter;
-
 class Genome::Model::GenotypeMicroarray::GenotypeFile::WriterFactory { 
     is => 'UR::Singleton',
 };
