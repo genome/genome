152058463e2521d6a5b75da532f811a1e83bc3a5
Eddie Belter <ebelter@genome.wustl.edu>
2014-03-04 15:21:21 -0600

    GM: updates to vcf helper and test

--- lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/VcfHelper.pm
+++ lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/VcfHelper.pm
@@ -66,7 +66,9 @@ sub format_key_idx {
 }
 
 sub header {
-    my ($self, @sample_names) = @_;
+    my ($self, $sample_name) = @_;
+
+    Carp::confess('No sample name given to get vcf header!') if not $sample_name;
 
     my @header_lines = (
         '##fileformat=VCFv4.1',
@@ -76,87 +78,10 @@ sub header {
     }
     push @header_lines, '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">';
     push @header_lines, '##FORMAT=<ID=AL,Number=1,Type=String,Description="Alleles">';
-    push @header_lines, '#'.join("\t", Genome::File::Vcf::Header->column_headers, @sample_names);
+    push @header_lines, '#'.join("\t", Genome::File::Vcf::Header->column_headers, $sample_name);
 
     my $header = Genome::File::Vcf::Header->create(lines => \@header_lines);
     return $header;
-}
-
-sub convert_genotype_to_vcf_entry {
-    my ($self, $genotype) = @_;
-
-    $genotype->{identifiers} = [ $genotype->{id} ];
-    $genotype->{chrom} = $genotype->{chromosome};
-    $genotype->{reference_allele} = $genotype->{reference};
-    $genotype->{alternate_alleles} = $self->alts_for_genotype($genotype);
-    $genotype->{quality} = '.';
-    $genotype->{_filter} = [];
-    $genotype->{info_fields} = $self->info_hash_for_genotype($genotype),
-    $genotype->{_format} = $self->formats;
-    $genotype->{_format_key_to_idx} = $self->format_key_idx;
-    $genotype->{_sample_data} = $self->sample_data_for_genotype($genotype);
-
-    return bless($genotype, 'Genome::File::Vcf::Entry');
-}
-
-sub alts_for_genotype {
-    my ($self, $genotype) = @_;
-
-    # alleles are dashes
-    return '.' if $genotype->{allele1} eq '-' and $genotype->{allele2} eq '-';
-
-    # see which match the ref
-    my @alts;
-    for my $allele_key ( map{ 'allele'.$_ } (1..2) ) {
-        next if $genotype->{$allele_key} eq '-';
-        push @alts, $genotype->{$allele_key} if $genotype->{$allele_key} ne $genotype->{reference_allele};
-    }
-
-    # both match!
-    return [] if not @alts;
-    # retrun alts
-    return [ sort { $a cmp $b } @alts ];
-}
-
-sub sample_data_for_genotype {
-    my ($self, $genotype) = @_;
-
-    my $allele1_matches_ref = $genotype->{allele1} eq $genotype->{reference};
-    my $allele2_matches_ref = $genotype->{allele2} eq $genotype->{reference};
-
-    my $gt;
-    if ( $genotype->{allele1} eq '-' and $genotype->{allele2} eq '-' ) {
-        $gt = './.';
-    }
-    elsif ( $allele1_matches_ref and $allele2_matches_ref ) { # ref and alleles match
-        $gt = '0/0'; # homozygous ref
-    }
-    elsif ( $allele1_matches_ref or $allele2_matches_ref ) { # ref matches one allele, but alleles are not the same
-        $gt = '0/1'; # heterozygous ref
-    }
-    elsif ( $genotype->{allele1} eq $genotype->{allele2} ) { # alleles match, but not the ref
-        $gt = '1/1'; # homozygous alt
-    }
-    else { # not $allele1_matches_ref and not $allele2_matches_ref
-        $gt = '1/2';
-    }
-
-    return [ [ $gt, $genotype->{alleles} ] ];
-
-}
-
-sub info_hash_for_genotype {
-    my ($self, $genotype) = @_;
-
-    my (%info_hash, @order);
-    for my $info_field ( @{$self->supported_info_fields} ) { 
-        my $value = $genotype->{ $info_field->{name} };
-        next if not defined $value or $value eq 'NA'; # FIXME convert undef/NA to . ??
-        $info_hash{ $info_field->{id} } = $value;
-        push @order, $info_field->{id};
-    }
-
-    return { hash => \%info_hash, order => \@order, };
 }
 
 1;
--- lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/VcfHelper.t
+++ lib/perl/Genome/Model/GenotypeMicroarray/GenotypeFile/VcfHelper.t
@@ -9,8 +9,8 @@ use Test::More;
 
 use_ok('Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper') or die;
 
-isa_ok(Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper->header_for_vcf, 'Genome::File::Vcf::Header');#, 'header_for_vcf');
-isa_ok(Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper->header_for_vcf('__TEST_SAMPLE__'), 'Genome::File::Vcf::Header');#, 'header_for_vcf');
+ok(!eval{Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper->header;}, 'Genome::File::Vcf::Header');#, 'header_for_vcf');
+isa_ok(Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper->header('__TEST_SAMPLE__'), 'Genome::File::Vcf::Header');#, 'header_for_vcf');
 is(ref Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper->info_order, 'ARRAY', 'info_order');
 is(ref Genome::Model::GenotypeMicroarray::GenotypeFile::VcfHelper->info_names_and_ids, 'HASH', 'info_names_and_ids');
 
