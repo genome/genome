ca82a377a4aa2e768071a55b6a39ed501c757a09
Susanna Siebert <ssiebert@genome.wustl.edu>
2014-03-07 10:31:17 -0600

    fixed formatting

--- lib/perl/Genome/Model/Tools/EpitopePrediction/GenerateVariantSeq.pm
+++ lib/perl/Genome/Model/Tools/EpitopePrediction/GenerateVariantSeq.pm
@@ -27,7 +27,6 @@ class Genome::Model::Tools::EpitopePrediction::GenerateVariantSeq {
             # the current code only works on missense. Will need furthur development for other trv_types.
             default_value => $DEFAULT_TRV_TYPE,
         },
-        
     ],
 };
 
@@ -44,74 +43,66 @@ sub execute {
 
     my $input_fh = Genome::Sys->open_file_for_reading($self->input_file);
     my $output_fh = Genome::Sys->open_file_for_writing($self->output_file);
-    
-    while (my $line = $input_fh->getline) {
-		chomp $line;
-		$line =~ s/[*]$//g;
-		my @prot_arr =  split(/\t/, $line);
-		if ( $prot_arr[15] =~ /^p.([A-Z])(\d+)([A-Z])/ && $prot_arr[13] eq $self->trv_type)
-		{
-			open(OUT, '>>', $self->output_file) or die $!;
-			my $wt_aa = $1;
-			my $position = ($2 - 1);
-			my $mt_aa = $3;
-			my $wt_seq = $prot_arr[21];
-			my @arr_wt_seq = split('',$wt_seq);
 
-    		if ($1 ne $arr_wt_seq[$position])
-    		{
-    		next;
-    		#TO DO :print OUT $prot_arr[0]."\t".$prot_arr[1]."\t".$prot_arr[2]."\t".$prot_arr[6]."\t".$1."\t".$2."\t".$3."\t".$prot_arr[11]."\t".$arr_wt_seq[$position]."\n";
-    		}
-   
-   		 	if ($1 eq $arr_wt_seq[$position])
-    		{
-    		 my @mt_arr;
-    	 	 my @wt_arr;
-         		if ($position < 8)
-        		{
-           			@wt_arr = @arr_wt_seq[ 0 ... 16];
-           			$arr_wt_seq[$position]=$mt_aa;
-           			@mt_arr = @arr_wt_seq[ 0 ... 16];
-		     		print OUT ">WT.".$prot_arr[6].".".$prot_arr[15]."\n";
-           			print OUT ( join "", @wt_arr);
-           			print OUT "\n";
-           			print OUT ">MT.".$prot_arr[6].".".$prot_arr[15]."\n";
-           			print OUT ( join "", @mt_arr);
-           			print OUT "\n";
-        		}  
-        		elsif ($position > ($#arr_wt_seq -8))
-        		{	
-           			@wt_arr = @arr_wt_seq[ $#arr_wt_seq -17 ... $#arr_wt_seq];
-           			$arr_wt_seq[$position]=$mt_aa;
-           			@mt_arr = @arr_wt_seq[ $#arr_wt_seq -17 ... $#arr_wt_seq];
-           			print OUT ">WT.".$prot_arr[6].".".$prot_arr[15]."\n";
-           			print OUT ( join "", @wt_arr);
-           			print OUT "\n";
-           			print OUT ">MT.".$prot_arr[6].".".$prot_arr[15]."\n";
-           			print OUT ( join "", @mt_arr);
-           			print OUT "\n";
-        		}	
-       		 	elsif (($position >= 8) && ($position  <= ($#arr_wt_seq -8)))
-        		{
-           			@wt_arr = @arr_wt_seq[ $position-8 ... $position+8];
-           			$arr_wt_seq[$position]=$mt_aa;
-           			@mt_arr = @arr_wt_seq[ $position-8 ... $position+8];
-           			print OUT ">WT.".$prot_arr[6].".".$prot_arr[15]."\n";
-           			print OUT ( join "", @wt_arr);
-           			print OUT "\n";
-           			print OUT ">MT.".$prot_arr[6].".".$prot_arr[15]."\n";
-           			print OUT ( join "", @mt_arr);
-           			print OUT "\n";
-       				}
-        		else 
-        		{
-        		print OUT "NULL"."\t".$position."\n";
-        		}
-        		OUT->close;
-        		
-    }
-}  
+    while (my $line = $input_fh->getline) {
+        chomp $line;
+        $line =~ s/[*]$//g;
+        my @prot_arr =  split(/\t/, $line);
+        if ( $prot_arr[15] =~ /^p.([A-Z])(\d+)([A-Z])/ && $prot_arr[13] eq $self->trv_type) {
+            open(OUT, '>>', $self->output_file) or die $!;
+            my $wt_aa = $1;
+            my $position = ($2 - 1);
+            my $mt_aa = $3;
+            my $wt_seq = $prot_arr[21];
+            my @arr_wt_seq = split('',$wt_seq);
+
+            if ($1 ne $arr_wt_seq[$position]) {
+                next;
+                #TO DO :print OUT $prot_arr[0]."\t".$prot_arr[1]."\t".$prot_arr[2]."\t".$prot_arr[6]."\t".$1."\t".$2."\t".$3."\t".$prot_arr[11]."\t".$arr_wt_seq[$position]."\n";
+            }
+
+            if ($1 eq $arr_wt_seq[$position]) {
+                my @mt_arr;
+                my @wt_arr;
+                if ($position < 8) {
+                    @wt_arr = @arr_wt_seq[ 0 ... 16];
+                    $arr_wt_seq[$position]=$mt_aa;
+                    @mt_arr = @arr_wt_seq[ 0 ... 16];
+                    print OUT ">WT.".$prot_arr[6].".".$prot_arr[15]."\n";
+                    print OUT ( join "", @wt_arr);
+                    print OUT "\n";
+                    print OUT ">MT.".$prot_arr[6].".".$prot_arr[15]."\n";
+                    print OUT ( join "", @mt_arr);
+                    print OUT "\n";
+                }
+                elsif ($position > ($#arr_wt_seq -8)) {
+                    @wt_arr = @arr_wt_seq[ $#arr_wt_seq -17 ... $#arr_wt_seq];
+                    $arr_wt_seq[$position]=$mt_aa;
+                    @mt_arr = @arr_wt_seq[ $#arr_wt_seq -17 ... $#arr_wt_seq];
+                    print OUT ">WT.".$prot_arr[6].".".$prot_arr[15]."\n";
+                    print OUT ( join "", @wt_arr);
+                    print OUT "\n";
+                    print OUT ">MT.".$prot_arr[6].".".$prot_arr[15]."\n";
+                    print OUT ( join "", @mt_arr);
+                    print OUT "\n";
+                }
+                elsif (($position >= 8) && ($position  <= ($#arr_wt_seq -8))) {
+                    @wt_arr = @arr_wt_seq[ $position-8 ... $position+8];
+                    $arr_wt_seq[$position]=$mt_aa;
+                    @mt_arr = @arr_wt_seq[ $position-8 ... $position+8];
+                    print OUT ">WT.".$prot_arr[6].".".$prot_arr[15]."\n";
+                    print OUT ( join "", @wt_arr);
+                    print OUT "\n";
+                    print OUT ">MT.".$prot_arr[6].".".$prot_arr[15]."\n";
+                    print OUT ( join "", @mt_arr);
+                    print OUT "\n";
+                }
+                else {
+                    print OUT "NULL"."\t".$position."\n";
+                }
+                OUT->close;
+            }
+        }
     }
 1;
 }
