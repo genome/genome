3126389ae53e308e13e003e445ab9c67c374eb19
Gabriel Sanderson <gsanders@genome.wustl.edu>
2014-03-11 17:48:57 -0500

    Cleaned up FastaKey.pm, mostly formatting

--- lib/perl/Genome/Model/Tools/EpitopePrediction/FastaKey.pm
+++ lib/perl/Genome/Model/Tools/EpitopePrediction/FastaKey.pm
@@ -3,57 +3,39 @@ package Genome::Model::Tools::EpitopePrediction::FastaKey;
 use strict;
 use warnings;
 use Genome;
-use Workflow;
-
-
 
 class Genome::Model::Tools::EpitopePrediction::FastaKey {
     is => 'Genome::Model::Tools::EpitopePrediction::Base',
+    doc => "Generates a key file to lookup original protein names in the output file of NetMHC 3.4 from the original 21-mer FASTA file for wildtype(WT) and mutant(MT) proteins",
     has_input => [
         input_file => {
             is => 'Text',
             doc => 'The input FASTA file with 21mer sequences for wildtype(WT) and mutant(MT) proteins generated using \'gmt epitope-prediction generat-variant-seq21mer\'',
         },
         output_file => {
-            is => 'Text',
+            is  => 'Text',
             doc => 'The output Key file for lookup',
         },
-        
     ],
 };
 
-
-sub help_brief {
-    "Generates a key file to lookup original protein names in the output file of NetMHC 3.4 from the original 21-mer FASTA file for wildtype(WT) and mutant(MT) proteins",
-}
-
-
-
 sub execute {
     my $self = shift;
-    
-    my $input_fh = Genome::Sys->open_file_for_reading($self->input_file);
-    my $output_fh = Genome::Sys->open_file_for_writing($self->output_file);
-    
-#>WT.BET1L.p.Q141R
-#RQRPTSHHSHQSQNPPMLGNG
-#>MT.BET1L.p.Q141R
-#RQRPTSHHSHRSQNPPMLGNG
- 	
- 	my $i=1;
-    while (my $line = $input_fh->getline) {
-		chomp $line;
-		if ($line =~ /^>/)
-		{
-			my $original_name = $line;
-			my $new_name = "Entry_".$i;
-			print $output_fh join("\t",$new_name,$original_name)."\n";
-		 $i++;	
-		}
-	} 
+
+    my $input_fh  = Genome::Sys->open_file_for_reading( $self->input_file );
+    my $output_fh = Genome::Sys->open_file_for_writing( $self->output_file );
+
+    my $i = 1;
+    while ( my $line = $input_fh->getline ) {
+        chomp $line;
+        if ( $line =~ /^>/ ) {
+            my $original_name = $line;
+            my $new_name      = "Entry_" . $i;
+            print $output_fh join( "\t", $new_name, $original_name ) . "\n";
+            $i++;
+        }
+    }
+    return 1;
+}
 
 1;
-
-}
-return 1;
-
