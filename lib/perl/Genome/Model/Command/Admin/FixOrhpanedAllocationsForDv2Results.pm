ce6e7bad1ba7ba81981ad2b01b8c5bb9ec5839ca
Nathaniel Nutter <nnutter@genome.wustl.edu>
2014-03-12 17:04:16 -0500

    rewrite FixOrhpanedAllocationsForDv2Results
    This was written before we switched to UUIDs
    * Update for UUIDs.
    * Trades parsing logs for parsing paths.
    * Uses parsed path to acquire lock on SoftwareResult which means it
      should be safer to use.
    * Did not reimplement the broken symlinks part.

--- lib/perl/Genome/Model/Command/Admin/FixOrhpanedAllocationsForDv2Results.pm
+++ lib/perl/Genome/Model/Command/Admin/FixOrhpanedAllocationsForDv2Results.pm
@@ -5,8 +5,8 @@ use strict;
 
 use Genome;
 
-require File::Find;
-use File::Grep qw( fgrep fmap );
+use File::Find::Rule qw();
+use List::MoreUtils qw(uniq);
 
 class Genome::Model::Command::Admin::FixOrhpanedAllocationsForDv2Results {
     is => 'Command::V2',
@@ -16,102 +16,64 @@ class Genome::Model::Command::Admin::FixOrhpanedAllocationsForDv2Results {
             shell_args_position => 1,
             doc => 'Build to fix.'
         },
+        dry_run => {
+            is => 'Boolean',
+            default_value => 0,
+        },
     ],
 };
 
 sub help_brief {
-    return 'Delete orphaned allocations and links for dv results'
+    return 'delete orphaned allocations and links for DV2 results'
 }
 
 sub help_detail {
-    return 'This command will parse the error files of a build to find and delete orphaned allocations for detect variants results. It then goes through the variant sub directory and recursively finds and removes broken symlinks. If no allocations are found, broken symlinks are removed.';
+    return 'Looks for orphaned allocations in the build directory (following symlinks) and then deletes them if it is believed to be safe.';
+}
+
+sub find_dir_symlinks {
+    my $in = shift;
+
+    my @l = grep { -d readlink $_ } File::Find::Rule->symlink()->in($in);
+    return uniq @l, map { find_dir_symlinks(readlink $_) } @l;
 }
 
 sub execute {
     my $self = shift;
 
+    my @symlinks = find_dir_symlinks($self->build->data_directory);
 
-    my $delete_orphaned_allocations = $self->_parse_error_logs_to_find_and_delete_orphaned_allocations;
-    return if not $delete_orphaned_allocations;
+    for my $s (@symlinks) {
+        my $t = readlink $s;
+        my @d = File::Spec->splitdir($t);
+        my $p = File::Spec->catdir(@d[4..$#d]);
+        my $a = Genome::Disk::Allocation->get(allocation_path => $p);
 
-    my $remove_broken_links = $self->_remove_broken_links_in_variants_directory;
-    return if not $remove_broken_links;
+        next unless $a->owner_class_name->isa('Genome::Model::Tools::DetectVariants2::Result::Base');
 
-    return 1;
-}
+        my $lookup_hash = (split /-/, $s)[-1];
+        my $owner_lock = $a->owner_class_name->_lock($lookup_hash, undef);
 
-sub _parse_error_logs_to_find_and_delete_orphaned_allocations {
-    my $self = shift;
+        # the owner is generating if it's locked?
+        next unless $owner_lock;
 
-    $self->status_message('Get error logs...');
-    $self->status_message('Log dir: '.$self->build->data_directory);
-    my $log_directory = $self->build->log_directory;
-    my @err_logs;
-    File::Find::find(
-        {
-            wanted => sub { 
-                push @err_logs, $_ if /\.err$/
-            },
-            no_chdir => 1,
-        },
-        $log_directory,
-    );
-    $self->status_message('Found '.@err_logs.' error logs');
-    return 1 if not @err_logs; # ok
+        if ($a->owner) {
+            $a->owner_class_name->_unlock_resource($owner_lock);
+            next;
+        }
 
-    $self->status_message('Find orphaned allocations in error logs...');
-    my @owner_ids;
-    for my $err_log ( @err_logs ) {
-        push @owner_ids, grep { defined } fmap { /no software result for it's owner ID \((\d+)\)\./; $1; } $err_log;
+        my $owner_class_name = $a->owner_class_name;
+        print join(' ', $a->id, $owner_class_name, $a->owner_id, $a->owner ? 'T' : 'F', $lookup_hash), "\n";
+        print "Unlinking $s\n";
+        print 'Deleting ' . $a->absolute_path . "\n";
+        unless ($self->dry_run) {
+            unlink $s;
+            $a->delete;
+        }
+        $owner_class_name->_unlock_resource($owner_lock);
     }
-    $self->status_message('Found '.@owner_ids.' owner ids for orphaned allocations');
-    return 1 if not @owner_ids;
-    my @allocations = Genome::Disk::Allocation->get(owner_id => \@owner_ids);
-    $self->status_message('Found '.@allocations.' possible orphaned allocations');
-
-    $self->status_message('Delete existing orphaned allocations...');
-    my $deleted = 0;
-    for my $allocation ( @allocations ) {
-        my $owner = $allocation->owner;
-        next if $owner; # do not delete allcoations with owners
-        $self->status_message('Owner: '.join(' ', map { $allocation->$_ } (qw/ id class /)) );
-        $allocation->delete;
-        $deleted++;
-    }
-    $self->status_message("Deleted $deleted orphaned allocations");
-
-    return 1;
-}
-
-sub _remove_broken_links_in_variants_directory {
-    my $self = shift;
-
-    $self->status_message('Find broken links...');
-    my $variants_directory = $self->build->data_directory.'/variants';
-    $self->status_message('Variants dir: '.$variants_directory);
-    my @broken_links;
-    File::Find::find(
-        {
-            wanted => sub{ 
-                return if not -l $_;
-                return if -e $_;
-                push @broken_links, $_;
-            },
-            no_chdir => 1,
-            follow_fast => 1,
-        },
-        $variants_directory,
-    );
-    $self->status_message('Found '.@broken_links.' broken links');
-
-    $self->status_message('Remove broken links...');
-    for my $broken_link ( @broken_links ) {
-        unlink $broken_link;
-    }
-    $self->status_message('Remove broken links...OK');
 
     return 1;
 }
 
 1;
-
