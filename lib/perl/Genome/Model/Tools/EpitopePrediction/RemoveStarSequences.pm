a49f01282f0b950f9d0464b654fc448181d1fcb0
Susanna Siebert <ssiebert@genome.wustl.edu>
2014-03-13 15:34:47 -0500

    refactored and added tests for RemoveStarSequences

--- lib/perl/Genome/Model/Tools/EpitopePrediction/RemoveStarSequences.pm
+++ lib/perl/Genome/Model/Tools/EpitopePrediction/RemoveStarSequences.pm
@@ -6,6 +6,7 @@ use Bio::SeqIO;
 
 class Genome::Model::Tools::EpitopePrediction::RemoveStarSequences {
     is => 'Genome::Model::Tools::EpitopePrediction::Base',
+    doc => "Outputs a FASTA file after removing *s from the input Fasta sequence",
     has_input => [
         input_file => {
             is => 'Text',
@@ -18,116 +19,30 @@ class Genome::Model::Tools::EpitopePrediction::RemoveStarSequences {
     ],
 };
 
-sub help_brief {
-    "Outputs a FASTA file after removing *s from the input Fasta sequence",
-}
-
 sub execute {
     my $self = shift;
-  #  my $input_fh = Genome::Sys->open_file_for_reading($self->input_file);
-    my $output_fh = Genome::Sys->open_file_for_writing($self->output_file);
 
     my $input_filename = $self->input_file ;
     my $output_filename= $self->output_file;
 
- #   while (my $line = $input_fh->getline) {
-#        chomp $line;
- #}
+    my $in  = Bio::SeqIO->new(
+        -file   => $input_filename ,
+        -format => 'Fasta'
+    );
 
-        my $in  = Bio::SeqIO->new(-file => $input_filename ,
-                      -format => 'Fasta');
-
-        my $out = Bio::SeqIO->$output_fh(-file => ">".$output_filename ,
-                       -format => 'Fasta');
-
-     #     my $out = Bio::SeqIO->new(-fh => $output_fh,
-      #                          -format => 'fasta');
-
-
-        while ( my $seq = $in->next_seq() )
-        {
-            my $seq_string = $seq->seq;
-            print $seq_string."\n";
-
-#               if ( $seq_string =~ /[^A-Z]/ )
-     #          {
-#                   print "Skipping Sequence:"."\t".$seq_string."\n";
-
-               }
-#            else
-  #             {
- #                  $out->write_seq($seq);
-  #             }
-    #    }
-
-1;
-}
-return 1;
-__END__
-
- my ($temp_fh_name, $temp_name) = Genome::Sys->create_temp_file();
-
-    my $temp_fh      =  Genome::Sys->open_file_for_writing($temp_name);
-    my $output_fh = Genome::Sys->open_file_for_writing($self->output_file);
-
-
-
-
-# READ THE SEQ FOF
-my $in_file = $ARGV[0];
-open (IN, "$in_file") or die 'could not open in file';
-while (<IN>) {
-    chomp;
-    my $parent_dir='/gscmnt/sata141/techd/jhundal/Schreiber_mouse/SchreiberProject/';
-    my $input_filename = $parent_dir.$_."_17_mer.fa";
-    my $output_filename = $parent_dir."testing/".$_."_17mer_NoStar.fa";
-  #  my $netmhc_output_filename = $parent_dir.$_.".netmhc.out";
-    print "Input File is".$input_filename."\n";
-    print "Output File is".$output_filename."\n";
-
-
-
-##########################################################################
-
-
-    my $in  = Bio::SeqIO->new(-file => $input_filename ,
-                      -format => 'Fasta');
-
-    my $out = Bio::SeqIO->new(-file => ">".$output_filename ,
-                        -format => 'Fasta');
+    my $out = Bio::SeqIO->new(
+        -file => ">".$output_filename ,
+        -format => 'Fasta'
+    );
 
     while ( my $seq = $in->next_seq() ) {
-    my $seq_string = $seq->seq;
-       if ( $seq_string =~ /[^A-Z]/ )
-       {
-           print $seq_string."\n";
-
-       }
-       else
-       {
-
-       $out->write_seq($seq);
-       }
+        my $seq_string = $seq->seq;
+        unless ( $seq_string =~ /[^A-Z]/ ) {
+            $out->write_seq($seq);
+        }
     }
-}
-__END__
-################CALCULATING NUMBER OF LINES IN INPUT FASTA FILE & SPLITTING#################
 
-    my $lines;
-    open (FILE, "$output_filename") or die 'Could not open output file';
-    $lines++ while (<FILE>);
-    close FILE;
-    #print "$lines\n";
-    print "Number of lines in ".$output_filename." is ".$lines."\n";
-               if ($lines <= 10000)
-               {
-                   #do nothing#
-               }
-               else
-               {
-                   my $command = 'split -l 8000 '.$output_filename." ".$output_filename."." ;
-                   #print $command."\n";
-                   system ( "$command" );
-               }
-
+    return 1;
 }
+
+1;
--- lib/perl/Genome/Model/Tools/EpitopePrediction/RemoveStarSequences.t
+++ lib/perl/Genome/Model/Tools/EpitopePrediction/RemoveStarSequences.t
@@ -4,10 +4,25 @@ use strict;
 use warnings;
 
 use above 'Genome';
-use Test::More tests => 1;
+use Test::More;
+use Genome::Utility::Test qw(compare_ok);
 
-# This test was auto-generated because './Model/Tools/EpitopePrediction/RemoveStarSequences.pm'
-# had no '.t' file beside it.  Please remove this test if you believe it was
-# created unnecessarily.  This is a bare minimum test that just compiles Perl
-# and the UR class.
-use_ok('Genome::Model::Tools::EpitopePrediction::RemoveStarSequences');
+my $class = 'Genome::Model::Tools::EpitopePrediction::RemoveStarSequences';
+my $TEST_DATA_VERSION= 1;
+use_ok($class);
+
+my $test_dir = Genome::Utility::Test->data_dir_ok($class, $TEST_DATA_VERSION);
+my $input_file = File::Spec->join($test_dir, "input.fasta");
+my $expected_output = File::Spec->join($test_dir, "expected.fasta");
+my $output_file = Genome::Sys->create_temp_file_path;
+
+my $cmd = $class->create(
+    input_file => $input_file,
+    output_file => $output_file,
+);
+
+ok($cmd->execute, "Command executed");
+
+compare_ok($output_file, $expected_output, "Output file is as expected");
+
+done_testing();
