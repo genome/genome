be97fe0526bb325643f74d0d7061eeda21b479b5
Anthony Brummett <abrummet@genome.wustl.edu>
2014-03-14 17:26:03 -0500

    Enviroment variables to fill and use test databases

--- lib/perl/Genome/DataSource/GMSchema.pm
+++ lib/perl/Genome/DataSource/GMSchema.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Genome;
 
+use File::Basename;
+use File::Spec;
 use Genome::DataSource::CommonRDBMS qw(log_error log_commit_time);
 
 class Genome::DataSource::GMSchema {
@@ -15,6 +17,18 @@ class Genome::DataSource::GMSchema {
         owner   => { default_value  => $ENV{GENOME_DS_GMSCHEMA_OWNER} },
     ],
 };
+
+BEGIN {
+    my($name, $path, $suffix) = File::Basename::fileparse($0, '.pm','.t','.pl');
+    my $testdb_dirname = File::Spec->catdir($path, $name) . '/';
+    if ($ENV{GENOME_TEST_FILLDB}) {
+        $ENV{UR_TEST_FILLDB} = "dbi:SQLite:dbname=$testdb_dirname";
+    }
+    if ($ENV{GENOME_TEST_USEDB}) {
+        $ENV{GENOME_DS_GMSCHEMA_TYPE} = 'UR::DataSource::SQLite';
+        $ENV{GENOME_DS_GMSCHEMA_SERVER} = $testdb_dirname;
+    }
+}
 
 sub table_and_column_names_are_upper_case { 0 }
 
