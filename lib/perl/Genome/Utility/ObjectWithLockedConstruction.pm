934de3b1e37804971382cc337453fe9c67b31eef
Adam Coffman <acoffman@genome.wustl.edu>
2014-03-19 15:49:46 -0500

    Add ObjectWithLockedConstruction mixin.
    You can mix this into your class and implement lock_id to avoid race
    conditions when multiple concurrent processes are trying to create the
    same object. lock_id will be passed all params given to the constructor
    and should return a string that is sufficient to uniquely identify the
    object being created.

---
+++ lib/perl/Genome/Utility/ObjectWithLockedConstruction.pm
@@ -0,0 +1,55 @@
+package Genome::Utility::ObjectWithLockedConstruction;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Utility::ObjectWithLockedConstruction {
+    is => 'UR::Object',
+    is_abstract => 1,
+};
+
+sub create {
+    my $class = shift;
+    my $lock_id = $class->lock_id(@_);
+
+    my $lock_var = sprintf('%s/%s/%s',
+        $ENV{GENOME_LOCK_DIR},
+        $class,
+        $lock_id);
+
+    my $obj = $class->get(@_);
+    if($obj) {
+        return $obj;
+    } else {
+        return $class->create_with_lock($lock_var, @_);
+    }
+}
+
+sub create_with_lock {
+    my $class = shift;
+    my $lock_var = shift;
+
+    my $lock = Genome::Sys->lock_resource(resource_lock => $lock_var);
+    die("Unable to get lock!") unless $lock;
+
+    my $obj = $class->load(@_);
+    if($obj) {
+        return $obj;
+    } else {
+       $obj = $class->SUPER::create(@_);
+       UR::Context->current->add_observer(
+           aspect => 'commit',
+           callback => sub {
+               Genome::Sys->unlock_resource(resource_lock => $lock_var);
+           }
+       );
+    }
+
+    return $obj;
+}
+
+sub lock_id {
+    die("You must implement lock_id in subclasses!");
+}
