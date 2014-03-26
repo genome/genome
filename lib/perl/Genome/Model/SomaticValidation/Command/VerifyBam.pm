5043b517276aa04c5a62a484547593af62931aff
apregier <aregier@genome.wustl.edu>
2014-03-26 10:21:29 -0500

    Check to see whether genotype data is available

--- lib/perl/Genome/Model/SomaticValidation/Command/VerifyBam.pm
+++ lib/perl/Genome/Model/SomaticValidation/Command/VerifyBam.pm
@@ -19,6 +19,10 @@ class Genome::Model::SomaticValidation::Command::CoverageStats {
             is => 'Genome::Model::Build::SomaticValidation',
             id_by => 'build_id',
         },
+        aligned_bam_result => {
+            is => 'Genome::InstrumentData::AlignedBamResult',
+            id_by => 'merged_alignment_result_id',
+        },
     ],
     has_transient_optional_output => [
         result => {
@@ -29,6 +33,11 @@ class Genome::Model::SomaticValidation::Command::CoverageStats {
 
 sub shortcut {
     my $self = shift;
+
+    unless ($self->should_run) {
+        return 1;
+    }
+
     my $params = $self->params_for_result;
     my $result = Genome::InstrumentData::VerifyBamIdResult->get_with_lock(
         %{$params}
@@ -42,6 +51,11 @@ sub shortcut {
 
 sub execute {
     my $self = shift;
+
+    unless($self->should_run) {
+        return 1;
+    }
+
     my $params = $self->params_for_result;
     my $result = Genome::InstrumentData::VerifyBamIdResult->get_or_create(
         %{$params}
@@ -56,11 +70,26 @@ sub execute {
     }
 }
 
+sub should_run {
+    my $self = shift;
+
+    unless (defined $self->aligned_bam_result) {
+        return 0;
+    }
+
+    if (defined $self->aligned_bam_result->instrument_data->sample->default_genotype_data) {
+        return 1;
+    }
+    else {
+        $self->debug_message('No default genotype data for sample '.$self->aligned_bam_result->instrument_data->sample->__display_name__.' Skipping VerifyBamId');
+        return 0;
+    }
+}
+
 sub params_for_result {
     my $self = shift;
-    my $aligned_bam_result = Genome::SoftwareResult->get($self->merged_alignment_result_id);
     my $genotype_vcf = Genome::InstrumentData::GenotypeVcf->get_or_create(
-        sample => $aligned_bam_result->instrument_data->sample,
+        sample => $self->aligned_bam_result->instrument_data->sample,
         known_sites_build => $self->build->previously_discovered_variations_build,
         filters => ["chromosome:exclude=".$self->build->previously_discovered_variations_build->reference_sequence_build->allosome_names],
     );
