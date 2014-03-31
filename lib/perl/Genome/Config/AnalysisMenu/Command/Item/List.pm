cdc78c9a25b59ed24c3772d905880a2e78b795c4
Thomas Mooney <tmooney@genome.wustl.edu>
2014-03-31 15:51:04 -0500

    Sort by name by default in the menu item lister.

--- lib/perl/Genome/Config/AnalysisMenu/Command/Item/List.pm
+++ lib/perl/Genome/Config/AnalysisMenu/Command/Item/List.pm
@@ -11,6 +11,7 @@ class Genome::Config::AnalysisMenu::Item::Command::List {
             is_constant => 1,
             value => 'Genome::Config::AnalysisMenu::Item'
         },
+        order_by => {default_value => 'name'},
         show => {default_value => 'id,name,created_by,created_at,updated_at,file_path'},
     ],
     doc => 'list Analysis Menu Items',
