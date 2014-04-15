bc55023135d94b8321cd897d034a28590c5a00f1
Avinash Ramu <aramu@genome.wustl.edu>
2014-04-15 10:18:36 -0500

    add method to access lane/bamqcpath easily

--- lib/perl/Genome/Model/ReferenceAlignment/Command/InstrumentDataAlignmentBams.pm
+++ lib/perl/Genome/Model/ReferenceAlignment/Command/InstrumentDataAlignmentBams.pm
@@ -14,11 +14,16 @@ class Genome::Model::ReferenceAlignment::Command::InstrumentDataAlignmentBams {
             shell_args_position => 1,
         },
     ],
+    has_optional => [
+        outdir => {
+            is => 'FileSystemPath'
+        },
+    ],
 };
 
 
 sub help_detail {
-    return "List the paths of the instrument data alignment BAMs for the provided build.";
+    return "List the paths of the instrument data alignment BAMs for the provided build. If outdir is specified results are written to a file in outdir else results are written to STDOUT.";
 }
 
 
@@ -29,35 +34,57 @@ sub execute {
     die $self->error_message('Please provide valid build id') unless $build;
     die $self->error_message('The provided build '.$build->id. ' is not reference alignment build.')
         unless $build->model->type_name eq 'reference alignment';
-
-    print join("\t", 'INSTRUMENT_DATA_ID', 'FLOW_CELL_ID', 'LANE', 'BAM_PATH', 'BAMQC_PATH') . "\n";
+    my $op_fh;
+    if($self->outdir) {
+        open $op_fh, ">",$self->outdir."/".$build->id.".instrumentdataalignmentbams.txt";
+    } else {
+        open $op_fh, ">-";
+    }
+    print $op_fh join("\t", 'INSTRUMENT_DATA_ID', 'FLOW_CELL_ID', 'LANE', 'BAM_PATH', 'BAMQC_PATH') . "\n";
     for my $instrument_data ($build->instrument_data) {
         my $instrument_data_id = $instrument_data->id;
         my $flow_cell_id = eval { $instrument_data->flow_cell_id } || '-';
         my $lane = eval { $instrument_data->lane } || '-';
         my ($alignment_result) = $build->alignment_results_for_instrument_data($instrument_data);
         my $bam_path = $alignment_result ? $alignment_result->output_dir . '/all_sequences.bam' : '-';
-        #Get the latest bamqc result
-        my @bamqc_results =  Genome::InstrumentData::AlignmentResult::Merged::BamQc->get(
-            alignment_result_id => $alignment_result->id
-        );
-
-        my $max = '0';
-        my $bamqc_result;
-        for(@bamqc_results) {
-            my $earliest_time = $_->best_guess_date_numeric;
-            if ($earliest_time > $max) {
-                $max = $earliest_time;
-                $bamqc_result = $_;
-            }
-        }
-
-        my $bamqc_path = $bamqc_result ? $bamqc_result->output_dir : '-';
-        print join("\t", $instrument_data_id, $flow_cell_id, $lane, $bam_path, $bamqc_path) . "\n";
+        my $bamqc_path = $self->_get_bamqc_path($alignment_result);
+        print $op_fh join("\t", $instrument_data_id, $flow_cell_id, $lane, $bam_path, $bamqc_path) . "\n";
     }
-
+    close $op_fh;
     return 1;
 }
 
-1;
+sub get_lane_bamqc_path {
+    my $self = shift;
+    my $build = shift;
+    my $lane_bamqcpath_ref = shift;
+    my %lane_bamqcpath = %$lane_bamqcpath_ref;
+    for my $instrument_data ($build->instrument_data) {
+        my $instrument_data_id = $instrument_data->id;
+        my $lane = eval { $instrument_data->lane } || '-';
+        my ($alignment_result) = $build->alignment_results_for_instrument_data($instrument_data);
+        #Get the latest bamqc result
+        my $bamqc_path = $self->_get_bamqc_path($alignment_result);
+        $lane_bamqcpath{$lane} = $bamqc_path;
+    }
+}
 
+sub _get_bamqc_path {
+    my $self = shift;
+    my $alignment_result = shift;
+    my @bamqc_results =  Genome::InstrumentData::AlignmentResult::Merged::BamQc->get(
+        alignment_result_id => $alignment_result->id
+    );
+    my $max = '0';
+    my $bamqc_result;
+    for(@bamqc_results) {
+        my $earliest_time = $_->best_guess_date_numeric;
+        if ($earliest_time > $max) {
+            $max = $earliest_time;
+            $bamqc_result = $_;
+        }
+    }
+    my $bamqc_path = $bamqc_result ? $bamqc_result->output_dir : '-';
+}
+
+1;
