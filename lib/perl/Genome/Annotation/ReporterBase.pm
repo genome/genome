45f2f7b6c455c458a5b847144afa1c8095b81b12
Gabriel Sanderson <gsanders@genome.wustl.edu>
2014-04-22 13:43:48 -0500

    Removed methods that were moved up

--- lib/perl/Genome/Annotation/ReporterBase.pm
+++ lib/perl/Genome/Annotation/ReporterBase.pm
@@ -13,24 +13,4 @@ sub name {
     die "abstract";
 }
 
-sub validate {
-    my $self = shift;
-    if (my @errors = $self->__errors__) {
-        $self->print_errors(@errors);
-        die $self->error_message("Report (%s) failed validation", $self->name);
-    }
-}
-
-sub print_errors {
-    my ($self, @errors) = @_;
-
-    for my $error (@errors) {
-        my @properties = $error->properties;
-        $self->error_message("Property " .
-            join(',', map { "'$_'" } @properties) .
-            ': ' . $error->desc);
-    }
-    return;
-}
-
 1;
