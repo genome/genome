1717b626a3e35dc294bf1bdd6f650b9b89ae079b
apregier <aregier@genome.wustl.edu>
2014-04-24 12:54:12 -0500

    Add output file functionality to the reporter base class
    We added initialize and finalize methods to open and close
    the filehandle.  By default, the file will be named the
    same as the reporter class.  The initialize also calls
    print_headers, which can be overridden by subclasses.

--- lib/perl/Genome/Annotation/ReporterBase.pm
+++ lib/perl/Genome/Annotation/ReporterBase.pm
@@ -7,6 +7,9 @@ use Genome;
 class Genome::Annotation::ReporterBase {
     is => 'Genome::Annotation::ComponentBase',
     is_abstract => 1,
+    has_transient_optional => [
+        _output_fh => {},
+    ],
 };
 
 sub name {
@@ -17,4 +20,20 @@ sub requires_interpreters {
     die "abstract - must return a list of one or more interpreter names";
 }
 
+sub initialize {
+    my $self = shift;
+    my $output_dir = shift;
+    my $fh = Genome::Sys->open_file_for_writing(File::Spec->join($output_dir, $self->name));
+    $self->_output_fh($fh);
+    $self->print_headers;
+}
+
+sub finalize {
+    my $self = shift;
+    $self->_output_fh->close;
+}
+
+sub print_headers {
+    #implement in subclass if you want to print a header
+}
 1;
