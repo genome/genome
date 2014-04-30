2f7356795e7a61b9c4d289deb667b3d1d5638842
dmorton <dmorton@genome.wustl.edu>
2014-04-30 15:35:32 -0500

    Remove unneeded use statements

--- lib/perl/Genome/Annotation/Expert/Joinx/Expert.pm
+++ lib/perl/Genome/Annotation/Expert/Joinx/Expert.pm
@@ -3,8 +3,6 @@ package Genome::Annotation::Expert::Joinx::Expert;
 use strict;
 use warnings FATAL => 'all';
 use Genome;
-use Genome::WorkflowBuilder::DAG;
-use Genome::WorkflowBuilder::Command;
 
 class Genome::Annotation::Expert::Joinx::Expert {
     is => 'Genome::Annotation::Expert::Base',
