64c26da57e6eeb98ba9a8df0a1649456b25cd36a
Gabriel Sanderson <gsanders@genome.wustl.edu>
2014-05-08 16:06:02 -0500

    account for instrument_data_segment_id being 0 sometimes.

--- lib/perl/Genome/InstrumentData/Command/AlignReads.pm
+++ lib/perl/Genome/InstrumentData/Command/AlignReads.pm
@@ -162,8 +162,8 @@ sub params_for_alignment {
 
     my %params = (
         instrument_data_id => $self->instrument_data_id || undef,
-        instrument_data_segment_type => $self->instrument_data_segment_type || undef,
-        instrument_data_segment_id => $self->instrument_data_segment_id || undef,
+        instrument_data_segment_type => $self->instrument_data_segment_type // undef,
+        instrument_data_segment_id => $self->instrument_data_segment_id // undef,
 
         aligner_name => $self->name || undef,
         aligner_version => $self->version || undef,
