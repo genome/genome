7552745770cea378ed6926ca6f95c1f7c200d706
Avinash Ramu <aramu@genome.wustl.edu>
2014-05-23 13:11:34 -0500

    add file accessors

--- lib/perl/Genome/Model/Build/ClinSeq/FileAccessors.pm
+++ lib/perl/Genome/Model/Build/ClinSeq/FileAccessors.pm
@@ -27,6 +27,72 @@ sub cnv_dir {
   return $cnv_dir;
 }
 
+sub input_dir {
+  my $self = shift;
+  my $input_dir = $self->case_dir . "/input";
+  return $input_dir;
+}
+
+sub rnaseq_dir {
+  my $self = shift;
+  my $rnaseq_dir = $self->case_dir . "/rnaseq";
+  return $rnaseq_dir;
+}
+
+sub rnaseq_tumor_dir {
+  my $self = shift;
+  my $rnaseq_tumor_dir = $self->rnaseq_dir . "/tumor";
+  return $rnaseq_tumor_dir;
+}
+
+sub rnaseq_tumor_cufflinks_dir {
+  my $self = shift;
+  my $rnaseq_dir = $self->rnaseq_tumor_dir . "/cufflinks_expression_absolute";
+  return $rnaseq_dir;
+}
+
+sub rnaseq_tumor_cufflinks_genes_dir {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_genes_dir = $self->rnaseq_tumor_cufflinks_dir . "/genes";
+  return $rnaseq_tumor_cufflinks_genes_dir;
+}
+
+sub rnaseq_tumor_cufflinks_isoforms_dir {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_isoforms_dir = $self->rnaseq_tumor_cufflinks_dir . "/isoforms";
+  return $rnaseq_tumor_cufflinks_isoforms_dir;
+}
+
+sub rnaseq_tumor_cufflinks_isoforms_merged_dir {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_isoforms_merged_dir = $self->rnaseq_tumor_cufflinks_dir . "/isoforms_merged";
+  return $rnaseq_tumor_cufflinks_isoforms_merged_dir;
+}
+
+sub rnaseq_tumor_cufflinks_genes_summary_dir {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_genes_summary_dir = $self->rnaseq_tumor_cufflinks_genes_dir . "/summary";
+  return $rnaseq_tumor_cufflinks_genes_summary_dir;
+}
+
+sub rnaseq_tumor_cufflinks_isoforms_summary_dir {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_isoforms_summary_dir = $self->rnaseq_tumor_cufflinks_isoforms_dir . "/summary";
+  return $rnaseq_tumor_cufflinks_isoforms_summary_dir;
+}
+
+sub rnaseq_tumor_cufflinks_isoforms_merged_summary_dir {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_isoforms_merged_summary_dir = $self->rnaseq_tumor_cufflinks_isoforms_merged_dir . "/summary";
+  return $rnaseq_tumor_cufflinks_isoforms_merged_summary_dir;
+}
+
+sub input_summary_dir {
+  my $self = shift;
+  my $input_summary_dir = $self->input_dir . "/summary";
+  return $input_summary_dir;
+}
+
 sub microarray_cnv_dir {
   my $self = shift;
   my $microarray_cnv_dir = $self->cnv_dir . "/microarray_cnv";
@@ -42,6 +108,19 @@ sub exome_cnv_dir {
   my $exome_cnv_dir = $self->cnv_dir . "/exome_cnv";
 }
 
+sub exome_snv_summary_dir {
+  my $self = shift;
+  my $exome_snv_summary_dir =
+      $self->exome_snv_dir . "/summary";
+  if(-e $exome_snv_summary_dir){
+    return $exome_snv_summary_dir;
+  } else {
+    $self->warning_message("unable to find " .
+        $exome_snv_summary_dir);
+    return 0;
+  }
+}
+
 sub wgs_snv_dir {
   my $self = shift;
   my $exome_snv_dir = $self->snv_dir . "/wgs";
@@ -49,7 +128,30 @@ sub wgs_snv_dir {
 
 sub wgs_cnv_dir {
   my $self = shift;
-  my $wgs_cnv_dir = $self->cnv_dir . "/wgs_cnv/cnview/CNView_All";
+  my $wgs_cnv_dir = $self->cnv_dir . "/wgs_cnv";
+}
+
+sub wgs_cnv_cnview_dir {
+  my $self = shift;
+  my $wgs_cnv_cnview_dir = $self->wgs_cnv_dir . "/cnview/CNView_All";
+}
+
+sub wgs_cnv_summary_dir {#CHANGEME
+  my $self = shift;
+  my $wgs_cnv_summary_dir = $self->cnv_dir . "/wgs_cnv";
+}
+
+sub wgs_snv_summary_dir {
+  my $self = shift;
+  my $wgs_snv_summary_dir =
+      $self->wgs_snv_dir . "/summary";
+  if(-e $wgs_snv_summary_dir){
+    return $wgs_snv_summary_dir;
+  } else {
+    $self->warning_message("unable to find " .
+        $wgs_snv_summary_dir);
+    return 0;
+  }
 }
 
 sub wgs_exome_snv_dir {
@@ -57,9 +159,22 @@ sub wgs_exome_snv_dir {
   my $exome_snv_dir = $self->snv_dir . "/wgs_exome";
 }
 
+sub wgs_exome_snv_summary_dir {
+  my $self = shift;
+  my $wgs_exome_snv_summary_dir =
+      $self->wgs_exome_snv_dir . "/summary";
+  if(-e $wgs_exome_snv_summary_dir){
+    return $wgs_exome_snv_summary_dir;
+  } else {
+    $self->warning_message("unable to find " .
+        $wgs_exome_snv_summary_dir);
+    return 0;
+  }
+}
+
 sub wgs_cnvhmm_file {
   my $self = shift;
-  my $wgs_cnvhmm_file = $self->wgs_cnv_dir . "/cnaseq.cnvhmm.tsv";
+  my $wgs_cnvhmm_file = $self->wgs_cnv_cnview_dir . "/cnaseq.cnvhmm.tsv";
   if(-e $wgs_cnvhmm_file) {
     return $wgs_cnvhmm_file;
   } else {
@@ -70,7 +185,7 @@ sub wgs_cnvhmm_file {
 
 sub wgs_cnv_wg_plot {
   my $self = shift;
-  my $wgs_cnv_wg_plot = $self->wgs_cnv_dir . "/Both_AllChrs.jpeg";
+  my $wgs_cnv_wg_plot = $self->wgs_cnv_cnview_dir . "/Both_AllChrs.jpeg";
   if(-e $wgs_cnv_wg_plot) {
     return $wgs_cnv_wg_plot;
   } else {
@@ -158,6 +273,111 @@ sub wgs_exome_snv_tier1_annotated_compact_readcounts_file {
   } else {
     $self->warning_message("unable to find " .
         $wgs_exome_snv_tier1_annotated_compact_readcounts_file);
+    return 0;
+  }
+}
+
+sub input_summary_stats_file {
+  my $self = shift;
+  my $input_summary_stats_file =
+      $self->input_summary_dir . "/Stats.tsv";
+  if(-e $input_summary_stats_file){
+    return $input_summary_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $input_summary_stats_file);
+    return 0;
+  }
+}
+
+sub exome_snv_summary_stats_file {
+  my $self = shift;
+  my $exome_snv_summary_stats_file =
+      $self->exome_snv_summary_dir . "/Stats.tsv";
+  if(-e $exome_snv_summary_stats_file){
+    return $exome_snv_summary_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $exome_snv_summary_stats_file);
+    return 0;
+  }
+}
+
+sub wgs_exome_snv_summary_stats_file {
+  my $self = shift;
+  my $wgs_exome_snv_summary_stats_file =
+      $self->wgs_exome_snv_summary_dir . "/Stats.tsv";
+  if(-e $wgs_exome_snv_summary_stats_file){
+    return $wgs_exome_snv_summary_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $wgs_exome_snv_summary_stats_file);
+    return 0;
+  }
+}
+
+sub wgs_snv_summary_stats_file {
+  my $self = shift;
+  my $wgs_snv_summary_stats_file =
+      $self->wgs_snv_summary_dir . "/Stats.tsv";
+  if(-e $wgs_snv_summary_stats_file){
+    return $wgs_snv_summary_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $wgs_snv_summary_stats_file);
+    return 0;
+  }
+}
+
+
+sub wgs_cnv_summary_stats_file {
+  my $self = shift;
+  my $wgs_cnv_summary_stats_file =
+      $self->wgs_cnv_summary_dir . "/Stats.tsv";
+  if(-e $wgs_cnv_summary_stats_file){
+    return $wgs_cnv_summary_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $wgs_cnv_summary_stats_file);
+    return 0;
+  }
+}
+
+sub rnaseq_tumor_cufflinks_genes_stats_file {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_genes_stats_file = $self->rnaseq_tumor_cufflinks_genes_summary_dir .
+      "/Stats.tsv";
+  if(-e $rnaseq_tumor_cufflinks_genes_stats_file){
+    return $rnaseq_tumor_cufflinks_genes_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $rnaseq_tumor_cufflinks_genes_stats_file);
+    return 0;
+  }
+}
+
+sub rnaseq_tumor_cufflinks_isoforms_stats_file {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_isoforms_stats_file = $self->rnaseq_tumor_cufflinks_isoforms_summary_dir .
+      "/Stats.tsv";
+  if(-e $rnaseq_tumor_cufflinks_isoforms_stats_file){
+    return $rnaseq_tumor_cufflinks_isoforms_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $rnaseq_tumor_cufflinks_isoforms_stats_file);
+    return 0;
+  }
+}
+
+sub rnaseq_tumor_cufflinks_isoforms_merged_stats_file {
+  my $self = shift;
+  my $rnaseq_tumor_cufflinks_isoforms_merged_stats_file = $self->rnaseq_tumor_cufflinks_isoforms_merged_summary_dir .
+      "/Stats.tsv";
+  if(-e $rnaseq_tumor_cufflinks_isoforms_merged_stats_file){
+    return $rnaseq_tumor_cufflinks_isoforms_merged_stats_file;
+  } else {
+    $self->warning_message("unable to find " .
+        $rnaseq_tumor_cufflinks_isoforms_merged_stats_file);
     return 0;
   }
 }
