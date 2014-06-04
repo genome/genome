2d06de4277dae8b81b2637edc4518d4745bbec2c
Eddie Belter <ebelter@genome.wustl.edu>
2014-06-04 16:58:13 -0500

    InstData: fix reimport class name; add reimported_from method

--- lib/perl/Genome/InstrumentData/Reimport.pm
+++ lib/perl/Genome/InstrumentData/Reimport.pm
@@ -1,17 +1,29 @@
-package Genome::InstrumentData::Imported::Reimport;
+package Genome::InstrumentData::Reimport;
 
 use strict;
 use warnings;
 
 use Genome;
 
-class Genome::InstrumentData::Imported::Reimport{ 
+class Genome::InstrumentData::Reimport{ 
 };
 
 sub attribute_label_for_reimported_from { 'reimported_from' }
 
 sub attribute_labels_to_ignore_when_reimporting {
     (qw/ bam_path genotype_file genotype_file_name ignored import_date import_format user_name /);
+}
+
+sub reimported_from {
+    my ($self, $instrument_data) = @_;
+
+    Carp::confess('No instrument data to get which instrument data it was reimported from.') if not $instrument_data;
+
+    my $attribute_label_for_reimported_from = $self->attribute_label_for_reimported_from;
+    my $attribute = $instrument_data->attributes(attribute_label => $attribute_label_for_reimported_from);
+    return if not $attribute;
+
+    return Genome::InstrumentData->get($attribute->attribute_value);
 }
 
 1;
