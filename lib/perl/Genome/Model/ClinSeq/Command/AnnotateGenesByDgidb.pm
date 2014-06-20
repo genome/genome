04e44d450acf9684dfc7e10877704b05ee71b439
Thomas Mooney <tmooney@genome.wustl.edu>
2014-06-19 22:27:15 -0500

    Allow output_dir to be specified as input.
    The command already offers output_dir as a command-line option, but
    any supplied value was ignored.  Now it will be used if provided.

--- lib/perl/Genome/Model/ClinSeq/Command/AnnotateGenesByDgidb.pm
+++ lib/perl/Genome/Model/ClinSeq/Command/AnnotateGenesByDgidb.pm
@@ -17,7 +17,7 @@ class Genome::Model::ClinSeq::Command::AnnotateGenesByDgidb {
             doc => 'regular expression of column names containing gene names/symbols'
         },
     ],
-    has_output => [
+    has_input_output => [
         output_dir => {
             is  => 'FilesystemPath',
             is_optional => 1,
@@ -62,12 +62,7 @@ sub execute {
         return 1;
     }
 
-    my ($outdir_name, $dir) = fileparse($infile);
-    $outdir_name .= '.dgidb';
-    $outdir_name = $dir . $outdir_name;
-
-    Genome::Sys->create_directory($outdir_name) or die "Failed to create directory $outdir_name\n";
-    $self->output_dir($outdir_name);
+    my $outdir_name = $self->_prepare_output_directory;
 
     my @parameter_sets = (
         {
@@ -117,6 +112,22 @@ sub convert {
     return join ',', sort keys %list;
 }
 
+sub _prepare_output_directory {
+    my $self = shift;
+
+    unless($self->output_dir) {
+        my $infile = $self->input_file;
+        my ($outdir_name, $dir) = fileparse($infile);
+        $outdir_name .= '.dgidb';
+        $outdir_name = $dir . $outdir_name;
+        $self->output_dir($outdir_name);
+    }
+
+    my $outdir_name = $self->output_dir;
+    Genome::Sys->create_directory($outdir_name) or die "Failed to create directory $outdir_name";
+
+    return $outdir_name;
+}
 
 1;
 
