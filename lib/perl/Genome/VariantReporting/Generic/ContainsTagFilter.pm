c87cd76c3839efe7bb8f7b1c5da86a12cfa50384
Susanna Siebert <ssiebert@genome.wustl.edu>
2014-07-02 15:14:03 -0500

    Custom interpret_entry method (mainly to provide a custom header)

--- lib/perl/Genome/VariantReporting/Generic/ContainsTagFilter.pm
+++ lib/perl/Genome/VariantReporting/Generic/ContainsTagFilter.pm
@@ -22,6 +22,10 @@ sub requires_experts {
     return ();
 }
 
+sub available_fields {
+    return qw/contains_tag/;
+}
+
 sub filter_entry {
     my $self = shift;
     my $entry = shift;
@@ -43,6 +47,20 @@ sub filter_entry {
         $return_values{$alt_allele} = $contains_tag;
     }
 
+    return %return_values;
+}
+
+sub interpret_entry {
+    my ($self, $entry, $passed_alt_alleles) = @_;
+
+    my %filter_output = $self->filter_entry($entry);
+
+    my %return_values;
+    for my $variant_allele (@$passed_alt_alleles) {
+        $return_values{$variant_allele} =  {
+            contains_tag => $filter_output{$variant_allele},
+        };
+    }
     return %return_values;
 }
 
