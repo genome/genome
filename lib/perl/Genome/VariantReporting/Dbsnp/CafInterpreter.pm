00fe33cb2c08aefe6ab6c8435d85d96ed952b502
Susanna Siebert <ssiebert@genome.wustl.edu>
2014-07-15 15:19:12 -0500

    dbsnp caf interpreter

---
+++ lib/perl/Genome/VariantReporting/Dbsnp/CafInterpreter.pm
@@ -0,0 +1,52 @@
+package Genome::VariantReporting::Dbsnp::CafInterpreter;
+
+use strict;
+use warnings;
+use Genome;
+
+class Genome::VariantReporting::Dbsnp::CafInterpreter {
+    is => 'Genome::VariantReporting::Framework::Component::Interpreter',
+    has => [
+        dummy => {
+            is_optional => 1,
+        },
+    ],
+};
+
+sub name {
+    return 'caf'
+}
+
+sub requires_annotations {
+    return qw/
+        dbsnp
+    /;
+}
+
+sub available_fields {
+    return qw/
+        caf
+    /;
+}
+
+sub interpret_entry {
+    my $self = shift;
+    my $entry = shift;
+    my $passed_alt_alleles = shift;
+
+    my %return_values;
+
+    for my $variant_allele (@$passed_alt_alleles) {
+        if (!defined $entry->info("CAF")) {
+            $return_values{$variant_allele}->{caf} = undef;
+        }
+        else {
+            $return_values{$variant_allele}->{caf} = $entry->info("CAF");
+        }
+    }
+
+    return %return_values;
+}
+
+1;
+
