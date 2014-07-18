965158859b8df0b8116dd38f28c655c0ca655f59
Susanna Siebert <ssiebert@genome.wustl.edu>
2014-07-18 13:26:42 -0500

    corrected the calculation of the minimum coverage observed

--- lib/perl/Genome/VariantReporting/BamReadcount/MinCoverageObservedInterpreter.pm
+++ lib/perl/Genome/VariantReporting/BamReadcount/MinCoverageObservedInterpreter.pm
@@ -30,17 +30,19 @@ sub interpret_entry {
 
     my %coverages;
     for my $sample_name ($self->sample_names) {
-        my $interpreter = Genome::VariantReporting::BamReadcount::CoverageInterpreter->create(sample_name => $sample_name);
+        my $interpreter = Genome::VariantReporting::BamReadcount::VafInterpreter->create(sample_name => $sample_name);
         my %result = $interpreter->interpret_entry($entry, $passed_alt_alleles);
         for my $alt_allele (@$passed_alt_alleles) {
-            $coverages{$alt_allele}->{$sample_name} = $result{$alt_allele}->{coverage};
+            $coverages{$alt_allele}->{ref_count}->{$sample_name} = $result{$alt_allele}->{ref_count};
+            $coverages{$alt_allele}->{var_count}->{$sample_name} = $result{$alt_allele}->{var_count};
         }
     }
 
     my %return_values;
     for my $alt_allele (@$passed_alt_alleles) {
-        my @coverages = values %{$coverages{$alt_allele}};
-        $return_values{$alt_allele} = { min_coverage_observed => min(@coverages) };
+        my @ref_counts = values %{$coverages{$alt_allele}->{ref_count}};
+        my @var_counts = values %{$coverages{$alt_allele}->{var_count}};
+        $return_values{$alt_allele} = { min_coverage_observed => min(@ref_counts, @var_counts) };
     }
     return %return_values;
 }
--- lib/perl/Genome/VariantReporting/BamReadcount/MinCoverageObservedInterpreter.t
+++ lib/perl/Genome/VariantReporting/BamReadcount/MinCoverageObservedInterpreter.t
@@ -26,15 +26,16 @@ my $entry = create_entry(bam_readcount_line);
 subtest 'all alt alleles' => sub {
     my %expected_return_values = (
         G => {
-            min_coverage_observed =>  344,
+            min_coverage_observed =>  3,
         },
         C => {
-            min_coverage_observed =>  344,
+            min_coverage_observed =>  1,
         },
         AA => {
-            min_coverage_observed =>  344,
+            min_coverage_observed =>  3,
         },
     );
+    $DB::single=1;
 
     is_deeply({$interpreter->interpret_entry($entry, ['G', 'C', 'AA'] )}, \%expected_return_values, "Entry gets interpreted correctly");
 };
