8a4552d41e84198e77848134ff144ed49ba9dcec
apregier <aregier@genome.wustl.edu>
2014-07-23 11:55:41 -0500

    Hard-code plan files

--- lib/perl/Genome/VariantReporting/Framework/Command/Wrappers/Trio.pm
+++ lib/perl/Genome/VariantReporting/Framework/Command/Wrappers/Trio.pm
@@ -3,6 +3,7 @@ package Genome::VariantReporting::Framework::Command::Wrappers::Trio;
 use strict;
 use warnings;
 use Genome;
+use File::Basename qw(dirname);
 
 class Genome::VariantReporting::Framework::Command::Wrappers::Trio {
     is => 'Command::V2',
@@ -10,12 +11,6 @@ class Genome::VariantReporting::Framework::Command::Wrappers::Trio {
         models => {
             is => 'Genome::Model::SomaticValidation',
             is_many => 1,
-        },
-        snvs_plan_file => {
-            is => 'File',
-        },
-        indels_plan_file => {
-            is => 'File',
         },
         output_directory => {
             is => 'Path',
@@ -42,6 +37,16 @@ sub execute {
     return 1;
 }
 
+sub plan_file {
+    my ($self, $type) = @_;
+    return File::Spec->join($self->_plan_search_dir, "cle_full_variant_report_$type.yaml");
+}
+
+sub _plan_search_dir {
+    my $variant_reporting_base_dir = dirname(dirname(dirname(dirname(__FILE__))));
+    return File::Spec->join($variant_reporting_base_dir, 'plan_files');
+}
+
 sub get_model_pairs {
     my $self = shift;
     my $factory = Genome::VariantReporting::Framework::Command::Wrappers::ModelPairFactory->create(
@@ -57,12 +62,11 @@ sub run_reports {
     my ($self, $model_pair) = @_;
 
     for my $variant_type(qw(snvs indels)) {
-        my $plan_accessor = join('_', $variant_type, "plan_file");
         my %params = (
             input_vcf => $model_pair->input_vcf($variant_type),
             variant_type => $variant_type,
             output_directory => $model_pair->reports_directory($variant_type),
-            plan_file => $self->$plan_accessor,
+            plan_file => $self->plan_file($variant_type),
             resource_file => $model_pair->resource_file,
             log_directory => $model_pair->logs_directory($variant_type),
         );
