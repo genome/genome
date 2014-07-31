e6177d8c2efbb44d114a869f0aa4093e100f1ea8
Eddie Belter <ebelter@genome.wustl.edu>
2014-07-31 14:26:33 -0500

    GMT: add gatk command tree class

---
+++ lib/perl/Genome/Model/Tools/Gatk.pm
@@ -0,0 +1,14 @@
+package Genome::Model::Tools::Gatk;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::Model::Tools::Gatk { 
+    is => 'Command::Tree',
+    doc => 'run tools in the GATK',
+};
+
+1;
+
