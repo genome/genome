7a5c99ab469a54a98bfb5e872c1174910f9b6cd3
Avinash Ramu <aramu@genome.wustl.edu>
2014-08-09 21:31:41 -0500

    get posterior probs from Cnmops

--- lib/perl/Genome/Model/Tools/CopyNumber/Cnmops.pm.R
+++ lib/perl/Genome/Model/Tools/CopyNumber/Cnmops.pm.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+library(cn.mops)
 
 usage <- function()
 {
@@ -11,7 +12,7 @@ convert_start_end_character <-function(t) {
   return(t)
 }
 
-get_capture_regions <- function()
+get_capture_regions <- function(capture_bed, test)
 {
   segments <- read.table(capture_bed,sep="\t",as.is=TRUE)
   gr <- GRanges(segments[,1],IRanges(segments[,2],segments[,3]))
@@ -21,7 +22,7 @@ get_capture_regions <- function()
   return(gr)
 }
 
-get_tumor_normal_logrr <- function(ref_analysis_norm)
+get_tumor_normal_logrr <- function(ref_analysis_norm, out_dir)
 {
   R  <- cn.mops:::.makeLogRatios(ref_analysis_norm, "CN2")
   regions <- data.frame(seqnames=as.vector(seqnames(R)),
@@ -36,10 +37,40 @@ get_tumor_normal_logrr <- function(ref_analysis_norm)
   return(bed_logrr)
 }
 
-get_read_counts <- function()
+getPosteriors <- function(ref_analysis_norm, out_dir) {
+  gr1<-ref_analysis_norm@gr
+  values(gr1)<-as.data.frame(posteriorProbs(ref_analysis_norm))
+  cn_regions <- data.frame(stringsAsFactors = FALSE)
+  for(i in 1:length(ref_analysis_norm@cnvr)){
+    cnvr<-ref_analysis_norm@cnvr[i]
+    cn_regions <- rbind(cn_regions, avgPP(cnvr, gr1))
+  }
+  colnames(cn_regions) <- c("chr", "start", "end", "width",
+    "posteriorProbability", "CN")
+  write.table(cn_regions,
+    file = paste(out_dir, "/cnmops.cnvs.filtered.txt", sep = ""),
+    row.names = F, quote = F)
+}
+
+avgPP <- function(x, gr) {
+  copyNumbers <- c("CN0", "CN1", "CN2", "CN3", "CN4",
+    "CN5", "CN6", "CN7", "CN8", "CN16", "CN32",
+    "CN64", "CN128")
+  v <- as.data.frame(values(subsetByOverlaps(gr, x)))
+  pp <- apply(v, 2, mean)
+  cn = copyNumbers[which.max(pp)]
+  chr = as.vector(seqnames(x))
+  start = start(x)
+  end = end(x)
+  if(cn != "CN2") {
+    return(data.frame(chr, start, end, end - start, max(pp), cn))
+  }
+}
+
+get_read_counts <- function(tumor_bam, normal_bam, gr, out_dir)
 {
-  X.nor <- getSegmentReadCountsFromBAM(normal_bam, GR=gr, mode="paired")
   X.tum <- getSegmentReadCountsFromBAM(tumor_bam, GR=gr, mode="paired")
+  X.nor <- getSegmentReadCountsFromBAM(normal_bam, GR=gr, mode="paired")
   X <- X.tum
   values(X) <- cbind(values(X.tum), values(X.nor))
   X <- normalizeGenome(X)
@@ -48,7 +79,7 @@ get_read_counts <- function()
   return(X)
 }
 
-plot_segments <- function()
+plot_segments <- function(ref_analysis_norm, out_dir)
 {
   segment_file=paste(out_dir, "/cnmops.segplot_WG.pdf", sep = "")
   pdf(segment_file, width = 14, height = 14)
@@ -63,15 +94,19 @@ plot_segments <- function()
       abline(h=log2(1/2), col = "green")
       abline(h=log2(3/2), col = "blue")
       abline(h=log2(4/2), col = "yellow")
-      legend("bottomright", inset=.05, title="Copy Number difference (tumor - normal)", c("1","2", "CNV segment", "-1"), horiz=TRUE, col = c("blue", "yellow", "red", "green"), lty=1)
+      legend("bottomright", inset=.05,
+        title="Copy Number difference (tumor - normal)",
+        c("1","2", "CNV segment", "-1"), horiz=TRUE,
+        col = c("blue", "yellow", "red", "green"), lty=1)
       dev.off()
     }
   }
 }
 
-save_ref_analysis_norm <- function(ref_analysis_norm)
+save_ref_analysis_norm <- function(ref_analysis_norm, out_dir)
 {
-  ref_analysis_norm_file = paste(out_dir, "/cnmops.ref_analysis_norm.Robject", sep = "")
+  ref_analysis_norm_file = paste(out_dir, "/cnmops.ref_analysis_norm.Robject",
+    sep = "")
   if(length(cnvr(ref_analysis_norm)) != 0) {
     ref_analysis_norm <- cn.mops:::.replaceNames(ref_analysis_norm,
         colnames(ref_analysis_norm@normalizedData),"Tumor_Normal_Log2Ratio")
@@ -79,10 +114,11 @@ save_ref_analysis_norm <- function(ref_analysis_norm)
   save(ref_analysis_norm, file=ref_analysis_norm_file)
 }
 
-write_cnvr<-function()
+write_cnvr<-function(ref_analysis_norm, out_dir)
 {
   cnv_r<-cnvs(ref_analysis_norm)
-  df <- data.frame(seqnames=as.vector(seqnames(cnv_r)), starts=start(cnv_r)-1, ends=end(cnv_r))
+  df <- data.frame(seqnames=as.vector(seqnames(cnv_r)), starts=start(cnv_r)-1, 
+      ends=end(cnv_r))
   colnames(df) <- c("chr", "start", "end")
   convert_start_end_character(df)
   df2 <- data.frame(seqnames=as.vector(seqnames(cnv_r)), starts=start(cnv_r)-1, ends=end(cnv_r), medians=values(cnv_r)$median, means=values(cnv_r)$mean, CN=values(cnv_r)$CN)
@@ -94,30 +130,37 @@ write_cnvr<-function()
   write.table(df2, file=op_cnv_txt, quote=F, sep="\t", row.names=F, col.names=T)
 }
 
-args <- commandArgs(trailingOnly = TRUE)
-if(length(args) != 5) {
-  usage()
-  quit()
+main <- function()
+{
+  args <- commandArgs(trailingOnly = TRUE)
+  if(length(args) != 5) {
+    usage()
+    quit()
+  }
+
+  tumor_bam = args[1]
+  normal_bam = args[2]
+  capture_bed = args[3]
+  out_dir = args[4]
+
+  if(args[5] == "--test") {
+    test = TRUE
+  } else {
+    test = FALSE
+  }
+  #use a deterministic seed, this enables testing
+  set.seed(42)
+  gr<-get_capture_regions(capture_bed, test)
+  RD<-get_read_counts(tumor_bam, normal_bam, gr, out_dir)
+  ref_analysis_norm <- referencecn.mops(RD[,1], RD[,2],
+    returnPosterior = TRUE, segAlgorithm = "DNAcopy", norm = 0,
+    lowerThreshold = -0.415, upperThreshold = 0.321, priorImpact = 20)
+  ref_analysis_norm <- calcIntegerCopyNumbers(ref_analysis_norm)
+  tumor_normal_logrr <- get_tumor_normal_logrr(ref_analysis_norm, out_dir)
+  save_ref_analysis_norm(ref_analysis_norm, out_dir)
+  plot_segments(ref_analysis_norm, out_dir)
+  write_cnvr(ref_analysis_norm, out_dir)
+  getPosteriors(ref_analysis_norm, out_dir)
 }
-library(cn.mops)
 
-tumor_bam = args[1]
-normal_bam = args[2]
-capture_bed = args[3]
-out_dir = args[4]
-
-if(args[5] == "--test") {
-  test = TRUE
-} else {
-  test = FALSE
-}
-
-set.seed(42)#use a deterministic seed, this enables testing and doesn't really affect the results.
-gr<-get_capture_regions()
-RD<-get_read_counts()
-ref_analysis_norm <- referencecn.mops(RD[,1], RD[,2], returnPosterior = TRUE, segAlgorithm = "DNAcopy", norm = 0, lowerThreshold = -0.415, upperThreshold = 0.321, priorImpact = 20)
-ref_analysis_norm <- calcIntegerCopyNumbers(ref_analysis_norm)
-tumor_normal_logrr <- get_tumor_normal_logrr(ref_analysis_norm)
-save_ref_analysis_norm(ref_analysis_norm)
-plot_segments()
-write_cnvr()
+main()
