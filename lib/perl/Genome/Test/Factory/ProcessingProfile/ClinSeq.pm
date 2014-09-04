a7d60c4bbd069ab90dd185736a6eac9e8274f6b5
Gabriel Sanderson <gsanders@genome.wustl.edu>
2014-09-04 15:55:13 -0500

    Add required params to clinseq profile factory

--- lib/perl/Genome/Test/Factory/ProcessingProfile/ClinSeq.pm
+++ lib/perl/Genome/Test/Factory/ProcessingProfile/ClinSeq.pm
@@ -2,4 +2,13 @@ package Genome::Test::Factory::ProcessingProfile::ClinSeq;
 use Genome::Test::Factory::ProcessingProfile;
 @ISA = (Genome::Test::Factory::ProcessingProfile);
 
+use strict;
+use warnings;
+
+our @required_params = qw(bam_readcount_version);
+
+sub create_bam_readcount_version {
+    return "0.9001";
+}
+
 1;
