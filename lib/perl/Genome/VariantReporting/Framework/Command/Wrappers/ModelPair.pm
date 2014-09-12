60f4bce62645e730697294035457a1549b673dbf
apregier <aregier@genome.wustl.edu>
2014-09-12 09:52:14 -0500

    Loop over variant types to reduce duplication

--- lib/perl/Genome/VariantReporting/Framework/Command/Wrappers/ModelPair.pm
+++ lib/perl/Genome/VariantReporting/Framework/Command/Wrappers/ModelPair.pm
@@ -66,18 +66,15 @@ sub create {
     my $class = shift;
     my $self = $class->SUPER::create(@_);
     Genome::Sys->create_directory($self->output_dir);
-    Genome::Sys->create_directory($self->reports_directory("snvs"));
-    my $snvs_plan = Genome::VariantReporting::Framework::Plan::MasterPlan->create_from_file($self->plan_file("snvs"));
-    $snvs_plan->write_to_file(File::Spec->join($self->reports_directory("snvs"), "plan.yaml"));
-    Genome::Sys->create_directory($self->reports_directory("indels"));
-    my $indel_plan = Genome::VariantReporting::Framework::Plan::MasterPlan->create_from_file($self->plan_file("indels"));
-    $indel_plan->write_to_file(File::Spec->join($self->reports_directory("indels"), "plan.yaml"));
-    Genome::Sys->create_directory($self->logs_directory("snvs"));
-    Genome::Sys->create_directory($self->logs_directory("indels"));
     $self->generate_resource_file;
     my $provider = Genome::VariantReporting::Framework::Component::ResourceProvider->create_from_file($self->resource_file);
-    $provider->write_to_file(File::Spec->join($self->reports_directory("snvs"), "resources.yaml"));
-    $provider->write_to_file(File::Spec->join($self->reports_directory("indels"), "resources.yaml"));
+    for my $variant_type (qw(snvs indels)) {
+        Genome::Sys->create_directory($self->reports_directory($variant_type));
+        my $plan = Genome::VariantReporting::Framework::Plan::MasterPlan->create_from_file($self->plan_file($variant_type));
+        $plan->write_to_file(File::Spec->join($self->reports_directory($variant_type), "plan.yaml"));
+        Genome::Sys->create_directory($self->logs_directory($variant_type));
+        $provider->write_to_file(File::Spec->join($self->reports_directory($variant_type), "resources.yaml"));
+    }
     return $self;
 };
 
