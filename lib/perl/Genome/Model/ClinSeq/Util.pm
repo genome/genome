05eccc1e9c9f712b05d2e983d6fd465f7772b89c
Avinash Ramu <aramu@genome.wustl.edu>
2014-09-16 12:27:16 -0500

    pass array by reference

--- lib/perl/Genome/Model/ClinSeq/Util.pm
+++ lib/perl/Genome/Model/ClinSeq/Util.pm
@@ -1249,9 +1249,9 @@ sub _get_si_report_tumor_prefix {
 
 sub resolve_rnaseq_builds {
   my $self = shift;
-  my @clinseq_builds = shift;
+  my $clinseq_builds = shift;
   my %rnaseq_builds;
-  foreach my $clinseq_build (@clinseq_builds){
+  foreach my $clinseq_build (@$clinseq_builds){
     my $tumor_build = $clinseq_build->tumor_rnaseq_build;
     $rnaseq_builds{$tumor_build->id}{build} = $tumor_build if $tumor_build;
     $rnaseq_builds{$tumor_build->id}{type} = 'tumor_rnaseq' if $tumor_build;
@@ -1264,9 +1264,9 @@ sub resolve_rnaseq_builds {
 
 sub resolve_somatic_builds{
   my $self = shift;
-  my @clinseq_builds = shift;
+  my $clinseq_builds = shift;
   my %somatic_builds;
-  foreach my $clinseq_build (@clinseq_builds){
+  foreach my $clinseq_build (@$clinseq_builds){
     my $wgs_build = $clinseq_build->wgs_build;
     $somatic_builds{$wgs_build->id}{build} = $wgs_build if $wgs_build;
     $somatic_builds{$wgs_build->id}{type} = 'wgs' if $wgs_build;
@@ -1338,7 +1338,9 @@ sub get_ref_align_builds{
     $ref_builds{$tumor_refalign_name}{day} = $tumor_time_point;
   }
 
-  $self->get_rnaseq_ref_builds(\%ref_builds, $rnaseq_builds);
+  if($rnaseq_builds) {
+    $self->get_rnaseq_ref_builds(\%ref_builds, $rnaseq_builds);
+  }
 
   #Set an order on refalign builds (use time points if available, otherwise name)
   my $o = 0;
