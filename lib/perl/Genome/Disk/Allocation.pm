e0e2a3701d7803b65590fe57594dd6208f6138cf
Mark Burnett <mburnett@genome.wustl.edu>
2014-09-18 16:28:05 -0500

    D:A delete archive inside commit observer

--- lib/perl/Genome/Disk/Allocation.pm
+++ lib/perl/Genome/Disk/Allocation.pm
@@ -329,17 +329,19 @@ sub _delete {
     }
 
     my $self = $class->get($id);
+    my $path = $self->absolute_path;
 
     if ($self->is_archived) {
-        $self->_cleanup_archive_directory($self->absolute_path);
         $self->_delete_timeline_events;
         $self->SUPER::delete;
 
+        $class->_create_observer(sub {
+            $class->_cleanup_archive_directory($path);
+        });
     } else {
         $self->_delete_timeline_events;
         $self->SUPER::delete;
 
-        my $path = $self->absolute_path;
         $class->_create_observer(
             $class->_mark_for_deletion_closure($path),
             $class->_remove_directory_closure($path),
