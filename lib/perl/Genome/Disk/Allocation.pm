c3b7b1beb21eb380c8b83ec2ea698e3933c17b7a
Thomas Mooney <tmooney@genome.wustl.edu>
2014-09-23 16:34:57 -0500

    Only reload if there's an existing object.
    Otherwise the new object gets dropped instead.

--- lib/perl/Genome/Disk/Allocation.pm
+++ lib/perl/Genome/Disk/Allocation.pm
@@ -640,7 +640,10 @@ sub _reload_allocation {
     } elsif ($mode eq 'load') {
         $allocation = UR::Context->current->reload($class, id => $id);
         if ($allocation) {
-            UR::Context->current->reload($allocation->owner_class_name, id => $allocation->owner_id);
+            my $owner = $allocation->owner;
+            if($owner and exists $owner->{db_committed}) {
+                UR::Context->current->reload($owner);
+            }
         }
     } else {
         die 'Unrecognized _retrieve_mode: ' . $class->_retrieve_mode;
