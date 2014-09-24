62dfd94965a3652479e780453b2fe357a0c014fa
Avinash Ramu <aramu@genome.wustl.edu>
2014-09-24 13:10:22 -0500

    add new pp params

--- lib/perl/Genome/Test/Factory/ProcessingProfile/ClinSeq.pm
+++ lib/perl/Genome/Test/Factory/ProcessingProfile/ClinSeq.pm
@@ -6,10 +6,33 @@ use Genome::Model::Tools::Sam::Readcount;
 use strict;
 use warnings;
 
-our @required_params = qw(bam_readcount_version);
+our @required_params = qw(bam_readcount_version
+    sireport_min_coverage
+    sireport_min_tumor_vaf
+    sireport_max_normal_vaf);
 
 sub create_bam_readcount_version {
     return Genome::Model::Tools::Sam::Readcount->default_version;
+}
+
+sub create_sireport_min_coverage {
+    return 20;
+}
+
+sub create_sireport_min_tumor_vaf {
+    return 2.5;
+}
+
+sub create_sireport_max_normal_vaf {
+    return 10;
+}
+
+sub create_sireport_min_mq{
+    return "30,40";
+}
+
+sub create_sireport_min_bq {
+    return "10,20";
 }
 
 1;
