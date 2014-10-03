83bbb82cdb308fe3fa45465d8c0173516e94010f
Avinash Ramu <aramu@genome.wustl.edu>
2014-10-03 00:00:19 -0500

    update tests to use new processing profile

--- lib/perl/Genome/Test/Factory/ProcessingProfile/ClinSeq.pm
+++ lib/perl/Genome/Test/Factory/ProcessingProfile/ClinSeq.pm
@@ -10,8 +10,8 @@ our @required_params = qw(bam_readcount_version
     sireport_min_coverage
     sireport_min_tumor_vaf
     sireport_max_normal_vaf
-    sireport_min_mq
-    sireport_min_bq);
+    sireport_min_mq_bq
+);
 
 sub create_bam_readcount_version {
     return Genome::Model::Tools::Sam::Readcount->default_version;
@@ -29,12 +29,8 @@ sub create_sireport_max_normal_vaf {
     return 10;
 }
 
-sub create_sireport_min_mq {
-    return "30,40";
-}
-
-sub create_sireport_min_bq {
-    return "10,20";
+sub create_sireport_min_mq_bq {
+    return "30,40;10,20";
 }
 
 1;
