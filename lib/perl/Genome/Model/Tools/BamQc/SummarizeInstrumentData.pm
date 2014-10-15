cd25b82f0fc7622e0d499b900f658458d2b25a94
Travis Abbott <tabbott@genome.wustl.edu>
2014-10-15 14:18:44 -0500

    add gmt bam-qc summarize-instrument-data

---
+++ lib/perl/Genome/Model/Tools/BamQc/SummarizeInstrumentData.pm
@@ -0,0 +1,152 @@
+package Genome::Model::Tools::BamQc::SummaryFromInstrumentData;
+
+use strict;
+use warnings;
+
+use File::Slurp qw(read_file);
+use Carp qw(confess);
+use Genome;
+
+my $R_SCRIPT = sprintf("%s.R", __FILE__);
+
+my %SAMPLE_PROPERTIES = (name => "sample_name", common_name => "common_name");
+my %LIBRARY_PROPERTIES = (name => "library_name");
+my %INSTRUMENT_DATA_PROPERTIES = (
+      id => "id"
+    , flow_cell_id => "flow_cell_id"
+    , lane => "lane"
+    , clusters => "clusters"
+    );
+
+class Genome::Model::Tools::BamQc::SummaryFromInstrumentData {
+    is => "Genome::Command::Base",
+    has_input => [
+        instrument_data => {
+            is => "Genome::InstrumentData",
+            is_many => 1,
+            shell_args_position => 1,
+            doc => "Instrument data to summarize"
+        },
+
+        output_tsv => {
+            is => "String",
+            doc => "Summary output file"
+        },
+
+        output_pdf => {
+            is => "String",
+            doc => "Summary pdf file",
+            is_optional => 1
+        }
+    ],
+};
+
+sub _make_bamqc {
+    my $obj = shift;
+
+    my $path = $obj->disk_allocations->absolute_path;
+
+    my $x = {
+        obj => $obj,
+        path => $path,
+        get_path => sub {
+            my $expr = shift;
+            my $test = File::Spec->catfile($path, $expr);
+            my @results = glob($test);
+            return $results[0] if @results;
+        },
+    };
+
+    return $x;
+}
+
+sub _get_duplication_rate {
+    my $bam_qc = shift;
+    my $path = $bam_qc->{get_path}->("*_fastqc/fastqc_data.txt");
+    confess sprintf("Failed to find fastqc report for bamqc %s", $bam_qc->{obj}->id) unless $path;
+    my @lines = grep {/^#Total Duplicate Percentage/} read_file($path);
+    chomp @lines;
+    confess "Failed to find duplicate percentage in fastqc file $path" unless @lines == 1;
+    my ($x, $percent) = split("\t", $lines[0]);
+    return sprintf("%.03f", $percent);
+}
+
+sub _find_bamqc {
+    my $idata = shift;
+
+    my @alignment_results = $idata->get_default_alignment_results;
+    confess sprintf("No alignment results for instrument data %s", $idata->id) unless @alignment_results;
+    my @ids = map {$_->id} @alignment_results;
+    my @bam_qc = Genome::InstrumentData::AlignmentResult::Merged::BamQc->get(
+        'alignment_result_id in' => \@ids);
+
+    confess sprintf("No BamQc found for instrument data %s", $idata->id) unless @bam_qc;
+
+    my @available = grep {!$_->disk_allocations->is_archived} @bam_qc;
+    if (!@available) {
+        confess sprintf("All BamQc results for instrument data %s are archived:\n\t%s",
+            join("\n\t", map {
+                    sprintf("BamQc id: %s, Allocation id: %s", $_->id, $_->disk_allocations->id)
+                } @bam_qc)
+            );
+    }
+
+    return _make_bamqc($bam_qc[0]);
+}
+
+sub _fields {
+    return
+          values %INSTRUMENT_DATA_PROPERTIES
+        , values %LIBRARY_PROPERTIES
+        , values %SAMPLE_PROPERTIES
+        , "duplicate_percent"
+        , "bamqc_path"
+        ;
+}
+
+sub _generate_plots {
+    my $self = shift;
+    return 1 unless $self->output_pdf;
+    my @cmd = ("Rscript", $R_SCRIPT, $self->output_tsv, $self->output_pdf);
+    return Genome::Sys->shellcmd(
+          cmd => join(" ", @cmd)
+        , input_files => [$self->output_tsv]
+        , output_files => [$self->output_pdf]
+        );
+}
+
+sub execute {
+    my $self = shift;
+
+    my @idata = $self->instrument_data;
+    my @bam_qcs = map {_find_bamqc($_)} @idata;
+
+    $self->status_message(sprintf("Found %d bam qc results", scalar(@bam_qcs)));
+
+    my @fields = _fields();
+
+    my $tsv_fh = Genome::Sys->open_file_for_writing($self->output_tsv);
+    $tsv_fh->print(join("\t", @fields), "\n");
+    for my $i (0..$#idata) {
+        my $id = $idata[$i];
+        my $bqc = $bam_qcs[$i];
+        my $sample = $id->sample;
+        my $library = $id->library;
+
+        my $dup_rate = _get_duplication_rate($bqc);
+        my %properties = (
+              (map {$LIBRARY_PROPERTIES{$_} => $library->$_} keys %LIBRARY_PROPERTIES)
+            , (map {$SAMPLE_PROPERTIES{$_} => $sample->$_} keys %SAMPLE_PROPERTIES)
+            , (map {$INSTRUMENT_DATA_PROPERTIES{$_} => $id->$_} keys %INSTRUMENT_DATA_PROPERTIES)
+            , duplicate_percent => $dup_rate
+            , bamqc_path => $bqc->{path}
+            );
+
+        $tsv_fh->print(join("\t", @properties{@fields}), "\n");
+    }
+    $tsv_fh->close();
+
+    return $self->_generate_plots();
+}
+
+1;
---
+++ lib/perl/Genome/Model/Tools/BamQc/SummarizeInstrumentData.pm.R
@@ -0,0 +1,136 @@
+#!/usr/bin/env Rscript
+
+library(plyr)
+library(gplots)
+library(ggplot2)
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 2) {
+    stop("Give input file, output pdf")
+}
+
+input_file <- args[1]
+output_pdf <- args[2]
+
+load_isize_data <- function(path) {
+    x <- read.table(path, head=TRUE, skip=10)
+    colnames(x) <- c("insert_size", "count")
+    x$count <- x$count / sum(x$count)
+    x
+}
+
+load_sample <- function(name, data) {
+    ds <- data[data$sample_name == name,]
+    rv <- NULL
+    for (i in 1:nrow(ds)) {
+        path <- Sys.glob(sprintf("%s/*insert_size_metrics", ds$bamqc_path[i]))
+        lib <- ds$library_name[i]
+        id <- ds$id[i]
+        x <- load_isize_data(path)
+        x$lib <- lib
+        x$id <- id
+        x$flow_cell_id <- ds$flow_cell_id[i]
+        x$lane <- ds$lane[i]
+        rv <- rbind(rv, x)
+    }
+    rv$lib <- factor(rv$lib)
+    o <- order(rv$lib, rv$id)
+    rv$id <- factor(rv$id, levels=unique(rv$id[o]))
+    rv$flow_cell_id <- paste(rv$flow_cell_id, rv$lane)
+    rv
+}
+
+isize_plot <- function(df) {
+    ggplot(df) +
+        geom_bar(mapping=aes(x=insert_size, y=count, color=lib, fill=lib),
+            stat="identity", position="stack") +
+        labs(x="Insert size", y="Density") +
+        facet_grid(id ~ ., scales="free_y") +
+        scale_color_brewer(palette="Set1", name="Library") +
+        scale_fill_brewer(palette="Set1", name="Library") +
+        opts(panel.background=theme_blank(), panel.grid=theme_blank(),
+            legend.position="top", legend.direction="vertical")
+}
+
+data <- read.table(input_file,
+                   head=TRUE, stringsAsFactors=FALSE)
+data$duplicate_percent <- as.numeric(as.character(data$duplicate_percent))
+
+dup.smp <- ddply(data, .(sample_name), function (x) {
+        dr <- sum(x$clusters * x$duplicate_percent) / sum(x$clusters)
+        data.frame(duplication=dr, common_name=unique(x$common_name), ndata=nrow(x))
+    })
+
+dup.lib <- ddply(data, .(library_name), function (x) {
+        dr <- sum(x$clusters * x$duplicate_percent) / sum(x$clusters)
+        data.frame(duplication=dr, common_name=unique(x$common_name), ndata=nrow(x))
+    })
+
+dup.smp$sample_name <- sprintf("%s (%d idata)", dup.smp$sample_name, dup.smp$ndata)
+dup.lib$library_name <- sprintf("%s (%d idata)", dup.lib$library_name, dup.lib$ndata)
+
+print(dup.smp)
+print(dup.lib)
+
+pdf(output_pdf, width=11, height=8)
+
+dup.smp$sample_name <- factor(
+    dup.smp$sample_name,
+    levels=dup.smp$sample_name[order(dup.smp$duplication)]
+    )
+
+dup.lib$library_name <- factor(
+    dup.lib$library_name,
+    levels=dup.lib$library_name[order(dup.lib$duplication)]
+    )
+
+maxdup <- min(100, max(data$duplicate_percent + 10))
+
+text_hjust <- 1.2
+
+ggplot(dup.smp, aes(sample_name, duplication)) +
+    geom_bar(mapping=aes(color=common_name), stat="identity", position="stack") +
+    coord_flip(ylim=c(0, maxdup)) +
+    geom_text(aes(label=sprintf("%.2f%%", duplication), color=common_name), hjust=text_hjust) +
+    opts(panel.background=theme_blank(), panel.grid=theme_blank()) +
+    opts(title="Duplication by Sample") +
+    scale_color_brewer(palette="Set2")
+
+ggplot(dup.lib, aes(library_name, duplication)) +
+    geom_bar(aes(color=common_name), stat="identity", position="stack") +
+    coord_flip(ylim=c(0, maxdup)) +
+    geom_text(aes(label=sprintf("%.2f%%", duplication)), hjust=text_hjust, color="lightgrey") +
+    opts(panel.background=theme_blank(), panel.grid=theme_blank()) +
+    opts(title="Duplication by Library") +
+    scale_color_brewer(palette="Set2")
+
+ggplot(data, aes(duplicate_percent, fill=common_name)) +
+    geom_histogram() +
+    facet_grid(common_name ~ .) +
+    labs(x="Duplicate percent") +
+    opts(legend.position="top", legend.direction="vertical", title="Duplication by Tissue Type") +
+    scale_fill_brewer(palette="Set2")
+
+ggplot(data, aes(clusters, duplicate_percent, color=common_name)) +
+    geom_point() +
+    labs(x="# Clusters", y="Duplicate percent") +
+    opts(title="Duplication by # Clusters") +
+    opts(legend.position="top", legend.direction="vertical") +
+    scale_color_brewer(palette="Set2")
+
+samples <- unique(data$sample_name)
+for (s in samples) {
+    cat(sprintf("Processing sample %s...\n", s))
+    ds <- data[data$sample_name == s,]
+    ds <- ds[order(ds$library_name, ds$flow_cell_id, ds$lane),]
+    libs <- unique(ds$library_name)
+    df <- load_sample(s, data)
+    title <- sprintf("Sample %s (%s)", s, unique(ds$common_name))
+    p <- isize_plot(df) + opts(title=title)
+    tbl <- ds[, c("library_name", "id", "flow_cell_id", "lane", "clusters", "duplicate_percent")]
+    textplot(tbl, show.rownames=FALSE)
+    title(title)
+    print(p)
+}
+
+dev.off()
