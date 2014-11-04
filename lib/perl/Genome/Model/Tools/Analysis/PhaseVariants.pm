ae51c2fe8577ede4a77835fb8b8866b80ca662ef
Thomas Mooney <tmooney@genome.wustl.edu>
2014-11-04 16:48:05 -0600

    Whitespace.

--- lib/perl/Genome/Model/Tools/Analysis/PhaseVariants.pm
+++ lib/perl/Genome/Model/Tools/Analysis/PhaseVariants.pm
@@ -3,29 +3,29 @@ package Genome::Model::Tools::Analysis::PhaseVariants;     # rename this when yo
 use strict;
 use warnings;
 use File::Basename;
-use File::Spec; 
+use File::Spec;
 use Genome;                                 # using the namespace authorizes Class::Autouse to lazy-load modules under it
 
 class Genome::Model::Tools::Analysis::PhaseVariants {
-    is => 'Command',                       
-    has => [                                # specify the command's single-value properties (parameters) <--- 
-       distance      	=> { is => 'String', is_optional => 1, doc => "Max Distance between SNV location to check if in phase" },
-         command    	=> { is => 'String', is_optional => 1, doc => "Command user wants to run - B for build or C for check"},
-        vcfFile   => { is => 'String', is_optional => 1, doc => "Path to VCF file to find SNVs"},
+    is => 'Command',
+    has => [                                # specify the command's single-value properties (parameters) <---
+        distance    => { is => 'String', is_optional => 1, doc => "Max Distance between SNV location to check if in phase" },
+        command     => { is => 'String', is_optional => 1, doc => "Command user wants to run - B for build or C for check"},
+        vcfFile     => { is => 'String', is_optional => 1, doc => "Path to VCF file to find SNVs"},
         bamFile     => { is => 'String', is_optional => 1, doc => "Path to bam file"},
-        chromosome => { is => 'String', is_optional => 1, doc => "Chromosome of interest"},  
-	snvs => { is => 'String', is_optional => 1, doc => "location and alt alleles"},
-	relax => { is => 'String', is_optional => 1, doc => "use intsec failed SNVs"},
-	sample => { is => 'String', is_optional => 1, doc => "sample name"}, 
-        printReads => { is => 'String', is_optional => 1, doc => " print reads?"}, 
-	outFile => { is => 'String', is_optional => 1, doc => " output file"}, 
-    ], 
+        chromosome  => { is => 'String', is_optional => 1, doc => "Chromosome of interest"},
+        snvs        => { is => 'String', is_optional => 1, doc => "location and alt alleles"},
+        relax       => { is => 'String', is_optional => 1, doc => "use intsec failed SNVs"},
+        sample      => { is => 'String', is_optional => 1, doc => "sample name"},
+        printReads  => { is => 'String', is_optional => 1, doc => " print reads?"},
+        outFile     => { is => 'String', is_optional => 1, doc => " output file"},
+    ],
 };
 
 sub new {
     my ($class) = @_;
     my $self = bless {}, $class;
-    return $self; 
+    return $self;
 }
 
 
@@ -34,58 +34,58 @@ sub help_brief {
 }
 
 sub help_detail {
-	
+
 }
 
 sub execute {
-	my $self=shift;
-	my $dirName = dirname(__FILE__); 
-	
-	my $command = "java -jar ";
-	$command .= File::Spec->catfile( $dirName, 'PhaseVariants.jar' );
-	
-	$command .= ' --o ';
-	$command .= $self->outFile;
+    my $self=shift;
+    my $dirName = dirname(__FILE__);
 
-	$command .= ' --p ';
-	$command .= $self->printReads;
+    my $command = "java -jar ";
+    $command .= File::Spec->catfile( $dirName, 'PhaseVariants.jar' );
 
-	$command .= ' --m ';
-	$command .= $self->distance;
+    $command .= ' --o ';
+    $command .= $self->outFile;
 
-	$command .= ' --c ';
-	$command .= $self->command;
-	
-	$command .= ' --f ';
-	$command .= $self->vcfFile;
+    $command .= ' --p ';
+    $command .= $self->printReads;
 
-	$command .= ' --b ';
-	$command .= $self->bamFile;
-		
-	$command .= ' --chr ';
-	$command .= $self->chromosome;	
-	
-	$command .= ' --s ';
-	$command .= $self->sample;
+    $command .= ' --m ';
+    $command .= $self->distance;
 
-	$command .= ' --R ';
-	$command .= $self->relax;
+    $command .= ' --c ';
+    $command .= $self->command;
 
-	$command .= ' --l ';
-	$command .= $self->snvs;
+    $command .= ' --f ';
+    $command .= $self->vcfFile;
 
-	# Figure out where the accessory files are for R (these are not provided at the command line by the user)
-	#my $javaLib = File::Spec->catfile( $dirName, 'OncoPlot', 'Sink.jar' );
-	#my $rHelpLib = File::Spec->catfile($dirName, 'OncoPlot', 'oncoPrintHelper.R');
-	
-	#$command .= ' --helper-file=';
-	#$command .= $rHelpLib;
+    $command .= ' --b ';
+    $command .= $self->bamFile;
 
-	#$command .= ' --java-lib-file=';
-	#$command .= $javaLib;
-	
-	print($command);
-	print("\n");
-	system($command); # this executes the command
-	1;
- }
+    $command .= ' --chr ';
+    $command .= $self->chromosome;
+
+    $command .= ' --s ';
+    $command .= $self->sample;
+
+    $command .= ' --R ';
+    $command .= $self->relax;
+
+    $command .= ' --l ';
+    $command .= $self->snvs;
+
+    # Figure out where the accessory files are for R (these are not provided at the command line by the user)
+    #my $javaLib = File::Spec->catfile( $dirName, 'OncoPlot', 'Sink.jar' );
+    #my $rHelpLib = File::Spec->catfile($dirName, 'OncoPlot', 'oncoPrintHelper.R');
+
+    #$command .= ' --helper-file=';
+    #$command .= $rHelpLib;
+
+    #$command .= ' --java-lib-file=';
+    #$command .= $javaLib;
+
+    print($command);
+    print("\n");
+    system($command); # this executes the command
+    1;
+}
--- lib/perl/Genome/Model/Tools/Analysis/PhaseVariants.t
+++ lib/perl/Genome/Model/Tools/Analysis/PhaseVariants.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 #use Genome::Model::phaseVariants;
-use above "Genome"; 
+use above "Genome";
 use Test::More tests => 1;
 use File::Basename;
 #use File::Spec my $dirName = dirname(__FILE__);
@@ -12,16 +12,16 @@ use File::Temp qw(tempfile);
 #print($INC{"phaseVariants.pm"}, "\n");
 
 my $plot = Genome::Model::Tools::Analysis::PhaseVariants->create(
-                                                         distance  => 700,
-                                                         command => "C",
-							 vcfFile => "/gscuser/cfederer/Documents/mysnvs.vcf",
-                                                         bamFile => "/gscuser/cfederer/Documents/p53.bam",
-                                                         chromosome => 17,
-							 sample => "none",
-							 relax => "false", 
-							 snvs => "7579646T7579653C", 
-	 						 printReads => "t", 
-							 outFile => "/gscuser/cfederer/git/genome/lib/perl/Genome/Model/phaseVariantsOutPut.txt"
-                                                        );
+    distance  => 700,
+    command => "C",
+    vcfFile => "/gscuser/cfederer/Documents/mysnvs.vcf",
+    bamFile => "/gscuser/cfederer/Documents/p53.bam",
+    chromosome => 17,
+    sample => "none",
+    relax => "false",
+    snvs => "7579646T7579653C",
+    printReads => "t",
+    outFile => "/gscuser/cfederer/git/genome/lib/perl/Genome/Model/phaseVariantsOutPut.txt"
+);
 ok($plot->execute,'Executed');
-                                                  
+
