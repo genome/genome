ea96a1a92fffba1c6c9cd26d204068b1ea288821
apregier <aregier@genome.wustl.edu>
2014-11-05 12:08:30 -0600

    Add test for translation validation

---
+++ lib/perl/Genome/VariantReporting/Framework/Test/WithTranslationsRun.pm
@@ -0,0 +1,23 @@
+package Genome::VariantReporting::Framework::Test::WithTranslationsRun;
+
+use strict;
+use warnings FATAL => 'all';
+use Genome;
+
+class Genome::VariantReporting::Framework::Test::WithTranslationsRun {
+    is => 'Genome::VariantReporting::Framework::Component::Expert::Command',
+    has_input => [
+        __planned__ => {},
+        __input__ => {
+            is_many => 1,
+        },
+    ],
+};
+
+sub name {
+    '__translated_test__';
+}
+
+sub result_class {
+    'Genome::VariantReporting::Framework::Test::RunResult';
+}
