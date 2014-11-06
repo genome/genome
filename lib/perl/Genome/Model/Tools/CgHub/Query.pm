9c6a59d94ef4d5c03a2f930a0c889ee68729fd0b
Eddie Belter <ebelter@gmail.com>
2014-11-06 15:29:50 -0600

    GMT CgHub: add output file handling to query cmd

--- lib/perl/Genome/Model/Tools/CgHub/Query.pm
+++ lib/perl/Genome/Model/Tools/CgHub/Query.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use Genome;
 
+use File::Temp;
+
 class Genome::Model::Tools::CgHub::Query {
     is => "Genome::Model::Tools::CgHub::Base",
     has_input => {
@@ -19,8 +21,11 @@ class Genome::Model::Tools::CgHub::Query {
             doc => 'Save XML output to this file instead of formatted results.',
         },
         output_file => {
-            doc => 'Save standard output/error to this file. If given the xml file, the will only contain the status of the query.',
+            doc => 'Save standard output/error to this file. If given the xml file, this fie will only contain the status of the query.',
         },
+    },
+    has_transient => {
+        _was_output_file_given => { is => 'Boolean', },
     },
     doc => 'Query CG Hub for metadata.',
 };
@@ -28,12 +33,19 @@ class Genome::Model::Tools::CgHub::Query {
 sub _build_command {
     my $self = shift;
 
-    my $cmd = "cgquery";
-    my $xml_file = $self->xml_file;
-    $cmd .= " -o $xml_file" if $xml_file;
-    $cmd .= " analysis_id=".$self->uuid;
+    my @cmd_parts = ( "cgquery", "analysis_id=".$self->uuid );
+    if ( $self->xml_file ) {
+        push @cmd_parts, "-o", $self->xml_file;
+    }
 
-    return $cmd;
+    my $output_file = $self->output_file;
+    if ( not $output_file ) {
+        $output_file = $self->output_file( File::Temp::tempfile(CLEANP => 1) );
+        $self->_was_output_file_given(1);
+    }
+    push @cmd_parts, '>', $output_file;
+
+    return join(' ', @cmd_parts);
 }
 
 sub _verify_success {
@@ -44,12 +56,21 @@ sub _verify_success {
         die $self->error_message('CG Query failed to generate output file!');
     }
 
+    my $print_output;
+    if ( $self->_was_output_file_given ) {
+        my $fh = Genome::Sys->open_file_for_reading($output_file);
+        $print_output = sub{ $fh->print($_); };
+    }
+    else {
+        $print_output = sub{ print $_; };
+    }
+
     my $output_fh = Genome::Sys->open_file_for_reading($output_file);
     my $number_of_hits = 0;
     while ( my $line = $output_fh->getline ) {
+        $print_output->($line);
         if ( $line =~ /Matching Objects\s+:\s+(\d+)/ ) {
             $number_of_hits = $1;
-            last;
         }
     }
     $output_fh->close;
