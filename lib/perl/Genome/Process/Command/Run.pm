814cea3c15e9bd6bba86d3a50aea894dc1a422a5
dmorton <dmorton@genome.wustl.edu>
2014-11-17 20:26:08 -0600

    Use 'error_message' instead of 'status_message'

--- lib/perl/Genome/Process/Command/Run.pm
+++ lib/perl/Genome/Process/Command/Run.pm
@@ -157,7 +157,7 @@ sub _ensure_bresume_if_commit {
         aspect => 'commit',
         callback => sub {
             my $bresume_output = `bresume $job_id`; chomp $bresume_output;
-            $self->status_message($bresume_output) unless ( $bresume_output =~ /^Job <$job_id> is being resumed$/ );
+            $self->error_message($bresume_output) unless ( $bresume_output =~ /^Job <$job_id> is being resumed$/ );
         },
     );
     unless ($commit_observer) {
