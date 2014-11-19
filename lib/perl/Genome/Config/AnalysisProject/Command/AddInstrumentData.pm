e3f539851f2285af47755b927b93cad529d6310c
Jim Weible <jweible@genome.wustl.edu>
2014-11-19 10:59:11 -0600

    Fail when adding instrument-data to stuff that is done or archived or otherwise should not be added to

--- lib/perl/Genome/Config/AnalysisProject/Command/AddInstrumentData.pm
+++ lib/perl/Genome/Config/AnalysisProject/Command/AddInstrumentData.pm
@@ -61,4 +61,18 @@ sub execute {
     return 1;
 }
 
+sub __errors__ {
+    my $self = shift;
+    my @errors = $self->SUPER::__errors__(@_);
+    my $status = $self->analysis_project->status;
+    unless(grep{$_ eq $status} ("Pending", "Hold", "In Progress")){
+        push @errors, UR::Object::Tag->create(
+            type => 'error',
+            properties => ['analysis_project'],
+            desc => "Can't assign to analysis project with status: $status" 
+        );
+    }
+    return @errors;
+}
+
 1;
