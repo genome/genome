60cec884d3f7e63d1cdba09e9e31fc163bdeaa3c
dmorton <dmorton@genome.wustl.edu>
2014-12-05 14:41:42 -0600

    Make CreateReport less noisy

--- lib/perl/Genome/VariantReporting/Command/CreateReport.pm
+++ lib/perl/Genome/VariantReporting/Command/CreateReport.pm
@@ -70,6 +70,7 @@ sub execute {
     $p->save_plan_file($self->plan_file);
     $p->save_translations_file($self->translations_file);
 
+    $self->status_message("Constructing workflow from plan file (%s)", $self->plan_file);
     $p->run(workflow_xml => $self->dag->get_xml,
         workflow_inputs => $self->workflow_inputs($p->id),
     );
@@ -112,7 +113,7 @@ sub provider {
     my $self = shift;
 
     unless (defined($self->__provider)) {
-        $self->status_message("Constructing translation-provider from file (%s)", $self->translations_file);
+        $self->debug_message("Constructing translation-provider from file (%s)", $self->translations_file);
         my $provider = Genome::VariantReporting::Framework::Component::RuntimeTranslations->create_from_file($self->translations_file);
 
         $self->__provider($provider);
@@ -124,7 +125,6 @@ sub dag {
     my $self = shift;
 
     unless (defined($self->__dag)) {
-        $self->status_message("Constructing workflow from plan.");
         my $dag = generate_dag($self->plan, $self->variant_type);
         $dag->declare_constant(
             input_vcf => $self->input_vcf,
