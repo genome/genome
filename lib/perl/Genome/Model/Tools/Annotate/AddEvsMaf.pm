87655ae1c1845d12c92e07dfe112bc830962b9c8
Chris Miller <cmiller@genome.wustl.edu>
2014-12-09 13:31:14 -0600

    adding additional documentation

--- lib/perl/Genome/Model/Tools/Annotate/AddEvsMaf.pm
+++ lib/perl/Genome/Model/Tools/Annotate/AddEvsMaf.pm
@@ -25,7 +25,7 @@ sub help_synopsis {
 }
 
 sub help_detail {
-    q(This tool appends allele frequency information from the Exome Variant Server to the end of an annotation file as three extra columns. Missing or undefined values are denoted by '-'.);
+    q(This tool appends allele frequency information from the Exome Variant Server to the end of an annotation file as three extra columns. (EA_MAF = frequency in European American population, AA_MAF = frequency in African-American population, All_MAF = frequency in all individuals).  Missing or undefined values are denoted by '-'.);
 }
 
 sub execute {
