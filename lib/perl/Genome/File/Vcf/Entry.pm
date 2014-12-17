a40e73ea13267236ab8499cd095a87c77f0b25e1
apregier <aregier@genome.wustl.edu>
2014-12-17 15:07:16 -0600

    Factor out is_deletion

--- lib/perl/Genome/File/Vcf/Entry.pm
+++ lib/perl/Genome/File/Vcf/Entry.pm
@@ -275,11 +275,22 @@ sub has_indel {
 sub has_deletion {
     my $self = shift;
     for my $alt (@{$self->{alternate_alleles}}) {
-        if (length($alt) < length($self->{reference_allele})) {
+        if ($self->is_deletion($alt)) {
             return 1;
         }
     }
     return 0;
+}
+
+sub is_deletion {
+    my ($self, $alt) = @_;
+
+    if (length($alt) < length($self->{reference_allele})) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 sub has_insertion {
