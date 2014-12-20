8b7210e87a35435cad64b20b5f2a6a11a418e5c5
dmorton <dmorton@genome.wustl.edu>
2014-12-19 18:10:16 -0600

    Use File::Location in BufferedReader

--- lib/perl/Genome/File/BamReadcount/BufferedReader.pm
+++ lib/perl/Genome/File/BamReadcount/BufferedReader.pm
@@ -3,8 +3,8 @@ package Genome::File::BamReadcount::BufferedReader;
 use strict;
 use warnings;
 use Genome::File::BamReadcount::Reader;
-use Set::Scalar;
-use Sort::strverscmp;
+use Genome::File::Location;
+use Data::Dump qw(pp);
 
 sub new {
     my ($class, @args) = @_;
@@ -14,10 +14,8 @@ sub new {
         _reader => $reader,
         _prev_entry => undef,
         _cur_entry => undef,
-        _prev_pos => -1,
-        _prev_chrom => -1,
-        _cur_pos => -1,
-        _cur_chrom => 1,
+        _prev_loc => Genome::File::Location->new(-1, -1),
+        _cur_loc => Genome::File::Location->new(-1, -1),
     };
 
     bless $self, $class;
@@ -27,23 +25,19 @@ sub new {
 sub get_entry {
     my ($self, $chrom, $pos) = @_;
 
-    while (
-        ($chrom eq $self->{_cur_chrom} and $pos > $self->{_cur_pos}) or
-        strverscmp($chrom, $self->{_cur_chrom}) == 1
-    ) {
+    my $target_loc = Genome::File::Location->new($chrom, $pos);
+    while ($target_loc->is_greater_than($self->{_cur_loc})) {
         $self->read_line;
         return unless defined $self->{_cur_entry};
     }
-    if ($pos == $self->{_prev_pos} and $chrom eq $self->{_prev_chrom}) {
+
+    if ($target_loc->is_equal_to($self->{_prev_loc})) {
         return $self->{_prev_entry};
     }
-    elsif ($pos == $self->{_cur_pos} and $chrom eq $self->{_cur_chrom}) {
+    elsif ($target_loc->is_equal_to($self->{_cur_loc})) {
         return $self->{_cur_entry};
     }
-    elsif (
-        ($chrom eq $self->{_prev_chrom} and $pos < $self->{_prev_pos}) or
-        strverscmp($chrom, $self->{_prev_chrom}) == -1
-    ) {
+    elsif ($target_loc->is_less_than($self->{_prev_loc})) {
         die sprintf("Position %s on chromosome %s has already been passed by.  Current state: %s",
             $pos,
             $chrom,
@@ -57,30 +51,39 @@ sub get_entry {
 sub to_string {
     my $self = shift;
     my %params = %$self;
+
     delete $params{_cur_entry};
     delete $params{_prev_entry};
+
+    $params{file_name} = $params{_reader}->{name};
     delete $params{_reader};
-    $params{file_name} = $self->{_reader}->{name};
-    Data::Dumper::Dumper(\%params);
+
+    $params{_prev_loc} = $params{_prev_loc}->to_string();
+    $params{_cur_loc} = $params{_cur_loc}->to_string();
+    return pp(\%params);
 }
 
 sub read_line {
     my $self = shift;
     $self->{_prev_entry} = $self->{_cur_entry};
-    $self->{_prev_pos} = $self->{_cur_pos};
-    $self->{_prev_chrom} = $self->{_cur_chrom};
+    $self->{_prev_loc}->set_equal_to($self->{_cur_loc});
 
-    $self->{_cur_entry} = $self->{_reader}->next;
-    if (defined $self->{_cur_entry}) {
-        if ($self->{_cur_pos} == $self->{_cur_entry}->{_position} and
-            $self->{_cur_chrom} eq $self->{_cur_entry}->{_chromosome} ) {
-            die sprintf("Duplicate entries detected at chromosome (%s) position (%s).
-                Bam readcount file must be deduplicated",
-                $self->{_cur_chrom},
-                $self->{_cur_pos});
+    my $entry = $self->{_reader}->next;
+    $self->{_cur_entry} = $entry;
+    if (defined $entry) {
+        my $entry_loc = Genome::File::Location->new(
+            $entry->{_chromosome},
+            $entry->{_position},
+        );
+        if ($self->{_cur_loc}->is_equal_to($entry_loc)) {
+            die sprintf("Duplicate entries detected at %s.  " .
+                "Bam readcount file (%s) must be deduplicated",
+                $self->{_cur_loc},
+                $self->{_reader}->{name}
+            );
+        } else {
+            $self->{_cur_loc}->set_equal_to($entry_loc);
         }
-        $self->{_cur_chrom} = $self->{_cur_entry}->{_chromosome};
-        $self->{_cur_pos} = $self->{_cur_entry}->{_position};
     }
     return;
 }
