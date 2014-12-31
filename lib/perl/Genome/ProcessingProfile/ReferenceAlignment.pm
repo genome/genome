82434c6d0ca8b3f2caa2ec96bda84b3dffc84033
Thomas Mooney <tmooney@genome.wustl.edu>
2014-12-31 08:54:13 -0600

    Remove old subclassification methods from ReferenceAlignment.
    These were replaced by the calculated+immutable version in 2010
    (in 35a703b98545aca94785442a997a95fc2b9178ae before the redaction).

--- lib/perl/Genome/ProcessingProfile/ReferenceAlignment.pm
+++ lib/perl/Genome/ProcessingProfile/ReferenceAlignment.pm
@@ -401,60 +401,6 @@ sub filter_ruleset_params {
 }
 
 
-#< SUBCLASSING >#
-#
-# This is called by the infrastructure to appropriately classify abstract processing profiles
-# according to their type name because of the "sub_classification_method_name" setting
-# in the class definiton...
-sub _X_resolve_subclass_name {
-    my $class = shift;
-
-    my $sequencing_platform;
-    if ( ref($_[0]) and $_[0]->can('params') ) {
-        my @params = $_[0]->params;
-        my @seq_plat_param = grep { $_->name eq 'sequencing_platform' } @params;
-        if (scalar(@seq_plat_param) == 1) {
-            $sequencing_platform = $seq_plat_param[0]->value;
-        }
-
-    }  else {
-        my %params = @_;
-        $sequencing_platform = $params{sequencing_platform};
-    }
-
-    unless ( $sequencing_platform ) {
-        my $rule = $class->define_boolexpr(@_);
-        $sequencing_platform = $rule->value_for('sequencing_platform');
-    }
-
-    return ( defined $sequencing_platform )
-    ? $class->_resolve_subclass_name_for_sequencing_platform($sequencing_platform)
-    : undef;
-}
-
-sub _resolve_subclass_name_for_sequencing_platform {
-    my ($class,$sequencing_platform) = @_;
-    my @type_parts = split(' ',$sequencing_platform);
-
-    my @sub_parts = map { ucfirst } @type_parts;
-    my $subclass = join('',@sub_parts);
-
-    my $class_name = join('::', 'Genome::ProcessingProfile::ReferenceAlignment' , $subclass);
-    return $class_name;
-}
-
-sub _resolve_sequencing_platform_for_class {
-    my $class = shift;
-
-    my ($subclass) = $class =~ /^Genome::ProcessingProfile::ReferenceAlignment::([\w\d]+)$/;
-    return unless $subclass;
-
-    return lc join(" ", ($subclass =~ /[a-z\d]+|[A-Z\d](?:[A-Z\d]+|[a-z]*)(?=$|[A-Z\d])/gx));
-
-    my @words = $subclass =~ /[a-z\d]+|[A-Z\d](?:[A-Z\d]+|[a-z]*)(?=$|[A-Z\d])/gx;
-    return lc(join(" ", @words));
-}
-
 #### IMPLEMENTATION #####
 
 sub stages {
