ac84fa8e1c38f752fc877ab02832f811719c8753
dmorton <dmorton@genome.wustl.edu>
2015-01-03 18:39:16 -0600

    Allow optional is_many translated plan inputs
    This was not working as intended, because if the plan didn't specify the
    optional input then it would be undef, which cannot be dereferenced as
    an array.

--- lib/perl/Genome/VariantReporting/Framework/Plan/Base.pm
+++ lib/perl/Genome/VariantReporting/Framework/Plan/Base.pm
@@ -186,8 +186,10 @@ sub _translate {
     }
 
     for my $name ($object_class->all_translated_is_many_input_names) {
-        my @old_values = @{$self->$params_accessor->{$name}};
-        next unless scalar(@old_values);
+        my $planned_value = $self->$params_accessor->{$name};
+        next unless defined($planned_value);
+
+        my @old_values = @{$planned_value};
         my $data_type = $object_class->__meta__->property_meta_for_name($name)->data_type;
         if (defined($data_type) && $data_type eq 'ARRAY') {
             $self->$params_accessor->{$name} = [map {$self->_translate_single($_, $translations, $name)} @old_values];
