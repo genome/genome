e937a337d4bddd67e52559e29e1e56dd337f5c0a
Tiandao Li <tli@genome.wustl.edu>
2015-01-20 16:31:58 -0600

    use whole_rmdup_bam_flagstat_file instead of reaching into build's data_directory

--- lib/perl/Genome/Model/ReferenceAlignment/Command/BsmapMethCalcConversionRate.pm
+++ lib/perl/Genome/Model/ReferenceAlignment/Command/BsmapMethCalcConversionRate.pm
@@ -31,40 +31,37 @@ sub execute {
 		$cfile = \*STDOUT;
 	}
 
-	# get the bam paths of the last succeeded build
-	my $dir = $self->model->last_succeeded_build->data_directory;
-	# flagstat 
-	my @flagstat = glob("$dir/alignments/*flagstat");
+	my $build = $self->model->last_succeeded_build;
+	my $flagstat = $build->whole_rmdup_bam_flagstat_file;
 	my (@field, $total, $duplicates, $mapped, $properly);
-	for my $flagstat (@flagstat) {
-		if (-s "$flagstat"){
-			print $cfile "\nMethylation alignment status:\n";
-			print $cfile $flagstat, "\n";
+	if (-s "$flagstat"){
+		print $cfile "\nMethylation alignment status:\n";
+		print $cfile $flagstat, "\n";
 
-			my $flagstat_data = Genome::Model::Tools::Sam::Flagstat->parse_file_into_hashref($flagstat);
-			print $cfile "Total read\t=\t", $flagstat_data->{total_reads}, "\n";
+		my $flagstat_data = Genome::Model::Tools::Sam::Flagstat->parse_file_into_hashref($flagstat);
+		print $cfile "Total read\t=\t", $flagstat_data->{total_reads}, "\n";
 
-			print $cfile "Duplicates\t=\t", $flagstat_data->{reads_marked_duplicates}, "\n";
-			if ($flagstat_data->{total_reads} != 0) {
-				my $dupe_rate = $flagstat_data->{reads_marked_duplicates} / $flagstat_data->{total_reads} * 100;
-				print $cfile "Duplicates rate (%)\t=\t", $dupe_rate, "\n";
-			}
-
-			print $cfile "Mapped read\t=\t", $flagstat_data->{reads_mapped}, "\n";
-			if ($flagstat_data->{total_reads} != 0) {
-				print $cfile "Mapped rate (%)\t=\t", $flagstat_data->{reads_mapped_percentage}, "\n";
-			}
-
-			print $cfile "Properly paired\t=\t", $flagstat_data->{reads_mapped_in_proper_pairs}, "\n";
-			if ($flagstat_data->{total_reads} != 0) {
-				print $cfile "Properly paired rate (%)\t=\t", $flagstat_data->{reads_mapped_in_proper_pairs_percentage}, "\n";
-			}
+		print $cfile "Duplicates\t=\t", $flagstat_data->{reads_marked_duplicates}, "\n";
+		if ($flagstat_data->{total_reads} != 0) {
+			my $dupe_rate = $flagstat_data->{reads_marked_duplicates} / $flagstat_data->{total_reads} * 100;
+			print $cfile "Duplicates rate (%)\t=\t", $dupe_rate, "\n";
 		}
-		else {
-			$self->error_message("can't find flagstat file");
+
+		print $cfile "Mapped read\t=\t", $flagstat_data->{reads_mapped}, "\n";
+		if ($flagstat_data->{total_reads} != 0) {
+			print $cfile "Mapped rate (%)\t=\t", $flagstat_data->{reads_mapped_percentage}, "\n";
+		}
+
+		print $cfile "Properly paired\t=\t", $flagstat_data->{reads_mapped_in_proper_pairs}, "\n";
+		if ($flagstat_data->{total_reads} != 0) {
+			print $cfile "Properly paired rate (%)\t=\t", $flagstat_data->{reads_mapped_in_proper_pairs_percentage}, "\n";
 		}
 	}
+	else {
+		$self->error_message("can't find flagstat file");
+	}
 
+	my $dir = $build->data_directory;
 	my %cases = (
 		MT => { glob => "$dir/variants/snv/meth-ratio-*/MT/snvs.hq", name => "mtDNA" },
 		lambda => { glob => "$dir/variants/snv/meth-ratio-*/gi_9626243_ref_NC_001416.1_/snvs.hq", name => "lambda" },
