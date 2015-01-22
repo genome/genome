1d9a81aec44d5a466347775f1da91d63a05153d7
Eddie Belter <ebelter@genome.wustl.edu>
2015-01-21 20:46:58 -0600

    InstData Import: unzip fastqs before running picard sam to fastq; up picard version to 1.113

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/FastqsToBam.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/FastqsToBam.pm
@@ -5,6 +5,9 @@ use warnings;
 
 use Genome;
 
+use Archive::Extract;
+$Archive::Extract::PREFER_BIN = 1;
+$Archive::Extract::DEBUG = 1;
 use Data::Dumper 'Dumper';
 require File::Basename;
 require List::Util;
@@ -44,6 +47,9 @@ sub execute {
     my $self = shift;
     $self->debug_message('Fastqs to bam...');
 
+    my $unarchive_if_necessary = $self->_unarchive_fastqs_if_necessary;
+    return if not $unarchive_if_necessary;
+
     my $fastq_to_bam_ok = $self->_fastqs_to_bam;
     return if not $fastq_to_bam_ok;
 
@@ -54,6 +60,40 @@ sub execute {
     return if not $cleanup_ok;
 
     $self->debug_message('Fastqs to bam...done');
+    return 1;
+}
+
+sub _unarchive_fastqs_if_necessary {
+    my $self = shift;
+    $self->debug_message('Unarchive fastqs if necessary...');
+
+    my @new_fastq_paths;
+    for my $fastq_path ( $self->fastq_paths ) {
+        if ( $fastq_path !~ /\.gz$/ ) {
+            $self->debug_message('Unarchive not necessary for '.$fastq_path);
+            push @new_fastq_paths, $fastq_path;
+            next;
+        }
+        my $unarchived_fastq_path = $fastq_path;
+        $unarchived_fastq_path =~ s/\.gz$//;
+        $self->debug_message('Unarchiving: '.$fastq_path);
+        my $rv = eval{ Genome::Sys->shellcmd(cmd => "gunzip $fastq_path"); };
+        #my $extract_ok = $extractor->extract(to => $unarchived_fastq);
+        if ( not $rv or not -s $unarchived_fastq_path ) {
+            #my $error_message = $extractor->error;
+            #$self->error_message($error_message) if defined $error_message;
+            #$self->error_message($error_message) if defined $error_message;
+            $self->error_message('Failed to unarchive!');
+            return;
+        }
+        #my $unarchived_files = $extractor->files;
+        $self->debug_message("Unarchived fastq: $unarchived_fastq_path");
+        push @new_fastq_paths, $unarchived_fastq_path;
+        unlink $fastq_path;
+    }
+    $self->fastq_paths(\@new_fastq_paths);
+
+    $self->debug_message('Unarchive fastqs if necessary...');
     return 1;
 }
 
@@ -71,6 +111,7 @@ sub _fastqs_to_bam {
         sample_name => $self->sample_name,
         library_name => $self->library_name,
         read_group_name => $self->library_name,
+        use_version => '1.113',
     );
     if ( $fastqs[1] ) {
         $self->debug_message("Fastq 2: $fastqs[1]");
