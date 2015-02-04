8203a52ae00e8daa0c5a20e3c84de2a20a938ad5
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-02-04 11:14:26 -0600

    move creation of VafInterpreter to take advantage of multi-sample capabilities

--- lib/perl/Genome/VariantReporting/Suite/BamReadcount/MinCoverageObservedInterpreter.pm
+++ lib/perl/Genome/VariantReporting/Suite/BamReadcount/MinCoverageObservedInterpreter.pm
@@ -31,13 +31,14 @@ sub _interpret_entry {
     my $entry = shift;
     my $passed_alt_alleles = shift;
 
+    my $interpreter = Genome::VariantReporting::Suite::BamReadcount::VafInterpreter->create(
+        sample_names => [$self->sample_names],
+        sample_name_labels => $self->sample_name_labels,
+    );
+    my %result = $interpreter->interpret_entry($entry, $passed_alt_alleles);
+
     my %coverages;
     for my $sample_name ($self->sample_names) {
-        my $interpreter = Genome::VariantReporting::Suite::BamReadcount::VafInterpreter->create(
-            sample_names => [$sample_name],
-            sample_name_labels => $self->sample_name_labels,
-        );
-        my %result = $interpreter->interpret_entry($entry, $passed_alt_alleles);
         for my $alt_allele (@$passed_alt_alleles) {
             my $ref_count = $result{$alt_allele}->{$interpreter->create_sample_specific_field_name('ref_count', $sample_name)};
             my $var_count = $result{$alt_allele}->{$interpreter->create_sample_specific_field_name('var_count', $sample_name)};
