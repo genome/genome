d59e48ed6c64d84007b25d99dcfb3b9343a7b23a
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-02-09 14:22:13 -0600

    Recognize new date format
    Capturing this new date format seems more desirable than changing how
    the logs are created for two reasons:
    1) changing how the logs are created would require re-packaging and
    re-deploying Flow
    2) updating the recognizer to allow this date format allows us to
    automatically determine historical errors

--- lib/perl/Genome/Model/Build/Command/DetermineError.pm
+++ lib/perl/Genome/Model/Build/Command/DetermineError.pm
@@ -23,6 +23,10 @@ our $WORKFLOW_DATE_AND_HOST = qr{
 our $PTERO_DATE = qr{
         \[(?<date>\d{4}/\d{2}/\d{2}\s\d{2}\:\d{2}\:\d{2}).\d+\]
     }x;
+our $FLOW_DATE = qr{
+        (?<date>\d{4}-\d{2}-\d{2}\s\d{2}\:\d{2}\:\d{2}).\d+
+    }x;
+
 our $ERROR_LOCATION = qr{
         at \s (?<error_source_file>\S+\.pm) \s line \s (?<error_source_line>\d+)
     }x;
@@ -30,8 +34,8 @@ our $ERROR_LOCATION = qr{
 our $ERROR_FINDING_REGEX = qr{
                 (?:
                     $WORKFLOW_DATE_AND_HOST
-                    |
-                    $PTERO_DATE
+                    | $PTERO_DATE
+                    | $FLOW_DATE
                 )
                 \s
                 (?:
