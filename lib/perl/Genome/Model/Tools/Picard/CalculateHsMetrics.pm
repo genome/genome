9fbeae3416954f8a1a86fd9647fc3403da48117b
Travis Abbott <tabbott@genome.wustl.edu>
2015-02-20 13:33:32 -0600

    convert CalculateHsMetrics to inherit gmt::picard::base

--- lib/perl/Genome/Model/Tools/Picard/CalculateHsMetrics.pm
+++ lib/perl/Genome/Model/Tools/Picard/CalculateHsMetrics.pm
@@ -6,43 +6,51 @@ use warnings;
 use Genome;
 
 class Genome::Model::Tools::Picard::CalculateHsMetrics {
-    is  => 'Genome::Model::Tools::Picard',
+    is  => 'Genome::Model::Tools::Picard::Base',
     has_input => [
         input_file   => {
             is  => 'String',
             doc => 'The SAM/BAM files to run on.  File type is determined by suffix.',
+            picard_param_name => 'INPUT',
         },
         output_file  => {
             is  => 'String',
             doc => 'The output metrics file',
+            picard_param_name => 'OUTPUT',
         },
         bait_intervals  => {
             is  => 'String',
             doc => 'An interval list file that contains the locations of the baits used.',
+            picard_param_name => 'BAIT_INTERVALS',
         },
         target_intervals => {
             is  => 'String',
             doc => 'An interval list file that contains the locations of the targets',
+            picard_param_name => 'TARGET_INTERVALS',
         },
         reference_sequence => {
             is => 'String',
             doc => 'The reference sequence aligned to. Default value: null.',
             is_optional => 1,
+            picard_param_name => 'REFERENCE_SEQUENCE',
         },
         per_target_coverage_file => {
             is => 'String',
             doc => '',
             is_optional => 1,
+            picard_param_name => 'PER_TARGET_COVERAGE',
         },
         bait_set_name => {
             is => 'String',
             doc => 'Bait set name. If not provided it is inferred from the filename of the bait intervals.',
             is_optional => 1,
+            picard_param_name => 'BAIT_SET_NAME',
         },
         metric_accumulation_level => {
             is => 'String',
             doc => 'The level(s) at which to accumulate metrics. Possible values: {ALL_READS, SAMPLE, LIBRARY, READ_GROUP} This option may be specified 0 or more times. This option can be set to \'null\' to clear the default list.',
             is_optional => 1,
+            picard_param_name => 'METRIC_ACCUMULATION_LEVEL',
         },
     ],
 };
@@ -58,42 +66,38 @@ sub help_detail {
 EOS
 }
 
-sub execute {
-    my $self = shift;
-
-    my $cmd = $self->picard_path .'/CalculateHsMetrics.jar net.sf.picard.analysis.directed.CalculateHsMetrics OUTPUT='. $self->output_file
-        .' INPUT='. $self->input_file .' BAIT_INTERVALS='. $self->bait_intervals .' TARGET_INTERVALS='. $self->target_intervals;
-    
-    my @input_files = ($self->input_file,$self->bait_intervals,$self->target_intervals);
-    my @output_files = ($self->output_file);
-    
-    if ($self->reference_sequence) {
-        push @input_files, $self->reference_sequence;
-        $cmd .= ' REFERENCE_SEQUENCE='. $self->reference_sequence;
-    }
-    if ($self->per_target_coverage_file) {
-        push @output_files, $self->per_target_coverage_file;
-        $cmd .= ' PER_TARGET_COVERAGE='. $self->per_target_coverage_file;
-    }
-    if ($self->metric_accumulation_level) {
-        $cmd .= ' METRIC_ACCUMULATION_LEVEL='. $self->metric_accumulation_level;
-    }
-    if ($self->bait_set_name) {
-        if ($self->use_version > '1.52') {
-            $cmd .= ' BAIT_SET_NAME='. $self->bait_set_name;
-        } else {
-            $self->warning_message('bait_set_name is not compatible with Picard version '. $self->use_version);
-        }
-    }
-
-    $self->run_java_vm(
-        cmd          => $cmd,
-        input_files  => \@input_files,
-        output_files => \@output_files,
-        skip_if_output_is_present => 0,
-    );
-    return 1;
+sub _jar_name {
+    return 'CalculateHsMetrics.jar';
 }
 
+sub _java_class_name {
+    return 'net.sf.picard.analysis.directed.CalculateHsMetrics';
+}
+
+sub _shellcmd_extra_params {
+    my $self = shift;
+
+    my @input_files = grep {defined $_} ($self->reference_sequence, $self->input_file);
+    my @output_files = grep {defined $_} ($self->per_target_coverage_file);
+
+    return (
+        input_files => \@input_files,
+        output_files => \@output_files,
+        skip_if_output_is_present => 0,
+        );
+}
+
+sub _validate_params {
+    my $self = shift;
+
+    if ($self->bait_set_name && $self->version_compare($self->use_version, '1.52') <= 0) {
+        $self->warning_message(
+            sprintf 'bait_set_name is not compatible with Picard version %s, ignoring.',
+                $self->use_version
+            );
+
+        $self->bait_set_name(undef);
+    }
+}
 
 1;
