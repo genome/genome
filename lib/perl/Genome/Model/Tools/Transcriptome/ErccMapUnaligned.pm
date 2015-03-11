6a1c758747a52837b3c6f9e50f33a16d0e16fbeb
Indraniel Das <idas@genome.wustl.edu>
2015-03-11 09:10:36 -0500

    + change algorithmic approach (bwa to bowtie)
      - using bowtie2 in a single-read way seems to give the desired results
      - using a unix "pipe" or "streaming" approach - this reduces the time
        to produce the desired alignment results

--- lib/perl/Genome/Model/Tools/Transcriptome/ErccMapUnaligned.pm
+++ lib/perl/Genome/Model/Tools/Transcriptome/ErccMapUnaligned.pm
@@ -42,11 +42,11 @@ class Genome::Model::Tools::Transcriptome::ErccMapUnaligned {
             default_value => '14000000000',
             is_optional => '1',
         },
-        bwa_version => {
+        bowtie2_version => {
             is => 'Text',
             doc => 'The version of bwa to run analysis.',
-            example_values => ['0.6.2'],
-            default_value => '0.6.2',
+            example_values => ['2.1.0'],
+            default_value => '2.1.0',
             is_optional => '1',
         },
     ],
@@ -69,26 +69,11 @@ EOS
 sub execute {
     my $self = shift;
     
-    my $ercc_bwa_index = $self->create_ercc_bwa_index();
-    my $unmapped_bam = $self->generate_unmapped_bam();
-
-    my $sai_read1 = $self->align_unmapped_reads(
-        bam => $unmapped_bam,
-        index => $ercc_bwa_index,
-        read_pair => 1
-    );
-
-#    my $sai_read2 = $self->align_unmapped_reads(
-#        bam => $unmapped_bam,
-#        index => $ercc_bwa_index,
-#        read_pair => 2
-#    );
+    my $ercc_bowtie_index = $self->create_ercc_bowtie_index();
 
     my $remapped_bam = $self->generate_remapped_bam(
-        index => $ercc_bwa_index,
-        sai_read1 => $sai_read1,
-        sai_read2 => $sai_read1,
-        unmapped_bam => $unmapped_bam,
+        index => $ercc_bowtie_index,
+        input_bam => $self->bam_file,
     );
 
     $self->index_bam($remapped_bam);
@@ -109,12 +94,26 @@ sub samtools {
     return $samtools_path;
 }
 
-sub bwa {
+sub bowtie2_align {
     my $self = shift;
-    my $version = $self->bwa_version;
-    my $bwa_path =
-      Genome::Model::Tools::Bwa->path_for_bwa_version($version);
-    return $bwa_path;
+    my $version = $self->bowtie2_version;
+    my $bowtie2_dir = Path::Class::Dir->new("/usr/bin");
+    my $bowtie2_align = $bowtie2_dir->file('bowtie2-align' . $version);
+    unless (-e $bowtie2_align) {
+        die "[err] Couldn't find '$bowtie2_align' on the file system!\n";
+    }
+    return $bowtie2_align;
+}
+
+sub bowtie2_build {
+    my $self = shift;
+    my $version = $self->bowtie2_version;
+    my $bowtie2_dir = Path::Class::Dir->new("/usr/bin");
+    my $bowtie2_build = $bowtie2_dir->file('bowtie2-build' . $version);
+    unless (-e $bowtie2_build) {
+        die "[err] Couldn't find '$bowtie2_build' on the file system!\n";
+    }
+    return $bowtie2_build;
 }
 
 sub ERCC_analysis_script {
@@ -146,103 +145,104 @@ sub load_ercc_control_info {
     return %ercc_control
 }
 
-sub generate_unmapped_bam {
-    my $self = shift;
-    my $unmapped_bam_basename = Genome::Sys->create_temp_file_path();
-    my $unmapped_bam_path = $unmapped_bam_basename .'.bam';
-
-    my $max_mem = $self->samtools_max_mem;
-    my $samtools_view_cmd = join(' ', 
-        $self->samtools, 'view', '-u -f 12', $self->bam_file ,
-        '|',
-        $self->samtools, 'sort', "-n -m $max_mem", '-',
-        $unmapped_bam_basename
-    );
-
-    Genome::Sys->shellcmd(
-        cmd => $samtools_view_cmd,
-        input_files => [$self->bam_file],
-        output_files => [$unmapped_bam_path],
-    );
-
-    return Path::Class::File->new($unmapped_bam_path);
-}
-
-sub create_ercc_bwa_index {
+sub create_ercc_bowtie_index {
     my $self = shift;
 
-    my $fasta_index_path = Genome::Sys->create_temp_file_path();
-
-    Genome::Sys->create_symlink($self->ercc_fasta_file,$fasta_index_path);
-    my $cmd = join(' ', $self->bwa, 'index', $fasta_index_path);
-
-    Genome::Sys->shellcmd(
-        cmd => $cmd,
-        input_files => [$fasta_index_path],
+    my $index_dir = Path::Class::Dir->new(
+        Genome::Sys->create_temp_directory('bowtie2-index')
     );
-
-    return Path::Class::File->new($fasta_index_path);
-}
-
-sub align_unmapped_reads {
-    my $self = shift;
-    my %args = @_;
-    my ($bam, $bwa_index, $read_pair) = @args{'bam', 'index', 'read_pair'};
-
-    my $sai_path = Genome::Sys->create_temp_file_path();
+    my $fasta = Path::Class::File->new($self->ercc_fasta_file);
 
     my $cmd = join(' ',
-        $self->bwa,
-        "aln",
-        "-b -${read_pair}",
-        "-f ${sai_path}",
-        $bwa_index->stringify,
-        $bam->stringify
-    );
+        "cd $index_dir &&",
+        $self->bowtie2_build->stringify, "$fasta", 'ERCC');
 
     Genome::Sys->shellcmd(
         cmd => $cmd,
-        input_files => ["$bwa_index", "$bam"],
-        output_files => [$sai_path],
+        input_files => [$fasta->stringify],
     );
 
-    return Path::Class::File->new($sai_path);
+    my $search_pattern = $index_dir->file('ERCC.*');
+    my @index_files = glob("$search_pattern");
+    unless (@index_files == 6) {
+        die "[err] Didn't create the proper bowtie2 index file set in ",
+          "$index_dir !\n";
+    }
+
+    return $index_dir->file('ERCC');
 }
 
 sub generate_remapped_bam {
     my $self = shift;
     my %args = @_;
-    my ($index, $sai_read1_path, $sai_read2_path, $unmapped_bam) =
-      @args{'index', 'sai_read1', 'sai_read2', 'unmapped_bam'};
+    my ($index, $input_bam) = @args{'index', 'input_bam'};
+
+    $input_bam = Path::Class::File->new($input_bam);
 
     my $remapped_bam_basename = Genome::Sys->create_temp_file_path();
-    my $remapped_bam_path = $remapped_bam_basename .'.bam';
+    my $remapped_bam_path =
+      Path::Class::File->new($remapped_bam_basename . '.bam');
+
 
     my $max_mem = $self->samtools_max_mem;
-    my $cmd = join(' ',
-        $self->bwa, 'sampe',
-        "$index",
-        "$sai_read1_path", "$sai_read2_path",
-        "$unmapped_bam", "$unmapped_bam",
-        '|',
-        $self->samtools, 'view', '-u -S', '-',
-        '|',
-        $self->samtools, 'sort', "-m $max_mem", '-',
-        $remapped_bam_basename
+
+    # get the unaligned records from the input bam
+    my $cmd1 = join(' ',
+        $self->samtools, 'view', '-@ 4 -h -b -f 12',
+        "$input_bam", q{'*'}
     );
 
-    Genome::Sys->shellcmd(
-        cmd => $cmd,
-        input_files => [
-            "$index",
-            "$sai_read1_path",
-            "$sai_read2_path",
-            "$unmapped_bam"
-        ],
-        output_files => [$remapped_bam_path],
+    # gather the unaligned reads
+    my $cmd2 = join(' ',
+        $self->samtools, 'bam2fq',
+        '-s /dev/null',
+        '-'
     );
 
-    return Path::Class::File->new($remapped_bam_path);
+    # align the unaligned reads against the ERCC reference with bowtie2
+    my $bowtie_stderr = Path::Class::Dir->new(
+        Genome::Sys->create_temp_file_path('bowtie2.stderr')
+    );
+    my $cmd3 = join(' ',
+        $self->bowtie2_align->stringify,
+        "-x $index",
+        '-U -',
+        '--very-fast',
+        '--threads 4',
+        "2>$bowtie_stderr"
+    );
+
+    # sort the remapped alignments and generate the remapped bam
+    my $cmd4 = join(' ',
+        $self->samtools, 'sort',
+        '-@ 4',
+        "-m $max_mem",
+        '-',
+        "$remapped_bam_basename"
+    );
+
+    my $stdout =
+      Path::Class::Dir->new(
+          Genome::Sys->create_temp_file_path('bowtie-remapped.stdout')
+      );
+    my $stderr = Path::Class::Dir->new(
+        Genome::Sys->create_temp_file_path('bowtie-remapped.stderr')
+    );
+
+    my $stream = Genome::Sys::ShellPipeline->create(
+        pipe_commands => [$cmd1, $cmd2, $cmd3, $cmd4],
+        redirects => "> $stdout 2> $stderr",
+    );
+
+    $DB::single = 1;
+    $stream->execute
+      or die "[err] Trouble executing remapped bam stream command!\n";
+
+    unless (-e $remapped_bam_path) {
+        die "[err] Couldn't find the remapped bam: $remapped_bam_path \n!";
+    }
+
+    return $remapped_bam_path;
 }
 
 sub index_bam {
