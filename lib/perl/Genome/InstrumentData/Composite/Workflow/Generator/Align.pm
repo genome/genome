05fc93f0418b545288f8ac0ad1c457bd02d1dced
Thomas Mooney <tmooney@genome.wustl.edu>
2015-03-17 13:46:37 -0500

    Move "operation" subs only used by Align.

--- lib/perl/Genome/InstrumentData/Composite/Workflow/Generator/Align.pm
+++ lib/perl/Genome/InstrumentData/Composite/Workflow/Generator/Align.pm
@@ -244,4 +244,83 @@ sub _instrument_data_workflow_input_properties {
 
     return @input_properties;
 }
+
+my $_generate_operation_tmpl;
+sub _generate_operation {
+    my $class = shift;
+    my $action = shift;
+    my $instrument_data = shift;
+    my %options = @_;
+
+    my $key = '_operation';
+    if($instrument_data) {
+        $key = $class->_operation_key($instrument_data, %options);
+    }
+    return $action->{$key} if exists $action->{$key}; #multiple leaves will point to the same parent, so stop if we've already worked on this subtree
+
+    my $action_class_bases = {
+        align => 'Genome::InstrumentData::Command::AlignReads',
+        #filter => 'Genome::InstrumentData::Command::Filter', #TODO implement filter support
+    };
+
+    # TODO WF now has better support for customizing this, so this hack could be replaced
+    # Use PerLaneTophat class to override default lsf_resource
+    my $aligner_name = $action->{name};
+
+    if ($aligner_name eq 'per-lane-tophat' or $aligner_name eq 'star') {
+        my $subclass_name = 'Genome::InstrumentData::AlignmentResult'->_resolve_subclass_name_for_aligner_name($aligner_name);
+        $action_class_bases->{align} = 'Genome::InstrumentData::Command::AlignReads::'.$subclass_name;
+    }
+
+    my $class_name = $action_class_bases->{$action->{type}};
+
+    eval {
+        $class_name->__meta__;
+    };
+    if($@){
+        $class->error_message("Could not create an instance of " . $class_name);
+        die $class->error_message;
+    }
+
+    unless ($_generate_operation_tmpl) {
+        # 'id' will still be first, since they're sorted alpha order
+#ccc
+        $_generate_operation_tmpl
+            = UR::BoolExpr::Template->resolve('Workflow::Operation', 'id','name','workflow_operationtype_id')->get_normalized_template_equivalent();
+    }
+
+    my $operation = Workflow::Operation->create(
+        $_generate_operation_tmpl->get_rule_for_values(UR::Object::Type->autogenerate_new_object_id_urinternal(),
+        $class->get_unique_action_name($action),
+        Workflow::OperationType::Command->get($class_name)->id),
+    );
+
+    $action->{$key} = $operation;
+
+    return $operation;
+}
+
+sub _operation_key {
+    my $class = shift;
+    my $instrument_data = shift;
+    my %options = @_;
+
+    my $key = '_operation_' . $instrument_data->id;
+    if(exists $options{instrument_data_segment_id}) {
+        $key .= '_' . $options{instrument_data_segment_id};
+    }
+
+    return $key;
+}
+
+sub get_unique_action_name {
+    my $class = shift;
+    my $action = shift;
+
+    my $index = $class->next_counter_value;
+
+    my $name = $action->{name};
+    return join('_', $name, $index);
+}
+
 1;
