0002e49726db03a909ef2552eb90300de86243a3
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-03-18 10:59:49 -0500

    Framework for metrics parsing of Picard output

--- lib/perl/Genome/Qc/Tool/Picard.pm
+++ lib/perl/Genome/Qc/Tool/Picard.pm
@@ -10,7 +10,38 @@ class Genome::Qc::Tool::Picard {
 };
 
 sub get_metrics {
+    my $self = shift;
 
+    my $file = $self->output_file;
+    my $gmt_class = $self->gmt_class;
+    my %metrics = $self->metrics;
+    my %metric_results = %{$gmt_class->parse_file_into_metrics_hashref($file)};
+    return $self->_get_metrics(\%metrics, \%metric_results);
+}
+
+sub metrics {
+    my $self = shift;
+    die $self->error_message("Abstract method metrics must be overridden by subclass");
+}
+
+sub gmt_class {
+    my $self = shift;
+    die $self->error_message("Abstract method gmt_class must be overridden by subclass");
+}
+
+sub _get_metrics {
+    my ($self, $metrics, $metric_results) = @_;
+
+    my %desired_metric_results;
+    while (my ($metric, $metric_details) = each %{$metrics}) {
+        my $metric_key = $metric_details->{metric_key};
+        unless (defined($metric_key)) {
+            ($metric_key) = keys %{$metric_results};
+        }
+        my $picard_metric = $metric_details->{picard_metric};
+        $desired_metric_results{$metric} = $metric_results->{$metric_key}{$picard_metric};
+    }
+    return %desired_metric_results;
 }
 
 1;
