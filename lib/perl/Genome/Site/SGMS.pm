d87c50d2f8867c8033164ae57f635f104cc1baac
Avinash Ramu <aramu@genome.wustl.edu>
2015-03-31 10:42:35 -0500

    scope specific subdirectories

--- lib/perl/Genome/Site/SGMS.pm
+++ lib/perl/Genome/Site/SGMS.pm
@@ -1,6 +1,7 @@
 package Genome::Site::SGMS;
 use strict;
 use warnings;
+use File::Spec;
 
 BEGIN {
     unless ($INC{'Genome/Sys/Lock.pm'}) {
@@ -11,13 +12,16 @@ BEGIN {
 require Genome::Sys::Lock::FileBackend;
 Genome::Sys::Lock->add_backend('site',
     Genome::Sys::Lock::FileBackend->new(is_mandatory => 1,
-        parent_dir => $ENV{GENOME_LOCK_DIR}));
+        parent_dir => File::Spec->catdir(
+            $ENV{GENOME_LOCK_DIR}, 'site')));
 
 Genome::Sys::Lock->add_backend('unknown',
     Genome::Sys::Lock::FileBackend->new(is_mandatory => 1,
-        parent_dir => $ENV{GENOME_LOCK_DIR}));
+        parent_dir => File::Spec->catdir(
+            $ENV{GENOME_LOCK_DIR}, 'unknown')));
 
 Genome::Sys::Lock->add_backend('tgisan',
     Genome::Sys::Lock::FileBackend->new(is_mandatory => 1,
-        parent_dir => $ENV{GENOME_LOCK_DIR}));
+        parent_dir => File::Spec->catdir(
+            $ENV{GENOME_LOCK_DIR}, 'tgisan')));
 1;
