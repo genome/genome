225fb54013bdc33ebc751e145353ede5dac8151d
Avinash Ramu <aramu@genome.wustl.edu>
2015-04-03 14:08:26 -0500

    4space indent

--- lib/perl/Genome/Model/ClinSeq/Command/IdentifyLoh.pm
+++ lib/perl/Genome/Model/ClinSeq/Command/IdentifyLoh.pm
@@ -7,60 +7,60 @@ use Genome;
 
 class Genome::Model::ClinSeq::Command::IdentifyLoh {
   is => ['Command::V2',
-    'Genome::Model::ClinSeq::Util',
+        'Genome::Model::ClinSeq::Util',
   ],
   has_input => [
     clinseq_build => {
-      is => 'Genome::Model::Build::ClinSeq',
-      doc => 'ClinSeq build to identify LOH regions in.
-      [Either this or a somatic variation build is required.]',
-      is_optional => 1,
+        is => 'Genome::Model::Build::ClinSeq',
+        doc => 'ClinSeq build to identify LOH regions in.
+            [Either this or a somatic variation build is required.]',
+        is_optional => 1,
     },
     outdir => {
-      is => 'FilesystemPath',
-      doc => 'Directory where output files will be written.',
+        is => 'FilesystemPath',
+        doc => 'Directory where output files will be written.',
     },
     somvar_build => {
-      is => 'Genome::Model::Build::SomaticVariation',
-      doc => 'SomVar build to identify LOH regions in.
-      [Either this or a clinseq build is required.]',
-      is_optional => 1,
+        is => 'Genome::Model::Build::SomaticVariation',
+        doc => 'SomVar build to identify LOH regions in.
+        [Either this or a clinseq build is required.]',
+        is_optional => 1,
     },
     minprobes=> {
-      is => 'Number',
-      is_optional => 1,
-      doc => 'Minimum number of probes to call a loh segment.',
-      default => 10,
+        is => 'Number',
+        is_optional => 1,
+        doc => 'Minimum number of probes to call a loh segment.',
+        default => 10,
     },
     segment_pc_cutoff => {
-      is => 'Number',
-      is_optional => 1,
-      doc => 'Minimum percent of LOH in a segment. n_loh_probes/n_probes * 100',
-      default => 0.95,
+        is => 'Number',
+        is_optional => 1,
+        doc => 'Minimum percent of LOH in a segment. n_loh_probes/n_probes * 100',
+        default => 0.95,
     },
     test => {
-      is => 'Boolean',
-      doc => 'set for test-cases',
-      is_optional => 1,
-      default => 0,
+        is => 'Boolean',
+        doc => 'set for test-cases',
+        is_optional => 1,
+        default => 0,
     },
-  ],
-  doc => 'Identify regions of LOH using clinseq tumor/normal pairs.',
+    ],
+    doc => 'Identify regions of LOH using clinseq tumor/normal pairs.',
 };
 
 sub help_synopsis {
-  return <<EOS
+    return <<EOS
 genome model clin-seq identify-loh\\
-  --outdir=/tmp/test/ \\
-  --clinseq-build='a4abcd1313eb4376b59e68a9dd9d5ad2'
+    --outdir=/tmp/test/ \\
+    --clinseq-build='a4abcd1313eb4376b59e68a9dd9d5ad2'
 genome model clin-seq identify-loh\\
-  --outdir=/tmp/test/ \\
-  --somvar-build='9dc0385a1b634c9bb85eb2017b3a0c73'
+    --outdir=/tmp/test/ \\
+    --somvar-build='9dc0385a1b634c9bb85eb2017b3a0c73'
 EOS
 }
 
 sub help_detail {
-  return <<EOS
+    return <<EOS
 Use the results from Varscan in the somatic-variation builds and identify
 regions of LOH. Specifically, look at heterozygous single nucleotide variants
 in the normal sample and compare with stretches of homozygosity in the tumor.
@@ -77,203 +77,203 @@ EOS
 }
 
 sub __errors__ {
-  my $self = shift;
-  my @errors = $self->SUPER::__errors__(@_);
-  unless (-e $self->outdir && -d $self->outdir) {
-    push @errors, UR::Object::Tag->create(
-      type => 'error',
-      properties => ['outdir'],
-      desc => "Outdir: " . $self->outdir .
-      " not found or not a directory",
-    );
-  }
-  return @errors;
+    my $self = shift;
+    my @errors = $self->SUPER::__errors__(@_);
+    unless (-e $self->outdir && -d $self->outdir) {
+        push @errors, UR::Object::Tag->create(
+            type => 'error',
+            properties => ['outdir'],
+            desc => "Outdir: " . $self->outdir .
+                " not found or not a directory",
+        );
+    }
+    return @errors;
 }
 
 #Get a somatic-variation build from input or from clinseq-build
 sub resolve_somvar {
-  my $self = shift;
-  my $somvar_build = $self->somvar_build;
-  unless($somvar_build) {
-    my $clinseq_build = $self->clinseq_build;
-    unless($clinseq_build) {
-      die $self->error_message("Please pass a somatic-variation
-        or clinseq build as input.");
+    my $self = shift;
+    my $somvar_build = $self->somvar_build;
+    unless($somvar_build) {
+        my $clinseq_build = $self->clinseq_build;
+        unless($clinseq_build) {
+            die $self->error_message("Please pass a somatic-variation
+                or clinseq build as input.");
+        }
+        $somvar_build = $self->get_best_somvar_build($clinseq_build);
     }
-    $somvar_build = $self->get_best_somvar_build($clinseq_build);
-  }
-  return $somvar_build;
+    return $somvar_build;
 }
 
 #Take Varscan SNVs and split into per-chromosome calls.
 sub split_snvs {
-  my $self = shift;
-  my $varscan_op = shift;
-  my $snv_prefix = shift;
-  my $awk_cmd;
-  #use subsample of chr1 variants for test
-  if($self->test) {
-    $awk_cmd = "awk '\$1==1 { if(NR\%1000 == 0) { print > \"$snv_prefix.\"\$1\".snp\" }}' $varscan_op";
-  } else {
-    $awk_cmd = "awk '{ print > \"$snv_prefix.\"\$1\".snp\" }' $varscan_op";
-  }
-  Genome::Sys->shellcmd(cmd => $awk_cmd);
-  return;
+    my $self = shift;
+    my $varscan_op = shift;
+    my $snv_prefix = shift;
+    my $awk_cmd;
+    #use subsample of chr1 variants for test
+    if($self->test) {
+        $awk_cmd = "awk '\$1==1 { if(NR\%1000 == 0) { print > \"$snv_prefix.\"\$1\".snp\" }}' $varscan_op";
+    } else {
+        $awk_cmd = "awk '{ print > \"$snv_prefix.\"\$1\".snp\" }' $varscan_op";
+    }
+    Genome::Sys->shellcmd(cmd => $awk_cmd);
+    return;
 }
 
 #Get Varscan SNVs from somatic-variation
 sub get_varscan_snvs {
-  my $self = shift;
-  my $somvar_build = shift;
-  my $snv_prefix = shift;
-  my $dd = $somvar_build->data_directory;
-  my @varscan_snvs =
+    my $self = shift;
+    my $somvar_build = shift;
+    my $snv_prefix = shift;
+    my $dd = $somvar_build->data_directory;
+    my @varscan_snvs =
     glob(File::Spec->join($dd, "variants/snv/varscan-somatic-*/snvs.hq.unfiltered"));
-  unless(scalar @varscan_snvs) {
-    die $self->error_message("Unable to find varscan SNVs in $dd");
-  }
-  $self->status_message("varscan snvs file found: " .
-    join("\t", @varscan_snvs));
-  $self->split_snvs($varscan_snvs[0], $snv_prefix);
+    unless(scalar @varscan_snvs) {
+        die $self->error_message("Unable to find varscan SNVs in $dd");
+    }
+    $self->status_message("varscan snvs file found: " .
+        join("\t", @varscan_snvs));
+    $self->split_snvs($varscan_snvs[0], $snv_prefix);
 }
 
 #Get tumor/normal BAMs from somatic-variation
 sub get_tumor_normal_bam {
-  my $self = shift;
-  my $somvar_build = shift;
-  my $tumor_bam = $somvar_build->tumor_model->last_succeeded_build->
+    my $self = shift;
+    my $somvar_build = shift;
+    my $tumor_bam = $somvar_build->tumor_model->last_succeeded_build->
     whole_rmdup_bam_file;
-  my $normal_bam = $somvar_build->normal_model->last_succeeded_build->
+    my $normal_bam = $somvar_build->normal_model->last_succeeded_build->
     whole_rmdup_bam_file;
-  unless (-e $tumor_bam and -e $normal_bam) {
-    die $self->error_message("tumor_bam $tumor_bam or normal_bam " .
-      "$normal_bam does not exist.");
-  }
-  return ($tumor_bam, $normal_bam);
+    unless (-e $tumor_bam and -e $normal_bam) {
+        die $self->error_message("tumor_bam $tumor_bam or normal_bam " .
+            "$normal_bam does not exist.");
+    }
+    return ($tumor_bam, $normal_bam);
 }
 
 #Get the reference-sequence build from the
 # ref-align builds used in somatic-variation
 sub reference_build {
-  my $self = shift;
-  my $somvar_build = shift;
-  my $normal_refalign_build = $somvar_build->normal_build if ($somvar_build);
-  my $tumor_refalign_build = $somvar_build->tumor_build if ($somvar_build);
-  my @input_builds = ($normal_refalign_build,
-                      $tumor_refalign_build, $somvar_build);
-  for my $build (@input_builds){
-    next unless $build;
-    my $m = $build->model;
-    if ($m->can("reference_sequence_build")){
-      return $m->reference_sequence_build;
+    my $self = shift;
+    my $somvar_build = shift;
+    my $normal_refalign_build = $somvar_build->normal_build if ($somvar_build);
+    my $tumor_refalign_build = $somvar_build->tumor_build if ($somvar_build);
+    my @input_builds = ($normal_refalign_build,
+        $tumor_refalign_build, $somvar_build);
+    for my $build (@input_builds){
+        next unless $build;
+        my $m = $build->model;
+        if ($m->can("reference_sequence_build")){
+            return $m->reference_sequence_build;
+        }
     }
-  }
 }
 
 #Filter out putative false-positive SNVs
 sub filter_snvs {
-  my $self = shift;
-  my $somvar_build = shift;
-  my $snv_prefix = shift;
-  my ($tumor_bam, $normal_bam) =
+    my $self = shift;
+    my $somvar_build = shift;
+    my $snv_prefix = shift;
+    my ($tumor_bam, $normal_bam) =
     $self->get_tumor_normal_bam($somvar_build);
-  my $refbuild = $self->reference_build($somvar_build);
-  my $ref_fa = $refbuild->full_consensus_path('fa');
-  my $filter = Genome::Model::Tools::Varscan::SomaticFilterWorkflow->
+    my $refbuild = $self->reference_build($somvar_build);
+    my $ref_fa = $refbuild->full_consensus_path('fa');
+    my $filter = Genome::Model::Tools::Varscan::SomaticFilterWorkflow->
     create(
-      outdir => $self->outdir,
-      tumor_bam => $tumor_bam,
-      normal_bam => $normal_bam,
-      prefix => $snv_prefix,
-      reference => $ref_fa,
+        outdir => $self->outdir,
+        tumor_bam => $tumor_bam,
+        normal_bam => $normal_bam,
+        prefix => $snv_prefix,
+        reference => $ref_fa,
     );
-  $filter->execute();
+    $filter->execute();
 }
 
 #Combine germline, LOH calls and sort them
 sub combine_sort_snvs {
-  my $self = shift;
-  my $snv_prefix = shift;
-  my $snv_combined = $snv_prefix . ".combined";
-  my $snv_combined_sorted = $snv_prefix . ".combined.sorted";
-  my $germline = $snv_prefix . "*.formatted.LOH.hc.filtered";
-  my $loh = $snv_prefix . "*.formatted.Germline.hc.filtered";
-  unless(scalar glob("$germline $loh")) {
-      die $self->error_message("Unable to find $germline or $loh");
-  }
-  my $cat_cmd = "cat $germline $loh > $snv_combined";
-  Genome::Sys->shellcmd(cmd => $cat_cmd);
-  my $sort = Genome::Model::Tools::Capture::SortByChrPos->
+    my $self = shift;
+    my $snv_prefix = shift;
+    my $snv_combined = $snv_prefix . ".combined";
+    my $snv_combined_sorted = $snv_prefix . ".combined.sorted";
+    my $germline = $snv_prefix . "*.formatted.LOH.hc.filtered";
+    my $loh = $snv_prefix . "*.formatted.Germline.hc.filtered";
+    unless(scalar glob("$germline $loh")) {
+        die $self->error_message("Unable to find $germline or $loh");
+    }
+    my $cat_cmd = "cat $germline $loh > $snv_combined";
+    Genome::Sys->shellcmd(cmd => $cat_cmd);
+    my $sort = Genome::Model::Tools::Capture::SortByChrPos->
     create(
-      input_file => $snv_combined,
-      output_file => $snv_combined_sorted,
+        input_file => $snv_combined,
+        output_file => $snv_combined_sorted,
     );
-  $sort->execute;
-  return $snv_combined_sorted;
+    $sort->execute;
+    return $snv_combined_sorted;
 }
 
 #Segment the combined calls
 sub segment_loh {
-  my $self = shift;
-  my $snvs_combined_sorted = shift;
-  my $loh_basename = shift;
-  my $segmenter = Genome::Model::Tools::Varscan::LohSegments->
+    my $self = shift;
+    my $snvs_combined_sorted = shift;
+    my $loh_basename = shift;
+    my $segmenter = Genome::Model::Tools::Varscan::LohSegments->
     create(
-      variant_file => $snvs_combined_sorted,
-      output_basename => $loh_basename,
+        variant_file => $snvs_combined_sorted,
+        output_basename => $loh_basename,
     );
-  $segmenter->execute();
+    $segmenter->execute();
 }
 
 #Filter out LOH segments without sufficient support
 sub filter_loh {
-  my $self = shift;
-  my $loh_basename = shift;
-  my $segment_pc_cutoff = $self->segment_pc_cutoff;
-  my $minprobes = $self->minprobes;
-  my $loh_segments =
+    my $self = shift;
+    my $loh_basename = shift;
+    my $segment_pc_cutoff = $self->segment_pc_cutoff;
+    my $minprobes = $self->minprobes;
+    my $loh_segments =
     $loh_basename . ".segments.cbs";
-  my $loh_segments_filtered = $loh_segments . ".filtered";
-  unless(-e $loh_segments) {
-    die $self->error_message("Unable to find loh file $loh_segments");
-  }
-  my $awk_filter =
+    my $loh_segments_filtered = $loh_segments . ".filtered";
+    unless(-e $loh_segments) {
+        die $self->error_message("Unable to find loh file $loh_segments");
+    }
+    my $awk_filter =
     "awk '\$4 > $segment_pc_cutoff && \$3 >= $minprobes' $loh_segments " .
-      "> $loh_segments_filtered";
-  Genome::Sys->shellcmd(cmd => $awk_filter);
+    "> $loh_segments_filtered";
+    Genome::Sys->shellcmd(cmd => $awk_filter);
 }
 
 #Get rid of intermediate files
 sub cleanup {
-  my $self = shift;
-  my $unwanted;
-  foreach my $suffix ("*.Somatic*", "*.readcounts", "*.hc", "*.LOH",
-    "*.lc", "*.Germline", "*.removed", "*.formatted",
-    "*.hc.err", "*.other") {
-    my $unwanted = File::Spec->join($self->outdir, $suffix);
-    if(-e $unwanted) {
-      unlink $unwanted;
+    my $self = shift;
+    my $unwanted;
+    foreach my $suffix ("*.Somatic*", "*.readcounts", "*.hc", "*.LOH",
+        "*.lc", "*.Germline", "*.removed", "*.formatted",
+        "*.hc.err", "*.other") {
+        my $unwanted = File::Spec->join($self->outdir, $suffix);
+        if(-e $unwanted) {
+            unlink $unwanted;
+        }
     }
-  }
 }
 
 #Get, Filter, Combine, Segment, Filter, Cleanup
 sub execute {
-  my $self = shift;
-  my $somvar_build = $self->resolve_somvar;
-  my $snv_prefix = File::Spec->join(
-      $self->outdir,
-      "varscan.snps");
-  $self->get_varscan_snvs($somvar_build, $snv_prefix);
-  $self->filter_snvs($somvar_build, $snv_prefix);
-  my $combined_sorted = $self->combine_sort_snvs($snv_prefix);
-  my $loh_basename = File::Spec->join(
-    $self->outdir,
-    "varscan.output.loh");
-  my $loh_segments = $self->segment_loh($combined_sorted, $loh_basename);
-  $self->filter_loh($loh_basename);
-  $self->cleanup;
-  return 1;
+    my $self = shift;
+    my $somvar_build = $self->resolve_somvar;
+    my $snv_prefix = File::Spec->join(
+        $self->outdir,
+        "varscan.snps");
+    $self->get_varscan_snvs($somvar_build, $snv_prefix);
+    $self->filter_snvs($somvar_build, $snv_prefix);
+    my $combined_sorted = $self->combine_sort_snvs($snv_prefix);
+    my $loh_basename = File::Spec->join(
+        $self->outdir,
+        "varscan.output.loh");
+    my $loh_segments = $self->segment_loh($combined_sorted, $loh_basename);
+    $self->filter_loh($loh_basename);
+    $self->cleanup;
+    return 1;
 }
 
 1;
--- lib/perl/Genome/Model/ClinSeq/Command/IdentifyLoh.t
+++ lib/perl/Genome/Model/ClinSeq/Command/IdentifyLoh.t
@@ -17,7 +17,9 @@ use_ok('Genome::Model::ClinSeq::Command::IdentifyLoh') or die;
 
 #Define the test where expected results are stored
 my $expected_output_dir =
-  Genome::Utility::Test->data_dir_ok('Genome::Model::ClinSeq::Command::IdentifyLoh', '2015-03-26');
+Genome::Utility::Test->data_dir_ok(
+    'Genome::Model::ClinSeq::Command::IdentifyLoh',
+    '2015-03-26');
 ok(-e $expected_output_dir, "Found test dir: $expected_output_dir") or die;
 
 my $temp_dir = Genome::Sys->create_temp_directory();
@@ -25,12 +27,12 @@ ok($temp_dir, "created temp directory: $temp_dir") or die;
 
 #Run IdentifyLoh on the 'apipe-test-clinseq-wer' model
 my $clinseq_build =
-  Genome::Model::Build->get(id => '35af836fbcd243c59c44825af7e3983b');
+Genome::Model::Build->get(id => '35af836fbcd243c59c44825af7e3983b');
 ok($clinseq_build, "Found clinseq build.");
 my $run_iloh = Genome::Model::ClinSeq::Command::IdentifyLoh->create(
-  outdir => $temp_dir,
-  clinseq_build => $clinseq_build,
-  test => 1,
+    outdir => $temp_dir,
+    clinseq_build => $clinseq_build,
+    test => 1,
 );
 $run_iloh->queue_status_messages(1);
 $run_iloh->execute();
@@ -42,18 +44,19 @@ my $log = IO::File->new(">$log_file");
 $log->print(join("\n", @output1));
 $log->close();
 ok(-e $log_file, "Wrote message file from identify-loh to a log
-     file: $log_file");
+    file: $log_file");
 
+#Check for diffs
 my @diff = `diff -r -x '*.log.txt' -x '*png' -x '*R' -x '*cbs*' -x '*seg' -x '*.err' -x '*.out' $expected_output_dir $temp_dir`;
 ok(@diff == 0, "Found only expected number of differences between expected
-  results and test results")
-or do {
-  diag("expected: $expected_output_dir\nactual: $temp_dir\n");
-  diag("differences are:");
-  diag(@diff);
-  my $diff_line_count = scalar(@diff);
-  Genome::Sys->shellcmd(cmd => "rm -fr /tmp/last-run-identifyloh/");
-  Genome::Sys->shellcmd(cmd => "mv $temp_dir /tmp/last-run-identifyloh");
-  die print "\n\nFound $diff_line_count differing lines\n\n";
+    results and test results")
+    or do {
+    diag("expected: $expected_output_dir\nactual: $temp_dir\n");
+    diag("differences are:");
+    diag(@diff);
+    my $diff_line_count = scalar(@diff);
+    Genome::Sys->shellcmd(cmd => "rm -fr /tmp/last-run-identifyloh/");
+    Genome::Sys->shellcmd(cmd => "mv $temp_dir /tmp/last-run-identifyloh");
+    die print "\n\nFound $diff_line_count differing lines\n\n";
 };
 done_testing();
