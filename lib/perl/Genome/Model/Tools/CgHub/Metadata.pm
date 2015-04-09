a4ef7911a3d7cb2d35b7078e53d2e97bec4d22c2
Eddie Belter <ebelter@genome.wustl.edu>
2015-04-09 15:56:47 -0500

    CG Hub: update metadata module to handle multiple hits

--- lib/perl/Genome/Model/Tools/CgHub/Metadata.pm
+++ lib/perl/Genome/Model/Tools/CgHub/Metadata.pm
@@ -16,16 +16,10 @@ class Genome::Model::Tools::CgHub::Metadata {
             doc => 'The hash version of the metadata XML.',
         },
     },
+    has_optional_transient => {
+        _lookup => { is => 'HASH', },
+    },
 };
-
-sub create_from_xml {
-    my ($class, $xml) = Params::Validate::validate_pos(@_, {type => SCALAR}, {type => SCALAR, called => 'XML',});
-
-    my $metadata = XMLin($xml);
-    die $class->error_message('Failed to load metadata XML!') if not $metadata;
-
-    return $class->SUPER::create(metadata => $metadata);
-}
 
 sub create_from_file {
     my ($class, $metadata_file) = Params::Validate::validate_pos(@_, {type => SCALAR}, {type => SCALAR});
@@ -38,96 +32,131 @@ sub create_from_file {
     return $class->create_from_xml($xml);
 }
 
-my %attribute_mapping = (
-    uuid => 'analysis_id',
-    tcga_name => 'legacy_sample_id',
-    import_source_name => 'center_name',
-);
+sub create_from_xml {
+    my ($class, $xml) = Params::Validate::validate_pos(@_, {type => SCALAR}, {type => SCALAR, called => 'XML',});
+
+    my $metadata = XMLin($xml);
+    die $class->error_message('Failed to load metadata XML!') if not $metadata;
+
+    my $self = $class->SUPER::create(metadata => $metadata);
+    return if not $self;
+
+    $self->_set_lookup_hash;
+
+    return $self;
+}
+
+sub _set_lookup_hash {
+    my $self = shift;
+
+    my $metadata = $self->metadata;
+    if ( not $metadata->{Hits} or $metadata->{Hits} == 0) {
+        # FIXME error?
+        return 1;
+    }
+
+    if ( $metadata->{Hits} == 1) {
+        my $result = delete $metadata->{Result};
+        $metadata->{Result}->{1} = $result;
+    }
+
+    my %lookup;
+    my $results = $metadata->{Result};
+    for my $num ( keys %$results ) {
+        for my $id_by (qw/ analysis_id legacy_sample_id /) {
+            $lookup{ $results->{$num}->{$id_by} } = $num;
+        }
+    }
+    $self->_lookup(\%lookup);
+
+    return 1;
+}
+
 sub get_attribute_value {
-    my ($self, $name) = @_;
+    my ($self, $lookup_id, $name) = Params::Validate::validate_pos(
+        @_, {type => HASHREF}, {type => SCALAR}, {type => SCALAR},
+    );
 
-    die $self->error_message('No metadata set to get attribute value!') if not $self->metadata;
-    die $self->error_message('No name to get attribute value!') if not $name;
-
-    $name = $attribute_mapping{$name} if exists $attribute_mapping{$name};
-    my $value = $self->metadata->{Result}->{$name};
+    my $num = $self->_lookup->{$lookup_id};
+    return if not defined $num;
+    my $value = $self->metadata->{Result}->{$num}->{$name};
 
     return if not defined $value or $value eq '';
     return $value;
 }
 
-sub uuid {
-    return $_[0]->get_attribute_value('uuid');
-}
-
 sub _files {
-    my $self = shift;
+    my ($self, $lookup_id) = Params::Validate::validate_pos(@_, {type => HASHREF}, {type => SCALAR});
 
-    die $self->error_message('No metadata set to get files!') if not $self->metadata;
+    my $num = $self->_lookup->{$lookup_id};
+    return if not defined $num;
 
-    my $files_ref = ref $self->metadata->{Result}->{files}->{file};
-    return (
-        $files_ref eq 'ARRAY'
-        ? @{$self->metadata->{Result}->{files}->{file}}
-        : $self->metadata->{Result}->{files}->{file}
-    );
+    my $files = $self->metadata->{Result}->{$num}->{files}->{file};
+    my $files_ref = ref $files;
+    return ( $files_ref eq 'ARRAY' ? @$files : $files );
 }
 
 sub file_names {
     my $self = shift;
-    my @files = $self->_files;
+    my @files = $self->_files(@_);
     return map { $_->{filename} } @files;
 }
+
 sub bam_file_names {
     my $self = shift;
-    my @file_names = $self->file_names;
+    my @file_names = $self->file_names(@_);
     my @bam_file_names = grep { $_ =~ m/\.bam$/} @file_names;
     return wantarray ? @bam_file_names : $bam_file_names[0];
 }
 
 sub _get_attribute_value_for_file_name {
-    my ($self, $file_name, @attr_names) = @_;
+    my ($self, $lookup_id, $file_name, $attr_names) = Params::Validate::validate_pos(
+        @_, {type => HASHREF}, {type => SCALAR}, {type => SCALAR}, {type => ARRAYREF},
+    );
 
-    die $self->error_message('No file name given to get attribute value!') if not $file_name;
-    die $self->error_message('No attribute name(s) to get attribute value!') if not @attr_names;
-
-    my @files = $self->_files;
+    my @files = $self->_files($lookup_id);
     return if not @files;
 
     my @matching_files = grep { $_->{filename} eq $file_name } @files;
     return if not @matching_files;
 
     my @values;
-    if ( @attr_names == 1 ) {
-        @values = map { $_->{$attr_names[0]} } @matching_files;
+    if ( @$attr_names == 1 ) {
+        @values = map { $_->{$attr_names->[0]} } @matching_files;
     }
-    elsif ( @attr_names == 2 ) { 
-        @values = map { $_->{$attr_names[0]}->{$attr_names[1]} } @matching_files;
+    elsif ( @$attr_names == 2 ) { 
+        @values = map { $_->{$attr_names->[0]}->{$attr_names->[1]} } @matching_files;
     }
     else {
         die $self->error_message('Unknown number of attribute names given to _get_attribute_value_for_file_name');
     }
 
     return if not @values;
-    die $self->error_message("Got multiple values (@values) for $file_name @attr_names!") if @values == 2;
+    die $self->error_message("Got multiple values (%s) for %s %s", join(' ', @values), $file_name, join(' ', @$attr_names)) if @values == 2;
 
     return $values[0];
 }
 
 sub checksum_type_for_file_name {
-    my ($self, $file_name) = @_;
-    my $checksum_type = $self->_get_attribute_value_for_file_name($file_name, 'checksum', 'type');
+    my ($self, $lookup_id, $file_name) = Params::Validate::validate_pos(
+        @_, {type => HASHREF}, {type => SCALAR}, {type => SCALAR},
+    );
+    my $checksum_type = $self->_get_attribute_value_for_file_name($lookup_id, $file_name, [qw/ checksum type /]);
     return uc $checksum_type;
 }
 
 sub checksum_content_for_file_name {
-    my ($self, $file_name) = @_;
-    return $self->_get_attribute_value_for_file_name($file_name, 'checksum', 'content');
+    my ($self, $lookup_id, $file_name) = Params::Validate::validate_pos(
+        @_, {type => HASHREF}, {type => SCALAR}, {type => SCALAR},
+    );
+    return $self->_get_attribute_value_for_file_name($lookup_id, $file_name, [qw/ checksum content /]);
 }
 
 sub file_size_for_file_name {
-    my ($self, $bam_file) = @_;
-    return $self->_get_attribute_value_for_file_name($bam_file, 'filesize');
+    my ($self, $lookup_id, $bam_file) = Params::Validate::validate_pos(
+        @_, {type => HASHREF}, {type => SCALAR}, {type => SCALAR},
+    );
+    return $self->_get_attribute_value_for_file_name($lookup_id, $bam_file, [qw/ filesize /]);
 }
 
 1;
--- lib/perl/Genome/Model/Tools/CgHub/Metadata.t
+++ lib/perl/Genome/Model/Tools/CgHub/Metadata.t
@@ -27,14 +27,25 @@ throws_ok(sub{ $class->create_from_xml('blah'); }, qr//, 'create failed w/ inval
 throws_ok(sub{ $class->create_from_file('blah'); }, qr/File \(blah\) does not exist/, 'create_from_file failed w/ non existing file');
 
 # Success [b37]
-my $uuid = '387c3f70-46e9-4669-80e3-694d450f2919';
+my $analysis_id1 = '387c3f70-46e9-4669-80e3-694d450f2919';
 my $metadata1 = $class->create_from_file($xml_file);
 ok($metadata1, 'create') or die;
 ok($metadata1->metadata, 'metadata');
-is($metadata1->uuid, $uuid, 'uuid');
+
+# Fails to get_attribute_value
+throws_ok(sub{ $metadata1->get_attribute_value(); }, qr/but 3 were expected/, 'get_attribute_value fails w/o attribute name');
+throws_ok(sub{ $metadata1->get_attribute_value($analysis_id1); }, qr/but 3 were expected/, 'get_attribute_value fails w/o attribute name');
+# Fails to get checksum type/content
+throws_ok(sub{ $metadata1->checksum_content_for_file_name(); }, qr/but 3 were expected/, 'checksum_content_for_file_name fails w/o file name');
+throws_ok(sub{ $metadata1->checksum_type_for_file_name($analysis_id1); }, qr/but 3 were expected/, 'checksum_type_for_file_name fails w/o file name');
+
 ok(
     _test_metadata(
         metadata => $metadata1,
+        lookup => {
+            legacy_sample_id => 'TCGA-77-8154-10A-01D-2244-08',
+            analysis_id => $analysis_id1,
+        },
         files => [
             { 
                 file_name => 'C508.TCGA-77-8154-10A-01D-2244-08.1.bam',
@@ -55,27 +66,26 @@ ok(
         ],
         instdata_attrs => {
             aliquot_id => 'f7de2e89-ee90-4098-b86e-57a489b3a71a',
-            analysis_id => $uuid,
             center_name => 'BI',
-            import_source_name => 'BI',
             library_strategy => 'WXS',
-            tcga_name => 'TCGA-77-8154-10A-01D-2244-08',
             sample_id => 'f39b4cc9-9253-4cf9-8827-ebf26af1003a',
-            uuid => $uuid,
         },
     ),
     'test metadata'
 );
 
 # Success - load [b36]
-my $uuid2 = 'a1d11d67-4d5f-4db9-a61d-a0279c3c3d4f';
+my $analysis_id2 = 'a1d11d67-4d5f-4db9-a61d-a0279c3c3d4f';
 my $metadata2 = $class->create_from_file($b36_xml_file);
 ok($metadata2, 'create w/ b36 xml file') or die;
 ok($metadata2->metadata, 'metadata');
-is($metadata2->uuid, $uuid2, 'uuid');
 ok(
     _test_metadata(
         metadata => $metadata2,
+        lookup => {
+            legacy_sample_id => 'TCGA-A6-2674-01A-02W-0831-10',
+            analysis_id => $analysis_id2,
+        },
         files => [
             {
                 file_name => 'TCGA-A6-2674-01A-02W-0831-10_SOLiD.bam',
@@ -96,21 +106,13 @@ ok(
         ],
         instdata_attrs => {
             aliquot_id => 'f957194b-6da9-4690-a87d-0051e239bf3f',
-            analysis_id => $uuid2,
             center_name => 'BCM',
-            import_source_name => 'BCM',
             library_strategy => 'WXS',
-            tcga_name => 'TCGA-A6-2674-01A-02W-0831-10',
             sample_id => '8aca008c-f55a-420a-82c7-acd2cca77d85',
-            uuid => $uuid2,
         },
     ),
     'test metadata'
 );
-
-# Fails to get checksum type/content
-throws_ok(sub{ $metadata2->checksum_content_for_file_name(); }, qr/No file name given to get attribute value!/, 'checksum_content_for_file_name fails w/o file name');
-throws_ok(sub{ $metadata2->checksum_type_for_file_name(); }, qr/No file name given to get attribute value!/, 'checksum_type_for_file_name fails w/o file name');
 
 done_testing();
 
@@ -120,38 +122,55 @@ sub _test_metadata {
     my %params = @_;
 
     my $metadata = $params{metadata};
-    my %instdata_attrs = %{$params{instdata_attrs}};
+    my $analysis_id = $params{lookup}->{analysis_id};
+    my $legacy_sample_id = $params{lookup}->{legacy_sample_id};
+
+    for my $lookup1 (qw/ analysis_id legacy_sample_id /) {
+        for my $lookup2 (qw/ analysis_id legacy_sample_id /) {
+            is(
+                $metadata->get_attribute_value($params{lookup}->{$lookup1}, $lookup1),
+                $params{lookup}->{$lookup1},
+                "get_attribute_value for $params{lookup}->{$lookup1} $lookup1",
+            );
+            is(
+                $metadata->get_attribute_value($params{lookup}->{$lookup1}, $lookup2),
+                $params{lookup}->{$lookup2},
+                "get_attribute_value for $params{lookup}->{$lookup1} $lookup2",
+            );
+        }
+    }
 
     # attrs values
-    throws_ok(sub{ $metadata->get_attribute_value(); }, qr/No name to get attribute value!/, 'get_attribute_value fails w/o attribute name');
+    my %instdata_attrs = %{$params{instdata_attrs}};
     for my $name ( sort keys %instdata_attrs ) {
-        is($metadata->get_attribute_value($name), $instdata_attrs{$name}, "get_attribute_value for $name");
+        is($metadata->get_attribute_value($analysis_id, $name), $instdata_attrs{$name}, "get_attribute_value for $analysis_id $name");
+        is($metadata->get_attribute_value($legacy_sample_id, $name), $instdata_attrs{$name}, "get_attribute_value for $legacy_sample_id $name");
     }
 
     # files
     my @files = @{$params{files}};
-    my @file_names = $metadata->file_names;
+    my @file_names = $metadata->file_names($analysis_id);
     is_deeply(\@file_names, [map { $_->{file_name} } @files], 'file_names');
-    is_deeply([$metadata->bam_file_names], [$files[0]->{file_name}], 'bam_file_names');
+    is_deeply([$metadata->bam_file_names($analysis_id)], [$files[0]->{file_name}], 'bam_file_names');
 
     # file attrs
     for my $file_name ( @file_names ) {
         my ($file) = grep { $_->{file_name} eq $file_name } @files;
         ok($file, "found file hash for $file_name");
         is(
-            $metadata->checksum_type_for_file_name($file_name),
+            $metadata->checksum_type_for_file_name($analysis_id, $file_name),
             $file->{checksum}->{type},
-            "checksum_content_for_file_name for $file_name",
+            "checksum_content_for_file_name for $analysis_id $file_name",
         );
         is(
-            $metadata->checksum_content_for_file_name($file_name),
+            $metadata->checksum_content_for_file_name($analysis_id, $file_name),
             $file->{checksum}->{content},
-            "checksum_type_for_file_name for $file_name",
+            "checksum_type_for_file_name for $analysis_id $file_name",
         );
         is(
-            $metadata->file_size_for_file_name($file_name),
+            $metadata->file_size_for_file_name($analysis_id, $file_name),
             $file->{file_size},
-            "checksum_type_for_file_name for $file_name",
+            "checksum_type_for_file_name for $analysis_id $file_name",
         );
     }
 
