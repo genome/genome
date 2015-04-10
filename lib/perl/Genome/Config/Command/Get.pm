2c75fef53d4fd7b312ae93083030cdbd0904720d
Nathaniel Nutter <nnutter@genome.wustl.edu>
2015-04-10 12:37:19 -0500

    add `genome config get` command

---
+++ lib/perl/Genome/Config/Command/Get.pm
@@ -0,0 +1,26 @@
+package Genome::Config::Command::Get;
+
+use strict;
+use warnings;
+
+use Genome qw();
+use Genome::Config qw();
+
+class Genome::Config::Command::Get {
+    is => 'Command',
+    has => [
+        key => {
+            is => 'Text',
+            shell_args_position => 1,
+        },
+    ],
+};
+
+sub execute {
+    my $self = shift;
+    my $value = Genome::Config::get($self->key);
+    print $value, "\n";
+    return 1;
+};
+
+1;
