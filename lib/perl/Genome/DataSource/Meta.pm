60c860de8c08f16033a9b86f9506e162a25c3636
Thomas Mooney <tmooney@genome.wustl.edu>
2015-04-14 16:41:57 -0500

    Set sqlite_use_immediate_transaction to restore old behaviour.

--- lib/perl/Genome/DataSource/Meta.pm
+++ lib/perl/Genome/DataSource/Meta.pm
@@ -6,12 +6,27 @@ package Genome::DataSource::Meta;
 use strict;
 use warnings;
 
+use version;
+
 use UR;
+use DBD::SQLite;
 
 UR::Object::Type->define(
     class_name => 'Genome::DataSource::Meta',
     is => ['UR::DataSource::Meta'],
 );
 
+sub _dbi_connect_args {
+    my $self = shift;
+
+    my @connection = $self->SUPER::_dbi_connect_args(@_);
+
+    if(version->parse($DBD::SQLite::VERSION) >= version->parse('1.38_01')) {
+        my $connect_attr = $connection[3] ||= {};
+        $connect_attr->{sqlite_use_immediate_transaction} = 0;
+    }
+
+    return @connection;
+}
 
 1;
