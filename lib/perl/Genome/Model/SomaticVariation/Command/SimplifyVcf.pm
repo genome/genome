cd066223791d9f5df08dc77492f24ea038ef9e75
Thomas Mooney <tmooney@genome.wustl.edu>
2015-04-16 15:31:58 -0500

    Whitespace.
    * Remove trailing whitespace.
    * Consistently use four-space indent.

--- lib/perl/Genome/Model/SomaticVariation/Command/SimplifyVcf.pm
+++ lib/perl/Genome/Model/SomaticVariation/Command/SimplifyVcf.pm
@@ -10,16 +10,16 @@ use File::Path;
 class Genome::Model::SomaticVariation::Command::SimplifyVcf {
     is => 'Command::V2',
     has_input => [
-        builds => { 
-              is => 'Genome::Model::Build::SomaticVariation',
-              is_many => 1,
-              shell_args_position => 1,
-              require_user_verify => 0,
-              doc => 'Somatic variation build to summarize',
+        builds => {
+            is => 'Genome::Model::Build::SomaticVariation',
+            is_many => 1,
+            shell_args_position => 1,
+            require_user_verify => 0,
+            doc => 'Somatic variation build to summarize',
         },
-        outdir => { 
-              is => 'FilesystemPath',
-              doc => 'Directory where output files will be written', 
+        outdir => {
+            is => 'FilesystemPath',
+            doc => 'Directory where output files will be written',
         },
         include_and_merge_indels => {
             is => 'Boolean',
@@ -54,7 +54,7 @@ EOS
 sub help_detail {
     return <<EOS
 
-Create a VCF file that is a simplified version of the default put out by the pipeline.  
+Create a VCF file that is a simplified version of the default put out by the pipeline.
 
 Remove all variants except those that pass: based on the FT value of the *tumor* sample column
 
@@ -62,15 +62,15 @@ EOS
 }
 
 sub __errors__ {
-  my $self = shift;
-  my @errors = $self->SUPER::__errors__(@_);
+    my $self = shift;
+    my @errors = $self->SUPER::__errors__(@_);
 
-  unless (-e $self->outdir && -d $self->outdir) {
-      push @errors, UR::Object::Tag->create(
-	                                          type => 'error',
-	                                          properties => ['outdir'],
-	                                          desc => RED . "Outdir: " . $self->outdir . " not found or not a directory" . RESET,
-                                          );
+    unless (-e $self->outdir && -d $self->outdir) {
+        push @errors, UR::Object::Tag->create(
+            type => 'error',
+            properties => ['outdir'],
+            desc => RED . "Outdir: " . $self->outdir . " not found or not a directory" . RESET,
+        );
   }
   return @errors;
 }
@@ -173,26 +173,26 @@ sub resolve_combined_vcf_filename {
 }
 
 sub resolve_vcf_filename{
-  my ($self, $build, $type) = @_;
+    my ($self, $build, $type) = @_;
 
-  my $build_id = $build->id;
-  my $subject = $build->subject;
-  my $subject_name = $subject->name;
-  my $subject_common_name = $subject->common_name;
-  $subject_name=~s/\(/_/g; $subject_name=~s/\)/_/g;
-  my $subject_tissue_desc = $subject->tissue_desc;
-  my $individual_common_name = $subject->individual_common_name;
+    my $build_id = $build->id;
+    my $subject = $build->subject;
+    my $subject_name = $subject->name;
+    my $subject_common_name = $subject->common_name;
+    $subject_name=~s/\(/_/g; $subject_name=~s/\)/_/g;
+    my $subject_tissue_desc = $subject->tissue_desc;
+    my $individual_common_name = $subject->individual_common_name;
 
-  #Name the output file in a human readable way ($individual_common_name . $subject_name . $somatic_variation_build_id . passing.somatic.snvs.vcf
-  my @names;
-  push(@names, $individual_common_name) if ($individual_common_name);
-  push(@names, $subject_name) if ($subject_name);
-  push(@names, $build_id);
-  push(@names, "passing.somatic." . $type . ".vcf");
-  my $filename = join("_", @names);
+    #Name the output file in a human readable way ($individual_common_name . $subject_name . $somatic_variation_build_id . passing.somatic.snvs.vcf
+    my @names;
+    push(@names, $individual_common_name) if ($individual_common_name);
+    push(@names, $subject_name) if ($subject_name);
+    push(@names, $build_id);
+    push(@names, "passing.somatic." . $type . ".vcf");
+    my $filename = join("_", @names);
 
-  $self->debug_message("Writing simplified VCF to file: $filename");
-  return $self->outdir . "/$filename";
+    $self->debug_message("Writing simplified VCF to file: $filename");
+    return $self->outdir . "/$filename";
 }
 
 sub find_snv_vcf_file {
@@ -224,105 +224,105 @@ sub find_vcf_file {
         return;
     }
     $self->debug_message("Found VCF file: $vcf_file");
-    return $vcf_file;  
+    return $vcf_file;
 }
 
 sub simplify_vcf{
-  my $self = shift;
-  my %args = @_;
-  my $infile = $args{'-infile'};
-  my $outfile = $args{'-outfile'};
-  my $tumor_sample_name = $args{'-tumor_sample'};
-  my $normal_sample_name = $args{'-normal_sample'};
+    my $self = shift;
+    my %args = @_;
+    my $infile = $args{'-infile'};
+    my $outfile = $args{'-outfile'};
+    my $tumor_sample_name = $args{'-tumor_sample'};
+    my $normal_sample_name = $args{'-normal_sample'};
 
-  open (VCF1, "zcat $infile |") || die "\n\nCould not open input VCF file: $infile\n\n";
-  open (VCF2, ">$outfile") || die "\n\nCould not open output VCF file: $outfile\n\n";
-  my %header;
-  my $header_found = 0;
-  my $variant_count = 0;
-  my $passing_variant_count = 0;
-  while(<VCF1>){
-    chomp($_);
-    my $record = $_;
-    my @line = split("\t", $record);
+    open (VCF1, "zcat $infile |") || die "\n\nCould not open input VCF file: $infile\n\n";
+    open (VCF2, ">$outfile") || die "\n\nCould not open output VCF file: $outfile\n\n";
+    my %header;
+    my $header_found = 0;
+    my $variant_count = 0;
+    my $passing_variant_count = 0;
+    while(<VCF1>){
+        chomp($_);
+        my $record = $_;
+        my @line = split("\t", $record);
 
-    #Deal with all header lines
-    if ($record =~ /^\#/){
-      #Get the column header line info
-      if ($line[0] =~ /CHROM/){
-        $header_found = 1;
-        my $p = 0;
-        foreach my $head (@line){
-          $header{$head}{pos} = $p;
-          $p++;
+        #Deal with all header lines
+        if ($record =~ /^\#/){
+            #Get the column header line info
+            if ($line[0] =~ /CHROM/){
+                $header_found = 1;
+                my $p = 0;
+                foreach my $head (@line){
+                    $header{$head}{pos} = $p;
+                    $p++;
+                }
+                unless ($header{$tumor_sample_name}){
+                    $self->error_message("Could not find tumor column for sample: $tumor_sample_name");
+                    return;
+                }
+                unless ($header{$normal_sample_name}){
+                    $self->error_message("Could not find normal column for sample: $normal_sample_name");
+                    return;
+                }
+                unless ($header{'FORMAT'}){
+                    $self->error_message("Could not find FORMAT column in header line");
+                    return;
+                }
+                #Print a header with only one column for normal and tumor
+                print VCF2 "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$line[5]\t$line[6]\t$line[7]\t$line[8]\t$line[$header{$normal_sample_name}{pos}]\t$line[$header{$tumor_sample_name}{pos}]\n";
+                next;
+            }else{
+                print VCF2 "$record\n";
+                next;
+            }
         }
-        unless ($header{$tumor_sample_name}){
-          $self->error_message("Could not find tumor column for sample: $tumor_sample_name");
-          return;
+
+        #Filter out entries that do not pass all filters in the tumor
+        my $format_string = $line[$header{'FORMAT'}{pos}];
+        my @format = split(":", $line[$header{'FORMAT'}{pos}]);
+        my $tumor_string = $line[$header{$tumor_sample_name}{pos}];
+        my @tumor = split(":", $line[$header{$tumor_sample_name}{pos}]);
+        my $normal_string = $line[$header{$normal_sample_name}{pos}];
+        my @normal = split(":", $line[$header{$normal_sample_name}{pos}]);
+        my $p;
+        my $ft_pos;
+        foreach my $format_tag (@format){
+            $ft_pos = $p if ($format_tag eq "FT");
+            $p++;
         }
-        unless ($header{$normal_sample_name}){
-          $self->error_message("Could not find normal column for sample: $normal_sample_name");
-          return;
+        unless (defined($ft_pos) or defined $self->unfiltered_status) {
+            # If 'FT' is not defined lq/hq status of a variant can not be determined.
+            $self->error_message("Could not determine position of FT tag in FORMAT column and no --unfiltered-status was specified:\n@line");
+            die($self->error_message);
         }
-        unless ($header{'FORMAT'}){
-          $self->error_message("Could not find FORMAT column in header line");
-          return;
+
+        $variant_count++;
+        my $tumor_filter_value;
+        if (defined $ft_pos) {
+            $tumor_filter_value = $tumor[$ft_pos];
+            unless ($tumor_filter_value){
+                $self->warning_message("Could not find filter value for line: @line");
+            }
+        } else {
+            $tumor_filter_value = uc $self->unfiltered_status;
         }
-        #Print a header with only one column for normal and tumor
+
+        next unless ($tumor_filter_value eq "PASS");
+        $passing_variant_count++;
         print VCF2 "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$line[5]\t$line[6]\t$line[7]\t$line[8]\t$line[$header{$normal_sample_name}{pos}]\t$line[$header{$tumor_sample_name}{pos}]\n";
-        next;
-      }else{
-        print VCF2 "$record\n";
-        next;
-      }
+
+    }
+    close(VCF1);
+    close(VCF2);
+
+    $self->debug_message("Found " . $variant_count . " total variants, of which " . $passing_variant_count . " pass all filters in the tumor");
+
+    unless ($header_found){
+        $self->error_message("Could not find VCF header line in file $infile");
+        return;
     }
 
-    #Filter out entries that do not pass all filters in the tumor
-    my $format_string = $line[$header{'FORMAT'}{pos}];
-    my @format = split(":", $line[$header{'FORMAT'}{pos}]);
-    my $tumor_string = $line[$header{$tumor_sample_name}{pos}];
-    my @tumor = split(":", $line[$header{$tumor_sample_name}{pos}]);
-    my $normal_string = $line[$header{$normal_sample_name}{pos}];
-    my @normal = split(":", $line[$header{$normal_sample_name}{pos}]);
-    my $p;
-    my $ft_pos;
-    foreach my $format_tag (@format){
-      $ft_pos = $p if ($format_tag eq "FT");
-      $p++;
-    }
-    unless (defined($ft_pos) or defined $self->unfiltered_status) {
-      # If 'FT' is not defined lq/hq status of a variant can not be determined.
-      $self->error_message("Could not determine position of FT tag in FORMAT column and no --unfiltered-status was specified:\n@line");
-      die($self->error_message);
-    }
-
-    $variant_count++;
-    my $tumor_filter_value;
-    if (defined $ft_pos) {
-        $tumor_filter_value = $tumor[$ft_pos];
-        unless ($tumor_filter_value){
-            $self->warning_message("Could not find filter value for line: @line");
-        }
-    } else {
-        $tumor_filter_value = uc $self->unfiltered_status;
-    }
-
-    next unless ($tumor_filter_value eq "PASS");
-    $passing_variant_count++;
-    print VCF2 "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$line[5]\t$line[6]\t$line[7]\t$line[8]\t$line[$header{$normal_sample_name}{pos}]\t$line[$header{$tumor_sample_name}{pos}]\n";
-
-  }
-  close(VCF1);
-  close(VCF2);
-
-  $self->debug_message("Found " . $variant_count . " total variants, of which " . $passing_variant_count . " pass all filters in the tumor");
-
-  unless ($header_found){
-    $self->error_message("Could not find VCF header line in file $infile");
     return;
-  }
-
-  return;
 }
 
 
