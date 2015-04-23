2029b67f6be80770c32005d53e6a8f31bbf36cc8
Nathaniel Nutter <nnutter@genome.wustl.edu>
2015-04-23 11:33:02 -0500

    fix validate command after validate removed from ConfigSpec

--- lib/perl/Genome/Config/Command/Validate.pm
+++ lib/perl/Genome/Config/Command/Validate.pm
@@ -23,9 +23,9 @@ sub execute {
         }
 
         if ($spec->has_default_value) {
-            my @errors = $spec->validate($spec->default_value);
+            my @errors = Genome::Config::validate($spec, $spec->default_value);
             if (@errors) {
-                my $msg = $spec->validation_error(@errors);
+                my $msg = Genome::Config::validation_error($spec, @errors);
                 croakf($msg);
             }
         }
