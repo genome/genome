8050554fc7fdf6fd6234d5ee71597ef006e38d59
Nathaniel Nutter <nnutter@genome.wustl.edu>
2015-04-25 20:24:44 -0500

    convert GENOME_SITE_LOCK_DIR to Genome::Config

--- lib/perl/Genome/Site/TGI/Extension/Sys/Lock.pm
+++ lib/perl/Genome/Site/TGI/Extension/Sys/Lock.pm
@@ -21,7 +21,7 @@ Genome::Sys::Lock->add_backend('host',
 
 Genome::Sys::Lock->add_backend('site',
     Genome::Sys::Lock::FileBackend->new(is_mandatory => 1,
-        parent_dir => $ENV{GENOME_SITE_LOCK_DIR}));
+        parent_dir => Genome::Config::get('site_lock_dir')));
 
 if ($ENV{GENOME_NESSY_SERVER}) {
     require Genome::Sys::Lock::NessyBackend;
