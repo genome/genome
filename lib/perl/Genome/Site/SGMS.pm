ec196b7f31248b1df9085062a04c72a9be74708d
Nathaniel Nutter <nnutter@genome.wustl.edu>
2015-04-27 18:59:26 -0500

    convert GENOME_HOST_LOCK_DIR to Genome::Config

--- lib/perl/Genome/Site/SGMS.pm
+++ lib/perl/Genome/Site/SGMS.pm
@@ -12,7 +12,7 @@ require Genome::Sys::Lock::FileBackend;
 
 Genome::Sys::Lock->add_backend('host',
     Genome::Sys::Lock::FileBackend->new(is_mandatory => 1,
-        parent_dir => $ENV{GENOME_HOST_LOCK_DIR}));
+        parent_dir => Genome::Config::get('host_lock_dir')));
 
 Genome::Sys::Lock->add_backend('site',
     Genome::Sys::Lock::FileBackend->new(is_mandatory => 1,
