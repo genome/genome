dc2ccdfc61d9b705367a1e68171db59d80c0e392
Nathaniel Nutter <nnutter@genome.wustl.edu>
2015-04-28 15:21:15 -0500

    convert GENOME_DISK_GROUP_ARCHIVE to Genome::Config

--- lib/perl/Genome/Disk/Command/Volume/CreateArchives.pm
+++ lib/perl/Genome/Disk/Command/Volume/CreateArchives.pm
@@ -20,7 +20,7 @@ sub execute {
     $self->status_message("Creating archive volumes for any volumes that lack one!");
 
     my %report;
-    my $archive_group = Genome::Disk::Group->get(disk_group_name => $ENV{GENOME_DISK_GROUP_ARCHIVE});
+    my $archive_group = Genome::Disk::Group->get(disk_group_name => Genome::Config::get('disk_group_archive'));
     unless ($archive_group) {
         $archive_group = Genome::Disk::Group->create(
             permissions => '775',
@@ -28,7 +28,7 @@ sub execute {
             subdirectory => 'info',
             unix_gid => '10006',
             unix_uid => '10102',
-            disk_group_name => $ENV{GENOME_DISK_GROUP_ARCHIVE},
+            disk_group_name => Genome::Config::get('disk_group_archive'),
         );
         unless ($archive_group) {
             die "No archive group found and could not create one!";
