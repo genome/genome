d9510ff756a790c419cdadc645b48f9a65352a86
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-05-06 10:49:53 -0500

    Make flagstat path optional

--- lib/perl/Genome/InstrumentData/AlignmentResult/Command/RecreatePerLaneBam.pm
+++ lib/perl/Genome/InstrumentData/AlignmentResult/Command/RecreatePerLaneBam.pm
@@ -40,6 +40,7 @@ class Genome::InstrumentData::AlignmentResult::Command::RecreatePerLaneBam {
         comparison_flagstat => {
             is  => 'FilePath',
             doc => 'The path of flagstat file that is used to compare with that of recreated bam',
+            is_optional => 1,
         },
         include_qc_failed => {
             is          => 'Boolean',
@@ -80,7 +81,7 @@ sub execute {
     my $flagstat     = $self->comparison_flagstat;
     
 
-    unless (-s $flagstat) {
+    if (defined($flagstat) && !(-s $flagstat)) {
         die $self->error_message("comparison_flagstat $flagstat is not valid");
     }
 
@@ -104,14 +105,18 @@ sub execute {
     $self->debug_message('Extract read group bam');
     $self->_extract_readgroup_bam($temp_bam);
 
-    $self->debug_message('Compare flagstat first time expect one diff');
-    $self->_compare_flagstat($temp_bam, $flagstat, 'ignore_duplicates');
+    if (defined($flagstat)) {
+        $self->debug_message('Compare flagstat first time expect one diff');
+        $self->_compare_flagstat($temp_bam, $flagstat, 'ignore_duplicates');
+    }
 
     $self->debug_message('Revert bam Markdup tag');
     $self->_revert_markdup($temp_bam, $no_markdup_bam);
-    
-    $self->debug_message('Compare flagstat second time expect no diff');
-    $self->_compare_flagstat($no_markdup_bam, $flagstat);
+
+    if (defined($flagstat)) {
+        $self->debug_message('Compare flagstat second time expect no diff');
+        $self->_compare_flagstat($no_markdup_bam, $flagstat);
+    }
 
     $self->debug_message('Reheader bam');
     $self->_reheader_bam($no_markdup_bam, $bam_header);
