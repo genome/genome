78efe3b7563425bfdca2d699214c6a7d74e96eb5
Nathaniel Nutter <nnutter@genome.wustl.edu>
2015-05-06 12:55:44 -0500

    change `env` prefixes to XGENOME_
    In order to break ties with the legacy config system I am changing the
    prefix for environment variables from `GENOME_` to `XGENOME_`.  This may
    mean that some scripts start behaving differently due to configuration
    not taking effect but it will allow us to immediately avoid the
    confusion of whether a value was set by our default or by the user and
    will allow us to change defaults without having to tell users to start
    new shells, etc.

--- gmt-sys-vmbuilder/user_script.sh
+++ gmt-sys-vmbuilder/user_script.sh
@@ -2,7 +2,7 @@
 SCRIPT_PATH=`readlink -f $0`
 DIR_PATH=`dirname $SCRIPT_PATH`
 # DIR_PATH is the location of the vmbuilder directory
-GENOME_DIR=`dirname $DIR_PATH`
+XGENOME_DIR=`dirname $DIR_PATH`
 
 if [ "$UID" -eq "0" ]
 then
@@ -18,8 +18,8 @@ fi
 
 EOF
 
-echo "export PERL5LIB=$GENOME_DIR/lib/perl:\$PERL5LIB" >> $HOME/.bashrc
-echo "export PATH=$GENOME_DIR/bin:\$PATH" >> $HOME/.bashrc
+echo "export PERL5LIB=$XGENOME_DIR/lib/perl:\$PERL5LIB" >> $HOME/.bashrc
+echo "export PATH=$XGENOME_DIR/bin:\$PATH" >> $HOME/.bashrc
 echo "export TNS_ADMIN=$DIR_PATH" >> $HOME/.bashrc
 echo "export ORACLE_HOME=/opt/oracle-instantclient" >> $HOME/.bashrc
 
