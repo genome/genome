c667125dade5009bb74e58e7827af27ed7375d35
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-05-08 14:02:08 -0500

    Consolidate duplicate code into a helper module

--- lib/perl/Genome/InstrumentData/AlignmentResult/Merged/Speedseq.pm
+++ lib/perl/Genome/InstrumentData/AlignmentResult/Merged/Speedseq.pm
@@ -3,7 +3,12 @@ package Genome::InstrumentData::AlignmentResult::Merged::Speedseq;
 use strict;
 use warnings;
 use Genome;
-use Sys::Hostname;
+use Genome::InstrumentData::AlignmentResult::Merged::Helpers qw(
+    create_bam_md5
+    resolve_allocation_subdirectory
+    resolve_alignment_subdirectory
+    resolve_allocation_disk_group_name
+);
 
 class Genome::InstrumentData::AlignmentResult::Merged::Speedseq {
     is => ['Genome::InstrumentData::AlignedBamResult::Merged', 'Genome::SoftwareResult::WithNestedResults'],
@@ -112,50 +117,6 @@ sub sort_memory {
     #We might want to set an lsf resource to be at least 20G which is the
     #default for speedseq -M parameter
     return '3';
-}
-
-
-# The following subroutines are all copied from G::ID::AR::M since we can't
-# inherit from it directly
-# We might want to make put these somewhere where both modules can inherit
-# from it
-
-sub create_bam_md5 {
-    my $self = shift;
-
-    my $bam_file = shift;
-    my $md5_file = $bam_file.'.md5';
-    my $cmd = "md5sum $bam_file > $md5_file";
-
-    $self->debug_message("Creating md5 file for the BAM file...");
-
-    Genome::Sys->shellcmd(
-        cmd                        => $cmd,
-        input_files                => [$bam_file],
-        output_files               => [$md5_file],
-        skip_if_output_is_present  => 0,
-    );
-
-    return 1;
-}
-
-sub resolve_allocation_subdirectory {
-    return $_[0]->resolve_alignment_subdirectory;
-}
-
-sub resolve_alignment_subdirectory {
-    my $self = shift;
-
-    my $hostname = hostname;
-    my $user = $ENV{'USER'};
-    my $base_dir = sprintf("merged-alignment-%s-%s-%s-%s", $hostname, $user, $$, $self->id);
-    # TODO: the first subdir is actually specified by the disk management system.
-    my $directory = join('/', 'build_merged_alignments', $base_dir);
-    return $directory;
-}
-
-sub resolve_allocation_disk_group_name {
-    $ENV{GENOME_DISK_GROUP_MODELS};
 }
 
 1;
