f31542b3e118e8cecda05e0137abe95d8e8cb2b1
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-05-26 08:43:14 -0500

    If more than one metric key is found, try to resolve by library name

--- lib/perl/Genome/Qc/Tool/Picard.pm
+++ lib/perl/Genome/Qc/Tool/Picard.pm
@@ -43,11 +43,21 @@ sub _get_metrics {
         my $metric_key = $metric_details->{metric_key};
         unless (defined($metric_key)) {
             my @metric_keys = keys %{$metric_results};
-            if (scalar(@metric_keys > 1)) {
-                die $self->error_message("More than one metrics key found in the metrics results.");
+            if (scalar(@metric_keys) == 1) {
+                $metric_key = $metric_keys[0];
             }
             else {
-                $metric_key = $metric_keys[0];
+                my $library_name = $self->alignment_result->instrument_data->library->name;
+                my @matching_metric_keys = grep {$_ =~ m/$library_name/} @metric_keys;
+                if (scalar(@matching_metric_keys) == 1) {
+                    $metric_key = $matching_metric_keys[0];
+                }
+                elsif (scalar(@matching_metric_keys) == 0) {
+                    die $self->error_message("No matching metric key found for library (%s) in metric keys list: %s", $library_name, join(', ', @metric_keys));
+                }
+                else {
+                    die $self->error_message("More than one metrics key found for library (%s) in metric keys list: %s", $library_name, join(', ', @metric_keys));
+                }
             }
         }
         my $picard_metric = $metric_details->{picard_metric};
