8108c18f11e23d6f28a94161337dec927dacb30f
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-05-27 08:35:27 -0500

    Use input bam file directly instead of piping it in through streamgraph

--- lib/perl/Genome/Qc/Config.pm
+++ lib/perl/Genome/Qc/Config.pm
@@ -21,30 +21,27 @@ sub get_commands_for_alignment_result {
         picard_collect_gc_bias_metrics => {
             class => 'Genome::Qc::Tool::Picard::CollectGcBiasMetrics',
             params => {
-                input_file => '/dev/stdin',
+                input_file => 'bam_file',
                 refseq_file => 'reference_sequence',
                 assume_sorted => 1,
                 use_version => 1.123,
                 output_file=> '/dev/null',
             },
-            in_file => "bam_file",
         },
         picard_mark_duplicates => {
             class => 'Genome::Qc::Tool::Picard::MarkDuplicates',
             params => {
                 output_file => '/dev/null',
-                input_file => '/dev/stdin',
+                input_file => 'bam_file',
                 use_version => 1.123,
             },
-            in_file => "bam_file",
         },
         picard_collect_multiple_metrics => {
             class => 'Genome::Qc::Tool::Picard::CollectMultipleMetrics',
             params => {
-                input_file => '/dev/stdin',
+                input_file => 'bam_file',
                 use_version => 1.123,
             },
-            in_file => "bam_file",
         },
     );
 
@@ -52,23 +49,21 @@ sub get_commands_for_alignment_result {
         $config{picard_calculate_hs_metrics} = {
             class => 'Genome::Qc::Tool::Picard::CalculateHsMetrics',
             params => {
-                input_file => '/dev/stdin',
+                input_file => 'bam_file',
                 bait_intervals => 'bait_intervals', #region_of_interest_set
                 target_intervals => 'target_intervals', #target_region_set
                 use_version => 1.123,
             },
-            in_file => "bam_file",
         };
     }
     else {
         $config{picard_collect_wgs_metrics} = {
             class => 'Genome::Qc::Tool::Picard::CollectWgsMetrics',
             params => {
-                input_file => '/dev/stdin',
+                input_file => 'bam_file',
                 reference_sequence => 'reference_sequence',
                 use_version => 1.123,
             },
-            in_file => "bam_file",
         };
     }
 
