7759d57997709cafe309d13b31d04323acedf392
Thomas Mooney <tmooney@genome.wustl.edu>
2015-06-11 17:36:46 -0500

    Also test for directory existence before trying to create it.
    Unmounted volumes will not return EEXIST from mkdir().  Checking for
    directory existence triggers the automounter to ensure the volume is
    mounted before trying to create it.

--- lib/perl/Genome/Sys.pm
+++ lib/perl/Genome/Sys.pm
@@ -847,6 +847,8 @@ sub make_path {
     for (my $i = 0; $i < @dirs; $i++) {
         my $subpath = File::Spec->catdir(@dirs[0..$i]);
 
+        next if -d $subpath;
+
         my $rv = mkdir $subpath;
         my $mkdir_errno = $!;
         if ($rv) {
