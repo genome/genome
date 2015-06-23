7d7c583d2f3945e6f9e14a2eeaaa9bd8ce8a8383
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-06-23 10:36:09 -0500

    Make variation_list_build an id_by property

--- lib/perl/Genome/Qc/Tool/WithVariationListVcf.pm
+++ lib/perl/Genome/Qc/Tool/WithVariationListVcf.pm
@@ -10,22 +10,27 @@ class Genome::Qc::Tool::WithVariationListVcf {
         variation_list_build_id => {
             is => 'Text',
             is_optional => 1,
-        }
+        },
+        variation_list_build => {
+            is => 'Genome::Model::Build::ImportedVariationList',
+            id_by => 'variation_list_build_id',
+        },
     ],
 };
 
 sub variation_list_vcf_file {
     my $self = shift;
-    my $variation_list_build = Genome::Model::Build::ImportedVariationList->get($self->variation_list_build_list);
+
     my $vcf_file = Genome::Sys->create_temp_file_path;
     my $cmd = Genome::Model::GenotypeMicroarray::Command::ExtractToVcf->create(
-        variation_list_build => $variation_list_build,
+        variation_list_build => $self->variation_list_build,
         sample => $self->sample,
         output => $vcf_file,
     );
     unless ($cmd->execute) {
         die $self->error_message("ExtractToVcf command execution failed");
     }
+
     return $vcf_file;
 }
 
