82a3b97bbc2171741631f70377750452d8fb8e7a
Susanna Siebert <ssiebert@genome.wustl.edu>
2015-07-08 13:47:04 -0500

    Use validate_pos callback functionality to check parameters

--- lib/perl/Genome/Config/Parser/YAML.pm
+++ lib/perl/Genome/Config/Parser/YAML.pm
@@ -12,18 +12,22 @@ class Genome::Config::Parser::YAML {
 };
 
 sub parse {
-    my ($self, $filename) = validate_pos(@_, 1, 1);
-    die('Failed to provide filename!') unless $filename;
-    die("$filename doesn't exist!") unless (-e $filename);
-    die("$filename is empty!") unless (-s $filename);
-    die("$filename doesn't appear to be a YAML file!") unless $filename =~ /.+\.(yaml|yml)$/i;
+    my ($self, $filename) = validate_pos(
+        @_,
+        1,
+        {
+            callbacks => {
+                'File exists' => sub { -e $_[0]; },
+                'File has size' => sub { -s $_[0]; },
+                'File is YAML' => sub { $_[0] =~ /.+\.(yaml|yml)$/i },
+            }
+        }
+    );
     return LoadFile($filename);
 }
 
 sub write {
     my ($self, $filename, $data) = validate_pos(@_, 1, 1, 1);
-    die('Failed to provide filename!') unless $filename;
-    die('Failed to provide content!') unless $data;
     return DumpFile($filename, $data);
 }
 
