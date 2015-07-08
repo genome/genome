f5bf38dc78e272847091f1b395279385c6b7254c
Eddie Belter <ebelter@genome.wustl.edu>
2015-07-08 14:49:34 -0500

    DV2: call error_message instead of using variable

--- lib/perl/Genome/Model/Tools/DetectVariants2/Result/DetectionBase.pm
+++ lib/perl/Genome/Model/Tools/DetectVariants2/Result/DetectionBase.pm
@@ -228,14 +228,13 @@ sub _validate_found_allocation {
     } else {
         # Allocation exists without a result the whole time the result is being created. Ideally locks
         # would prevent us from getting here during that window but our locks are not 100% reliable.
-        my @error_message = (
-            "Found allocation at (%s) but no software result for it's owner ID (%s). ".
+        Genome::Utility::Instrumentation::increment('dv2.result.found_orphaned_allocation');
+        die $self->error_message(
+            "Found allocation at (%s) but no software result for its owner ID (%s). ".
             "This is either because the software result is currently being generated or because the allocation has been orphaned. ".
             "If it is determined that the allocation has been orphaned then the allocation will need to be removed.",
             $allocation->absolute_path, $allocation->owner_id,
         );
-        Genome::Utility::Instrumentation::increment('dv2.result.found_orphaned_allocation');
-        die $class->error_message(join(' ', @error_message));
     }
 }
 
