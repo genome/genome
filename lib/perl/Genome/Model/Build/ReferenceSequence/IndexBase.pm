93676f9c5cec1df48bd0e04df1b7fd6ef25a3235
Thomas Mooney <tmooney@genome.wustl.edu>
2015-07-14 14:51:02 -0500

    Reformat class definition (whitespace).

--- lib/perl/Genome/Model/Build/ReferenceSequence/IndexBase.pm
+++ lib/perl/Genome/Model/Build/ReferenceSequence/IndexBase.pm
@@ -10,42 +10,47 @@ class Genome::Model::Build::ReferenceSequence::IndexBase {
     is_abstract => 1,
     is => ['Genome::SoftwareResult::Stageable', 'Genome::SoftwareResult::WithNestedResults'],
     has => [
-        reference_build         => {
-                                    is => 'Genome::Model::Build::ImportedReferenceSequence',
-                                    id_by => 'reference_build_id',
-                                },
-        reference_name          => { via => 'reference_build', to => 'name', is_mutable => 0, is_optional => 1 },
-        aligner                 => {
-                                    calculate_from => [qw/aligner_name aligner_version aligner_params/],
-                                    calculate => q|no warnings; "$aligner_name $aligner_version $aligner_params"|
-                                },
+        reference_build => {
+            is => 'Genome::Model::Build::ImportedReferenceSequence',
+            id_by => 'reference_build_id',
+        },
+        reference_name => {
+            via => 'reference_build',
+            to => 'name',
+            is_mutable => 0,
+            is_optional => 1
+        },
+        aligner => {
+            calculate_from => [qw/aligner_name aligner_version aligner_params/],
+            calculate => q|no warnings; "$aligner_name $aligner_version $aligner_params"|
+        },
     ],
     has_input => [
-        reference_build_id      => {
-                                    is => 'Number',
-                                    doc => 'the reference to use by id',
-                                },
+        reference_build_id  => {
+            is => 'Number',
+            doc => 'the reference to use by id',
+        },
     ],
     has_param => [
-        aligner_name            => {
-                                    is => 'Text', default_value => 'maq',
-                                    doc => 'the name of the aligner to use, maq, blat, newbler etc.',
-                                },
-        aligner_version         => {
-                                    is => 'Text',
-                                    doc => 'the version of the aligner to use, i.e. 0.6.8, 0.7.1, etc.',
-                                    is_optional=>1,
-                                },
-        aligner_params          => {
-                                    is => 'Text',
-                                    is_optional=>1,
-                                    doc => 'any additional params for the aligner in a single string',
-                                },
+        aligner_name => {
+            is => 'Text', default_value => 'maq',
+            doc => 'the name of the aligner to use, maq, blat, newbler etc.',
+        },
+        aligner_version => {
+            is => 'Text',
+            doc => 'the version of the aligner to use, i.e. 0.6.8, 0.7.1, etc.',
+            is_optional=>1,
+        },
+        aligner_params => {
+            is => 'Text',
+            is_optional=>1,
+            doc => 'any additional params for the aligner in a single string',
+        },
     ],
     has_transient => [
-        aligner_class_name      => {
-                                    is => 'Text',
-                                    is_optional => 1,
+        aligner_class_name => {
+            is => 'Text',
+            is_optional => 1,
         }
     ]
 };
