7c41446c52fae60f841202dee78b4b7cde250733
Eddie Belter <ebelter@genome.wustl.edu>
2015-07-27 13:35:12 -0500

    AR BLAT: rm unneeded comments and commented out code

--- lib/perl/Genome/InstrumentData/AlignmentResult/Blat.pm
+++ lib/perl/Genome/InstrumentData/AlignmentResult/Blat.pm
@@ -18,7 +18,6 @@ class Genome::InstrumentData::AlignmentResult::Blat {
 
 sub required_arch_os { 'x86_64' }
 
-# fill me in here with what compute resources you need.
 sub required_rusage { 
     "-R 'select[tmp>90000 && mem>12000] span[hosts=1] rusage[tmp=90000, mem=12000]' -M 12000000 -n 6";
 }
@@ -34,29 +33,16 @@ sub _run_aligner {
 
     my $staging_sam_file = $tmp_dir . "/" . "all_sequences.sam";
     
-    # get refseq info
     my $reference_build = $self->reference_build;
     my $ref_path = $reference_build->full_consensus_path('fa');
-    #my $reference_fasta_path = sprintf("%s/%s", $reference_build->data_directory, $ref_basename);
 
     my $blat_path = Genome::Model::Tools::Blat->path_for_blat_version($self->aligner_version);
 
     my $aligner_params = $self->decomposed_aligner_params;
 
-    #### STEP 1: Align and convert for each input all the while merging them together.
-
     foreach my $cur_input (@input_pathnames) {
 
         my $cur_input_fa = $cur_input.".fa";
-# TODO this doesn't work right:
-#        $self->debug_message("Converting $cur_input (FastQ) to $cur_input_fa (FastA).");
-#        Genome::Sys->shellcmd(
-#            cmd             => "fastq_to_fasta <$cur_input >$cur_input_fa",
-#            input_files     => [ $cur_input ],
-#            output_files    => [ $cur_input_fa ],
-#            skip_if_output_is_present => 0,
-#        );
-# using this instead:
         my $fastq_fh = IO::File->new($cur_input);
         my $fasta_fh = IO::File->new(">$cur_input_fa");
         my $line_type;
@@ -94,22 +80,11 @@ sub _run_aligner {
         die "Failed to process sam command line, error_message is ".$self->error_message unless $self->_filter_sam_output($tmp_sam_file, $staging_sam_file);
     }
 
-    #### STEP 2: There is no step 2
-    
-    # TODO (iferguso) should do something to handle the log files
-
     return 1;
 }
 
 sub _filter_sam_output {
     my ($self, $cur_sam_file, $all_sequences_sam_file) = @_;
-
-#    my $sam_run_output_fh = IO::File->new( $sam_cmd . "|" );
-#    $self->debug_message("Running $sam_cmd");
-#    if ( !$sam_run_output_fh ) {
-#            $self->error_message("Error running $sam_cmd $!");
-#            return;
-#    }
 
     my $cur_sam_fh = IO::File->new( $cur_sam_file );
     if ( !$cur_sam_fh ) {
@@ -147,8 +122,5 @@ sub aligner_params_for_sam_header {
     
     my $params = $self->decomposed_aligner_params;
     
-    # TODO (iferguso) i don't think it's necessary to include the actual file paths used amirite?
     return "blat $params";
-
-    # for bwa this looks like "bwa aln -t4; bwa samse 12345'
 }
