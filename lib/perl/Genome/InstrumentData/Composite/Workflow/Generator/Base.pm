b6ff0a870fb1137c76e1b4ed2e7afb01c064e130
Thomas Mooney <tmooney@genome.wustl.edu>
2015-08-10 16:02:58 -0500

    Remove UR optimization--it cloned objects with WorkflowBuilder links.

--- lib/perl/Genome/InstrumentData/Composite/Workflow/Generator/Base.pm
+++ lib/perl/Genome/InstrumentData/Composite/Workflow/Generator/Base.pm
@@ -11,33 +11,10 @@ class Genome::InstrumentData::Composite::Workflow::Generator::Base {
 };
 
 
-my @_properties_for_add_link = ('id','destination','destination_property','source','source_property');
-my @_properties_in_template_order;
-my $_add_link_to_workflow_tmpl;
-my $_add_link_workflow_id_pos;
-my $_add_link_id_pos;
 sub _add_link_to_workflow {
     my($class, $workflow_obj, %params) = @_;
 
-    unless ($_add_link_to_workflow_tmpl) {
-        my $tmpl = UR::BoolExpr::Template->resolve('Genome::WorkflowBuilder::Link', @_properties_for_add_link);
-        unless ($tmpl) {
-            Carp::croak('Cannot resolve BoolExpr template for adding Workflow::Link objects');
-        }
-        $tmpl = $tmpl->get_normalized_template_equivalent();
-        for my $name ( @_properties_for_add_link ) {
-            my $pos = $tmpl->value_position_for_property_name($name);
-            if ($name eq 'id') {
-                $_add_link_id_pos = $pos;
-            }
-            $_properties_in_template_order[$pos] = $name;
-        }
-        $_add_link_to_workflow_tmpl = $tmpl;
-    }
-    my @values = @params{@_properties_in_template_order};
-    $values[$_add_link_id_pos] = UR::Object::Type->autogenerate_new_object_id_urinternal();
-    my $rule = $_add_link_to_workflow_tmpl->get_rule_for_values(@values);
-    return $workflow_obj->add_link(UR::Context->create_entity('Genome::WorkflowBuilder::Link', $rule));
+    return $workflow_obj->add_link(Genome::WorkflowBuilder::Link->create(%params));
 }
 
 my $counter = 0;
