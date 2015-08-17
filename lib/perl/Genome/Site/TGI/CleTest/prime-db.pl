8eb61d1fb069a78d8ee5a1491dae6581e5a18309
apregier <aregier@genome.wustl.edu>
2015-08-17 11:37:47 -0500

    Move all object ids into config

--- lib/perl/Genome/Site/TGI/CleTest/prime-db.pl
+++ lib/perl/Genome/Site/TGI/CleTest/prime-db.pl
@@ -154,36 +154,19 @@ sub main {
     my @blessed_builds = @{$config->{blessed_builds}};
     load_builds(@blessed_builds);
 
-    my @build_ids = (
-        'd00a39c84382427fa0efdec3229e8f5f', #annotation build
-    );
+    my @build_ids = @{$config->{annotation_builds}};
 
-    my @imported_variation_list_ids = (
-        '7fd9ca9a6bf74a098c23b32c1b00fbc0', #dbsnp
-        '126747180', #NHLBI version 2012.07.23
-        '847b3cacad1249b8b7e46f89e02d96da', #DOCM SNV
-        '1040bf09070c4176a5256fa8a075378f', #DOCM INDEL
+    my @imported_variation_list_ids = @{$config->{imported_variation_builds}};
 
-    );
     load_builds(@build_ids);
     load_imported_annotation_builds(@imported_variation_list_ids);
 
-    my @reference_sequences = (
-        '106942997', #build37, other references may crop up in feature lists
-        '108563338', #nimblegen refseq for feature list
-        '102671028', #g1k-human-build37 for segdups
-    );
+    my @reference_sequences = @{$config->{reference_sequence_builds}};
     load_builds_without_results(@reference_sequences);
     load_converters(@reference_sequences);
 
 
-    my @featurelist_ids = (
-        'a2733b8111c24110805025137c878e13', #Full ROI
-        '0e4973c600244c3f804d54bee6f81145', #RMG ROI
-        'ef6583623a614c8ebaea471a4c9748fc', #AML Complex Mutation Region ROI
-        '696318bab30d47d49fab9afa845691b7', #homopolymer regions from G::VR::C::Wrapper::ModelPair
-        '4e77d10f29f44a0792ebc8d6ea0c4a2b', #segdups my $b = Genome::Model::Build->get('26e65adaa8034dd99ef92b27f61ad862'); $b->get_feature_list("segmental_duplications")->id
-    );
+    my @featurelist_ids = @{$config->{feature_lists}};
     load_feature_lists(@featurelist_ids);
 
     my %tag_to_menu_item = %{$config->{tag_to_menu_item}};
@@ -192,10 +175,7 @@ sub main {
     my @menu_items = uniq(values %tag_to_menu_item);
     load_menu_items(@menu_items);
 
-    my @processing_profiles = (
-        'a813f8067a8c4c9791fec53dd29d85ca', #somatic CLE PP
-        '6cab54acdf704c7c9e8adc7aa8facff4', #germline CLE PP
-    );
+    my @processing_profiles = @{$config->{processing_profiles}};
     load_processing_profiles(@processing_profiles);
 
     load_users_by_username('apipe-tester');
@@ -203,8 +183,9 @@ sub main {
     my $process = $config->{blessed_process};
     load_processes($process); #our report process to diff
 
-    my @vep_results = Genome::SoftwareResult->get(id => ['265673ea574246b49d211151107dfee9', '44d72413f6af43e99386f7f6c92ed59a']);
-    load_software_results(@vep_results); #vep cache and api
+    my @misc_software_results = Genome::SoftwareResult->get(
+        id => $config->{misc_software_results});
+    load_software_results(@misc_software_results);
     return UR::Context->commit();
 }
 
