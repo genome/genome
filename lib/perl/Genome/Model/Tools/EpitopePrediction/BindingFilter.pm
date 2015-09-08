fe9db0c443de6fda70387e421f129d87139cf040
Chris Miller <cmiller@genome.wustl.edu>
2015-09-08 15:53:21 -0500

    new options for epitope binding filter

--- lib/perl/Genome/Model/Tools/EpitopePrediction/BindingFilter.pm
+++ lib/perl/Genome/Model/Tools/EpitopePrediction/BindingFilter.pm
@@ -17,6 +17,18 @@ class Genome::Model::Tools::EpitopePrediction::BindingFilter {
             doc => 'FOF containing list of parsed epitope files for different allele-length combinations (same sample)',
         },
     ],
+    has_optional_input => [
+        variant_file => {
+            is  => 'FilePath',
+            doc => 'Original variant file in TGI annotation format. If provided, this will create a second output file that appends binding prediction information to the variant annotation lines and outputs those that pass filters',
+        },
+        minimum_fold_change => {
+            is => 'Numeric',
+            doc => 'Minimum fold change between mutant binding score and wild-type score. The default is 0, which filters no results, but 1 is often a sensible default (requiring that binding is better to the MT than WT)',
+            default => 0,
+        },
+
+    ],
     has_output => [
         output_file => {
             is  => 'FilePath',
@@ -32,6 +44,30 @@ sub help_brief {
 sub execute {
     my $self = shift;
 
+
+    # if provided, grab the variants in TGI format and hash them by gene and mutation
+    my %variants;
+    if(defined($self->variant_file)){
+        open(VARFILE,">" . $self->output_file . ".varanno");
+        my $var_fh = Genome::Sys->open_file_for_reading($self->variant_file);
+        while (my $line = $var_fh->getline) {
+            chomp $line;
+            #header?
+            if($line=~/chromosome_name/){
+                $variants{"header"} = $line;
+                next;
+            }
+            my @F = split("\t",$line);            
+            my $gene = $F[6];
+            my $amino_acid_change = $F[15];
+            $amino_acid_change =~ s/p\.//; #change p.E435G to E435G like the netMHC output
+            $variants{join("\t",($gene,$amino_acid_change))} = $line;
+        }
+    }
+
+
+
+    # read in the netMHC predictions and filter
     my $fof_fh = Genome::Sys->open_file_for_reading($self->fof_file);
     my $out_fh = Genome::Utility::IO::SeparatedValueWriter->create(
         output    => $self->output_file,
@@ -112,11 +148,40 @@ sub execute {
     }
 
 # REPORTING
+    
+    #print header for variant output, if needed
+    if(defined($self->variant_file)){
+        if(defined($variants{"header"})){
+            print VARFILE join("\t",($variants{"header"},
+                                     "GeneName","MutationEffect","HLAallele","PeptideLength",
+                                     'SubPeptidePosition','MT Score',
+                                     'WT Score','MT Epitope Seq',
+                                     'WT Epitope Seq','Fold Change')) . "\n";
+        }
+    }
+                                 
+    
     foreach my $sample (sort keys %best) {
         foreach my $gene (sort keys %{ $best{$sample} }) {
             foreach my $entry (@{ $best{$sample}->{$gene}->{GENES} }) {
-                if ($entry->{'MT Score'} < $threshold) {
+                if (($entry->{'MT Score'} < $threshold) && ($entry->{'Fold Change'} > $self->minimum_fold_change)){
                     $out_fh->write_one($entry);
+
+                    #also write out variants
+                    if(defined($self->variant_file)){
+                        my $key = join("\t",$gene,$entry->{'Point Mutation'});
+                        if(defined($variants{$key})){                            
+                            print VARFILE join("\t", ($variants{$key},
+                                                      $gene,$entry->{'Point Mutation'},$entry->{Allele},
+                                                      $entry->{Length},$entry->{'Sub Peptide Position'},$entry->{'MT Score'},
+                                                      $entry->{'WT Score'},$entry->{'MT Epitope Seq'},
+                                                      $entry->{'WT Epitope Seq'},$entry->{'Fold Change'})) . "\n";
+
+                        } else {
+                            
+                            $self->warning_message("Couldn't find variant for " . $gene . " " . $entry->{'Point Mutation'});
+                        }
+                    }
                 }
             }
         }
