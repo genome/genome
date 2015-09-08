0d74fb84f9535bd827bce6468f7217b87071255e
Adam Coffman <acoffman@genome.wustl.edu>
2015-09-08 16:34:59 -0500

    add param mapping for metric accumulation level

--- lib/perl/Genome/Model/Tools/Picard/CollectAlignmentSummaryMetrics.pm
+++ lib/perl/Genome/Model/Tools/Picard/CollectAlignmentSummaryMetrics.pm
@@ -51,6 +51,13 @@ class Genome::Model::Tools::Picard::CollectAlignmentSummaryMetrics {
             is_optional   => 1,
             picard_param_name => 'IS_BISULFITE_SEQUENCED',
         },
+        metric_accumulation_level => {
+            is => 'Text',
+            is_many => 1,
+            is_optional => 1,
+            default_value => ['ALL_READS'],
+            picard_param_name => 'METRIC_ACCUMULATION_LEVEL',
+        }
     ],
 };
 
