e06d26798746a1d754ff3d59511c789d49217916
Eddie Belter <ebelter@genome.wustl.edu>
2015-09-24 14:34:58 -0500

    Site TGI: add command tree

---
+++ lib/perl/Genome/Site/TGI/Command.pm
@@ -0,0 +1,15 @@
+package Genome::Site::TGI::Command;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::InstrumentData::Command {
+    is => 'Command::Tree',
+    english_name => 'genome site tgi command',
+    doc => 'MGI site specific commands',
+};
+
+1;
+
