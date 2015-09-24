dface6a84556b45ac9087ed32215593103b64a56
Eddie Belter <ebelter@genome.wustl.edu>
2015-09-24 16:18:29 -0500

    Stie TGI: add sync command tree

---
+++ lib/perl/Genome/Site/TGI/Command/Sync.pm
@@ -0,0 +1,15 @@
+package Genome::Site::TGI::Command::Sync;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::InstrumentData::Command::Sync {
+    is => 'Command::Tree',
+    english_name => 'genome site tgi command sync',
+    doc => 'MGI site sync commands',
+};
+
+1;
+
