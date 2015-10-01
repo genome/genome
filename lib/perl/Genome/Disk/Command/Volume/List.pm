a9ce9f59ac5acb62385142e9a40fed3f3c333ba1
Anthony Brummett <abrummet@genome.wustl.edu>
2015-10-01 14:59:23 -0500

    Make the volume lister work
    Disk Groups validate that their disk_group_name belongs to a short list
    and dies otherwise to ensure Genome stuff only messes with analysis
    allocations.  For a basic lister, we don't want want this safety net.
    
    Getting the free space of an unmounted volume also throws an exception
    if the volume is unmounted to avoid mounting tons of volumes.  For this
    simple lister, we just print out "<unmounted>" for such volumes instead of
    dying.

--- lib/perl/Genome/Disk/Command/Volume/List.pm
+++ lib/perl/Genome/Disk/Command/Volume/List.pm
@@ -19,5 +19,34 @@ class Genome::Disk::Command::Volume::List {
     doc => 'Lists Genome::Disk::Volume objects',
 };
 
+sub execute {
+    my $self = shift;
+
+    require Genome::Disk::Group;
+    require Genome::Disk::Volume;
+    no warnings 'redefine';
+    local *Genome::Disk::Group::validate = sub { 1 };  # For just listing, don't validate them
+    local *Genome::Disk::Volume::used_kb = make_is_mounted_wrapper(\&Genome::Disk::Volume::used_kb);
+    local *Genome::Disk::Volume::percent_used = make_is_mounted_wrapper(\&Genome::Disk::Volume::percent_used);
+
+    my $super_execute = $self->super_can('_execute_body');
+    return $self->$super_execute(@_);
+}
+
+my %cached_is_mounted;
+sub make_is_mounted_wrapper {
+    my $original_sub = shift;
+
+    return sub {
+        my $volume = shift;
+
+        if ($cached_is_mounted{$volume->id} //= $volume->is_mounted) {
+            return $volume->$original_sub(@_);
+        } else {
+            return '<unmounted>';
+        }
+    };
+}
+
 1;
 
