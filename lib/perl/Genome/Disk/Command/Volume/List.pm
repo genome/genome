aadaa80f0366321d213bfc5d41cc5905c60c0eec
Anthony Brummett <abrummet@genome.wustl.edu>
2015-10-01 16:54:47 -0500

    Add --accurate-size option
    If true, it will attempt to mount all the displayed volumes so the
    percent_used output column is accurate and doesn't just show it's
    unmounted.

--- lib/perl/Genome/Disk/Command/Volume/List.pm
+++ lib/perl/Genome/Disk/Command/Volume/List.pm
@@ -16,6 +16,11 @@ class Genome::Disk::Command::Volume::List {
         show => { 
             default_value => 'mount_path,disk_group_names,total_kb,percent_used,percent_allocated', 
         },
+        accurate_size => {
+            is => 'Boolean',
+            default_value => 0,
+            doc => 'Mount each displayed volume to display used/free sizes accurately',
+        },
     ],
     doc => 'Lists Genome::Disk::Volume objects',
 };
@@ -27,20 +32,20 @@ sub execute {
     require Genome::Disk::Volume;
     no warnings 'redefine';
     local *Genome::Disk::Group::validate = sub { 1 };  # For just listing, don't validate them
-    local *Genome::Disk::Volume::used_kb = make_is_mounted_wrapper(\&Genome::Disk::Volume::used_kb);
-    local *Genome::Disk::Volume::percent_used = make_is_mounted_wrapper(\&Genome::Disk::Volume::percent_used);
+    local *Genome::Disk::Volume::used_kb = $self->make_is_mounted_wrapper(\&Genome::Disk::Volume::used_kb);
+    local *Genome::Disk::Volume::percent_used = $self->make_is_mounted_wrapper(\&Genome::Disk::Volume::percent_used);
 
     my $super_execute = $self->super_can('_execute_body');
     return $self->$super_execute(@_);
 }
 
 sub make_is_mounted_wrapper {
-    my $original_sub = shift;
+    my($self, $original_sub) = @_;
 
     return sub {
         my $volume = shift;
 
-        if (is_volume_mounted($volume)) {
+        if ($self->is_volume_mounted($volume)) {
             return $volume->$original_sub(@_);
         } else {
             return '<unmounted>';
@@ -50,17 +55,30 @@ sub make_is_mounted_wrapper {
 
 my %cached_is_mounted;
 sub is_volume_mounted {
-    my $volume = shift;
+    my($self, $volume) = @_;
 
     unless (%cached_is_mounted) {
-        my $mounts = IO::File->new('/proc/mounts');
-        while(my $line = $mounts->getline) {
-            my(undef, $mount_path) = split(/\s/, $line);
-            $cached_is_mounted{$mount_path} = 1;
-        }
+        _populate_cached_is_mounted();
     }
 
-    return $cached_is_mounted{$volume->mount_path};
+    my $mount_path = $volume->mount_path;
+
+    if (! exists($cached_is_mounted{$mount_path}) && $self->accurate_size) {
+        my $dir = IO::Dir->new($mount_path);
+        $dir->read if $dir;
+        _populate_cached_is_mounted();
+        $cached_is_mounted{$mount_path} //= 0;
+    }
+
+    return $cached_is_mounted{$volume->mount_path}
+}
+
+sub _populate_cached_is_mounted {
+    my $mounts = IO::File->new('/proc/mounts');
+    while(my $line = $mounts->getline) {
+        my(undef, $mount_path) = split(/\s/, $line);
+        $cached_is_mounted{$mount_path} = 1;
+    }
 }
 
 1;
