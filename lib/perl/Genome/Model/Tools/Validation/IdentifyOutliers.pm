8251f279d2aaea48d116c726e88d1a9ed1021a30
Chris Miller <cmiller@genome.wustl.edu>
2015-10-02 16:37:25 -0500

    Adds sanity checks to header and required cols

--- lib/perl/Genome/Model/Tools/Validation/IdentifyOutliers.pm
+++ lib/perl/Genome/Model/Tools/Validation/IdentifyOutliers.pm
@@ -14,61 +14,61 @@ class Genome::Model::Tools::Validation::IdentifyOutliers {
     is => 'Command',
 
     has => [
-        input_file => { 
-            is => 'Text', 
+        input_file => {
+            is => 'Text',
             doc => "annotation format with readcounts added and headers required. Often named snvs.indels.annotated",
             is_optional => 0,
         },
-        output_file => { 
-            is => 'Text', 
+        output_file => {
+            is => 'Text',
             doc => "file into which high-confidence output should be placed",
             is_optional => 0,
         },
-        lq_output_file => { 
-            is => 'Text', 
-            doc => "file into which low-confidence output should be placed",            
+        lq_output_file => {
+            is => 'Text',
+            doc => "file into which low-confidence output should be placed",
             is_optional => 1,
         },
-        error_rate => { 
-            is => 'Number', 
-            doc => "The assumed per site error rate. Should be between 0 and 1", 
-            is_optional => 1, 
+        error_rate => {
+            is => 'Number',
+            doc => "The assumed per site error rate. Should be between 0 and 1",
+            is_optional => 1,
             default => '0.01'
         },
-        tumor_purity => { 
-            is => 'Number', 
-            doc => "The estimated purity of the tumor. Should be between 0 and 1", 
-            is_optional => 1, 
+        tumor_purity => {
+            is => 'Number',
+            doc => "The estimated purity of the tumor. Should be between 0 and 1",
+            is_optional => 1,
             default => '1.0',
             is_optional => 1,
         },
-        normal_contamination_rate => { 
-            is => 'Number', 
-            doc => "The estimated contamination rate of the normal with the tumor. Should be between 0 and 1", is_optional => 1, 
+        normal_contamination_rate => {
+            is => 'Number',
+            doc => "The estimated contamination rate of the normal with the tumor. Should be between 0 and 1", is_optional => 1,
             default => '0.0',
             is_optional => 1,
         },
-        llr_cutoff => { 
-            is => 'Number', 
+        llr_cutoff => {
+            is => 'Number',
             doc => "minimum log likelihood to report a potential miscall",
             default => 10,
             is_optional => 1,
         },
-        # min_p_value => { 
-        #     is => 'Number', 
-        #     doc => "minimum somatic confidence of Varscan to report the potential miscall", 
+        # min_p_value => {
+        #     is => 'Number',
+        #     doc => "minimum somatic confidence of Varscan to report the potential miscall",
         #     default => 0.001
         # },
         # print_all => {
-        #     is => 'Boolean', 
-        #     doc => 'print information on all sites regardless of cutoffs', 
+        #     is => 'Boolean',
+        #     doc => 'print information on all sites regardless of cutoffs',
         #     default => 0,
         #     is_optional => 1,
         # },
-        
+
         print_only_somatic => {
-            is => 'Boolean', 
-            doc => 'only print somatic sites, remove non-somatic', 
+            is => 'Boolean',
+            doc => 'only print somatic sites, remove non-somatic',
             default => 0,
             is_optional => 1,
         }
@@ -105,27 +105,38 @@ sub execute {
     my $normal_var_col;
     my $tumor_ref_col;
     my $tumor_var_col;
+    my $linecount = 0;
 
     #scan through file and identify where we think there are outliers
     while(my $line = $fh->getline) {
         chomp $line;
         my @F = split /\t/, $line;
-        
-        #get fields to query from header
-        if($line =~ /chromosome_name/){
-            $normal_ref_col = List::MoreUtils::first_index {$_ =~ /Normal_ref_count/ } @F;
-            $normal_var_col = List::MoreUtils::first_index {$_ =~ /Normal_var_count/ } @F;
-            $tumor_ref_col = List::MoreUtils::first_index {$_ =~ /Tumor_ref_count/ } @F;
-            $tumor_var_col = List::MoreUtils::first_index {$_ =~ /Tumor_var_count/ } @F;
 
-            print OUTFILE $line . "\tFilterCall\tProb\tLLR\n";
-            if(defined($self->lq_output_file)){
-                print LQFILE $line . "\tFilterCall\tProb\tLLR\n";
+        #get fields to query from header
+        if($linecount == 0){
+            if($line =~ /chromosome_name/){
+                $normal_ref_col = List::MoreUtils::first_index {$_ =~ /Normal_ref_count/ } @F;
+                $normal_var_col = List::MoreUtils::first_index {$_ =~ /Normal_var_count/ } @F;
+                $tumor_ref_col = List::MoreUtils::first_index {$_ =~ /Tumor_ref_count/ } @F;
+                $tumor_var_col = List::MoreUtils::first_index {$_ =~ /Tumor_var_count/ } @F;
+
+                if(($normal_ref_col == -1) || ($normal_var_col == -1) ||($tumor_ref_col == -1) ||($tumor_var_col == -1)){
+                    $self->error_message("At least one column is missing. Expected headers Normal_ref_count, Normal_var_count, Tumor_ref_count, Tumor_var_count");
+                    return(undef);
+                }
+
+                print OUTFILE $line . "\tFilterCall\tProb\tLLR\n";
+                if(defined($self->lq_output_file)){
+                    print LQFILE $line . "\tFilterCall\tProb\tLLR\n";
+                }
+            } else {
+                $self->error_message("Expected header line was not present. TGI annotation format required, with \"chromosome_name\" as first column and columns named Normal_ref_count, Normal_var_count, Tumor_ref_count, Tumor_var_count");
+                return(undef)
             }
+            $linecount++;
             next;
         }
 
-        
         my $normal_ref = $F[$normal_ref_col];
         my $normal_var = $F[$normal_var_col];
         my $tumor_ref = $F[$tumor_ref_col];
@@ -141,13 +152,13 @@ sub execute {
         my $max2_llr;
         my $max2_call;
         my $marginal_probability;
-        
+
         #pass through long indels because we can't grab readcounts.
         if($normal_ref eq "NA" || $normal_var eq "NA" || $tumor_ref eq "NA" || $tumor_var eq "NA"){
             print OUTFILE $line . "\tSomatic\tNA\tNA\n";
             next;
         }
-        
+
 
         if($normal_ref + $normal_var > 0 && $tumor_ref + $tumor_var > 0) {
 
@@ -172,7 +183,7 @@ sub execute {
 
 
             #weighted average of the frequencies
-            my $error_expect = ($tumor_var + $normal_var)/($tumor_var + $tumor_ref + $normal_var + $normal_ref); 
+            my $error_expect = ($tumor_var + $normal_var)/($tumor_var + $tumor_ref + $normal_var + $normal_ref);
             #my $error_expect = ($tumor_var/($tumor_var + $tumor_ref) + $normal_var/($normal_var + $normal_ref))/2; #average of the frequencies
 
 
@@ -188,19 +199,19 @@ sub execute {
             elsif($tumor_freq == 1) {
                 $tumor_freq -= $error_rate;
             }
-            my %calls = (   
+            my %calls = (
                 Reference       => [$error_rate, $error_rate],
                 Germline_het    => [$heterozygous_expect, $heterozygous_expect],
                 Germline_hom    => [$homozygous_expect, $homozygous_expect],
                 Somatic     => [$error_rate + ($tumor_freq / $tumor_purity * $normal_contamination_rate), $tumor_freq],
                 LOH_variant     => [$heterozygous_expect, $homozygous_expect],
                 LOH_ref         => [$heterozygous_expect, $error_rate],
-                NotSomatic           => [$error_expect, $error_expect], #In general this fits too well. It would need some other parameter that competed equally well to work. Perhaps, for instance, the existing tumor frequency and error in the normal 
+                NotSomatic           => [$error_expect, $error_expect], #In general this fits too well. It would need some other parameter that competed equally well to work. Perhaps, for instance, the existing tumor frequency and error in the normal
             );
 
             my $llr_calculator = $self->generate_llr_calculator($normal_ref,$normal_var,$tumor_ref,$tumor_var);
 
-            for my $call (keys %calls) { 
+            for my $call (keys %calls) {
                 my $llr = $llr_calculator->(@{$calls{$call}});
                 #add to marginal
                 unless(defined $marginal_probability) {
@@ -231,9 +242,9 @@ sub execute {
             }
             my $llr = defined $max_llr ? $max_llr-$max2_llr : 0;
 
-            # if((($varscan_call eq 'Somatic' && $max_call ne 'Somatic') || 
-            #     (($varscan_call ne 'Somatic' && $varscan_call ne 'LOH')  && $max_call eq 'Somatic')) || 
-            #    ($llr < $self->llr_cutoff && ($varscan_call eq 'Somatic')) || 
+            # if((($varscan_call eq 'Somatic' && $max_call ne 'Somatic') ||
+            #     (($varscan_call ne 'Somatic' && $varscan_call ne 'LOH')  && $max_call eq 'Somatic')) ||
+            #    ($llr < $self->llr_cutoff && ($varscan_call eq 'Somatic')) ||
             #    $self->print_all) {
             my $call = (defined $max_call ? $max_call : '-');
 
@@ -256,7 +267,7 @@ sub execute {
                     print OUTFILE "\n";
                 }
             }
-            
+
         } else {
             # if there are no reads in either the tumor or normal, we set it to Non-Somatic with low probability
             # (no confidence if no reads)
@@ -267,7 +278,7 @@ sub execute {
                 }
             }
         }
-        
+
 
     }
     close(OUTFILE);
