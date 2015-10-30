5d6106d129c86a8e0a4705c58a1b71b0848f1497
apregier <aregier@genome.wustl.edu>
2015-10-30 15:30:19 -0500

    Make NCallersFilter and VariantCallersInterpreter both use NCallersBase

--- lib/perl/Genome/VariantReporting/Generic/VariantCallersInterpreter.pm
+++ lib/perl/Genome/VariantReporting/Generic/VariantCallersInterpreter.pm
@@ -3,18 +3,10 @@ package Genome::VariantReporting::Generic::VariantCallersInterpreter;
 use strict;
 use warnings;
 use Genome;
-use List::MoreUtils qw/uniq/;
 
 class Genome::VariantReporting::Generic::VariantCallersInterpreter {
-    is => ['Genome::VariantReporting::Framework::Component::Interpreter', 'Genome::VariantReporting::Framework::Component::WithSampleName'],
+    is => ['Genome::VariantReporting::Framework::Component::Interpreter', 'Genome::VariantReporting::Generic::NCallersBase'],
     doc => 'Output a list of variant callers that called this position for the specified sample, as well as the number of callers',
-    has => [
-        valid_callers => {
-            is => 'String',
-            is_many => 1,
-            doc => 'List of variant callers to include in determination for filtering',
-        },
-    ],
 };
 
 sub name {
@@ -39,26 +31,11 @@ sub _interpret_entry {
     my $passed_alt_alleles = shift;
 
     my %return_values;
-    my %callers;
     for my $alt_allele (@$passed_alt_alleles) {
         $return_values{$alt_allele} = { variant_callers => [] };
-        $callers{$alt_allele} = [];
     }
 
-    for my $caller_name ($self->valid_callers) {
-        my $sample_name = $self->sample_name_with_suffix($caller_name);
-        my $sample_index = eval{ $entry->{header}->index_for_sample_name($sample_name) };
-        my $error = $@;
-        if (defined($error) &&
-            $error =~ /^\QSample name $sample_name not found in header\E/) {
-            next;
-        }
-        my @sample_alt_alleles = $entry->alt_bases_for_sample($sample_index);
-        for my $sample_alt_allele (uniq @sample_alt_alleles) {
-            push(@{$callers{$sample_alt_allele}}, $caller_name);
-        }
-    }
-
+    my %callers = $self->get_callers($entry, $passed_alt_alleles);
     for my $alt_allele (keys %return_values) {
         $return_values{$alt_allele} = {
             variant_callers => $callers{$alt_allele},
