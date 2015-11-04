f268f18fb33aa8fa37c63ddbadc09227278944d6
Thomas Mooney <tmooney@genome.wustl.edu>
2015-11-04 08:39:50 -0600

    Remove _collect_call_info.
    This temporary data collection was added in PR #573.

--- lib/perl/Genome/Model/Tools/Picard.pm
+++ lib/perl/Genome/Model/Tools/Picard.pm
@@ -345,38 +345,8 @@ sub run_java_vm {
     return 1;
 }
 
-sub _collect_call_info {
-    my $class = shift;
-    my @args = @_;
-
-    eval {
-        use Devel::StackTrace;
-        use JSON;
-        use HTTP::Request;
-        use LWP::UserAgent;
-
-        my $trace = Devel::StackTrace->new();
-        my @frames;
-        while ( my $frame = $trace->prev_frame() ) {
-            push @frames, $frame->as_string;
-        }
-
-        my $request = HTTP::Request->new(GET => 'http://linus47.gsc.wustl.edu:3000');
-        $request->content_type('application/json');
-        $request->content(encode_json({
-            frames => [@frames],
-            args => [@args],
-        }));
-
-        my $ua = LWP::UserAgent->new();
-        $ua->request($request);
-    };
-    $class->warning_message($@) if $@;
-}
-
 sub create {
     my $class = shift;
-    $class->_collect_call_info(@_);
     my $self = $class->SUPER::create(@_);
 
     if (not defined $self->use_version) {
