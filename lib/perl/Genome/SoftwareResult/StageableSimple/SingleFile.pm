45468ac17db425875b35461eea952576380eaf85
Susanna Kiwala <ssiebert@genome.wustl.edu>
2015-11-18 10:30:39 -0600

    New StageableSimple subclass for a single file

---
+++ lib/perl/Genome/SoftwareResult/StageableSimple/SingleFile.pm
@@ -0,0 +1,27 @@
+package Genome::SoftwareResult::StageableSimple::SingleFile;
+
+use strict;
+use warnings;
+use Genome;
+use File::Basename;
+
+class Genome::SoftwareResult::StageableSimple::SingleFile {
+    is => 'Genome::SoftwareResult::StageableSimple',
+};
+
+sub _temp_staging_file_path {
+    my $self = shift;
+    return File::Spec->join($self->temp_staging_directory, $self->_file_name);
+}
+
+sub _file_name {
+    my $self = shift;
+    $self->fatal_message("Abstract method (_vcf_file_name). Needs to be overwritten in your child class");
+}
+
+sub file_path {
+    my $self = shift;
+    return File::Spec->join($self->output_dir, $self->_file_name);
+}
+
+1;
