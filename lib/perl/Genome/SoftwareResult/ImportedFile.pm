9491295bb0057af49a5a87f6907ae7c1fd11eeb9
Susanna Kiwala <ssiebert@genome.wustl.edu>
2015-11-18 14:59:36 -0600

    Make source_file_path a metric and add the md5sum as an input

--- lib/perl/Genome/SoftwareResult/ImportedFile.pm
+++ lib/perl/Genome/SoftwareResult/ImportedFile.pm
@@ -7,11 +7,35 @@ use Genome;
 class Genome::SoftwareResult::ImportedFile {
     is => 'Genome::SoftwareResult::StageableSimple::SingleFile',
     has_input => [
+        file_content_hash => {
+            is => 'Text',
+            doc => 'MD5 hash of the file that is contained in this result',
+        },
+    ],
+    has_optional_metric => [
         source_file_path => {
             is => 'Path',
+            doc => 'Path to the original file used to create this result',
         }
     ],
 };
+
+sub create {
+    my ($class, %params) = @_;
+
+    my $file = delete $params{source_file_path};
+    $class->fatal_message('No source file path given to create software result!') if not defined $file;
+    $file = Cwd::abs_path($file);
+    $class->fatal_message('Source file path (%s) given to create software result does not exist!', $file) if not -s $file;
+
+    my $md5 = Genome::Sys->md5sum($file);
+    $class->fatal_message('No md5 for source file! %s', $file) if not $md5;
+
+    return $class->SUPER::create(
+        source_file_path => $file,
+        file_content_hash => $md5,
+    );
+}
 
 sub _run{
     my $self = shift;
