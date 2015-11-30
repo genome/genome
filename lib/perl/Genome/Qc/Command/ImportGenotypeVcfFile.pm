939d1b38e710f75c9c6cd573e126229fe363b2a5
Susanna Kiwala <ssiebert@genome.wustl.edu>
2015-11-30 10:15:08 -0600

    Add reference sequence build to G::Qc::C::ImportGenotypeVcfFile to use as requestor

--- lib/perl/Genome/Qc/Command/ImportGenotypeVcfFile.pm
+++ lib/perl/Genome/Qc/Command/ImportGenotypeVcfFile.pm
@@ -10,7 +10,21 @@ class Genome::Qc::Command::ImportGenotypeVcfFile {
         genotype_vcf_file => {
             is => 'Path',
             doc => 'Path to the genotype vcf file to import',
-        }
+        },
+        reference_sequence_build => {
+            is => 'Genome::Model::Build::ReferenceSequence',
+            id_by => 'reference_sequence_build_id',
+            doc => 'Reference sequence build that the genotype vcf is aligned to',
+        },
+    ],
+    has_optional_input => [
+        requestor => { via => '__self__', to => 'reference_sequence_build' },
+        user => { via => '__self__', to => 'reference_sequence_build' },
+        label => { value => 'reference sequence build' },
+        reference_sequence_build_id => {
+            is => 'Number',
+            doc => 'the reference to use by id',
+        },
     ],
     doc => 'Import a genotype vcf file for QC. This creates a software result and the genotype vcf file is copied into its allocation.',
 };
--- lib/perl/Genome/Qc/Command/ImportGenotypeVcfFile.t
+++ lib/perl/Genome/Qc/Command/ImportGenotypeVcfFile.t
@@ -10,17 +10,20 @@ use warnings;
 
 use above "Genome";
 use Test::More;
-use Genome::Test::Factory::SoftwareResult::User;
+use Genome::Test::Factory::Model::ReferenceSequence;
+use Genome::Test::Factory::Build;
 
 my $pkg = 'Genome::Qc::Command::ImportGenotypeVcfFile';
 use_ok($pkg);
 
 my $test_dir = __FILE__.'.d';
 my $genotype_vcf_file = File::Spec->join($test_dir, 'genotype.vcf');
+my $reference_sequence_model = Genome::Test::Factory::Model::ReferenceSequence->setup_object();
+my $reference_sequence_build = Genome::Test::Factory::Build->setup_object(model_id => $reference_sequence_model->id);
 
 my $cmd = $pkg->create(
     genotype_vcf_file => $genotype_vcf_file,
-    %{Genome::Test::Factory::SoftwareResult::User->setup_user_hash},
+    reference_sequence_build => $reference_sequence_build,
 );
 isa_ok($cmd, $pkg);
 ok($cmd->execute, 'Command executes correctly');
