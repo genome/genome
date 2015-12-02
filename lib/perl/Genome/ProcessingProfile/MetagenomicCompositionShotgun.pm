e5952977f95770461b49201d0e42952be757bb61
Eddie Belter <ebelter@genome.wustl.edu>
2015-12-02 14:00:24 -0600

    CHANGELOG: Deprecate MetagenomicCompositionShotgun

---
+++ lib/perl/Genome/ProcessingProfile/MetagenomicCompositionShotgun.pm
@@ -0,0 +1,17 @@
+package Genome::ProcessingProfile::MetagenomicCompositionShotgun;
+
+use strict;
+use warnings;
+
+use Genome;
+
+class Genome::ProcessingProfile::MetagenomicCompositionShotgun {
+    is => 'Genome::ProcessingProfile',
+};
+
+sub create {
+    die __PACKAGE__ . ' is deprecated.';
+}
+
+1;
+
