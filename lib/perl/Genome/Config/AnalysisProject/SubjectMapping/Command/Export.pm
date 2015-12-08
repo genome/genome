0b58923d7b277236cee3dd9cc7f4eaed9bc297ae
Thomas Mooney <tmooney@genome.wustl.edu>
2015-12-08 09:57:40 -0600

    New command to export subject mappings back out.
    (This will only work for somatic-validation subject mappings previously
    created with the import command.  Any others will produce a bad file.)

---
+++ lib/perl/Genome/Config/AnalysisProject/SubjectMapping/Command/Export.pm
@@ -0,0 +1,11 @@
+package Genome::Config::AnalysisProject::SubjectMapping::Command::Export;
+
+use strict;
+use warnings;
+
+class Genome::Config::AnalysisProject::SubjectMapping::Command::Export {
+    is => 'Command::Tree',
+    doc => 'commands that deal with subject mapping for analysis projects',
+};
+
+1;
