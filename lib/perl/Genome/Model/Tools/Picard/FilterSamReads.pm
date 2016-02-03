1a22487d0c46b0229ccd27463629796cf669f71d
Jason Walker <jwalker@genome.wustl.edu>
2016-02-03 11:56:12 -0600

    Update the FilterSamReads picard command to use the Base picard class.  Also an integration test since the old test only covered the command creation logic.

--- lib/perl/Genome/Model/Tools/Picard/FilterSamReads.pm
+++ lib/perl/Genome/Model/Tools/Picard/FilterSamReads.pm
@@ -6,38 +6,43 @@ use warnings;
 use Genome;
 
 class Genome::Model::Tools::Picard::FilterSamReads {
-    is => 'Genome::Model::Tools::Picard',
+    is => 'Genome::Model::Tools::Picard::Base',
     has_input => [
         input_file => {
           is  => 'String',
           doc => 'The SAM or BAM file that will be filtered.',
+          picard_param_name => 'INPUT',
       },
       output_file => {
           is  => 'String',
-          doc => 'The resulting SAM/BAM file'
+          doc => 'The resulting SAM/BAM file',
+          picard_param_name => 'OUTPUT',
       },
       filter => {
           is           => 'String',
           doc          => 'Filter to pass to picard, if selected filter uses a Read List, you must specify one',
           valid_values => ['includeAligned', 'excludeAligned', 'includeReadList', 'excludeReadList'],
+          picard_param_name => 'FILTER',
       },
       read_list_file => {
           is          => 'String',
           doc         => 'File containing reads that will be included or excluded from the output',
           is_optional => 1,
+          picard_param_name => 'READ_LIST_FILE',
       },
       sort_order => {
           is           => 'String',
           doc          => 'Specify only if you want a different sort order than the input file',
           is_optional  => 1,
           valid_values => ['unsorted', 'queryname', 'coordinate'],
+          picard_param_name => 'SORT_ORDER',
       },
-      write_reads_file => {
-          is            => 'String',
-          doc           => 'Create .reads file during run',
+      write_reads_files => {
+          is            => 'Boolean',
+          doc           => 'Create .reads files during run',
           is_optional   => 1,
-          default_value => 'true',
-          valid_values  => ['true', 'false']
+          default_value => '1',
+          picard_param_name => 'WRITE_READS_FILES',
       },
     ],
 };
@@ -53,51 +58,18 @@ sub help_detail {
 EOS
 }
 
-sub execute {
+sub minimum_version_required { '1.77'; }
+sub _jar_name { 'FilterSamReads.jar'; }
+sub _java_class { 'net.sf.picard.sam.FilterSamReads'; }
+
+sub _shellcmd_extra_params {
     my $self = shift;
-
-    my $input_file = $self->input_file;
-    my $output_file = $self->output_file;
-
-    my $jar_path = $self->picard_path . '/FilterSamReads.jar';
-    unless ( -e $jar_path ) {
-        die( "Failed to find $jar_path!" .
-          "This command may not be available in version " . $self->use_version );
-    }
-
-    my $cmd = $self->_generate_cmd_string( $jar_path );
-
-    $self->run_java_vm(
-        cmd                       => $cmd,
-        input_files               => [$input_file],
-        output_files              => [$output_file],
+    return (
+        input_files               => [$self->input_file],
+        output_files              => [$self->output_file],
         skip_if_output_is_present => 0,
     );
-
-    return 1;
 }
 
-sub _generate_cmd_string {
-    my $self = shift;
-    my $jar_path = shift;
-
-    my $input_file = $self->input_file;
-    my $output_file = $self->output_file;
-
-    #build command, including required args by default
-    my $cmd = $jar_path . " net.sf.picard.sam.FilterSamReads O=$output_file" .
-      " I=$input_file FILTER=" . $self->filter .
-      " WRITE_READS_FILES=" . $self->write_reads_file;
-
-    #optional params
-    if ( defined( $self->read_list_file ) ) {
-        $cmd .= " READ_LIST_FILE=" . $self->read_list_file;
-    }
-    if ( defined( $self->sort_order ) ) {
-        $cmd .= " SORT_ORDER=" . $self->sort_order;
-    }
-
-    return $cmd;
-}
 
 1;
