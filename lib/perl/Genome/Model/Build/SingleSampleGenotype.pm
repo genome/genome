f40aa351de3dee1f42451b3d421ca598cc0f6d46
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-02-26 14:08:33 -0600

    Add _compare_output_files for SSG builds

--- lib/perl/Genome/Model/Build/SingleSampleGenotype.pm
+++ lib/perl/Genome/Model/Build/SingleSampleGenotype.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use Genome;
+use Set::Scalar;
+use Data::Compare;
 
 class Genome::Model::Build::SingleSampleGenotype {
     is => ['Genome::Model::Build'],
@@ -66,6 +68,69 @@ sub reference_being_replaced_for_input {
 
     if($input->name eq 'instrument_data') {
         return 1; #we'll re-align
+    }
+
+    return;
+}
+
+sub _compare_output_files {
+    my ($self, $other_build) = @_;
+
+    my %diffs = (
+        $self->_compare_merged_alignment_result($other_build),
+        $self->_compare_haplotype_caller_results($other_build),
+        $self->_compare_qc_metrics($other_build),
+    );
+
+    return %diffs;
+}
+
+sub _compare_merged_alignment_result {
+    my ($self, $other_build) = @_;
+
+    my $blessed_merged_alignment_result = $self->merged_alignment_result->bam_file;
+    my $other_merged_alignment_result = $other_build->merged_alignment_result->bam_file;
+    my $diff_result = !$self->file_comparer->compare($blessed_merged_alignment_result, $other_merged_alignment_result);
+
+    unless ($diff_result) {
+        return ('merged_alignment_result' => "files are not the same (diff -u $blessed_merged_alignment_result $other_merged_alignment_result)");
+    }
+
+    return;
+}
+
+sub _compare_haplotype_caller_results {
+    my ($self, $other_build) = @_;
+
+    my @blessed_haplotype_caller_results = $self->haplotype_caller_result;
+    my @other_haplotype_caller_results = $other_build->haplotype_caller_result;
+
+    my %diffs;
+    for my $blessed_haplotype_caller_result (@blessed_haplotype_caller_results) {
+        my $blessed_haplotype_caller_result_intervals = Set::Scalar->new($blessed_haplotype_caller_result->intervals);
+        my ($other_haplotype_caller_result) = grep {
+            my $other_haplotype_caller_result_intervals = Set::Scalar->new($_->intervals);
+            $other_haplotype_caller_result_intervals->is_equal($blessed_haplotype_caller_result_intervals);
+        } @other_haplotype_caller_results;
+        my $diff_result = !$self->file_comparer->compare($blessed_haplotype_caller_result, $other_haplotype_caller_result);
+
+        unless ($diff_result) {
+            $diffs{sprintf("haplotype_caller_result-%s", $blessed_haplotype_caller_result->id)} =
+                "files are not the same (diff -u $blessed_haplotype_caller_result->vcf_file $other_haplotype_caller_result->vcf_file)";
+        }
+    }
+
+    return %diffs;
+}
+
+sub _compare_qc_metrics {
+    my ($self, $other_build) = @_;
+
+    my %blessed_qc_metrics = $self->qc_result->get_metrics;
+    my %other_qc_metrics = $other_build->qc_result->get_metrics;
+
+    unless (Compare(\%blessed_qc_metrics, \%other_qc_metrics)) {
+        return ('qc_result' => "qc metrics are not the same");
     }
 
     return;
