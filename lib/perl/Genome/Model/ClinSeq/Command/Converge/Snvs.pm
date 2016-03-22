628e985edfbec3e29757c29862eabf85b95e86f4
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-03-22 11:17:48 -0500

    perltidy Converge Snvs

--- lib/perl/Genome/Model/ClinSeq/Command/Converge/Snvs.pm
+++ lib/perl/Genome/Model/ClinSeq/Command/Converge/Snvs.pm
@@ -4,17 +4,17 @@ use warnings;
 use Genome;
 
 class Genome::Model::ClinSeq::Command::Converge::Snvs {
-    is => 'Genome::Model::ClinSeq::Command::Converge::Base',
+    is  => 'Genome::Model::ClinSeq::Command::Converge::Base',
     doc => 'converge SNV results from mutiple clinseq builds'
 };
 
 sub resolve_labels {
     my $self = shift;
-  
+
     # tweak what gets passed in
-    
+
     my %results = $self->SUPER::resolve_labels(@_);
-    
+
     # tweak what came out
 
     return %results;
@@ -25,10 +25,10 @@ sub execute {
 
     my @stuff;
     my @labels;
-    my %labeled_stuff = $self->resolve_labels(\@stuff,\@labels);   
+    my %labeled_stuff = $self->resolve_labels(\@stuff, \@labels);
 
     return 1;
-};
+}
 
 1;
 
