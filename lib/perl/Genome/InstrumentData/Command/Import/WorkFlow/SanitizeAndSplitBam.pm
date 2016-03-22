b700148c03c77522ec565433c0b8de29286d0418
Eddie Belter <ebelter@genome.wustl.edu>
2016-03-22 14:20:19 -0500

    Sanitize and Split Bam
    consolidate prcoess reads
    collect reads by template and read 1/2
    remove supplementary reads
    only use one read 1/2 each [including non primary alignments]
    write all singletons to one file instead of read1/2
    use set flags for read 1/2 singleton paired
    sanitize alignment info
    remove all read tags
    add new read group tag

--- lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/SanitizeAndSplitBam.pm
+++ lib/perl/Genome/InstrumentData/Command/Import/WorkFlow/SanitizeAndSplitBam.pm
@@ -63,44 +63,28 @@ sub _process_reads {
         die $self->error_message('Failed to open file handle to samtools command!');
     }
 
-    my $previous_tokens;
-    my $previous_read_group_id;
+    my ($template_name, @reads);
     while ( my $line = $bam_fh->getline ) {
         chomp $line;
         my @tokens = split(/\t/, $line);
-        my $read_group_id = $self->_get_rg_id_from_sam_tokens(\@tokens);
 
-        unless($previous_tokens) {
-            $previous_tokens = \@tokens;
-            $previous_read_group_id = $read_group_id;
+        if ( not $template_name ) {
+            $template_name = $tokens[0];
+        }
+
+        if ( $tokens[0] eq $template_name ) {
+            # group reads by template name
+            push @reads, \@tokens;
             next;
         }
 
-        if($tokens[0] eq $previous_tokens->[0] and $previous_read_group_id eq $read_group_id) {
-            $self->_write_reads_based_on_read_group_and_type(
-                rg_id => $read_group_id,
-                type => 'paired',
-                reads => [ $previous_tokens, \@tokens ],
-            );
-            undef $previous_tokens;
-            undef $previous_read_group_id;
-        } else {
-            $self->_write_reads_based_on_read_group_and_type(
-                rg_id => $previous_read_group_id,
-                type => _read1_or_read2($previous_tokens->[1]),
-                reads => [ $previous_tokens, ],
-            );
-            $previous_tokens = \@tokens;
-            $previous_read_group_id = $read_group_id;
-        }
+        $self->_write_reads(@reads);
+        $template_name = $tokens[0];
+        @reads = ( \@tokens );
     }
 
-    if ($previous_tokens) {
-        $self->_write_reads_based_on_read_group_and_type(
-            rg_id => $previous_read_group_id,
-            type => _read1_or_read2($previous_tokens->[1]),
-            reads => [ $previous_tokens, ],
-        );
+    if ( @reads ) {
+        $self->_write_reads(@reads);
     }
 
     for my $fh ( $bam_fh, values %{$self->_read_group_fhs} ) {
@@ -109,23 +93,6 @@ sub _process_reads {
 
     $self->debug_message('Processing reads...done');
     return 1;
-}
-
-sub _get_rg_id_from_sam_tokens {
-    my ($self, $tokens) = @_;
-
-    my $rg_tag_idx = List::MoreUtils::firstidx { $_ =~ m/^RG:/ } @$tokens;
-    my $rg_tag;
-    if ( defined $rg_tag_idx ) {
-        return (split(':', $tokens->[$rg_tag_idx]))[2];
-    }
-    else {
-        return 'unknown';
-    }
-}
-
-sub _read1_or_read2 {
-    $_[0] & 0x80 ? 'read2' : 'read1'; # unlabeled reads will be read1
 }
 
 sub _verify_read_count {
@@ -165,29 +132,99 @@ sub _verify_read_count {
     return 1;
 }
 
-sub _write_reads_based_on_read_group_and_type {
-    my ($self, %params) = @_;
+sub _write_reads {
+    my ($self, $reads) = @_;
+
+    my $rg_id = $self->_read_group_id_for_reads($reads);
+    my ($read1, $read2) =_separate_reads($reads);
+    my $type = _determine_type($read1, $read2);
 
     my $fhs = $self->_read_group_fhs;
-    my $key = join('*', $params{rg_id}, $params{type});
+    my $key = join('*', $rg_id, $type);
     unless(exists $fhs->{$key}) {
-        $fhs->{$key} = $self->_open_fh_for_read_group_and_type($params{rg_id}, $params{type});
+        $fhs->{$key} = $self->_open_fh_for_read_group_and_type($rg_id, $type);
         $self->_read_group_fhs($fhs);
     }
 
-    for my $read_tokens ( @{$params{reads}} ) {
+    for my $read_tokens ( grep { defined } ( $read1, $read2 ) ) {
         # Set flag
-        $read_tokens->[1] = ( $read_tokens->[1] & 0x80 ? 141 : 77 );
+        $read_tokens->[1] = _get_new_flag($read_tokens->[1], $type);
         # Remove alignment information
         splice @$read_tokens, 2, 7, (qw/ * 0 0 * * 0 0 /);
         # Remove ALL tags, add new RG tag
         splice @$read_tokens, 11;
-        push @$read_tokens, 'RG:Z:'.$self->old_and_new_read_group_ids->{ $params{rg_id} }->{ $params{type} };
+        push @$read_tokens, 'RG:Z:'.$self->old_and_new_read_group_ids->{$rg_id}->{$type};
         #print( join( "\t", @$read_tokens)."\n");
         $fhs->{$key}->print( join( "\t", @$read_tokens)."\n");
     }
 
     return 1;
+}
+
+sub _read_group_id_for_reads {
+    my ($self, @reads) = @_;
+
+    my @rg_ids;
+    for my $read ( @reads ) {
+        my $rg_tag_idx = List::MoreUtils::firstidx { $_ =~ m/^RG:/ } @$read;
+        next if defined $rg_tag_idx;
+        push @rg_ids, (split(':', $read->[$rg_tag_idx]))[2];
+    }
+
+    return 'unknown' if not @rg_ids;
+    return (List::MoreUtils::uniq(@rg_ids))[0];
+}
+
+sub _separate_reads {
+    # Separate reads into 1/2 removing supplementary but including the secondary alignments
+    my (@read1s, @read2s);
+    for my $read ( @_ ) {
+        # Remove Supplementary 2048 0x800
+        next if !$read->[1] & 0x800;
+        if ( $_[0] & 0x80 ) {
+            # read 2
+            push @read2s, $read; # flagged read2
+        }
+        else {
+            push @read1s, $read; # unflagged reads will be read1
+        }
+    }
+
+    return ( $read1s[0], $read2s[0] );
+}
+
+sub _determine_type {
+    my ($read1, $read2) = @_;
+
+    if ( $read1 and $read2 ) {
+        return 'paired';
+    }
+    elsif ( $read1 ) {
+        return 'singleton';
+    }
+}
+
+sub _get_new_flag {
+    my ($flag, $type) = @_;
+
+    # sigleton will get:
+    # 4  first in pair
+    # 64 read unmapped
+    if ( $type eq 'singleton' ) {
+        return 68;
+    }
+
+    # paired will get:
+    # 64/128 first/second in pair
+    # 1      read paired
+    # 4      read unmapped
+    # 8      mate unmapped
+    if ( $flag & 0x80 ) {
+        return 141;
+    }
+    else {
+        return 77;
+    }
 }
 
 sub _open_fh_for_read_group_and_type {
@@ -217,8 +254,7 @@ sub _create_new_read_group_ids {
     return 1 if exists $self->old_and_new_read_group_ids->{$rg_id};
     $self->old_and_new_read_group_ids->{$rg_id} = {
         paired => UR::Object::Type->autogenerate_new_object_id_uuid,
-        read1 => UR::Object::Type->autogenerate_new_object_id_uuid,
-        read2 => UR::Object::Type->autogenerate_new_object_id_uuid,
+        singleton => UR::Object::Type->autogenerate_new_object_id_uuid,
     };
 }
 
