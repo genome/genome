4a175a8692178f60238585c62eaf5d99a4283036
Eddie Belter <ebelter@genome.wustl.edu>
2016-04-07 16:31:40 -0500

    QC Config has config_to_yaml

--- lib/perl/Genome/Qc/Config.pm
+++ lib/perl/Genome/Qc/Config.pm
@@ -5,6 +5,7 @@ use warnings;
 use Genome;
 use JSON qw(decode_json);
 use Set::Scalar;
+use YAML qw( Dump );
 
 class Genome::Qc::Config {
     roles => [
@@ -37,6 +38,8 @@ class Genome::Qc::Config {
 };
 
 sub __display_name__ { sprintf('%s for %s (%s)', $_[0]->name, $_[0]->type, $_[0]->id); }
+
+sub config_to_yaml { Dump decode_json $_[0]->config; }
 
 sub __errors__ {
     my $self = shift;
