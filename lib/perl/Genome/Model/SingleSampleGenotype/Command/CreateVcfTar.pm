2e41a508eb5138bc21c2720e0a6529f1af4f150b
Ricky Ng <ricky.ng@wustl.edu>
2016-04-25 16:02:25 -0500

    Using user specific output_dir

--- lib/perl/Genome/Model/SingleSampleGenotype/Command/CreateVcfTar.pm
+++ lib/perl/Genome/Model/SingleSampleGenotype/Command/CreateVcfTar.pm
@@ -15,15 +15,20 @@ class Genome::Model::SingleSampleGenotype::Command::CreateVcfTar {
             is => "Genome::Model::Build::SingleSampleGenotype",
             is_many => 1,
         },
+        output_dir => {
+            is => 'DirectoryPath',
+            doc => 'The directory for the created tars.'
+        }
     },
 };
 
 sub execute {
     my $self = shift;
+
     for my $build ($self->builds) {
-        my $cwd = getcwd;
+        my $output_dir = $self->output_dir;
         my $build_id = $build->build_id;
-        my $tar_fullpath = File::Spec->join($cwd, "$build_id.tar");
+        my $tar_fullpath = File::Spec->join($output_dir, "$build_id.tar");
 
         if (-e $tar_fullpath) {
             $self->fatal_message("Tar file already exists, please remove: $tar_fullpath");
