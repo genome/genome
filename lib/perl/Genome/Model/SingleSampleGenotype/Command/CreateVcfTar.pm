ad9b87fbf3017ebc27b56cce2417021bf2e912e9
Ricky Ng <dummey@gmail.com>
2016-04-27 10:51:50 -0500

    Adds command for dumping out VCFs from a build_id into a tar. (#1378)
    * Adding new cmd for creating vcf tar files.
    
    * Using Genome::Sys->shellcmd instead
    
    * Using File::Spec instead
    
    * Using user specific output_dir
    
    * Removed unused package
    
    * Using list form for shellcmd instead of string interpolation
    
    * Using array form for tar_cmd
    
    * Silly perl arrays.
    
    * Nitpicking the description
    
    * Explicitly requiring File::Spec
    
    * Adding tests for CreateVcfTar

---
+++ lib/perl/Genome/Model/SingleSampleGenotype/Command/CreateVcfTar.pm
@@ -0,0 +1,54 @@
+package Genome::Model::SingleSampleGenotype::Command::CreateVcfTar;
+
+use strict;
+use warnings;
+
+use Genome;
+
+use File::Spec;
+
+class Genome::Model::SingleSampleGenotype::Command::CreateVcfTar {
+    is        => 'Command::V2',
+    doc       => 'Create tar(s) of all vcfs for the build(s). One tar will be created for each build.',
+    has_input => {
+        builds => {
+            is      => "Genome::Model::Build::SingleSampleGenotype",
+            is_many => 1,
+        },
+        output_dir => {
+            is  => 'DirectoryPath',
+            doc => 'The directory for the created tars.'
+        }
+    },
+};
+
+sub execute {
+    my $self = shift;
+
+    for my $build ( $self->builds ) {
+        my $output_dir   = $self->output_dir;
+        my $build_id     = $build->build_id;
+        my $tar_fullpath = File::Spec->join( $output_dir, "$build_id.tar" );
+
+        if ( -e $tar_fullpath ) {
+            $self->fatal_message("Tar file already exists, please remove: $tar_fullpath");
+        }
+
+        my @tar_cmd = ( "tar", "-rvf", $tar_fullpath );
+
+        for my $r ( sort { ( $a->intervals )[0] cmp( $b->intervals )[0] } $build->haplotype_caller_result ) {
+            my $filename = $r->_vcf_filename;
+            my $dir      = $r->output_dir;
+            Genome::Sys->shellcmd( cmd => [ @tar_cmd, "-C", $dir, $filename ] );
+        }
+
+        Genome::Sys->shellcmd( cmd => [ "gzip", $tar_fullpath ] );
+    }
+    return 1;
+}
+
+sub sub_command_category {
+    return 'analyst tools';
+}
+
+1;
