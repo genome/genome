f7438e45f2fe57875b5ef7a7bc0bc42eda5ee580
Eddie Belter <ebelter@genome.wustl.edu>
2016-06-01 19:00:40 -0500

    Split By Ns: convert to use iterator

--- lib/perl/Genome/Model/Tools/Sx/Split/ByNs.pm
+++ lib/perl/Genome/Model/Tools/Sx/Split/ByNs.pm
@@ -27,14 +27,15 @@ sub execute {
     my $reader = $self->_reader;
     while ( my $seqs = $reader->read ) {
         for my $seq ( @$seqs ) {
-            $self->_split_and_write_seq($seq);
+            my $it = $self->iterator_to_split_sequence($seq);
+            while ( my $new_seq = $it->() ) { $self->_writer->write([ $new_seq ]); }
         }
     }
 
     return 1;
 }
 
-sub _split_and_write_seq {
+sub iterator_to_split_sequence {
     my ($self, $seq) = @_;
 
     my ($split_bases, $ns);
@@ -42,20 +43,21 @@ sub _split_and_write_seq {
     my $remaining_bases = $seq->{seq};
     my $cnt = 0;
     my $start = 0;
-    while ( $remaining_bases && length $remaining_bases ) {
-        ($split_bases, $ns, $remaining_bases) = split(/(n{$n,})/i, $remaining_bases, 2);
-        my %split_seq = (
-            id => join('.', $seq->{id}, ++$cnt),
-            seq => $split_bases,
-        );
-        $split_seq{qual} = substr($seq->{qual}, $start, length($split_bases)) if $seq->{qual};
+    return sub{
+        while ( $remaining_bases && length $remaining_bases ) {
+            ($split_bases, $ns, $remaining_bases) = split(/(n{$n,})/i, $remaining_bases, 2);
+            my %split_seq = (
+                id => join('.', $seq->{id}, ++$cnt),
+                seq => $split_bases,
+            );
+            $split_seq{qual} = substr($seq->{qual}, $start, length($split_bases)) if $seq->{qual};
 
-        $self->_writer->write([ \%split_seq ]);
-        $start += length($split_bases);
-        $start += length($ns) if $ns;
-    }
+            $start += length($split_bases);
+            $start += length($ns) if $ns;
 
-    return 1;
+            return \%split_seq;
+        }
+    };
 }
 
 1;
