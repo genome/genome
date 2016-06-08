9ee67102851078345879c510ddb7767b8d7c371a
Feiyu Du <fdu@genome.wustl.edu>
2016-06-08 14:33:45 -0500

    add bed/vcf converter

---
+++ lib/perl/Genome/Model/Tools/Bed/Convert/Indel/PindelRegionToBed.pm
@@ -0,0 +1,11 @@
+package Genome::Model::Tools::Bed::Convert::Indel::PindelRegionToBed;
+
+use warnings;
+use strict;
+
+use Genome;
+use Workflow;
+
+class Genome::Model::Tools::Bed::Convert::Indel::PindelRegionToBed {
+    is => ['Genome::Model::Tools::Bed::Convert::Indel::PindelToBed'],
+} 
---
+++ lib/perl/Genome/Model/Tools/Vcf/Convert/Indel/PindelRegion.pm
@@ -0,0 +1,14 @@
+package Genome::Model::Tools::Vcf::Convert::Indel::PindelRegion;
+
+use strict;
+use warnings;
+use Genome;
+
+#This module is exact copy of Genome::Model::Tools::Vcf::Convert::Indel::Pindel
+
+class Genome::Model::Tools::Vcf::Convert::Indel::PindelRegion {
+    is =>  'Genome::Model::Tools::Vcf::Convert::Indel::Pindel',
+};
+
+1;
+
