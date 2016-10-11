be6a56ee5c001851dc78b125275fac2fcf8d97e5
Eddie Belter <ebelter@wustl.edu>
2016-10-11 16:12:02 -0500

    Make export a genome command

--- lib/perl/Genome/Model/DeNovoAssembly/Command/ExportAsGscProject.pm
+++ lib/perl/Genome/Model/DeNovoAssembly/Command/ExportAsGscProject.pm
@@ -1,122 +1,128 @@
-#!/usr/bin/perl
+package Genome::Model::DeNovoAssembly::Command::ExportAsGscProject;
 
 use strict;
 use warnings;
 
 use File::Basename;
-use Genome;
-use Data::Dumper;
 
-my $usage = "$0 <WORK_ORDER_ID> <DIR_TO_COPY_TO> <?LIST_OF_MODEL_SUBJECT_NAMES_TO_COPY .. only copy these models?>"; # TODO <LIST_OF_SAMPLE_NAMES>
-print "USAGE: $usage\n" and exit unless @ARGV && @ARGV >= 2;
+class Genome::Model::DeNovoAssembly::Command::ExportAsGscProject {
+    is => 'Command::V2',
+};
 
-my $wo_id = $ARGV[0];
-my $gp = Genome::Project->get( $wo_id );
-die "Can't find genome::project for work-order, $wo_id\n" unless $gp;
+sub execute {
+    my $self = shift;
 
-my $copy_dir = $ARGV[1];
-die "Invalid directory to copy builds to, $copy_dir\n" unless -d $copy_dir;
+    my $usage = "$0 <WORK_ORDER_ID> <DIR_TO_COPY_TO> <?LIST_OF_MODEL_SUBJECT_NAMES_TO_COPY .. only copy these models?>"; # TODO <LIST_OF_SAMPLE_NAMES>
+    print "USAGE: $usage\n" and exit unless @ARGV && @ARGV >= 2;
 
-my %subjects_to_copy;
-if( $ARGV[2] ) {
-    die "Can't file of list of samples to copy or file is empty, $ARGV[2]\n" unless -s $ARGV[2];
-    my $fh = Genome::Sys->open_file_for_reading( $ARGV[2] );
-    while( my $line = $fh->getline ) {
-        chomp $line;
-        $subjects_to_copy{ $line } = 1;
+    my $wo_id = $ARGV[0];
+    my $gp = Genome::Project->get( $wo_id );
+    die "Can't find genome::project for work-order, $wo_id\n" unless $gp;
+
+    my $copy_dir = $ARGV[1];
+    die "Invalid directory to copy builds to, $copy_dir\n" unless -d $copy_dir;
+
+    my %subjects_to_copy;
+    if( $ARGV[2] ) {
+        die "Can't file of list of samples to copy or file is empty, $ARGV[2]\n" unless -s $ARGV[2];
+        my $fh = Genome::Sys->open_file_for_reading( $ARGV[2] );
+        while( my $line = $fh->getline ) {
+            chomp $line;
+            $subjects_to_copy{ $line } = 1;
+        }
+        $fh->close;
     }
-    $fh->close;
-}
 
 # project parts
-my @project_parts = $gp->parts;
-die "Exiting .. can't find any project parts for work order, $wo_id\n" unless @project_parts;
+    my @project_parts = $gp->parts;
+    die "Exiting .. can't find any project parts for work order, $wo_id\n" unless @project_parts;
 #print Dumper \@project_parts; exit;
 
 # display list of samples and inst_data/models available for work-order
-print "Checking workorder for instrument-data and models\n";
-&map_inst_data_and_models_to_samples;
+    print "Checking workorder for instrument-data and models\n";
+    &map_inst_data_and_models_to_samples;
 
-my @models = grep{ $_->entity_class_name eq 'Genome::Model::DeNovoAssembly' } @project_parts;
-die "Exiting .. didn't find any de-novo assembly project parts\n" unless @models;
+    my @models = grep{ $_->entity_class_name eq 'Genome::Model::DeNovoAssembly' } @project_parts;
+    die "Exiting .. didn't find any de-novo assembly project parts\n" unless @models;
 
-print "Finding succeeded builds\n";
+    print "Finding succeeded builds\n";
 # find succeeded builds
-my @succeeded_builds;
-for my $model_part( @models ) {
-    my $model = Genome::Model->get( $model_part->entity_id );
-    die "Can't find genome::model for model id ".$model_part->entity_id."\n" unless $model;
+    my @succeeded_builds;
+    for my $model_part( @models ) {
+        my $model = Genome::Model->get( $model_part->entity_id );
+        die "Can't find genome::model for model id ".$model_part->entity_id."\n" unless $model;
 
-    next if $ARGV[2] && ! exists $subjects_to_copy{ $model->subject->name };
-    #print "Skipping model, ".$model->name.", which is not found in the input list of subjects to copy\n" and next
-    #if $ARGV[2] && ! exists $subjects_to_copy{ $model->subject->name };
+        next if $ARGV[2] && ! exists $subjects_to_copy{ $model->subject->name };
+        #print "Skipping model, ".$model->name.", which is not found in the input list of subjects to copy\n" and next
+        #if $ARGV[2] && ! exists $subjects_to_copy{ $model->subject->name };
 
-    next if model_is_newbler_assembly_with_multiple_inst_data( $model );
+        next if model_is_newbler_assembly_with_multiple_inst_data( $model );
 
-    my $build = $model->last_succeeded_build;
-    print "Skipping model, ".$model->name.", sample ".$model->subject->name.", no succeeded build found\n" and next
-    unless $build;
+        my $build = $model->last_succeeded_build;
+        print "Skipping model, ".$model->name.", sample ".$model->subject->name.", no succeeded build found\n" and next
+        unless $build;
 
-    push @succeeded_builds, $build;
-}
+        push @succeeded_builds, $build;
+    }
 
-print "Found ".scalar( @succeeded_builds )." succeeded builds to copy\n";#.join( "\n", map{ $_->data_directory }  @succeeded_builds ),"\n";
-exit unless @succeeded_builds > 0;
-for( @succeeded_builds ) {
-    print "\t".$_->model->subject->name.' '.$_->data_directory."\n";
-}
-print "Continue and copy these assemblies? yes/no?\n";
-my $ans = <STDIN>;
-exit unless $ans =~ /yes/i;
+    print "Found ".scalar( @succeeded_builds )." succeeded builds to copy\n";#.join( "\n", map{ $_->data_directory }  @succeeded_builds ),"\n";
+    exit unless @succeeded_builds > 0;
+    for( @succeeded_builds ) {
+        print "\t".$_->model->subject->name.' '.$_->data_directory."\n";
+    }
+    print "Continue and copy these assemblies? yes/no?\n";
+    my $ans = <STDIN>;
+    exit unless $ans =~ /yes/i;
 
 # copy succeeded builds;
-for my $build( @succeeded_builds ) {
-    my $build_dir = $build->data_directory;
-    die "No data directory found, $build_dir\n" unless -d $build_dir;
+    for my $build( @succeeded_builds ) {
+        my $build_dir = $build->data_directory;
+        die "No data directory found, $build_dir\n" unless -d $build_dir;
 
-    # project dir
-    my $output_dir = $copy_dir.'/'.$build->model->subject->name;
-    $output_dir =~ s/\s+/_/g;
-    Genome::Sys->create_directory( $output_dir ) unless -d $output_dir;
+        # project dir
+        my $output_dir = $copy_dir.'/'.$build->model->subject->name;
+        $output_dir =~ s/\s+/_/g;
+        Genome::Sys->create_directory( $output_dir ) unless -d $output_dir;
 
-    # create build id empty file
-    my $build_id_file = $output_dir."/build_id=".$build->id;
-    Genome::Sys->touch( $build_id_file ) if not -e $build_id_file;
+        # create build id empty file
+        my $build_id_file = $output_dir."/build_id=".$build->id;
+        Genome::Sys->touch( $build_id_file ) if not -e $build_id_file;
 
-    my $assembler = $build->model->processing_profile->assembler_name;
-    # create subdirs
-    my @sub_dirs = subdirs_for_assembler( $assembler );
-    die "Can't determine which subdirs to create for assembler, $assembler\n" unless @sub_dirs;
-    for my $dir_name( @sub_dirs ) {
-        my $sub_dir = $output_dir."/$dir_name";
-        Genome::Sys->create_directory( $sub_dir ) unless -d $sub_dir;
-    }
+        my $assembler = $build->model->processing_profile->assembler_name;
+        # create subdirs
+        my @sub_dirs = subdirs_for_assembler( $assembler );
+        die "Can't determine which subdirs to create for assembler, $assembler\n" unless @sub_dirs;
+        for my $dir_name( @sub_dirs ) {
+            my $sub_dir = $output_dir."/$dir_name";
+            Genome::Sys->create_directory( $sub_dir ) unless -d $sub_dir;
+        }
 
-    # edit_dir files to copy
-    my $builds_edit_dir = assemblers_edit_dir( $assembler );
-    die "Can't determine locations of builds edit_dir for assembler, $assembler\n" unless $builds_edit_dir;
-    for my $file( glob("$build_dir/$builds_edit_dir/*") ) {
-        my $file_name = basename( $file );
-        my $to = $output_dir.'/edit_dir/'.$file_name;
-        print "skipping, $file already exists\n" and next if -s $to;
-        print "Copying $file to $to\n";
-        Genome::Sys->copy_file( $file, $to );
-    }
+        # edit_dir files to copy
+        my $builds_edit_dir = assemblers_edit_dir( $assembler );
+        die "Can't determine locations of builds edit_dir for assembler, $assembler\n" unless $builds_edit_dir;
+        for my $file( glob("$build_dir/$builds_edit_dir/*") ) {
+            my $file_name = basename( $file );
+            my $to = $output_dir.'/edit_dir/'.$file_name;
+            print "skipping, $file already exists\n" and next if -s $to;
+            print "Copying $file to $to\n";
+            Genome::Sys->copy_file( $file, $to );
+        }
 
-    my @addl_files = additional_files_for_assembler( $assembler, $build );
-    if( @addl_files ) {
-        while( @addl_files ) {
-            my( $from, $to ) = split(/\s+/, shift @addl_files);
-            $from = $build_dir.'/'.$from;
-            $to   = $output_dir.'/'.$to;
-            die "Can't find file, $from to link\n" unless -s $from;
-            print "Linking $from to $to\n";
-            Genome::Sys->create_symlink( $from, $to ) if not -l $to;
+        my @addl_files = additional_files_for_assembler( $assembler, $build );
+        if( @addl_files ) {
+            while( @addl_files ) {
+                my( $from, $to ) = split(/\s+/, shift @addl_files);
+                $from = $build_dir.'/'.$from;
+                $to   = $output_dir.'/'.$to;
+                die "Can't find file, $from to link\n" unless -s $from;
+                print "Linking $from to $to\n";
+                Genome::Sys->create_symlink( $from, $to ) if not -l $to;
+            }
         }
     }
-}
 
-exit;
+    return 1;
+}
 
 sub model_is_newbler_assembly_with_multiple_inst_data {
     my $model = shift;
@@ -243,4 +249,6 @@ sub get_genome_class_obj {
     return $obj if $obj;
     return;
 }
+
+1;
 
