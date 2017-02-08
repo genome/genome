55d6410baa68167f612423be4b7bf371576d6445
Thomas B. Mooney <tmooney@genome.wustl.edu>
2017-02-08 09:48:36 -0600

    Stop writing unarchive logs to /gsc/var/tmp.

--- lib/perl/Genome/Model/Build/Command/Unarchive.pm
+++ lib/perl/Genome/Model/Build/Command/Unarchive.pm
@@ -40,17 +40,7 @@ sub _execute {
 
     my %failed_allocations_per_build;
 
-    # Create a temp directory on network accessible disk for logs.
-    # If no errors occur, this is cleaned up. Otherwise the user is 
-    # told to clean it up after reviewing any failures.
-    my $dir = File::Temp::tempdir(
-        'build_unarchive_XXXXXX', 
-        DIR => "/gsc/var/tmp/build_unarchive", 
-        CLEANUP => 0
-    );
-    chmod 0750, $dir;
-
-    $self->status_message("Unarchiving data for $total_builds builds, logs being written to $dir");
+    $self->status_message("Unarchiving data for $total_builds builds");
 
     BUILD: for my $build (@builds) {
         # Print progress information
@@ -65,7 +55,7 @@ sub _execute {
             $self->status_message("Found $num_allocations archived allocations related to this build, " .
                 "starting unarchive process now.");
             # Schedule unarchive of all allocations related to build via LSF
-            my %jobs_to_allocations = $self->_bsub_unarchives_and_wait_completion($dir, @allocations);
+            my %jobs_to_allocations = $self->_bsub_unarchives_and_wait_completion(@allocations);
             %job_to_allocation_mapping = %jobs_to_allocations;
             my @job_ids = keys %job_to_allocation_mapping;
             $self->debug_message("Unarchives scheduled, waiting for completion");
@@ -83,7 +73,7 @@ sub _execute {
         if ( @symlinked_allocations_that_need_unarchiving ) {
             $num_allocations += @symlinked_allocations_that_need_unarchiving;
             $self->status_message("Found ".@symlinked_allocations_that_need_unarchiving." archived symlinked allocations. Unarchiving...");
-            my %jobs_to_allocations = $self->_bsub_unarchives_and_wait_completion($dir, @symlinked_allocations_that_need_unarchiving);
+            my %jobs_to_allocations = $self->_bsub_unarchives_and_wait_completion(@symlinked_allocations_that_need_unarchiving);
             %job_to_allocation_mapping = %jobs_to_allocations;
             my @symlinked_allocation_job_ids = keys %jobs_to_allocations;
             $self->debug_message("Unarchives for symlinked allocations scheduled, waiting for completion");
@@ -123,7 +113,7 @@ sub _execute {
 
         # Find failures and report them (now and once all builds have completed)
         $self->debug_message("Unarchived finished, combing over return statuses and gathering up any failures");
-        my %failed_allocations = $self->_get_failed_unarchives(\%job_statuses, \%job_to_allocation_mapping, $dir);
+        my %failed_allocations = $self->_get_failed_unarchives(\%job_statuses, \%job_to_allocation_mapping);
         my $fail_count = %failed_allocations ? keys %failed_allocations : 0;
         $failed_allocations_per_build{$build->id} = \%failed_allocations if %failed_allocations;
 
@@ -145,14 +135,12 @@ sub _execute {
     # Print error summary (if needed) and exit
     $self->status_message("\nUnarchives for all builds have completed.");
     if (%failed_allocations_per_build) {
-        $self->status_message("Errors occurred during unarchiving. An error summary follows. " .
-            "Once all errors have been addressed, please remove the log directory $dir");
+        $self->status_message("Errors occurred during unarchiving. An error summary follows.");
         $self->_print_error_summary(%failed_allocations_per_build);
         die "Encountered errors during unarchiving!";
     }
     else {
-        $self->status_message("No errors found while unarchiving, removing log directory $dir!");
-        $self->_remove_log_dir($dir);
+        $self->status_message("No errors found while unarchiving.");
     }
 
     return 1;
@@ -160,7 +148,6 @@ sub _execute {
 
 sub _bsub_unarchives_and_wait_completion {
     my $self = shift;
-    my $log_file_dir = shift;
     my @allocations = @_;
 
     my $analysis_project = $self->analysis_project->id;
@@ -174,10 +161,7 @@ sub _bsub_unarchives_and_wait_completion {
             '--analysis-project', $analysis_project, '--requestor', $requestor,
         );
         push @unarchive_commands,
-                { cmd => \@cmd,
-                  log_file => "$log_file_dir/$allocation_id.out",
-                  err_file => "$log_file_dir/$allocation_id.err",
-                };
+                { cmd => \@cmd, };
     }
 
     my %job_to_allocation_mapping;
@@ -201,7 +185,6 @@ sub _get_failed_unarchives {
     my $self = shift;
     my %job_statuses = %{shift @_};
     my %job_to_allocation_mapping = %{shift @_};
-    my $dir = shift;
 
     my %failed_allocations;
     for my $job_id (sort keys %job_statuses) {
@@ -211,22 +194,9 @@ sub _get_failed_unarchives {
         my $allocation_id = $job_to_allocation_mapping{$job_id};
         $self->warning_message("Allocation $allocation_id failed to unarchive via LSF job $job_id");
 
-        $failed_allocations{$allocation_id} = [$job_id, "$dir/$allocation_id"];
+        $failed_allocations{$allocation_id} = [$job_id];
     }
     return %failed_allocations;
-}
-
-sub _remove_log_dir {
-    my ($self, $dir) = @_;
-    my $rv = eval { Genome::Sys->remove_directory_tree($dir) };
-    my $error = $@;
-    if ($error) {
-        $self->warning_message("Could not remove log directory $dir, please fix and remove manually.");
-    }
-    else {
-        $self->status_message("Log directory $dir removed!");
-    }
-    return 1;
 }
 
 sub _print_error_summary {
@@ -235,13 +205,13 @@ sub _print_error_summary {
 
     $self->status_message("Error summary:");
     my @lines;
-    push @lines, join("\t", 'allocation_id', 'build_id', 'lsf_job_id', 'log_file');
+    push @lines, join("\t", 'allocation_id', 'build_id', 'lsf_job_id');
 
     for my $build (sort keys %failed_allocations_per_build) {
         my %failed_unarchives = %{$failed_allocations_per_build{$build}};
         for my $allocation (sort keys %failed_unarchives) {
             my ($job_id, $log_file) = @{$failed_unarchives{$allocation}};
-            push @lines, join("\t", $allocation, $build, $job_id, $log_file);
+            push @lines, join("\t", $allocation, $build, $job_id);
         }
     }
 
