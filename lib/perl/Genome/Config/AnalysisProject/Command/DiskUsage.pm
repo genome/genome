1d772fe117ca03085024d33fc7b277304ee75e11
Jason Walker <jwalker@genome.wustl.edu>
2017-02-20 15:36:59 -0600

    Initial revision of command to report disk usage. WIP.

---
+++ lib/perl/Genome/Config/AnalysisProject/Command/DiskUsage.pm
@@ -0,0 +1,72 @@
+package Genome::Config::AnalysisProject::Command::DiskUsage;
+
+use strict;
+use warnings;
+
+use feature qw(say);
+
+use Genome;
+
+class Genome::Config::AnalysisProject::Command::DiskUsage {
+    is => 'Command::V2',
+    has => [
+        analysis_projects => {
+            is                  => 'Genome::Config::AnalysisProject',
+            doc                 => 'the analysis projects to release for processing',
+            is_many             => 1,
+            shell_args_position => 1,
+        },
+    ],
+    doc => 'report on the disk usage of analysis projects',
+};
+
+sub help_detail {
+    return <<EOHELP
+This command will take several minutes to run.
+EOHELP
+}
+
+sub execute {
+    my $self = shift;
+
+    # Remove normal BAM from disk usgage for somatic
+    for my $anp ($self->analysis_projects) {
+        for my $config_item ($anp->config_items) {
+            # TODO: Add model_type
+            my %model_type_disk_usage;
+            my $model_count = 0;
+            my $build_count = 0;
+            for my $model ($anp->models) {
+                my $subclass_name = $model->subclass_name;
+                $model_type_disk_usage{$subclass_name}{'model_count'}++;
+                for my $build ($model->builds) {
+                    #unless ($build->status eq 'Succeeded') { next; }
+                    $model_type_disk_usage{$subclass_name}{'build_count'}++;
+                    my @allocations = $build->disk_usage_allocations;
+                    for my $allocation (@allocations) {
+                        $model_type_disk_usage{$subclass_name}{'allocations'}{$allocation->id} = $allocation->kilobytes_used;
+                    }
+                    my @instrument_data = $build->instrument_data;
+                    for my $instrument_data (@instrument_data) {
+                        $model_type_disk_usage{$subclass_name}{'instrument_data'}{$instrument_data->id} = $instrument_data->total_bases_read;
+                    }
+                }
+            }
+            for my $model_type (sort keys %model_type_disk_usage) {
+                my $total_kb = 0;
+                map { $total_kb += $_ } values(%{$model_type_disk_usage{$model_type}{'allocations'}});
+
+                my $total_bp = 0;
+                map { $total_bp += $_ } values (%{$model_type_disk_usage{$model_type}{'instrument_data'}});
+
+                my $bytes_per_base = ($total_kb * 1024) / $total_bp;
+
+                print $anp->id ."\t". $config_item->id ."\t". $model_type_disk_usage{$model_type}{'model_count'} ."\t". $model_type_disk_usage{$model_type}{'build_count'} ."\t". $total_kb ."\t". $total_bp ."\t". $bytes_per_base ."\n";
+            }
+        }
+    }
+    return 1;
+}
+
+
+1;
