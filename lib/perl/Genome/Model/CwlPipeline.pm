707dc3ece324ff00d685ce6a17ee2fb3348ea6ca
Thomas B. Mooney <tmooney@genome.wustl.edu>
2017-05-17 15:17:43 -0500

    First draft of CWL Pipeline type. (Not yet functional.)

---
+++ lib/perl/Genome/Model/CwlPipeline.pm
@@ -0,0 +1,50 @@
+package Genome::Model::CwlPipeline;
+
+use strict;
+use warnings;
+
+class Genome::Model::CwlPipeline {
+    is => 'Genome::Model',
+    has_param => {
+        main_workflow_file => {
+            is => 'Text',
+        },
+    },
+};
+
+sub map_workflow_inputs {
+    my $self = shift;
+    my $build = shift;
+
+    return (build => $build);
+}
+
+sub _resolve_workflow_for_build {
+    my $self = shift;
+    my $build = shift;
+
+    my $wf = Genome::WorkflowBuilder::DAG->create(
+        name => $build->workflow_name,
+        log_dir => $build->log_directory,
+    );
+
+    my $cmd = Genome::WorkflowBuilder::Command->create(
+        name => 'Toil Runner',
+        command => 'Genome::Model::CwlPipeline::Command::Run',
+    );
+    $wf->add_operation($cmd);
+    $wf->connect_input(
+        input_property => 'build',
+        destination => $cmd,
+        destination_property => 'build'
+    );
+    $wf->connect_output(
+        source => $cmd,
+        source_property => 'result',
+        output_property => 'result',
+    );
+
+    return $wf;
+}
+
+1;
