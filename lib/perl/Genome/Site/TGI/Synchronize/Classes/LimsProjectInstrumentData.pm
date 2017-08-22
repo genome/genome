b5f939b6c73242068aecf6d4bbf6377621c1e2ad
Thomas B. Mooney <tmooney@genome.wustl.edu>
2017-08-22 12:59:51 -0500

    Remove obsolete 'dw' cross-db link.
    The two databases are combined now.

--- lib/perl/Genome/Site/TGI/Synchronize/Classes/LimsProjectInstrumentData.pm
+++ lib/perl/Genome/Site/TGI/Synchronize/Classes/LimsProjectInstrumentData.pm
@@ -15,7 +15,7 @@ class Genome::Site::TGI::Synchronize::Classes::LimsProjectInstrumentData {
 		from administration_project ap
 		join project_work_order pwo on pwo.project_id = ap.project_id
 		join work_order_item woi on woi.setup_wo_id = pwo.setup_wo_id
-		join gsc.woi_sequence_product\@dw woisp on woisp.woi_id = woi.woi_id
+		join gsc.woi_sequence_product woisp on woisp.woi_id = woi.woi_id
 		where ap.project_id > 2570000
 		 and ap.status != 'abandoned'
 		union
@@ -23,7 +23,7 @@ class Genome::Site::TGI::Synchronize::Classes::LimsProjectInstrumentData {
 		select distinct woi.setup_wo_id project_id, woisp.seq_id entity_id
 		from setup s
 		join work_order_item woi on woi.setup_wo_id = s.setup_id
-		join gsc.woi_sequence_product\@dw woisp on woisp.woi_id = woi.woi_id
+		join gsc.woi_sequence_product woisp on woisp.woi_id = woi.woi_id
 		where s.setup_id > 2570000
 		 and s.setup_status != 'abandoned'
     ) lims_project_instrument_data
