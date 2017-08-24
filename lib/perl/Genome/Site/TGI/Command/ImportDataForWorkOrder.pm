c7004dd51f4be8ce7ed1b0e7119c3a4e23d98d7f
Thomas B. Mooney <tmooney@genome.wustl.edu>
2017-08-24 09:48:10 -0500

    Support synching project-sample associations.

--- lib/perl/Genome/Site/TGI/Command/ImportDataForWorkOrder.pm
+++ lib/perl/Genome/Site/TGI/Command/ImportDataForWorkOrder.pm
@@ -19,8 +19,9 @@ sub execute {
     my $self = shift;
 
     my $wo = $self->_resolve_work_order;
-    my @items = $self->_resolve_items;
+    $self->_resolve_samples;
 
+    my @items = $self->_resolve_instrument_data;
     my @existing = Genome::InstrumentData->get(id => [map $_->entity_id, @items]);
 
     $self->status_message(
@@ -61,7 +62,7 @@ sub _resolve_work_order {
     return $existing_project;
 }
 
-sub _resolve_items {
+sub _resolve_instrument_data {
     my $self = shift;
 
     my @existing_items = Genome::ProjectPart->get(
@@ -80,6 +81,37 @@ sub _resolve_items {
     for my $item (@to_import) {
         push @existing_items, $item->create_in_genome;
     }
+
+    return @existing_items;
+}
+
+sub _resolve_samples {
+    my $self = shift;
+
+    my @existing_items = Genome::ProjectPart->get(
+        label => 'sample',
+        project_id => $self->work_order_id,
+    );
+
+    my @importable_items = Genome::Site::TGI::Synchronize::Classes::LimsProjectSample->get(
+        project_id => $self->work_order_id,
+    );
+
+    my %found;
+    $found{$_->entity_id}++ for @existing_items;
+    my @to_import = grep { !$found{$_->entity_id} } @importable_items;
+
+    for my $item (@to_import) {
+        push @existing_items, $item->create_in_genome;
+    }
+
+    my @existing_samples = Genome::Sample->get(id => [map $_->entity_id, @existing_items]);
+
+    my %found_samples;
+    $found_samples{$_->id}++ for @existing_samples;
+    my @samples_to_import = grep { !$found_samples{$_->entity_id} } @existing_items;
+    my @os = Genome::Site::TGI::Synchronize::Classes::OrganismSample->get(id => [map $_->entity_id, @samples_to_import]);
+    $self->_import_organismsample($_) for @os;
 
     return @existing_items;
 }
