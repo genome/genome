b549c3b03d4e0b077555cfbe1b6da7f2515a633e
Thomas B. Mooney <tmooney@genome.wustl.edu>
2018-02-14 13:32:41 -0600

    Fix error message to include value.

--- lib/perl/Genome/Model/CwlPipeline.pm
+++ lib/perl/Genome/Model/CwlPipeline.pm
@@ -157,7 +157,7 @@ sub determine_input_objects {
     for my $name (keys %data) {
         my $value = $class->determine_input_object($name, $data{$name});
         unless($value) {
-            $class->fatal_message('Unable to determine input for name "%s" and value "%s".', $name, $data{name});
+            $class->fatal_message('Unable to determine input for name "%s" and value "%s".', $name, $data{$name});
         }
 
         $inputs{$name} = $value;
