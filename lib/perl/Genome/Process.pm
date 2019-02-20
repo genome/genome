32f70e8a6ceea82cc61fdd249f0b8c20f9ac005a
Thomas B. Mooney <tmooney@genome.wustl.edu>
2019-02-20 17:28:01 -0600

    New WorkflowBuilder backend: "simple".
    This `bsub`s a single job which then runs the workflow inline.  For
    one-step workflows (like CwlPipeline or ReferenceSequence) this is
    approximately the same as launching with Ptero, but without the
    overhead!  For more complicated workflows (like SomaticValidation) this
    would almost certainly be a poor choice.
    
    This is pretty much equivalent to this combo of options from "Workflow":
       --server-dispatch lsf
       --job-dispatch inline

--- lib/perl/Genome/Process.pm
+++ lib/perl/Genome/Process.pm
@@ -141,6 +141,8 @@ sub run {
             $self->_submit_process($transaction);
         } elsif ($backend eq 'inline') {
             $self->_execute_process($transaction);
+        } elsif ($backend eq 'simple') {
+            $self->_dispatch_process($transaction);
         } else {
             $self->fatal_message('Unknown backend chosen to run process: %s', $backend);
         }
@@ -220,6 +222,46 @@ sub _submit_process {
     my $cmd = Genome::Process::Command::Run->create(
         process => $self, update_with_commit => 0);
     $cmd->submit();
+}
+
+sub _dispatch_process {
+    my $self = shift;
+    my $transaction = shift;
+
+    unless ($transaction->commit()) {
+        $transaction->rollback();
+        $self->fatal_message(
+            "Failed to submit process (%s): %s",
+            $self->id, $transaction->error_message || 'Reason Unknown'
+        );
+    }
+
+    my $job_group = join('/',
+        Genome::Config::get('lsf_job_group'),
+        Genome::Sys->username,
+    );
+
+    my $log_file_base = File::Spec->join($self->log_directory, 'process-%J');
+
+    my $job_id = Genome::Sys->bsub(
+        queue => Genome::Config::get('lsf_queue_build_worker'),
+        cmd => [qw(genome process run), $self->id],
+        hold_job => 1,
+        err_file => "${log_file_base}.err",
+        out_file => "${log_file_base}.out",
+        project => $self->lsf_project_name,
+        job_group => $job_group,
+        resource_string => Genome::Config::get('lsf_resource_cwl_runner'),
+    );
+
+    $self->lsf_job_id($job_id);
+    Genome::Sys::CommitAction->create(
+        on_commit => sub {
+            Genome::Sys->shellcmd(cmd => ['bresume', $job_id]);
+        },
+    );
+
+    return 1;
 }
 
 sub create {
