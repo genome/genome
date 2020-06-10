24bf8930c8966c040a1787c6ff86e2f75538bf43
Thomas B. Mooney <tmooney@genome.wustl.edu>
2020-06-10 16:46:58 -0500

    Mitigate having multiple workflows per build.
    The "right" answer here would be to link them to the corresponding
    Genome::Process.  The issue right now is that the process is created
    *after* the workflow inputs are assigned, so we have no way to
    accurately determine the correct process for our current run.  There are
    some strategies for trying to figure it out.  For example:
     - look up our current LSF Job ID and check the database for a matching
       process.
     - look for the one process that is currently marked as "running" and
       die if there's a second one.
    
    For now this assumes only one process ever "succeeds" for a build and
    the build view displays all the workflows in an arbitrary order.

--- lib/perl/Genome/Model/CwlPipeline/Command/Run.pm
+++ lib/perl/Genome/Model/CwlPipeline/Command/Run.pm
@@ -447,9 +447,9 @@ sub _stage_cromwell_outputs {
 
     my $build = $self->build;
 
-    my $results = Genome::Cromwell->query( [{ label => 'build:' . $build->id }] );
+    my $results = Genome::Cromwell->query( [{ label => 'build:' . $build->id, status => 'Succeeded' }] );
     if ($results->{totalResultsCount} != 1) {
-        $self->fatal_message('Failed to find workflow.  Got: %s', scalar(Data::Dumper::dumper($results)));
+        $self->fatal_message('Failed to find workflow.  Got: %s', scalar(Data::Dumper::Dumper($results)));
     }
 
     my $workflow_id = $results->{results}->[0]->{id};
