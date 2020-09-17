1c318d91bb93548806e82f8b69bac3dec2308c77
Thomas B. Mooney <tmooney@genome.wustl.edu>
2020-09-17 15:23:27 -0500

    Support importing LIMS data into the GMS on a remote volume.

--- lib/perl/Genome/Site/TGI/Command/ImportDataFromLims.pm
+++ lib/perl/Genome/Site/TGI/Command/ImportDataFromLims.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use File::Basename qw();
 use File::Spec qw();
+use Sub::Override;
 use Try::Tiny qw(try catch);
 
 use Genome;
@@ -22,6 +23,25 @@ class Genome::Site::TGI::Command::ImportDataFromLims {
             doc => 'The Analysis Project for which this instrument data is being imported',
         },
     },
+    has_optional_param => {
+        remote_host => {
+            is => 'Text',
+            doc => 'ssh-enabled remote host for transfer--required for remote volumes',
+        },
+        remote_user => {
+            is => 'Text',
+            doc => 'remote username for ssh--required for remote volumes',
+        },
+        remote_port => {
+            is => 'Number',
+            doc => 'remote port to use for SSH--required for remote volumes',
+            default_value => 22,
+        },
+        authorization_key => {
+            is => 'Text',
+            doc => 'path to the SSH key to use for rsync transfer--required for remote volumes',
+        },
+    },
     doc => 'Import instrument data files into the GMS out of the LIMS system',
 };
 
@@ -33,6 +53,11 @@ EOHELP
 
 sub execute {
     my $self = shift;
+
+    my $remote_params_defined =  grep { defined $self->$_ } (qw(remote_host remote_user remote_port authorization_key));
+    if ($remote_params_defined != 0 and $remote_params_defined != 4) {
+        $self->fatal_message('All four remote parameters must be defined if any are, but found only %s defined.', $remote_params_defined);
+    }
 
     for my $i ($self->instrument_data) {
         $self->_process_instrument_data($i);
@@ -82,12 +107,31 @@ sub _process_instrument_data {
             die $self->error_message('Unknown LIMS filetype: %s', $lims_path);
         }
 
-        Genome::Sys->rsync_directory(
-            source_directory => $lims_source_dir,
-            target_directory => $allocation->absolute_path,
-            chmod => 'Dug=rx,Fug=r',
-            chown => ':' . $self->_user_group,
-        );
+        if ($allocation->volume->is_remote_volume) {
+            my $source_path = $lims_source_dir . '/';
+            my $destination_path = $allocation->absolute_path;
+            my $user = $self->remote_user;
+            my $host = $self->remote_host;
+
+            my @ssh_opts = ('ssh', '-p', $self->remote_port, '-i', $self->authorization_key, '-o', 'StrictHostKeyChecking=no');
+
+            my $remote_destination = sprintf('%s@%s:%s', $user, $host, $destination_path);
+
+            #first ensure directory exists as target for rsync
+            Genome::Sys->shellcmd(
+                cmd => [@ssh_opts, sprintf('%s@%s', $user, $host), 'mkdir', '-p', $destination_path],
+            );
+            Genome::Sys->shellcmd(
+                cmd => ['rsync', '-av', '-e', join(' ', @ssh_opts), $source_path, $remote_destination],
+            );
+        } else {
+            Genome::Sys->rsync_directory(
+                source_directory => $lims_source_dir,
+                target_directory => $allocation->absolute_path,
+                chmod => 'Dug=rx,Fug=r',
+                chown => ':' . $self->_user_group,
+            );
+        }
 
         if ($bam_file) {
             my $new_path = File::Spec->join($allocation->absolute_path, $bam_file);
@@ -164,10 +208,25 @@ sub _create_allocation {
         owner_id => $data->id,
     );
 
-    my $create_cmd = Genome::Disk::Command::Allocation::Create->create(%params);
-    unless ($create_cmd->execute) {
-        $self->error_message('Could not create allocation for instrument data: %s', $data->__display_name__);
-        return;
+    if ($self->remote_host) {
+        my @volumes = Genome::Disk::Detail::Allocation::Creator->get_candidate_volumes(disk_group_name => $params{disk_group_name});
+        if (my @bad = grep !$_->is_remote_volume, @volumes) {
+            $self->fatal_message('Remote options specified but disk group contains non-remote volumes: %s', join(',', map $_->mount_path, @bad));
+        }
+
+        my $creator_override = Sub::Override->new('Genome::Disk::Detail::Allocation::Creator::create_directory_or_delete_allocation', sub { return 1; }); #we're handling directory creation remotely
+        my $volume_override = Sub::Override->new('Genome::Disk::Volume::is_mounted', sub { return 1; }); #mounted remotely
+
+        Genome::Disk::Allocation->_create(
+            id => Genome::Disk::Allocation->__meta__->autogenerate_new_object_id,
+            %params,
+        );
+    } else {
+        my $create_cmd = Genome::Disk::Command::Allocation::Create->create(%params);
+        unless ($create_cmd->execute) {
+            $self->error_message('Could not create allocation for instrument data: %s', $data->__display_name__);
+            return;
+        }
     }
 
     return Genome::Disk::Allocation->get(allocation_path => $params{allocation_path});
