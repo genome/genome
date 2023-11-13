91015349e1fcb86edfa07b5db14a66ffa60879c1
Thomas B. Mooney <tmooney@genome.wustl.edu>
2023-11-13 15:04:34 -0600

    Remove LIMS-sepcific extensions.
    With the retirement of the legacy LIMS system these are no longer
    functional.  (Many of the classes under Genome/Site/TGI/Synchronize are
    also likely candidates for removal, but there will be a final database
    snapshot we could theoretically query for a time as needed.)

--- lib/perl/Genome/Site/TGI/Observers.pm
+++ lib/perl/Genome/Site/TGI/Observers.pm
@@ -20,33 +20,5 @@ UR::Observer->register_callback(
     },
 );
 
-my $unarchive_observer;
-$unarchive_observer = UR::Observer->register_callback(
-    subject_class_name => 'UR::Object::Type',
-    aspect => 'load',
-    callback => sub {
-        my $meta = shift;
-        my $class_name = $meta->class_name;
-        if ($class_name eq 'Genome::InstrumentData::Command::Unarchive') {
-            require Genome::Site::TGI::Extension::UnarchiveInstrumentData;
-            UR::Observer->unregister_callback(id => $unarchive_observer);
-        }
-    },
-);
-
-my $cqid_observer;
-$cqid_observer = UR::Observer->register_callback(
-    subject_class_name => 'UR::Object::Type',
-    aspect => 'load',
-    callback => sub {
-        my $meta = shift;
-        my $class_name = $meta->class_name;
-        if ($class_name eq 'Genome::Config::Command::ConfigureQueuedInstrumentData') {
-            require Genome::Site::TGI::Extension::ConfigureQueuedInstrumentData;
-            UR::Observer->unregister_callback(id => $cqid_observer);
-        }
-    },
-);
-
 1;
 
