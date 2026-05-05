ab9730302d1c128881f6e55747a76af15a08bbc4
Thomas Mooney <tmooney@genome.wustl.edu>
2026-05-05 11:48:14 -0500

    Ensure we examine the FQDN for loading site modules.
    Includes a temporary module for "cm.cluster" domains due to a
    configuration issue with compute2.  In an update later this year, they
    will report "ris.wustl.edu" and this module can be removed.

---
+++ lib/perl/Genome/Site/cluster/cm.pm
@@ -0,0 +1,17 @@
+package Genome::Site::cluster::cm;
+use Genome::Site::TGI;
+1;
+
+=pod
+
+=head1 NAME
+
+Genome::Config::edu::cluster::cm - WU TGI host configuration (temporary)
+
+=head1 SYNOPSIS
+
+Ensures that Genome::Site::TGI is used by all *.cm.cluster hosts until
+their FQDN is properly updated to ris.wustl.edu.
+
+=cut
+
